2fa85077f430d37ff9899daa89499cc682407b77
Rene Meusel <rene@renemeusel.de>
2011-07-15 14:06:40 +0000

    minor refinements

--- trunk/prebuildscripts/buildLibcurl.sh
+++ trunk/prebuildscripts/buildLibcurl.sh
@@ -1,5 +1,39 @@
 #!/bin/sh
 
-sh configure.gnu
+sh ./configure LDFLAGS="$LDFLAGS -rdynamic" CFLAGS="$CFLAGS -D_FILE_OFFSET_BITS=64 -fno-strict-aliasing -fasynchronous-unwind-tables -fno-omit-frame-pointer -fno-optimize-sibling-calls" \
+  $CVMFS_ZLIB --enable-warnings \
+  --disable-ares \
+  --disable-shared \
+  --enable-static \
+  --disable-ftp \
+  --disable-file \
+  --disable-ldap \
+  --disable-ldaps \
+  --disable-rtsp \
+  --enable-http \
+  --enable-proxy \
+  --disable-dict \
+  --disable-telnet \
+  --disable-tftp \
+  --disable-pop3 \
+  --disable-imap \
+  --disable-smtp \
+  --disable-gopher \
+  --disable-threaded-resolver \
+  --disable-manual \
+  --disable-ipv6 \
+  --disable-sspi \
+  --disable-crypto-auth \
+  --disable-cookies \
+  --enable-hidden-symbols \
+  --without-ssl \
+  --without-gnutls \
+  --without-polarssl \
+  --without-nss \
+  --without-ca-bundle \
+  --without-ca-path \
+  --without-libssh2 \
+  --without-libidn
+
 make clean
 make
--- trunk/prebuildscripts/buildZlib.sh
+++ trunk/prebuildscripts/buildZlib.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sh configure.gnu
+env LDFLAGS="$LDFLAGS -rdynamic" CFLAGS="$CFLAGS -fno-strict-aliasing -fasynchronous-unwind-tables -fno-omit-frame-pointer -fno-optimize-sibling-calls" ./configure 
 make clean
 make
