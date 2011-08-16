eb7963ee7187e045a94d1db37869803ac400a119
Rene Meusel <rene@renemeusel.de>
2011-08-16 10:56:32 +0000

    improved downloadCatalogs.py to merge nested catalogs into one big database

--- tools/downloadCatalogs.py
+++ tools/downloadCatalogs.py
@@ -1,10 +1,30 @@
 #!/usr/bin/python
 
-import sqlite
 import urllib2
 import sys
 import os
 import zlib
+import shutil
+from optparse import OptionParser
+
+foundSqlite = False
+foundSqlite3 = False
+
+# figure out which sqlite module to use
+# in Python 2.4 an old version is present
+# which does not allow proper read out of
+# long int and therefore cannot merge catalogs
+try:
+	import sqlite3 as sqlite
+	foundSqlite3 = True
+except:
+	pass
+if not foundSqlite3:
+	try:
+		import sqlite
+		foundSqlite = True
+	except ImportError, e:
+		pass
 
 def doHttpRequest(url):
 	response = urllib2.urlopen(url)
@@ -31,7 +51,7 @@ def createDirectory(directory):
 def getCatalogFilePath(catalogName, catalogDirectory):
 	return catalogDirectory + "/" + catalogName[0:2] + "/" + catalogName[2:] + "C"
 
-def downloadCatalog(repositoryUrl, catalogName, catalogDirectory):
+def downloadCatalog(repositoryUrl, catalogName, catalogDirectory, beVerbose):
 	# find out some pathes and init the zlib decompressor
 	subdir = catalogName[0:2]
 	filename = catalogName[2:] + "C"
@@ -48,7 +68,9 @@ def downloadCatalog(repositoryUrl, catalogName, catalogDirectory):
 	u = urllib2.urlopen(url)
 	meta = u.info()
 	fileSize = int(meta.getheaders("Content-Length")[0])
-	print "retrieving " + catalogName + " - " + str(fileSize) + " bytes"
+	
+	if beVerbose:
+		print "retrieving " + catalogName + " - " + str(fileSize) + " bytes"
 	
 	blockSize = 8192
 	while True:
@@ -71,28 +93,98 @@ def findNestedCatalogs(catalogName, catalogDirectory):
 	dbHandle.close()
 	return result
 
-def retrieveCatalogsRecursively(repositoryUrl, catalogName, catalogDirectory):
-	downloadCatalog(repositoryUrl, catalogName, catalogDirectory)
+def retrieveCatalogsRecursively(repositoryUrl, catalogName, catalogDirectory, beVerbose):
+	catalogs = [catalogName]
+	downloadCatalog(repositoryUrl, catalogName, catalogDirectory, beVerbose)
 	nestedCatalogs = findNestedCatalogs(catalogName, catalogDirectory)
 	for catalog in nestedCatalogs:
-		retrieveCatalogsRecursively(repositoryUrl, catalog[0], catalogDirectory)
+		newCatalogs = retrieveCatalogsRecursively(repositoryUrl, catalog[0], catalogDirectory, beVerbose)
+		catalogs.extend(newCatalogs)
+	return catalogs
+
+def mergeCatalogs(rootCatalog, catalogList, catalogDirectory, mergeTo, beVerbose):
+	# create destination file by copying the root catalog
+	try:
+		shutil.copyfile(getCatalogFilePath(rootCatalog, catalogDirectory), mergeTo)
+	except:
+		printError("Cannot write '" + mergeTo + "'")
+	
+	# open destination database file
+	destDatabaseHandle = sqlite.connect(mergeTo)
+	
+	# merge catalogs
+	for catalog in catalogList:
+		if catalog == rootCatalog:
+			continue
+		mergeCatalogDatabases(destDatabaseHandle, catalog, catalogDirectory, beVerbose)
+	
+	# close destination database
+	destDatabaseHandle.close()
+
+def countEntriesInDatabase(dbCursor):
+	dbCursor.execute("SELECT count(*) FROM catalog")
+	return dbCursor.fetchone()[0]
+
+def mergeCatalogDatabases(destDatabase, catalogName, catalogDirectory, beVerbose):
+	# open database of source catalog
+	catalogFile = getCatalogFilePath(catalogName, catalogDirectory)
+	dbHandle = sqlite.connect(catalogFile)
+	destCursor = destDatabase.cursor()
+	srcCursor = dbHandle.cursor()
+	
+	if beVerbose:
+		print "merging " + catalogName + " with " + str(countEntriesInDatabase(srcCursor)) + " entries"
+	
+	# merge data from source database to given destination
+	srcCursor.execute("SELECT * FROM catalog")
+	skips = 0
+	while True:
+		result = srcCursor.fetchone()
+		if result == None:
+			break
+		
+		try:
+			destCursor.execute("INSERT INTO catalog VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", tuple(result))
+		except:
+			skips += 1
+	
+	if beVerbose:
+		print str(skips) + " non-unique entries skipped"
+
+	# finish
+	destDatabase.commit()
+	dbHandle.close()
 
 def main():
-	if len(sys.argv) != 3:
-		usage()
-		sys.exit(1)
-
-	repo = "http://cvmfs-stratum-one.cern.ch/opt/" + sys.argv[1]
-	catalogDirectory = sys.argv[2]
+	usage = "usage: %prog [options] <repository url>\nThis script walks through all nested catalogs of a repository and\ndownloads these catalogs to the given destination directory\nTake care: the catalogs are saved uncompressed, so do not use cvmfs_zpipe"
+	parser = OptionParser(usage)
+	parser.add_option("-d", "--directory", dest="catalogDirectory", default="catalogs", help="the directory to download catalogs to")
+	parser.add_option("-m", "--merge", metavar="FILE", dest="mergeToFile", help="merge all catalogs into one given file")
+	parser.add_option("-q", "--quiet", action="store_false", dest="verbose", default=True, help="don't print status messages to stdout")
+	(options, args) = parser.parse_args()
 	
-	rootCatalog = getRootCatalogName(repo)
-	retrieveCatalogsRecursively(repo, rootCatalog, catalogDirectory)
+	if len(args) != 1:
+		parser.error("Please provide the repository url as argument")
 
-def usage():
-	print "Usage: " + sys.argv[0] + " <repository name> <download directory>"
-	print "This script walks through all nested catalogs of a repository and"
-	print "downloads these catalogs to the given destination directory"
-	print "Take care: the catalogs are saved uncompressed, so do not use cvmfs_zpipe"
+	# read command line arguments
+	repositoryUrl = args[0]
+	catalogDirectory = options.catalogDirectory
+	merge = options.mergeToFile
+	verbose = options.verbose
+	
+	# check option consistency
+	if os.path.exists(catalogDirectory) and os.listdir(catalogDirectory) != []:
+		printError("Directory '" + catalogDirectory + "' exists and is not empty")
+	
+	if merge and foundSqlite and not foundSqlite3:
+		printError("unfortunately merging is not possible with your version of the python sqlite module")
+	
+	# do the job
+	rootCatalog = getRootCatalogName(repositoryUrl)
+	catalogs = retrieveCatalogsRecursively(repositoryUrl, rootCatalog, catalogDirectory, verbose)
+	
+	if merge:
+		mergeCatalogs(rootCatalog, catalogs, catalogDirectory, merge, verbose)
 
 def printError(errorMessage):
 	print "[ERROR] " + errorMessage
