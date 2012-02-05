86f39251a31f0afbf10cb98ffba5c241f7ff6373
Jakob Blomer <jblomer@cern.ch>
2012-02-05 18:46:24 +0000

    cleanup atomic.h

--- cvmfs/src/atomic.h
+++ cvmfs/src/atomic.h
@@ -15,32 +15,32 @@ typedef int64_t atomic_int64;
 
 
 static void inline __attribute__((used)) atomic_init32(atomic_int32 *a) {
-   *a = 0;
+  *a = 0;
 }
 
 
 static void inline __attribute__((used)) atomic_init64(atomic_int64 *a) {
-   *a = 0;
+  *a = 0;
 }
 
 
 static int32_t inline __attribute__((used)) atomic_read32(atomic_int32 *a) {
-   return __sync_fetch_and_add(a, 0);
+  return __sync_fetch_and_add(a, 0);
 }
 
 
 static int64_t inline __attribute__((used)) atomic_read64(atomic_int64 *a) {
-   return __sync_fetch_and_add(a, 0);
+  return __sync_fetch_and_add(a, 0);
 }
 
 
 static void inline __attribute__((used)) atomic_inc32(atomic_int32 *a) {
-   (void) __sync_fetch_and_add(a, 1);
+  (void) __sync_fetch_and_add(a, 1);
 }
 
 
 static void inline __attribute__((used)) atomic_inc64(atomic_int64 *a) {
-   (void) __sync_fetch_and_add(a, 1);
+  (void) __sync_fetch_and_add(a, 1);
 }
 
 
@@ -52,14 +52,14 @@ static void inline __attribute__((used)) atomic_dec32(atomic_int32 *a) {
 static int32_t inline __attribute__((used)) atomic_xadd32(atomic_int32 *a,
                                                           int32_t offset)
 {
-   return __sync_fetch_and_add(a, offset);
+  return __sync_fetch_and_add(a, offset);
 }
 
 
 static int64_t inline __attribute__((used)) atomic_xadd64(atomic_int64 *a,
                                                           int64_t offset)
 {
-   return __sync_fetch_and_add(a, offset);
+  return __sync_fetch_and_add(a, offset);
 }
 
 
@@ -67,7 +67,7 @@ static int32_t inline __attribute__((used)) atomic_cas32(atomic_int32 *a,
                                                          int32_t cmp,
                                                          int32_t newval)
 {
-   return __sync_bool_compare_and_swap(a, cmp, newval);
+  return __sync_bool_compare_and_swap(a, cmp, newval);
 }
 
 #endif  // CVMFS_ATOMIC_H_
