8e789cef0f30079a29860d3dd1755a1f701b8caf
Jakob Blomer <jblomer@cern.ch>
2012-03-20 15:18:40 +0000

    cache size adjustment

--- cvmfs/cvmfs.cc
+++ cvmfs/cvmfs.cc
@@ -82,9 +82,9 @@ using namespace std;  // NOLINT
 
 namespace cvmfs {
 
-const unsigned int kInodeCacheSize = 32000;
-const unsigned int kPathCacheSize = 32000;
-const unsigned int kMd5pathCacheSize = 32000;
+const unsigned int kInodeCacheSize = 6400;
+const unsigned int kPathCacheSize = 1600;
+const unsigned int kMd5pathCacheSize = 6400;
 
 const unsigned int kShortTermTTL = 180;  /**< If catalog reload fails, try again
                                               in 3 minutes */
