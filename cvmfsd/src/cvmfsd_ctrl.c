1f1a493a4437b21f59515970411375da3fad1158
Jakob Blomer <jblomer@cern.ch>
2012-04-14 22:20:31 +0200

    added mount commands to sudoers to allow non-root to publish repositories

--- cvmfsd/src/cvmfsd_ctrl.c
+++ cvmfsd/src/cvmfsd_ctrl.c
@@ -6,21 +6,21 @@
 #include <string.h>
 
 
-static void usage() 
+static void usage()
 {
    printf("Usage: cvmfs_ctrl [pause|resume|flush|lvmrotate]\n");
 }
 
 int main(int argc, char **argv)
 {
-   if ( (argc != 2) || 
+   if ( (argc != 2) ||
         ((strcmp(argv[1], "pause") != 0) && (strcmp(argv[1], "resume") != 0) &&
          (strcmp(argv[1], "flush") != 0) && (strcmp(argv[1], "lvmrotate") != 0)) )
    {
       usage();
       return 1;
    }
-   
+
    setuid(0);
    int retval;
    if (strcmp(argv[1], "pause") == 0)
@@ -35,10 +35,10 @@ int main(int argc, char **argv)
       usage();
       return 1;
    }
-      
+
    int exit_code;
    if (WIFEXITED(retval)) exit_code = WEXITSTATUS(retval);
    else exit_code = WTERMSIG(retval);
-   
+
    return exit_code;
 }
