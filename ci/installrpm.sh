76f28a6a06abe03000f235749d64e58c5a8b915b
Jakob Blomer <jblomer@cern.ch>
2012-07-24 17:40:12 +0200

    new url for packages

--- ci/installrpm.sh
+++ ci/installrpm.sh
@@ -9,8 +9,8 @@ selinuxrpm=$3
 initScriptsVersion=1.0.18-2
 keysVersion=1.4-1
 
-curl -k https://cernvm.cern.ch/project/trac/downloads/cernvm/cvmfs-init-scripts-${initScriptsVersion}.noarch.rpm > $tmpdir/cvmfs-init-scripts-${initScriptsVersion}.noarch.rpm
-curl -k https://cernvm.cern.ch/project/trac/downloads/cernvm/cvmfs-keys-${keysVersion}.noarch.rpm > $tmpdir/cvmfs-keys-${keysVersion}.noarch.rpm
+curl -k https://cvmrepo.web.cern.ch/cvmrepo/yum/cvmfs/EL/5/x86_64/cvmfs-init-scripts-${initScriptsVersion}.noarch.rpm > $tmpdir/cvmfs-init-scripts-${initScriptsVersion}.noarch.rpm
+curl -k https://cvmrepo.web.cern.ch/cvmrepo/yum/cvmfs/EL/5/x86_64/cvmfs-keys-${keysVersion}.noarch.rpm > $tmpdir/cvmfs-keys-${keysVersion}.noarch.rpm
 
 sudo rpm -vi $tmpdir/cvmfs-keys-${keysVersion}.noarch.rpm $cvmfsrpm $selinuxrpm
 sudo rpm -vi $tmpdir/cvmfs-init-scripts-${initScriptsVersion}.noarch.rpm
