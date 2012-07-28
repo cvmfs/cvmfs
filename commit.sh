692034700a9ca075dbb07373389c9d7ac17386bf
Jakob Blomer <jblomer@cern.ch>
2012-07-28 16:26:37 +0200

    move packaging into subfolder

--- commit.sh
+++ commit.sh
@@ -17,6 +17,6 @@ make dist
 scp cvmfs-2.0.$1.tar.gz root@cvmappi02:/srv/www/html/repository/cernvm/
 cd $OLDPWD
 sed -i s/2\.0\.[0-9]*/2.0.$[$1+1]/ configure.ac
-sed -i s/2\.0\.[0-9]*/2.0.$[$1+1]/ rpm/cvmfs.spec
-sed -i s/2\.0\.[0-9]*/2.0.$[$1+1]/ rpm/cvmfs-server.spec
-sed -i s/2\.0\.[0-9]*/2.0.$[$1+1]/ rpm/cvmfsflt.spec
+sed -i s/2\.0\.[0-9]*/2.0.$[$1+1]/ packaging/rpm/cvmfs.spec
+sed -i s/2\.0\.[0-9]*/2.0.$[$1+1]/ packaging/rpm/cvmfs-server.spec
+sed -i s/2\.0\.[0-9]*/2.0.$[$1+1]/ packaging/rpm/cvmfsflt.spec
