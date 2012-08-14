89457282a505ac94bf14e589d24ea0dba2166d96
Jakob Blomer <jblomer@cern.ch>
2012-08-14 09:36:52 +0200

    update nested catalog fix

--- cvmfs/catalog_rw.cc
+++ cvmfs/catalog_rw.cc
@@ -458,11 +458,12 @@ bool WritableCatalog::RemoveNestedCatalog(const string &mountpoint,
 bool WritableCatalog::UpdateNestedCatalog(const string &path,
                                           const hash::Any &hash)
 {
+  const string sha1_str = hash.ToString();
   const string sql = "UPDATE nested_catalogs SET sha1 = :sha1 "
     "WHERE path = :path;";
   Sql stmt(database(), sql);
 
-  stmt.BindText(1, hash.ToString());
+  stmt.BindText(1, sha1_str);
   stmt.BindText(2, path);
 
   return stmt.Execute();
