ba40aa8eeaa9110c37a2e7f38d15669fcca918f1
Jakob Blomer <jblomer@cern.ch>
2012-08-15 17:24:56 +0200

    catalog counter classes

--- cvmfs/catalog.h
+++ cvmfs/catalog.h
@@ -47,6 +47,41 @@ struct InodeRange {
 };
 
 
+struct Counters {
+  Counters() {
+    self_regular = self_symlink = self_dir = self_nested =
+      subtree_regular = subtree_symlink = subtree_dir = subtree_nested = 0;
+  }
+
+  uint64_t self_regular;
+  uint64_t self_symlink;
+  uint64_t self_dir;
+  uint64_t self_nested;
+  uint64_t subtree_regular;
+  uint64_t subtree_symlink;
+  uint64_t subtree_dir;
+  uint64_t subtree_nested;
+};
+
+
+struct DeltaCounters {
+  DeltaCounters() {
+    d_self_regular = d_self_symlink = d_self_dir = d_self_nested =
+      d_subtree_regular = d_subtree_symlink = d_subtree_dir =
+      d_subtree_nested = 0;
+  }
+
+  int64_t d_self_regular;
+  int64_t d_self_symlink;
+  int64_t d_self_dir;
+  int64_t d_self_nested;
+  int64_t d_subtree_regular;
+  int64_t d_subtree_symlink;
+  int64_t d_subtree_dir;
+  int64_t d_subtree_nested;
+};
+
+
 /**
  * This class wraps a catalog database and provides methods
  * to query for directory entries.
