7b062d9b843cf4f92c0394e3ff7cbcbca1257416
Rene Meusel <rene.meusel@cern.ch>
2012-11-09 12:08:57 +0100

    adapted test scripts to use the debug mode of cvmfs_server

--- add-ons/testscripts/testNestedCatalogs.sh
+++ add-ons/testscripts/testNestedCatalogs.sh
@@ -22,29 +22,27 @@ mkdir test1/test
 mkdir test1/test/test
 mkdir test1/test/test/test
 
-mkdir test2
+mkdir test2/
 mkdir test2/test
+mkdir test2/tset
 mkdir test2/test/test
-mkdir test2/tset 
+mkdir test2/tset/test
+mkdir test2/test/test/test
+mkdir test2/tset/test/test
 
 echo "creating some files"
 echo "lol" > test1/lol
 echo "rofl" > test1/test/test/rofl
+echo "lol" > test1/test/lol
+echo "lol" > test1/test/test/lol
+echo "rofl" > test1/test/roflcopter
+
+echo "create a nested catalog mark at test1/test/.cvmfscatalog"
+touch test1/test/.cvmfscatalog
 
 cd
 
 echo "commit this simple directory structure"
-cvmfs_server publish || die "failed to publish"
-
-cvmfs_server transaction || die "failed to init next transaction"
-
-cd $UNION
-
-echo "delete the directory structure"
-rm -fR test1
-
-cd
-
 if [ $# -eq 0 ]
 then
   cvmfs_server publish $REPO -d || die "failed to publish"
