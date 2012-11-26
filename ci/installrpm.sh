697231d38c5e45f68e6113eea496edccab96329c
Jakob Blomer <jblomer@cern.ch>
2012-11-26 13:47:22 +0100

    CI scripts: install server rpm

--- ci/installrpm.sh
+++ ci/installrpm.sh
@@ -5,6 +5,7 @@ tmpdir=$1
 cvmfsrpm=$2
 [ -z $cvmfsrpm ] && exit 2
 selinuxrpm=$3
+serverrpm=$4
 
 initScriptsVersion=1.0.18-2
 keysVersion=1.4-1
@@ -12,6 +13,6 @@ keysVersion=1.4-1
 curl -k https://cvmrepo.web.cern.ch/cvmrepo/yum/cvmfs/EL/5/x86_64/cvmfs-init-scripts-${initScriptsVersion}.noarch.rpm > $tmpdir/cvmfs-init-scripts-${initScriptsVersion}.noarch.rpm
 curl -k https://cvmrepo.web.cern.ch/cvmrepo/yum/cvmfs/EL/5/x86_64/cvmfs-keys-${keysVersion}.noarch.rpm > $tmpdir/cvmfs-keys-${keysVersion}.noarch.rpm
 
-sudo rpm -vi $tmpdir/cvmfs-keys-${keysVersion}.noarch.rpm $cvmfsrpm $selinuxrpm
+sudo rpm -vi $tmpdir/cvmfs-keys-${keysVersion}.noarch.rpm $cvmfsrpm $selinuxrpm $serverrpm
 sudo rpm -vi $tmpdir/cvmfs-init-scripts-${initScriptsVersion}.noarch.rpm
 sudo cvmfs_config setup
