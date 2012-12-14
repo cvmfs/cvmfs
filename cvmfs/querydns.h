33cb05e88b12d425fa782a9848107e3908126390
wchang <e4523744@gmail.com>
2012-12-14 16:03:10 +0000

    add querydns.h

---
+++ cvmfs/querydns.h
@@ -0,0 +1,20 @@
+/**
+ * This file belongs to CERN.
+ */
+
+#ifndef QUERYDNS_H_
+#define QUERYDNS_H_
+
+#include <ares.h>
+#include <arpa/nameser.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdarg.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <iostream>
+#endif
