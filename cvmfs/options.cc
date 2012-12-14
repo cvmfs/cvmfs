53894f1c7fa303a881a3484aa2196ccb64744852
wchang <e4523744@gmail.com>
2012-12-14 16:12:06 +0000

    add function ResolveParameters()

--- cvmfs/options.cc
+++ cvmfs/options.cc
@@ -157,6 +157,37 @@ void ParseDefault(const string &repository_name) {
   ParsePath("/etc/cvmfs/config.d/" + repository_name + ".local");
 }
 
+bool ResolveParameters()
+{
+  bool retval;
+  string flag = "@";
+  string res_result;
+  string res_host;
+
+  int type =  AF_INET;
+  const uint16_t port = 53;
+  const string ns_server="137.138.234.60";
+
+  for (map<string, ConfigValue>::iterator iter = conf_->begin(),
+       iEnd = conf_->end(); iter != iEnd; ++iter)
+  {   
+    string  conf_val = iter->second.value;
+
+    if( conf_val.find_first_of(flag) == 0 )
+    {
+       res_host = res_host.assign(conf_val, 1, conf_val.length()-1);
+       retval = QueryDns(res_host, type, &ns_server, port, &res_result);
+       if (retval == 1)
+          iter->second.value = res_result;
+       else
+          iter->second.value = "";
+
+       setenv(iter->first.c_str(), my_string.c_str(),1);
+    }
+  }
+
+  return true;
+}
 
 void ClearConfig() {
   config_->clear();
