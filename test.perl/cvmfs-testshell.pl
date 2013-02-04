30601eb05e364c0fbfe76f571ed426139ccf8d7d
Francesco Ruvolo <ruvolof@gmail.com>
2013-02-04 19:49:10 +0100

    added command history to the shell. Up Arrow works

--- test.perl/cvmfs-testshell.pl
+++ test.perl/cvmfs-testshell.pl
@@ -13,6 +13,7 @@ use Functions::Shell qw(set_remote check_command start_daemon get_daemon_output 
 use Functions::Active qw(check_daemon add_active);
 use Functions::ShellSocket qw(connect_shell_socket receive_shell_msg send_shell_msg close_shell_socket term_shell_ctxt bind_shell_socket);
 use Getopt::Long;
+use Term::ReadLine;
 
 my $command = undef;
 my $wait_daemon = undef;
@@ -105,6 +106,15 @@ if (defined($command)) {
 	exit_shell($socket, $ctxt, 1);
 }
 
+# Term::ReadLine instance for both shells: connected and disconnected
+my $rlactive = Term::ReadLine->new('ShellConnected');
+$rlactive->Attribs->ornaments(0);
+my $rlactive_prompt = '-> ';
+
+my $rldisco = Term::ReadLine->new('ShellDisconnected');
+$rldisco->Attribs->ornaments(0);
+my $rldisco_prompt = '(Daemon not running) -> ';
+
 if ($interactive) {
 	# A simple welcome.
 	print '#'x80 . "\n";
@@ -135,10 +145,7 @@ if ($interactive) {
 	while(1){
 		# This is the first shell, the one used to communicate with the daemon
 		while(check_daemon()){
-			print '-> ';
-			# Reading an input line.
-			my $line = STDIN->getline;
-			chomp($line);
+			my $line = $rlactive->readline($rlactive_prompt);
 			
 			# Checking again if the daemon is running, maybe something killed it.
 			unless (check_daemon()) {
@@ -169,10 +176,7 @@ if ($interactive) {
 		
 		# This is the second shell, use when the daemon is closed
 		while(!check_daemon()){
-			print '(Daemon not running) -> ';
-			# Reading an input line.
-			my $line = STDIN->getline;
-			chomp($line);
+			my $line = $rldisco->readline($rldisco_prompt);
 			
 			# Launching the command
 			($continue, $socket, $ctxt) = check_command($socket, $ctxt, $daemon_path, $line);
