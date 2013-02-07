044488033a4130fe9488bcb7b575c7c2a08adb05
Rene Meusel <rene.meusel@cern.ch>
2013-02-07 18:59:48 +0100

    changed debug.cc ... will be removed at some time

--- cvmfs/debug.cc
+++ cvmfs/debug.cc
@@ -2,31 +2,41 @@
 #include <algorithm>
 
 #include "util.h"
-#include "upload_file_chunker.h"
+#include "upload.h"
 
 using namespace upload;
 
-int main() {
-  MemoryMappedFile file("Makefile");
-  file.Map();
-  assert (file.IsMapped());
+void SplCallback(const upload::SpoolerResult &result) {
+  std::cout << "spooler sent callback" << std::endl;
 
-  UniquePtr<ChunkGenerator> chunk_generator(ChunkGenerator::Construct(file));
-  assert (chunk_generator);
-
-  uint64_t sum = 0;
-  uint64_t num = 0;
-
-  while (chunk_generator->HasMoreData()) {
-    // find the next file chunk boundary
-    Chunk chunk = chunk_generator->Next();
-    sum += chunk.size();
-    num++;
-
-    std::cout << chunk.offset() << " | " << chunk.size() << std::endl;
+  if (result.IsChunked()) {
+    std::cout << "CHUNKS:" << std::endl;
   }
 
-  std::cout << "average size: " << (sum / num) << std::endl;
+  FileChunks::const_iterator i    = result.file_chunks.begin();
+  FileChunks::const_iterator iend = result.file_chunks.end();
+  for (; i != iend; ++i) {
+    std::cout << "offset: " << i->offset() << " size: " << i->size() << " hash: " << i->content_hash().ToString() << std::endl;
+  }
+
+  std::cout << "Bulk: " << result.content_hash.ToString() << std::endl;
+}
+
+int main() {
+
+  std::string sds = "riak:/home/rene/Documents/Schweinestall/cvmfs/build/ramdisk:http://cernvmbl005:8098/riak/cvmfs@http://cernvmbl006:8098/riak/cvmfs@http://cernvmbl007:8098/riak/cvmfs@http://cernvmbl008:8098/riak/cvmfs@http://cernvmbl009:8098/riak/cvmfs";
+  SpoolerDefinition sd(sds, true, 4000000, 8000000, 16000000);
+
+  UniquePtr<AbstractSpooler> spooler(AbstractSpooler::Construct(sd));
+  spooler->RegisterListener(&SplCallback);
+
+  spooler->Process("/mnt/atlas-condb/cond12_data.000002.lar.COND._0003.pool.root");
+  spooler->Process("/mnt/atlas-condb/cond12_data.000002.lar.COND._0002.pool.root");
+  spooler->Process("/mnt/atlas-condb/cond12_data.000002.lar.COND._0001.pool.root");
+  spooler->Process("/mnt/atlas-condb/cond12_data.000002.lar.COND._0004.pool.root");
+  spooler->Process("/mnt/atlas-condb/cond12_data.000002.lar.COND._0005.pool.root");
+
+  spooler->WaitForTermination();
 
   return 0;
 }
