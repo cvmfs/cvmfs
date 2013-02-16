568a0eb5dd5271d65ebbca792532ac516cc078e6
Jakob Blomer <jblomer@cern.ch>
2013-02-16 13:02:26 +0100

    typo

--- cvmfs/platform_osx.h
+++ cvmfs/platform_osx.h
@@ -106,7 +106,7 @@ inline bool platform_getxattr(const std::string &path, const std::string &name,
                               std::string *value)
 {
   int size = 0;
-  void *buffer = NULL
+  void *buffer = NULL;
   int retval;
   retval = getxattr(path.c_str(), name.c_str(), buffer, size, 0, 0);
   if (retval >= 1) {
