d3a9784701943408f1172fafd323f658706e6836
Rene Meusel <rene.meusel@cern.ch>
2013-02-19 14:42:15 +0100

    follow up

--- cvmfs/util_concurrency.h
+++ cvmfs/util_concurrency.h
@@ -35,7 +35,10 @@ class Lockable : SingleCopy {
   void Unlock() const        { pthread_mutex_unlock(&mutex_);     }
 
  protected:
-  Lockable() { assert (pthread_mutex_init(&mutex_, NULL) == 0); };
+  Lockable() {
+    const int retval = pthread_mutex_init(&mutex_, NULL);
+    assert (retval == 0);
+  };
 
  private:
   mutable pthread_mutex_t mutex_;
