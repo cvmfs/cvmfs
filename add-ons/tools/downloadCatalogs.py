1d2472f6130657a28e660961fbf4c8d162d6fb08
Rene Meusel <rene.meusel@cern.ch>
2013-03-26 17:03:45 +0100

    downloadCatalogs.py now downloads the catalogs without decompressing them and also considers catalog history

--- add-ons/tools/downloadCatalogs.py
+++ add-ons/tools/downloadCatalogs.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 
-import urllib2
+from urllib2 import urlopen, URLError, HTTPError
 import sys
 import os
 import zlib
 import shutil
+import tempfile
 from optparse import OptionParser
 
 foundSqlite = False
@@ -26,9 +27,11 @@ if not foundSqlite3:
 	except ImportError, e:
 		pass
 
+
 def doHttpRequest(url):
-	response = urllib2.urlopen(url)
+	response = urlopen(url)
 	return response.read()
+
 
 def getRootCatalogName(repositoryUrl):
 	result = ""
@@ -36,20 +39,23 @@ def getRootCatalogName(repositoryUrl):
 		result = doHttpRequest(repositoryUrl + "/.cvmfspublished")
 	except:
 		printError("cannot load .cvmfspublished")
-		
+
 	lines = result.split('\n')
 	if len(lines) < 1:
 		printError(".cvmfspublished is malformed")
-	
+
 	return lines[0][1:]
+
 
 def createDirectory(directory):
 	d = os.path.dirname(directory)
 	if not os.path.exists(d):
 		os.makedirs(d)
 
+
 def getCatalogFilePath(catalogName, catalogDirectory):
 	return catalogDirectory + "/" + catalogName[0:2] + "/" + catalogName[2:] + "C"
+
 
 def downloadCatalog(repositoryUrl, catalogName, catalogDirectory, beVerbose):
 	# find out some pathes and init the zlib decompressor
@@ -58,102 +64,83 @@ def downloadCatalog(repositoryUrl, catalogName, catalogDirectory, beVerbose):
 	url = repositoryUrl + "/data/" + subdir + "/" + filename
 	destDir = catalogDirectory + "/" + subdir + "/"
 	dest = destDir + filename
-	decoder = zlib.decompressobj()
-	
+	#decoder = zlib.decompressobj()
+
 	# create target directory if not existing and open output file
 	createDirectory(destDir)
 	outputFile = open(dest, 'wb')
-	
-	# download and decompress on the fly
-	u = urllib2.urlopen(url)
-	meta = u.info()
-	fileSize = int(meta.getheaders("Content-Length")[0])
-	
-	if beVerbose:
-		print "retrieving " + catalogName + " - " + str(fileSize) + " bytes"
-	
-	blockSize = 8192
-	while True:
-		buffer = u.read(blockSize)
-		if not buffer:
-			break
-		data = decoder.decompress(buffer)
-		outputFile.write(data)
 
-	# write remaining stuff to file
-	outputFile.write(decoder.decompress(buffer))
-	outputFile.close()
-	
+	# download the catalog
+	try:
+		f = urlopen(url)
+		meta = f.info()
+		fileSize = int(meta.getheaders("Content-Length")[0])
+
+		if beVerbose:
+			print "retrieving " + catalogName + " - " + str(fileSize) + " bytes"
+
+		with open(dest, "wb") as local_file:
+			local_file.write(f.read())
+	except HTTPError, e:
+		printError("HTTP: " + e.code + url)
+	except URLError, e:
+		printError("URL:" + e.reason + url)
+
+
+def decompressCatalog(filename, destination):
+	str_object1 = open(filename, 'rb').read()
+	str_object2 = zlib.decompress(str_object1)
+	f = open(destination, 'wb')
+	f.write(str_object2)
+	f.close()
+
+
 def findNestedCatalogs(catalogName, catalogDirectory):
 	catalogFile = getCatalogFilePath(catalogName, catalogDirectory)
-	dbHandle = sqlite.connect(catalogFile)
+	tempFile    = tempfile.NamedTemporaryFile('wb')
+	decompressCatalog(catalogFile, tempFile.name)
+
+	dbHandle = sqlite.connect(tempFile.name)
 	cursor = dbHandle.cursor()
+	catalogs = []
+
+	# nested catalog references
 	cursor.execute("SELECT sha1 FROM nested_catalogs")
 	result = cursor.fetchall()
+	for catalog in result:
+		catalogs.append(catalog[0])
+
+	# history references
+	cursor.execute("SELECT value FROM properties WHERE key = 'previous_revision' LIMIT 1")
+	result = cursor.fetchall()
+	if result:
+		catalogs.append(result[0][0])
+
 	dbHandle.close()
-	return result
+	tempFile.close()
+	return catalogs
+
 
 def retrieveCatalogsRecursively(repositoryUrl, catalogName, catalogDirectory, beVerbose):
 	catalogs = [catalogName]
-	downloadCatalog(repositoryUrl, catalogName, catalogDirectory, beVerbose)
-	nestedCatalogs = findNestedCatalogs(catalogName, catalogDirectory)
-	for catalog in nestedCatalogs:
-		newCatalogs = retrieveCatalogsRecursively(repositoryUrl, catalog[0], catalogDirectory, beVerbose)
-		catalogs.extend(newCatalogs)
-	return catalogs
-
-def mergeCatalogs(rootCatalog, catalogList, catalogDirectory, mergeTo, beVerbose):
-	# create destination file by copying the root catalog
-	try:
-		shutil.copyfile(getCatalogFilePath(rootCatalog, catalogDirectory), mergeTo)
-	except:
-		printError("Cannot write '" + mergeTo + "'")
-	
-	# open destination database file
-	destDatabaseHandle = sqlite.connect(mergeTo)
-	
-	# merge catalogs
-	for catalog in catalogList:
-		if catalog == rootCatalog:
+	downloads = 0
+	while catalogs:
+		catalog = catalogs.pop(0)
+		if os.path.exists(getCatalogFilePath(catalog, catalogDirectory)):
+			if beVerbose:
+				print "--> skipping already loaded catalog:" , catalog
 			continue
-		mergeCatalogDatabases(destDatabaseHandle, catalog, catalogDirectory, beVerbose)
-	
-	# close destination database
-	destDatabaseHandle.close()
 
-def countEntriesInDatabase(dbCursor):
-	dbCursor.execute("SELECT count(*) FROM catalog")
-	return dbCursor.fetchone()[0]
+		downloadCatalog(repositoryUrl, catalog, catalogDirectory, beVerbose)
+		nestedCatalogs = findNestedCatalogs(catalog, catalogDirectory)
+		downloads += 1
 
-def mergeCatalogDatabases(destDatabase, catalogName, catalogDirectory, beVerbose):
-	# open database of source catalog
-	catalogFile = getCatalogFilePath(catalogName, catalogDirectory)
-	dbHandle = sqlite.connect(catalogFile)
-	destCursor = destDatabase.cursor()
-	srcCursor = dbHandle.cursor()
-	
-	if beVerbose:
-		print "merging " + catalogName + " with " + str(countEntriesInDatabase(srcCursor)) + " entries"
-	
-	# merge data from source database to given destination
-	srcCursor.execute("SELECT * FROM catalog")
-	skips = 0
-	while True:
-		result = srcCursor.fetchone()
-		if result == None:
-			break
-		
-		try:
-			destCursor.execute("INSERT INTO catalog VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", tuple(result))
-		except:
-			skips += 1
-	
-	if beVerbose:
-		print str(skips) + " non-unique entries skipped"
+		if beVerbose:
+			print "--> found" , len(nestedCatalogs) , "catalog references |" , len(catalogs) , "in queue"
+			catalogs.extend(nestedCatalogs)
 
-	# finish
-	destDatabase.commit()
-	dbHandle.close()
+	return downloads
+
 
 def main():
 	usage = "usage: %prog [options] <repository url>\nThis script walks through all nested catalogs of a repository and\ndownloads these catalogs to the given destination directory\nTake care: the catalogs are saved uncompressed, so do not use cvmfs_zpipe"
@@ -162,7 +149,7 @@ def main():
 	parser.add_option("-m", "--merge", metavar="FILE", dest="mergeToFile", help="merge all catalogs into one given file")
 	parser.add_option("-q", "--quiet", action="store_false", dest="verbose", default=True, help="don't print status messages to stdout")
 	(options, args) = parser.parse_args()
-	
+
 	if len(args) != 1:
 		parser.error("Please provide the repository url as argument")
 
@@ -171,23 +158,25 @@ def main():
 	catalogDirectory = options.catalogDirectory
 	merge = options.mergeToFile
 	verbose = options.verbose
-	
+
 	# check option consistency
 	if os.path.exists(catalogDirectory) and os.listdir(catalogDirectory) != []:
 		printError("Directory '" + catalogDirectory + "' exists and is not empty")
-	
+
 	if merge and foundSqlite and not foundSqlite3:
 		printError("unfortunately merging is not possible with your version of the python sqlite module")
-	
+
 	# do the job
 	rootCatalog = getRootCatalogName(repositoryUrl)
-	catalogs = retrieveCatalogsRecursively(repositoryUrl, rootCatalog, catalogDirectory, verbose)
-	
+	numCatalogs = retrieveCatalogsRecursively(repositoryUrl, rootCatalog, catalogDirectory, verbose)
+
+	print "downloaded" , numCatalogs , "catalogs"
+
 	if merge:
 		mergeCatalogs(rootCatalog, catalogs, catalogDirectory, merge, verbose)
 
 def printError(errorMessage):
 	print "[ERROR] " + errorMessage
 	sys.exit(1)
-	
+
 main()
