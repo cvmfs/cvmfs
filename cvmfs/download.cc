5f99c29e0d73d97f60eac164ef1982b3285528ce
Jakob Blomer <jblomer@cern.ch>
2013-05-19 19:22:55 +0200

    perform host failover on HTTP 504 errors

--- cvmfs/download.cc
+++ cvmfs/download.cc
@@ -338,9 +338,15 @@ static size_t CallbackCurlHeader(void *ptr, size_t size, size_t nmemb,
     } else {
       LogCvmfs(kLogDownload, kLogDebug, "http status error code: %s",
                header_line.c_str());
-      info->error_code = (info->proxy == "") ? kFailHostHttp :
-                                               kFailProxyHttp;
-      // code dependent error heuristics?
+      if ((header_line.length() >= i+2) &&
+          (header_line[i] == '5') && (header_line[i+1] == '0') &&
+          (header_line[i+2] == '4'))
+      {
+        info->error_code = kFailHostHttp;
+      } else {
+        info->error_code = (info->proxy == "") ? kFailHostHttp :
+                                                 kFailProxyHttp;
+      }
       return 0;
     }
   }
