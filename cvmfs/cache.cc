301db043ed01c9b64301968e7e0cbf759bc79b26
Jakob Blomer <jblomer@cern.ch>
2013-05-26 14:28:27 +0200

    remove running sentinel when switching into read-only mode

--- cvmfs/cache.cc
+++ cvmfs/cache.cc
@@ -58,6 +58,7 @@
 #include "shortstring.h"
 #include "manifest.h"
 #include "manifest_fetch.h"
+#include "cvmfs.h"
 
 using namespace std;  // NOLINT
 
@@ -194,6 +195,7 @@ void TearDown2ReadOnly() {
   cache_mode_ = kCacheReadOnly;
   CallGuard::Drainout();
   quota::Fini();
+  unlink(("running." + *cvmfs::repository_name_).c_str());
   LogCvmfs(kLogCache, kLogSyslog, "switch to read-only cache mode");
   SetLogMicroSyslog("");
 }
