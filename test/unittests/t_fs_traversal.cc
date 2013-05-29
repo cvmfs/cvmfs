01d85efeec318c47cf6739edba90c78d7a7ad309
Rene Meusel <rene.meusel@cern.ch>
2013-05-29 10:27:58 +0200

    add t_fs_traversal testcase

---
+++ test/unittests/t_fs_traversal.cc
@@ -0,0 +1,19 @@
+#include <gtest/gtest.h>
+
+#include "../../cvmfs/fs_traversal.h"
+
+class T_FsTraversal : public ::testing::Test {
+  virtual void SetUp() {
+
+  }
+
+  virtual void TearDown() {
+
+  }
+
+};
+
+
+TEST_F(T_FsTraversal, FullTraversal) {
+
+}
