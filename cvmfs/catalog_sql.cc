6e60a6c83a63ba1a2595d3e1121ff1dfc72a7d71
Rene Meusel <rene.meusel@cern.ch>
2013-06-19 11:19:23 +0200

    cosmetics

--- cvmfs/catalog_sql.cc
+++ cvmfs/catalog_sql.cc
@@ -432,28 +432,29 @@ DirectoryEntry SqlLookup::GetDirent(const Catalog *catalog) const {
 
   // retrieve the hardlink information from the hardlinks database field
   if (catalog->schema() < 2.1-Database::kSchemaEpsilon) {
-    result.linkcount_ = 1;
-    result.hardlink_group_ = 0;
-    result.inode_ = catalog->GetMangledInode(RetrieveInt64(12), 0);
-    result.uid_ = g_uid;
-    result.gid_ = g_gid;
+    result.linkcount_       = 1;
+    result.hardlink_group_  = 0;
+    result.inode_           = catalog->GetMangledInode(RetrieveInt64(12), 0);
+    result.uid_             = g_uid;
+    result.gid_             = g_gid;
     result.is_chunked_file_ = false;
   } else {
     const uint64_t hardlinks = RetrieveInt64(1);
-    result.linkcount_ = Hardlinks2Linkcount(hardlinks);
-    result.hardlink_group_ = Hardlinks2HardlinkGroup(hardlinks);
-    result.inode_ = catalog->GetMangledInode(RetrieveInt64(12),
-                                             result.hardlink_group_);
-    result.uid_ = RetrieveInt64(13);
-    result.gid_ = RetrieveInt64(14);
-    result.is_chunked_file_ = (database_flags & kFlagFileChunk);
+    result.linkcount_        = Hardlinks2Linkcount(hardlinks);
+    result.hardlink_group_   = Hardlinks2HardlinkGroup(hardlinks);
+    result.inode_            = catalog->GetMangledInode(RetrieveInt64(12),
+                                                        result.hardlink_group_);
+    result.uid_              = RetrieveInt64(13);
+    result.gid_              = RetrieveInt64(14);
+    result.is_chunked_file_  = (database_flags & kFlagFileChunk);
   }
-  result.mode_ = RetrieveInt(3);
-  result.size_ = RetrieveInt64(2);
-  result.mtime_ = RetrieveInt64(4);
+
+  result.mode_     = RetrieveInt(3);
+  result.size_     = RetrieveInt64(2);
+  result.mtime_    = RetrieveInt64(4);
   result.checksum_ = RetrieveSha1Blob(0);
-  result.name_.Assign(name, strlen(name));
-  result.symlink_.Assign(symlink, strlen(symlink));
+  result.name_     = name;
+  result.symlink_  = symlink;
   ExpandSymlink(&result.symlink_);
 
   return result;
