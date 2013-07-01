c62692f13f79beccf5ead1541c8bf3b940413289
Rene Meusel <rene@renemeusel.de>
2013-07-01 08:24:09 +0200

    synchronous IO

---
+++ spike.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+ramdisk_name="ramdisk"
+
+clang++ -o spike -O3 -std=c++11 -stdlib=libc++ -ltbb -ltbbmalloc -lz -lcrypto spike.cc cvmfs/logging.cc
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+
+umount /Volumes/$ramdisk_name
+diskutil erasevolume HFS+ "$ramdisk_name" `hdiutil attach -nomount ram://4388608`
+
+purge
+
+echo "- - - - - -"
+
+./spike
