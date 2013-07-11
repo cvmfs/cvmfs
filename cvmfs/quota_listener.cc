be20a766435fa12394cab1a8a5d19da01a8623f9
Jakob Blomer <jblomer@cern.ch>
2013-07-11 19:53:54 +0200

    add machinery to start and stop unpin listener

--- cvmfs/quota_listener.cc
+++ cvmfs/quota_listener.cc
@@ -5,19 +5,96 @@
 #include "cvmfs_config.h"
 #include "quota_listener.h"
 
+#include <pthread.h>
+#include <poll.h>
+
 #include "quota.h"
 #include "catalog_mgr.h"
+#include "smalloc.h"
+#include "logging.h"
 
 using namespace std;  // NOLINT
 
 namespace quota {
 
+struct ListenerHandle {
+  int pipe_backchannel[2];
+  int pipe_terminate[2];
+  catalog::AbstractCatalogManager *catalog_manager;
+  string repository_name;
+  pthread_t thread_listener;
+};
+
+
+static void *MainUnpinListener(void *data) {
+  ListenerHandle *handle = static_cast<ListenerHandle *>(data);
+  LogCvmfs(kLogQuota, kLogDebug, "starting unpin listener for %s",
+           handle->repository_name.c_str());
+
+  struct pollfd *watch_fds =
+    static_cast<struct pollfd *>(smalloc(2 * sizeof(struct pollfd)));
+  watch_fds[0].fd = handle->pipe_terminate[0];
+  watch_fds[0].events = POLLIN | POLLPRI;
+  watch_fds[0].revents = 0;
+  watch_fds[1].fd = handle->pipe_backchannel[0];
+  watch_fds[1].events = POLLIN | POLLPRI;
+  watch_fds[1].revents = 0;
+  while (true) {
+    int retval = poll(watch_fds, 2, -1);
+    if (retval < 0) {
+      continue;
+    }
+
+    // Terminate I/O thread
+    if (watch_fds[0].revents)
+      break;
+
+    // Release pinned catalogs
+    if (watch_fds[1].revents) {
+      watch_fds[1].revents = 0;
+      char cmd;
+      ReadPipe(handle->pipe_backchannel[0], &cmd, sizeof(cmd));
+      if (cmd == 'C') {
+        LogCvmfs(kLogQuota, kLogDebug | kLogSyslog, "released nested catalogs");
+      }
+    }
+  }
+  free(watch_fds);
+
+  LogCvmfs(kLogQuota, kLogDebug, "stopping unpin listener for %s",
+           handle->repository_name.c_str());
+  return NULL;
+}
+
+
 /**
  * Registers a back channel that reacts on high watermark of pinned chunks
  */
-void RegisterUnpinListener(catalog::AbstractCatalogManager *catalog_manager,
-                           const string &repository_name)
+ListenerHandle *
+RegisterUnpinListener(catalog::AbstractCatalogManager *catalog_manager,
+                      const string &repository_name)
 {
+  ListenerHandle *handle = new ListenerHandle();
+  quota::RegisterBackChannel(handle->pipe_backchannel, repository_name);
+  MakePipe(handle->pipe_terminate);
+  handle->catalog_manager = catalog_manager;
+  handle->repository_name = repository_name;
+  int retval = pthread_create(&handle->thread_listener, NULL, MainUnpinListener,
+                              static_cast<void *>(handle));
+  assert(retval == 0);
+  return handle;
+}
+
+
+void UnregisterUnpinListener(ListenerHandle *handle) {
+  UnregisterBackChannel(handle->pipe_backchannel, handle->repository_name);
+
+  const char terminate = 'T';
+  WritePipe(handle->pipe_terminate[1], &terminate, sizeof(terminate));
+  pthread_join(handle->thread_listener, NULL);
+  ClosePipe(handle->pipe_terminate);
+
+  delete handle;
 }
 
 }  // namespace quota
--- cvmfs/quota_listener.h
+++ cvmfs/quota_listener.h
@@ -1,5 +1,17 @@
 /**
  * This file is part of the CernVM File System.
+ *
+ * The unpin listener acts as a mediator between the cache manager (quota.cc)
+ * and the catalog manager.  If the cache fills up with pinned catalog, the
+ * cache manager will broadcast a "release" request to all clients.  The unpin
+ * listener handles the request and asks the cache manager to detach all
+ * catalogs except from the root catalog.  The next access on a file deep
+ * in the hierarchy will automatically trigger the respective nested catalogs
+ * to be loaded.
+ *
+ * It allows to get rid of pinned catalogs that are lingering in the cache but
+ * not used.  As a result, the cache size can be reduced.  If it is too small,
+ * however, the cache starts thrashing.
  */
 
 #ifndef CVMFS_QUOTA_LISTENER_H_
@@ -13,8 +25,12 @@ namespace catalog {
 
 namespace quota {
 
-void RegisterUnpinListener(catalog::AbstractCatalogManager *catalog_manager,
-                           const std::string &repository_name);
+struct ListenerHandle;
+
+ListenerHandle *
+RegisterUnpinListener(catalog::AbstractCatalogManager *catalog_manager,
+                      const std::string &repository_name);
+void UnregisterUnpinListener(ListenerHandle *handle);
 
 }  // namespace quota
 
