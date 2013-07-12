007dc13c999bc0e11b0a5b97d26820f9074b48c2
Jakob Blomer <jblomer@cern.ch>
2013-07-12 16:25:56 +0200

    store magic marker at beginning of mmap'd regions

--- cvmfs/smalloc.h
+++ cvmfs/smalloc.h
@@ -9,6 +9,7 @@
 
 #include <sys/mman.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <cassert>
 //#include <cstdio>
 
@@ -22,6 +23,9 @@
 #ifdef CVMFS_NAMESPACE_GUARD
 namespace CVMFS_NAMESPACE_GUARD {
 #endif
+
+// Checkerboard marker (binary 101010...)
+const uint32_t kMemMarker = 0xAAAAAAAA;
 
 static inline void * __attribute__((used)) smalloc(size_t size) {
   void *mem = malloc(size);
@@ -43,21 +47,22 @@ static inline void * __attribute__((used)) scalloc(size_t count, size_t size) {
 
 static inline void * __attribute__((used)) smmap(size_t size) {
   assert(size > 0);
-  size_t pages = ((size + sizeof(size_t))+4095)/4096;
+  size_t pages = ((size + 2*sizeof(size_t))+4095)/4096;
   unsigned char *mem =
     static_cast<unsigned char *>(mmap(NULL, pages*4096, PROT_READ | PROT_WRITE,
                                  MAP_PRIVATE | PLATFORM_MAP_ANONYMOUS, -1, 0));
   //printf("SMMAP %d bytes at %p\n", pages*4096, mem);
   assert((mem != MAP_FAILED) && "Out Of Memory");
-  *((size_t *)(mem)) = pages;
-  return mem + sizeof(size_t);
+  *((size_t *)(mem)) = kMemMarker;
+  *((size_t *)(mem)+1) = pages;
+  return mem + 2*sizeof(size_t);
 }
 
 static inline void __attribute__((used)) smunmap(void *mem) {
   unsigned char *area = static_cast<unsigned char *>(mem);
   area = area - sizeof(size_t);
   size_t pages = *((size_t *)(area));
-  int retval = munmap(area, pages*4096);
+  int retval = munmap(area-sizeof(size_t), pages*4096);
   //printf("SUNMMAP %d bytes at %p\n", pages*4096, area);
   assert((retval == 0) && "Invalid umnmap");
 }
