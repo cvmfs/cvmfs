31b7acc1d8f560f7621da92d23a42a0bb51fdd4c
Jakob Blomer <jblomer@cern.ch>
2013-07-15 16:59:56 +0200

    switch to c-ares 1.10.0

--- externals/c-ares/src/configureHook.sh
+++ externals/c-ares/src/configureHook.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-sh configure LDFLAGS="$LDFLAGS -rdynamic" CFLAGS="$CFLAGS -D_FILE_OFFSET_BITS=64 -fno-strict-aliasing -fasynchronous-unwind-tables -fno-omit-frame-pointer -fno-optimize-sibling-calls -fvisibility=hidden"
+sh configure LDFLAGS="$LDFLAGS -rdynamic" CPPFLAGS="$CPPFLAGS -D_FILE_OFFSET_BITS=64" CFLAGS="$CFLAGS -fno-strict-aliasing -fasynchronous-unwind-tables -fno-omit-frame-pointer -fno-optimize-sibling-calls -fvisibility=hidden"
+  
