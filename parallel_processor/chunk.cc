38607c5b44bb74b49ba90261c17a50dd0a714f3e
Rene Meusel <rene@renemeusel.de>
2013-07-17 06:36:36 +0000

    allow for deferred write of chunks

--- parallel_processor/chunk.cc
+++ parallel_processor/chunk.cc
@@ -1,14 +1,32 @@
 #include "chunk.h"
 
+#include <sstream>
+
 #include "io_dispatcher.h"
 
 void Chunk::ScheduleWrite(CharBuffer *buffer) {
-  // if (deferred_write_) {
-  //   assert (!HasFileDescriptor());
-  //   Print("Deferring write...");
-  //   deferred_buffers_.push_back(buffer);
-  //   return;
-  // }
+  if (deferred_write_) {
+    assert (! HasFileDescriptor());
+    deferred_buffers_.push_back(buffer);
+    return;
+  }
 
   IoDispatcher::Instance()->ScheduleWrite(this, buffer);
+}
+
+
+void Chunk::Done() {
+  assert (! done_);
+
+  if (deferred_write_) {
+    assert (deferred_buffers_.size() > 0);
+    std::vector<CharBuffer*>::const_iterator i    = deferred_buffers_.begin();
+    std::vector<CharBuffer*>::const_iterator iend = deferred_buffers_.end();
+    for (; i != iend; ++i) {
+      IoDispatcher::Instance()->ScheduleWrite(this, *i);
+    }
+    deferred_write_ = false;
+  }
+
+  done_ = true;
 }
