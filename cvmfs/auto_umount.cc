2e3a550c4ce7e14b8b1b49eddcae6c44eb1d0322
Jakob Blomer <jblomer@cern.ch>
2013-07-26 13:17:55 +0200

    crash handler: unmount stalled mountpoint

--- cvmfs/auto_umount.cc
+++ cvmfs/auto_umount.cc
@@ -5,7 +5,19 @@
 #include "config.h"
 #include "auto_umount.h"
 
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <dirent.h>
+
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+
+#include "platform.h"
 #include "logging.h"
+#include "util.h"
 
 using namespace std;
 
@@ -28,6 +40,57 @@ void UmountOnCrash() {
     LogCvmfs(kLogCvmfs, kLogSyslogErr, "crash cleanup handler: no mountpoint");
     return;
   }
+
+  std::vector<std::string> all_mountpoints = platform_mountlist();
+  if (all_mountpoints.empty()) {
+    LogCvmfs(kLogCvmfs, kLogSyslogErr, "crash cleanup handler: "
+             "failed to read mount point list");
+    return;
+  }
+
+  // Mitigate auto-mount - crash - umount - auto-mount loops
+  SafeSleepMs(2000);
+
+  // Check if *mountpoint_ is still mounted
+  // (we don't want to trigger a mount by immediately doing stat *mountpoint_)
+  bool still_mounted = false;
+  for (unsigned i = 0; i < all_mountpoints.size(); ++i) {
+    if (*mountpoint_ == all_mountpoints[i]) {
+      still_mounted = true;
+      break;
+    }
+  }
+  if (!still_mounted) {
+    LogCvmfs(kLogCvmfs, kLogDebug, "crash cleanup handler: %s not mounted",
+             mountpoint_->c_str());
+    return;
+  }
+
+  // stat() might be served from caches.  Opendir ensures fuse module is called.
+  DIR *dirp = opendir(mountpoint_->c_str());
+  if (dirp || (errno != ENOTCONN)) {
+    if (dirp) closedir(dirp);
+    LogCvmfs(kLogCvmfs, kLogSyslog, "crash cleanup handler: "
+             "%s seems not to be stalled (%d)", mountpoint_->c_str(), errno);
+    return;
+  }
+
+  // sudo umount -l *mountpoint_
+  if (!SwitchCredentials(0, getegid(), true)) {
+    LogCvmfs(kLogCvmfs, kLogSyslogErr, "crash cleanup handler: "
+             "failed to re-gain root privileges");
+    return;
+  }
+  const bool lazy = true;
+  bool retval = platform_umount(mountpoint_->c_str(), lazy);
+  if (!retval) {
+    LogCvmfs(kLogCvmfs, kLogSyslogErr, "crash cleanup handler: "
+             "failed to unmount %s", mountpoint_->c_str());
+    return;
+  }
+
+  LogCvmfs(kLogCvmfs, kLogSyslog, "crash cleanup handler: unmounted %s",
+           mountpoint_->c_str());
 }
 
 }  // namespace auto_umount
