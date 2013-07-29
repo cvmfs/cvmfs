4a891630d236fb9b3534d081027cfaae6ae91e8f
Jakob Blomer <jblomer@cern.ch>
2013-07-29 13:41:23 +0200

    add wpad translation units for automatic proxy discovery

---
+++ cvmfs/wpad.cc
@@ -0,0 +1,15 @@
+/**
+ * This file is part of the CernVM File System.
+ */
+
+#include "config.h"
+#include "wpad.h"
+
+#include "download.h"
+#include "logging.h"
+
+using namespace std;  // NOLINT
+
+namespace download {
+
+}  // namespace download
---
+++ cvmfs/wpad.h
@@ -0,0 +1,14 @@
+/**
+ * This file is part of the CernVM File System.
+ */
+
+#ifndef CVMFS_WPAD_H_
+#define CVMFS_WPAD_H_
+
+namespace download {
+
+
+
+}  // namespace download
+
+#endif  // CVMFS_WPAD_H_
