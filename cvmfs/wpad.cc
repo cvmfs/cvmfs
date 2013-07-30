e164542abf215778f9600dac27769020db9b8e58
Jakob Blomer <jblomer@cern.ch>
2013-07-30 14:50:53 +0200

    parse pac files

--- cvmfs/wpad.cc
+++ cvmfs/wpad.cc
@@ -6,7 +6,13 @@
 #include "wpad.h"
 
 #include <cstdlib>
+#include <cstdarg>
+#include <cstdio>
+
 #include <vector>
+#include <string>
+
+#include "pacparser.h"
 
 #include "download.h"
 #include "logging.h"
@@ -16,10 +22,124 @@ using namespace std;  // NOLINT
 
 namespace download {
 
-static string ParsePac(const char *pac_string, const size_t size,
-                       DownloadManager *download_manager)
+static int PrintPacError(const char *fmt, va_list argp) {
+  char *msg = NULL;
+
+  int retval = vasprintf(&msg, fmt, argp);
+  assert(retval != -1);  // else: out of memory
+
+  LogCvmfs(kLogDownload, kLogDebug, "(pacparser) %s", msg);
+  free(msg);
+  return retval;
+}
+
+
+static string PacProxy2Cvmfs(const string &pac_proxy,
+                             const bool report_errors)
 {
-  return "DIRECT";
+  int log_flags = report_errors ? kLogDebug | kLogSyslogWarn : kLogDebug;
+  if (pac_proxy == "")
+    return "DIRECT";
+
+  string cvmfs_proxy;
+  vector<string> components = SplitString(pac_proxy, ';');
+  for (unsigned i = 0; i < components.size(); ++i) {
+    // Remove white spaces
+    string next_proxy;
+    for (unsigned j = 0; j < components[i].length(); ++j) {
+      if ((components[i][j] != ' ') && (components[i][j] != '\t'))
+        next_proxy.push_back(components[i][j]);
+    }
+
+    // No SOCKS support
+    if (HasPrefix(next_proxy, "SOCKS", false)) {
+      LogCvmfs(kLogDownload, log_flags,
+               "no support for SOCKS proxy, skipping %s",
+               next_proxy.substr(5).c_str());
+      continue;
+    }
+
+    if ((next_proxy != "DIRECT") &&
+        !HasPrefix(next_proxy, "PROXY", false))
+    {
+      LogCvmfs(kLogDownload, log_flags, "invalid proxy definition: %s",
+               next_proxy.c_str());
+      continue;
+    }
+
+    if (HasPrefix(next_proxy, "PROXY", false))
+      next_proxy = next_proxy.substr(5);
+
+    if (cvmfs_proxy == "")
+      cvmfs_proxy = next_proxy;
+    else
+      cvmfs_proxy += ";" + next_proxy;
+  }
+
+  return cvmfs_proxy;
+}
+
+
+static bool ParsePac(const char *pac_data, const size_t size,
+                     DownloadManager *download_manager,
+                     string *proxies)
+{
+  *proxies = "";
+
+  pacparser_set_error_printer(PrintPacError);
+  bool retval = pacparser_init();
+  if (!retval)
+    return false;
+
+  const string pac_string(pac_data, size);
+  retval = pacparser_parse_pac_string(pac_string.c_str());
+  if (!retval) {
+    pacparser_cleanup();
+    return false;
+  }
+
+  // For every stratum 1: get proxy
+  vector<string> host_list;
+  vector<int> rtt;
+  unsigned current_host;
+  download_manager->GetHostInfo(&host_list, &rtt, &current_host);
+  for (unsigned i = 0; i < host_list.size(); ++i) {
+    size_t hostname_begin = 7;  // Strip http:// or file://
+    size_t hostname_end = host_list[i].find_first_of(":/", hostname_begin);
+    size_t hostname_len =
+      (hostname_end == string::npos) ?
+      string::npos : hostname_end-hostname_begin;
+    const string hostname = host_list[i].substr(hostname_begin, hostname_len);
+    const string url = host_list[i] + "/.cvmfspublished";
+
+    // pac_proxy is freed by JavaScript GC
+    char *pac_proxy = pacparser_find_proxy(url.c_str(), hostname.c_str());
+    if (pac_proxy == NULL) {
+      pacparser_cleanup();
+      return false;
+    }
+    if (*proxies == "") {
+      *proxies = PacProxy2Cvmfs(pac_proxy, true);
+      if (*proxies == "") {
+        LogCvmfs(kLogDownload, kLogDebug | kLogSyslogWarn,
+                 "no valid proxy found (%s returned from pac file)", pac_proxy);
+        pacparser_cleanup();
+        return false;
+      }
+    } else {
+      const string alt_proxies = PacProxy2Cvmfs(pac_proxy, false);
+      if (*proxies != alt_proxies) {
+        LogCvmfs(kLogDownload, kLogDebug | kLogSyslogWarn,
+                 "proxy settings for host %s differ from proxy settings for "
+                 "other hosts (%s / %s). Not using proxy setting %s.",
+                 host_list[i].c_str(), proxies->c_str(), alt_proxies.c_str(),
+                 alt_proxies.c_str());
+      }
+    }
+  }
+
+  pacparser_cleanup();
+  return true;
 }
 
 
@@ -46,17 +166,26 @@ string AutoProxy(DownloadManager *download_manager) {
     download::JobInfo download_pac(&pac_paths[i], false, false, NULL);
     int retval = download_manager->Fetch(&download_pac);
     if (retval == download::kFailOk) {
-      const string proxies =
-        ParsePac(download_pac.destination_mem.data,
-                 download_pac.destination_mem.size,
-                 download_manager);
+      string proxies;
+      retval = ParsePac(download_pac.destination_mem.data,
+                        download_pac.destination_mem.size,
+                        download_manager,
+                        &proxies);
       free(download_pac.destination_mem.data);
-      if (proxies != "") {
-        LogCvmfs(kLogDownload, kLogDebug | kLogSyslog, "CernVM-FS: "
-                 "using HTTP proxy server(s) %s from pac file %s",
-                 proxies.c_str(), pac_paths[i].c_str());
-        return proxies;
+      if (!retval) {
+        LogCvmfs(kLogDownload, kLogDebug | kLogSyslogWarn,
+                 "failed to parse pac file %s",  pac_paths[i].c_str());
+      } else {
+        if (proxies != "") {
+          LogCvmfs(kLogDownload, kLogDebug | kLogSyslog, "CernVM-FS: "
+                   "using HTTP proxy server(s) %s from pac file %s",
+                   proxies.c_str(), pac_paths[i].c_str());
+          return proxies;
+        }
       }
+
+      LogCvmfs(kLogDownload, kLogDebug, "no proxy settings found in %s",
+              pac_paths[i].c_str());
     }
   }
 
