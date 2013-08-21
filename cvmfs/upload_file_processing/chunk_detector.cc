325034ae034daf27983e08857d8c073c6382a07b
Rene Meusel <rene@renemeusel.de>
2013-08-21 08:50:20 +0000

    minor refactorings and compile warning fixes

--- cvmfs/upload_file_processing/chunk_detector.cc
+++ cvmfs/upload_file_processing/chunk_detector.cc
@@ -10,7 +10,7 @@
 using namespace upload;
 
 off_t StaticOffsetDetector::FindNextCutMark(CharBuffer *buffer) {
-  const off_t next_cut = last_cut() + offset_;
+  const off_t next_cut = last_cut() + chunk_size_;
   if (next_cut >= buffer->base_offset() + static_cast<off_t>(buffer->size())) {
     return NoCut(next_cut);
   }
@@ -30,12 +30,25 @@ off_t StaticOffsetDetector::FindNextCutMark(CharBuffer *buffer) {
 // of cut marks.
 const int32_t Xor32Detector::magic_number_   = // TODO: C++11 (constexpr (?))
   std::numeric_limits<uint32_t>::max() / 2;
-const int32_t Xor32Detector::threshold_ =
-  std::numeric_limits<uint32_t>::max() / ChunkDetector::kAvgChunkSize;
+
+
+Xor32Detector::Xor32Detector(const size_t minimal_chunk_size,
+                             const size_t average_chunk_size,
+                             const size_t maximal_chunk_size) :
+  minimal_chunk_size_(minimal_chunk_size),
+  average_chunk_size_(average_chunk_size),
+  maximal_chunk_size_(maximal_chunk_size),
+  xor32_ptr_(0), xor32_(0),
+  threshold_(std::numeric_limits<uint32_t>::max() / average_chunk_size)
+{
+  assert (minimal_chunk_size_ > 0);
+  assert (minimal_chunk_size_ < average_chunk_size_);
+  assert (average_chunk_size_ < maximal_chunk_size_);
+}
 
 
 off_t Xor32Detector::FindNextCutMark(CharBuffer *buffer) {
-  assert (ChunkDetector::kMinChunkSize >= xor32_influence);
+  assert (minimal_chunk_size_ >= xor32_influence);
   const unsigned char *data = buffer->ptr();
 
   // get the offset where the next xor32 computation needs to be continued
@@ -45,24 +58,25 @@ off_t Xor32Detector::FindNextCutMark(CharBuffer *buffer) {
   const off_t global_offset =
     std::max(
            last_cut() +
-           static_cast<off_t>(ChunkDetector::kMinChunkSize - xor32_influence),
+           static_cast<off_t>(minimal_chunk_size_ - xor32_influence),
            xor32_ptr_);
 
   // check if the next xor32 computation is taking place in the current buffer
-  if (global_offset >= buffer->base_offset() + buffer->used_bytes()) {
+  if (global_offset >= buffer->base_offset()
+                       + static_cast<off_t>(buffer->used_bytes())) {
     return NoCut(global_offset);
   }
 
   // get the byte offset in the current buffer
   off_t internal_offset = global_offset - buffer->base_offset();
   assert (internal_offset >= 0);
-  assert (internal_offset < buffer->used_bytes());
+  assert (internal_offset < static_cast<off_t>(buffer->used_bytes()));
 
   // precompute the xor32 rolling checksum for finding the next cut mark
   // Note: this might be skipped, if the precomputation was already performed
   //       for the current rolling checksum
   //       (internal_precompute_end will be negative --> loop is not entered)
-  const off_t precompute_end = last_cut() + ChunkDetector::kMinChunkSize;
+  const off_t precompute_end = last_cut() + minimal_chunk_size_;
   const off_t internal_precompute_end =
     std::min(precompute_end - buffer->base_offset(),
              static_cast<off_t>(buffer->used_bytes()));
@@ -76,7 +90,7 @@ off_t Xor32Detector::FindNextCutMark(CharBuffer *buffer) {
   // Note: this loop is bound either by kMaxChunkSize or by the size of the
   //       current buffer, thus the computation would continue later
   const off_t internal_max_chunk_size_end =
-    last_cut() + ChunkDetector::kMaxChunkSize - buffer->base_offset();
+    last_cut() + maximal_chunk_size_ - buffer->base_offset();
   const off_t internal_compute_end =
     std::min(internal_max_chunk_size_end,
              static_cast<off_t>(buffer->used_bytes()));
