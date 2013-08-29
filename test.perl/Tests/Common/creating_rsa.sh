066488c3c27f3f81127a22076c03b44b29aea7fc
Rene Meusel <rene@renemeusel.de>
2013-08-29 09:16:43 +0000

    remove legacy perl test system and perl mock services

--- test.perl/Tests/Common/creating_rsa.sh
+++
@@ -1,3 +0,0 @@
-openssl genrsa -out /tmp/cvmfs_test.key 2048
-openssl req -new -subj "/C=CH/ST=n\/a/L=Geneva/O=CERN/OU=PH-SFT/CN=CVMFS Test Certificate" -key /tmp/cvmfs_test.key -out /tmp/cvmfs_test.csr
-openssl x509 -req -days 365 -in /tmp/cvmfs_test.csr -signkey /tmp/cvmfs_test.key -out /tmp/cvmfs_test.crt 
