dd2031a01d9c7d9bc8043c7b70e7bd5d4e0c689d
Rene Meusel <rene@renemeusel.de>
2013-08-29 09:22:10 +0000

    remove test exclusions for perl based services

--- test/cloud_testing/platforms/slc5_x86_64_test.sh
+++ test/cloud_testing/platforms/slc5_x86_64_test.sh
@@ -10,12 +10,7 @@ run_unittests --gtest_shuffle || retval=$?
 
 echo "running CernVM-FS test cases..."
 cd ${SOURCE_DIRECTORY}/test
-./run.sh $TEST_LOGFILE -x src/016-perl_environment      \
-                          src/017-dns_timeout           \
-                          src/018-dns_injection         \
-                          src/019-faulty_proxy          \
-                          src/020-server_timeout        \
-                          src/024-reload-during-asetup  \
+./run.sh $TEST_LOGFILE -x src/024-reload-during-asetup  \
                           src/5* || retval=$?
 
 exit $retval
