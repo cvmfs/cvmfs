4ec50fb3e67ea88345b9abfe74a5d3c24a770a38
Rene Meusel <rene@renemeusel.de>
2013-09-23 13:13:00 +0000

    run hotpatch test on SLC5 x86

--- test/cloud_testing/platforms/slc5_i386_test.sh
+++ test/cloud_testing/platforms/slc5_i386_test.sh
@@ -4,9 +4,12 @@
 script_location=$(dirname $(readlink --canonicalize $0))
 . ${script_location}/common_test.sh
 
-# run tests
-retval=0
-run_unittests --gtest_shuffle || retval=$?
+ut_retval=0
+it_retval=0
+mg_retval=0
+
+# running unit test suite
+run_unittests --gtest_shuffle || ut_retval=$?
 
 echo "running CernVM-FS test cases..."
 cd ${SOURCE_DIRECTORY}/test
@@ -14,6 +17,9 @@ cd ${SOURCE_DIRECTORY}/test
                           src/005-asetup                \
                           src/007-testjobs              \
                           src/024-reload-during-asetup  \
-                          src/5* || retval=$?
+                          src/5* || it_retval=$?
 
-exit $retval
+echo "running CernVM-FS migration test cases..."
+./run.sh $TEST_LOGFILE migration_tests/001-hotpatch || mg_retval=$?
+
+[ $ut_retval -eq 0 ] && [ $it_retval -eq 0 ] && [ $mg_retval -eq 0 ]
