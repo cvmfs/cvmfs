4464017d77e1b9df59351cfbeb665f93e6942769
Jakob Blomer <jblomer@cern.ch>
2013-09-24 11:04:19 +0200

    exclude pointless tests for NFS mode

--- test/cloud_testing/platforms/slc6_x86_64-NFS_test.sh
+++ test/cloud_testing/platforms/slc6_x86_64-NFS_test.sh
@@ -8,9 +8,11 @@ script_location=$(dirname $(readlink --canonicalize $0))
 retval=0
 echo "running CernVM-FS test cases..."
 cd ${SOURCE_DIRECTORY}/test
-./run.sh $TEST_LOGFILE -x src/004-davinci              \
+./run.sh $TEST_LOGFILE -x src/001-chksetup             \
+                          src/004-davinci              \
                           src/005-asetup               \
                           src/007-testjobs             \
+                          src/008-default_domain       \
                           src/024-reload-during-asetup \
                           src/5* || retval=$?
 
