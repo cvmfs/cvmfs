b8cb07e26ddaa211a31fe91c3f2b4413256e2917
Rene Meusel <rene@renemeusel.de>
2013-11-07 15:57:29 +0000

    abstract Repository for addition of 'RemoteRepository'

--- python/cvmfs/repository.py
+++ python/cvmfs/repository.py
@@ -6,14 +6,19 @@ This file is part of the CernVM File System auxiliary tools.
 """
 
 import os
-import _common
+import urlparse
+import tempfile
+from urllib2 import urlopen, URLError, HTTPError
 
-class RepositoryNotFoundError(Exception):
+import _common
+from manifest import Manifest
+
+class RepositoryNotFound(Exception):
     def __init__(self, repo_fqrn):
         self.fqrn = repo_fqrn
 
     def __str__(self):
-        return self.fqrn , "not found"
+        return self.fqrn + " not found"
 
 class UnknownRepositoryType(Exception):
     def __init__(self, repo_fqrn, repo_type):
@@ -24,37 +29,80 @@ class UnknownRepositoryType(Exception):
         return self.fqrn + " (" + self.type + ")"
 
 class ConfigurationNotFound(Exception):
-    def __init__(self, repo):
-        self.repo = repo
+    def __init__(self, repo, config_field):
+        self.repo         = repo
+        self.config_field = config_field
 
     def __str__(self):
-        return repr(self.repo)
+        return repr(self.repo) + " " + self.config_field
+
+class FileNotFoundInRepository(Exception):
+    def __init__(self, repo, file_name):
+        self.repo      = repo
+        self.file_name = file_name
+
+    def __str__(self):
+        return repr(self.repo) + " - " + repr(self.file_name)
+
 
 
 class Repository:
+    """ Abstract Wrapper around a CVMFS Repository representation """
+    def __init__(self):
+        with self.retrieve_file(_common._MANIFEST_NAME) as manifest_file:
+            self.manifest = Manifest(manifest_file)
+        self.fqrn = self.manifest.repository_name
+
+
+    def retrieve_file(self, file_name):
+        """ Abstract method to retrieve a file from the repository """
+        raise Exception("Not implemented!")
+
+
+
+class LocalRepository(Repository):
     def __init__(self, repo_fqrn):
+        repo_config_dir = os.path.join(_common._REPO_CONFIG_PATH, repo_fqrn)
+        if not os.path.isdir(repo_config_dir):
+            raise RepositoryNotFound(repo_fqrn)
+        self._server_config = os.path.join(repo_config_dir, _common._SERVER_CONFIG_NAME)
+        self.type           = self.read_server_config("CVMFS_REPOSITORY_TYPE")
+        if self.type != 'stratum0' and self.type != 'stratum1':
+            raise UnknownRepositoryType(repo_fqrn, self.type)
+        self._storage_location = self._get_repo_location()
+        Repository.__init__(self)
         self.fqrn = repo_fqrn
-        self._repo_config_dir = os.path.join(_common._REPO_CONFIG_PATH, repo_fqrn)
-        if not os.path.isdir(self._repo_config_dir):
-            raise RepositoryNotFoundError(repo_fqrn)
-        self._server_config = os.path.join(self._repo_config_dir, _common._SERVER_CONFIG_NAME)
-        repo_type = self.read_server_config("CVMFS_REPOSITORY_TYPE")
-        if repo_type != 'stratum0' and repo_type != 'stratum1':
-            raise UnknownRepositoryType(repo_fqrn, repo_type)
-        self.type = repo_type
+
 
     def read_server_config(self, config_field):
         with open(self._server_config) as config_file:
             for config_line in config_file:
                 if config_line.startswith(config_field):
                     return config_line[len(config_field)+1:].strip()
-        raise ConfigurationNotFound(self)
+        raise ConfigurationNotFound(self, config_field)
+
+
+    def _get_repo_location(self):
+        upstream = self.read_server_config("CVMFS_UPSTREAM_STORAGE")
+        upstream_type, tmp_dir, upstream_cfg = upstream.split(',')
+        if upstream_type != 'local': # might be riak, s3, ... (not implemented)
+            raise UnknownRepositoryType(repo_fqrn, upstream_type)
+        return upstream_cfg # location of the repository backend storage
+
+
+    def retrieve_file(self, file_name):
+        file_path = os.path.join(self._storage_location, file_name)
+        if not os.path.exists(file_path):
+            raise FileNotFoundInRepository(self, file_name)
+        return open(file_path, "rb")
+
 
     def __str__(self):
         return self.fqrn
 
     def __repr__(self):
-        return "<CVMFS Repository " + self.fqrn + ">"
+        return "<Local Repository " + self.fqrn + ">"
+
 
 
 def all():
