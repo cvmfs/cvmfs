0620af2281a0c3d5ee861beac4e4f9a2dc052870
Rene Meusel <rene@renemeusel.de>
2013-11-13 10:20:31 +0000

    load AUFS kernel module

--- test/cloud_testing/platforms/ubuntu_x86_64_setup.sh
+++ test/cloud_testing/platforms/ubuntu_x86_64_setup.sh
@@ -40,3 +40,8 @@ install_from_repo gcc                           || die "fail (installing gcc)"
 install_from_repo make                          || die "fail (installing make)"
 install_from_repo sqlite3                       || die "fail (installing sqlite3)"
 install_from_repo linux-image-extra-$(uname -r) || die "fail (installing AUFS)"
+
+# setting up the AUFS kernel module
+echo -n "loading AUFS kernel module..."
+sudo modprobe aufs || die "fail"
+echo "done"
