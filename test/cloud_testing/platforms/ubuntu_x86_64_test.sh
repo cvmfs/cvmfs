1a478cbff8e7f2c919f8ad487d6758efae4613d8
Rene Meusel <rene@renemeusel.de>
2013-11-13 10:32:20 +0000

    enable unit tests and server test cases on ubuntu

--- test/cloud_testing/platforms/ubuntu_x86_64_test.sh
+++ test/cloud_testing/platforms/ubuntu_x86_64_test.sh
@@ -4,17 +4,21 @@
 script_location=$(dirname $(readlink --canonicalize $0))
 . ${script_location}/common_test.sh
 
-ut_retval=0 # TODO: support unit tests on Ubuntu...
+ut_retval=0
 it_retval=0
 mg_retval=0
+
+# run tests
+retval=0
+run_unittests --gtest_shuffle \
+              --gtest_death_test_use_fork || ut_retval=$?
 
 echo "running CernVM-FS test cases..."
 cd ${SOURCE_DIRECTORY}/test
 ./run.sh $TEST_LOGFILE -x src/004-davinci               \
                           src/005-asetup                \
                           src/007-testjobs              \
-                          src/024-reload-during-asetup  \
-                          src/5* || it_retval=$?
+                          src/024-reload-during-asetup || it_retval=$?
 
 echo "running CernVM-FS migration test cases..."
 ./run.sh $MIGRATIONTEST_LOGFILE migration_tests/001-hotpatch || mg_retval=$?
