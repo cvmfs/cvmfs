1aca9c9f1420d435cc306e033d829d3bd0784f03
Jakob Blomer <jblomer@cern.ch>
2013-11-14 14:06:37 +0100

    ARM build system fixes

--- externals/c-ares/src/configureHook.sh
+++ externals/c-ares/src/configureHook.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-sh configure LDFLAGS="$LDFLAGS -rdynamic" CPPFLAGS="$CPPFLAGS -D_FILE_OFFSET_BITS=64" CFLAGS="$CFLAGS -fno-strict-aliasing -fasynchronous-unwind-tables -fno-omit-frame-pointer -fno-optimize-sibling-calls -fvisibility=hidden"
+sh configure LDFLAGS="$LDFLAGS -rdynamic" CPPFLAGS="$CPPFLAGS -D_FILE_OFFSET_BITS=64" CFLAGS="$CFLAGS -fno-strict-aliasing -fasynchronous-unwind-tables -fno-omit-frame-pointer -fno-optimize-sibling-calls -fvisibility=hidden -fPIC"
   
