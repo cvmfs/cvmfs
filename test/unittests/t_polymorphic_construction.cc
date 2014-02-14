eba788e6fa6ec2c732cb1199e6469185725a0f3f
Rene Meusel <rene@renemeusel.de>
2014-02-14 15:18:17 +0000

    TEST: first unit tests, more to come

--- test/unittests/t_polymorphic_construction.cc
+++ test/unittests/t_polymorphic_construction.cc
@@ -1,6 +1,9 @@
 #include <gtest/gtest.h>
+#include <string>
 
 #include "../../cvmfs/util.h"
+
+#include "testutil.h"
 
 struct DecisionType {
   DecisionType() : type(-1), fail(false) {}
@@ -9,6 +12,10 @@ struct DecisionType {
 };
 
 struct IntrospectionType {
+  IntrospectionType(const int type, const std::string &message) :
+    message(message), type(type) {}
+  std::string  message;
+  int          type;
 };
 
 class AbstractPolyCtorMock : public PolymorphicConstruction<AbstractPolyCtorMock,
@@ -35,7 +42,7 @@ class AbstractPolyCtorMock : public PolymorphicConstruction<AbstractPolyCtorMock
 
   virtual bool Initialize() {
     AbstractPolyCtorMock::initialize_calls++;
-    return param_.fail;
+    return !param_.fail;
   }
 
  protected:
@@ -50,10 +57,10 @@ unsigned int AbstractPolyCtorMock::register_plugin_calls = 0;
 //
 
 class FirstPolyCtorMock : public AbstractPolyCtorMock {
- private:
-  static const int type_id = 0;
-
  public:
+  static const int          type_id = 0;
+  static const std::string  message;
+
   static unsigned int constructor_calls;
   static void Reset() {
     FirstPolyCtorMock::constructor_calls++;
@@ -68,20 +75,24 @@ class FirstPolyCtorMock : public AbstractPolyCtorMock {
     FirstPolyCtorMock::constructor_calls++;
   }
 
-  static IntrospectionType GetInfo() { return IntrospectionType(); }
+  static IntrospectionType GetInfo() {
+    return IntrospectionType(FirstPolyCtorMock::type_id,
+                             FirstPolyCtorMock::message);
+  }
 
 };
-unsigned int FirstPolyCtorMock::constructor_calls = 0;
+unsigned int      FirstPolyCtorMock::constructor_calls = 0;
+const std::string FirstPolyCtorMock::message           = "Hello from First.";
 
 //
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 //
 
 class SecondPolyCtorMock : public AbstractPolyCtorMock {
- private:
-  static const int type_id = 1;
-
  public:
+  static const int          type_id = 1;
+  static const std::string  message;
+
   static unsigned int constructor_calls;
   static void Reset() {
     SecondPolyCtorMock::constructor_calls++;
@@ -96,20 +107,24 @@ class SecondPolyCtorMock : public AbstractPolyCtorMock {
     SecondPolyCtorMock::constructor_calls++;
   }
 
-  static IntrospectionType GetInfo() { return IntrospectionType(); }
+  static IntrospectionType GetInfo() {
+    return IntrospectionType(SecondPolyCtorMock::type_id,
+                             SecondPolyCtorMock::message);
+  }
 
 };
-unsigned int SecondPolyCtorMock::constructor_calls = 0;
+unsigned int      SecondPolyCtorMock::constructor_calls = 0;
+const std::string SecondPolyCtorMock::message           = "Second calling!";
 
 //
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 //
 
 class ThirdPolyCtorMock : public AbstractPolyCtorMock {
- private:
-  static const int type_id = 2;
-
  public:
+  static const int          type_id = 2;
+  static const std::string  message;
+
   static unsigned int constructor_calls;
   static void Reset() {
     ThirdPolyCtorMock::constructor_calls++;
@@ -124,16 +139,21 @@ class ThirdPolyCtorMock : public AbstractPolyCtorMock {
     ThirdPolyCtorMock::constructor_calls++;
   }
 
-  static IntrospectionType GetInfo() { return IntrospectionType(); }
+  static IntrospectionType GetInfo() {
+    return IntrospectionType(ThirdPolyCtorMock::type_id,
+                             ThirdPolyCtorMock::message);
+  }
 
 };
-unsigned int ThirdPolyCtorMock::constructor_calls = 0;
+unsigned int      ThirdPolyCtorMock::constructor_calls = 0;
+const std::string ThirdPolyCtorMock::message           = "Third à l'appareil.";
 
 //
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 //
 
 void AbstractPolyCtorMock::RegisterPlugins() {
+  AbstractPolyCtorMock::register_plugin_calls++;
   RegisterPlugin<FirstPolyCtorMock>();
   RegisterPlugin<SecondPolyCtorMock>();
   RegisterPlugin<ThirdPolyCtorMock>();
@@ -146,13 +166,17 @@ void AbstractPolyCtorMock::ResetAll() {
   ThirdPolyCtorMock::Reset();
 }
 
+typedef AbstractPolyCtorMock::IntrospectionData IntrospectionData;
+
 //
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 //
 
+
 class T_PolymorphicConstruction : public ::testing::Test {
  protected:
   void SetUp() {
+    PolymorphicConstructionUnittestAdapter::UnregisterAllPlugins<AbstractPolyCtorMock>();
     AbstractPolyCtorMock::Reset();
   }
 };
@@ -163,6 +187,64 @@ class T_PolymorphicConstruction : public ::testing::Test {
 //
 
 
-TEST(T_PolymorphicConstruction, Initialize) {
+TEST_F(T_PolymorphicConstruction, Noop) {
+  EXPECT_EQ (0u, AbstractPolyCtorMock::register_plugin_calls);
+}
 
+
+//
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//
+
+
+TEST_F(T_PolymorphicConstruction, Introspect) {
+  EXPECT_EQ (0u, AbstractPolyCtorMock::register_plugin_calls);
+  IntrospectionData v = AbstractPolyCtorMock::Introspect();
+  EXPECT_EQ (1u, AbstractPolyCtorMock::register_plugin_calls);
+  EXPECT_EQ (0u, AbstractPolyCtorMock::constructor_calls);
+  EXPECT_EQ (0u, FirstPolyCtorMock::constructor_calls);
+  EXPECT_EQ (0u, SecondPolyCtorMock::constructor_calls);
+  EXPECT_EQ (0u, ThirdPolyCtorMock::constructor_calls);
+
+  EXPECT_EQ (3u, v.size());
+  bool found_1 = false; bool found_2 = false; bool found_3 = false;
+  EXPECT_TRUE (!found_1 && !found_2 && !found_3);
+  IntrospectionData::const_iterator i    = v.begin();
+  IntrospectionData::const_iterator iend = v.end();
+  for (; i != iend; ++i) {
+    if (i->type    == FirstPolyCtorMock::type_id &&
+        i->message == FirstPolyCtorMock::message) {
+      found_1 = true;
+    }
+    if (i->type    == SecondPolyCtorMock::type_id &&
+        i->message == SecondPolyCtorMock::message) {
+      found_2 = true;
+    }
+    if (i->type    == ThirdPolyCtorMock::type_id &&
+        i->message == ThirdPolyCtorMock::message) {
+      found_3 = true;
+    }
+  }
+  EXPECT_TRUE (found_1 && found_2 && found_3);
+
+  IntrospectionData w = AbstractPolyCtorMock::Introspect();
+  EXPECT_EQ (1u, AbstractPolyCtorMock::register_plugin_calls);
+}
+
+
+//
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//
+
+
+TEST_F(T_PolymorphicConstruction, CreateFirst) {
+  EXPECT_EQ (0u, AbstractPolyCtorMock::register_plugin_calls);
+
+  DecisionType t;
+  t.type = 0;
+  AbstractPolyCtorMock *mock = AbstractPolyCtorMock::Construct(t);
+  EXPECT_NE (static_cast<AbstractPolyCtorMock*>(NULL), mock);
+  EXPECT_EQ (1u, AbstractPolyCtorMock::register_plugin_calls);
+  EXPECT_EQ (1u, AbstractPolyCtorMock::constructor_calls);
+  EXPECT_EQ (1u, FirstPolyCtorMock::constructor_calls);
 }
