6736943b870d675624427ea81f786693953aeb36
Jakob Blomer <jblomer@cern.ch>
2014-04-17 12:04:25 +0200

    fix debase64

--- cvmfs/util.cc
+++ cvmfs/util.cc
@@ -65,12 +65,12 @@ const char b64_table[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
 const signed char db64_table[] =
   { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
     -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, -1, -1, 63, 52,
-    53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -1, -1, -1, -1,
-     0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15,
-    16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1, -1,
-    26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41,
-    42, 43, 44, 45, 46, 47, 48, 49, 50, 51, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, -1, -1, 63,
+    52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1,  0, -1, -1,
+    -1,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14,
+    15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1,
+    -1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+    41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, -1, -1, -1, -1, -1,
 
     -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
     -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
@@ -1290,7 +1290,7 @@ static inline void Base64Block(const unsigned char input[3], const char *table,
 
 string Base64(const string &data) {
   string result;
-  result.reserve((data.length()+4)*4/3);
+  result.reserve((data.length()+3)*4/3);
   unsigned pos = 0;
   const unsigned char *data_ptr =
     reinterpret_cast<const unsigned char *>(data.data());
@@ -1321,30 +1321,31 @@ static bool Debase64Block(const unsigned char input[4],
                           const signed char *d_table,
                           unsigned char output[3])
 {
-  int32_t decoded[4];
+  int32_t dec[4];
   for (int i = 0; i < 4; ++i) {
-    decoded[i] = db64_table[input[i]];
-    if (decoded[i] < 0) return false;
+    dec[i] = db64_table[input[i]];
+    if (dec[i] < 0) return false;
   }
 
-  uint32_t *output_u24 = reinterpret_cast<uint32_t *>(output);
-  *output_u24 = decoded[0];
-  *output_u24 |= decoded[1] << 6;
-  *output_u24 |= decoded[2] << 12;
-  *output_u24 |= decoded[3] << 18;
+  output[0] = (dec[0] << 2) | (dec[1] >> 4);
+  output[1] = ((dec[1] & 0x0F) << 4) | (dec[2] >> 2);
+  output[2] = ((dec[2] & 0x03) << 6) | dec[3];
   return true;
 }
 
 
 bool Debase64(const string &data, string *decoded) {
-  decoded->reserve((data.length()+3)*3/4);
+  decoded->clear();
+  decoded->reserve((data.length()+4)*3/4);
   unsigned pos = 0;
   const unsigned char *data_ptr =
     reinterpret_cast<const unsigned char *>(data.data());
   const unsigned length = data.length();
-  if ((length > 0) && (length < 4))
+  if (length == 0) return true;
+  if ((length % 4) != 0)
     return false;
-  while (pos+4 <= length) {
+
+  while (pos < length) {
     unsigned char decoded_block[3];
     bool retval = Debase64Block(data_ptr+pos, db64_table, decoded_block);
     if (!retval)
@@ -1352,14 +1353,12 @@ bool Debase64(const string &data, string *decoded) {
     decoded->append(reinterpret_cast<char *>(decoded_block), 3);
     pos += 4;
   }
+
   for (int i = 0; i < 2; ++i) {
-    if (pos < length) {
-      if (data[pos] != '=') return false;
+    pos--;
+    if (data[pos] == '=')
       decoded->erase(decoded->length()-1);
-      pos++;
-    }
   }
-  if (pos < length) return false;
   return true;
 }
 
