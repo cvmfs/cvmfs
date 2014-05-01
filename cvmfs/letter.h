94c322d52f4ad13b24ba60d5cdd756a8452d443f
Jakob Blomer <jblomer@cern.ch>
2014-05-01 09:48:31 +0200

    add missing files

---
+++ cvmfs/letter.h
@@ -0,0 +1,60 @@
+/**
+ * This file is part of the CernVM File System.
+ */
+
+#ifndef CVMFS_LETTER_H_
+#define CVMFS_LETTER_H_
+
+#include <string>
+#include "hash.h"
+
+namespace signature{
+class SignatureManager;
+}
+
+namespace letter {
+
+enum Failures {
+  kFailOk = 0,
+  kFailBadBase64,
+  kFailMalformed,
+  kFailExpired,
+  kFailBadSignature,
+  kFailBadCertificate,
+  kFailNameMismatch,
+};
+
+inline const char *Code2Ascii(const Failures error) {
+  const int kNumElems = 7;
+  if (error >= kNumElems)
+    return "no text available (internal error)";
+
+  const char *texts[kNumElems];
+  texts[0] = "OK";
+  texts[1] = "invalid Base64 input";
+  texts[2] = "letter malformed";
+  texts[3] = "letter expired";
+  texts[4] = "signature verification failed";
+  texts[5] = "certificate is not whitelisted";
+  texts[6] = "repository name mismatch";
+
+  return texts[error];
+}
+
+class Letter {
+ public:
+  Letter(const std::string &fqrn,
+         const std::string &text,
+         signature::SignatureManager *signature_manager);
+  std::string Sign(const shash::Algorithms hash_algorithm);
+  Failures Verify(uint64_t max_age, std::string *msg, std::string *cert);
+
+ private:
+  std::string fqrn_;
+  std::string text_;
+  signature::SignatureManager *signature_manager_;
+};  // class Letter
+
+}  // namespace letter
+
+#endif  // CVMFS_LETTER_H_
