17a019196962c7bdaf1a6e17439f772edcdb6a7e
Rene Meusel <rene@renemeusel.de>
2014-05-05 08:46:10 +0000

    FIX: / is an invalid pathspec

--- test/unittests/t_pathspec.cc
+++ test/unittests/t_pathspec.cc
@@ -30,13 +30,13 @@ TEST(T_Pathspec, ParseSimpleAbsolute) {
   const Pathspec p6(" /foo/bar/baz.foo");
   const Pathspec p7("  /foo/bar/baz.foo");
 
-  EXPECT_TRUE (p1.IsValid()); EXPECT_TRUE (p1.IsAbsolute()); EXPECT_FALSE (p1.IsNegation());
-  EXPECT_TRUE (p2.IsValid()); EXPECT_TRUE (p2.IsAbsolute()); EXPECT_FALSE (p2.IsNegation());
-  EXPECT_TRUE (p3.IsValid()); EXPECT_TRUE (p3.IsAbsolute()); EXPECT_FALSE (p3.IsNegation());
-  EXPECT_TRUE (p4.IsValid()); EXPECT_TRUE (p4.IsAbsolute()); EXPECT_FALSE (p4.IsNegation());
-  EXPECT_TRUE (p5.IsValid()); EXPECT_TRUE (p5.IsAbsolute()); EXPECT_FALSE (p5.IsNegation());
-  EXPECT_TRUE (p6.IsValid()); EXPECT_TRUE (p6.IsAbsolute()); EXPECT_FALSE (p6.IsNegation());
-  EXPECT_TRUE (p7.IsValid()); EXPECT_TRUE (p7.IsAbsolute()); EXPECT_FALSE (p7.IsNegation());
+  EXPECT_FALSE (p1.IsValid()); EXPECT_TRUE (p1.IsAbsolute()); EXPECT_FALSE (p1.IsNegation());
+  EXPECT_TRUE  (p2.IsValid()); EXPECT_TRUE (p2.IsAbsolute()); EXPECT_FALSE (p2.IsNegation());
+  EXPECT_TRUE  (p3.IsValid()); EXPECT_TRUE (p3.IsAbsolute()); EXPECT_FALSE (p3.IsNegation());
+  EXPECT_TRUE  (p4.IsValid()); EXPECT_TRUE (p4.IsAbsolute()); EXPECT_FALSE (p4.IsNegation());
+  EXPECT_TRUE  (p5.IsValid()); EXPECT_TRUE (p5.IsAbsolute()); EXPECT_FALSE (p5.IsNegation());
+  EXPECT_TRUE  (p6.IsValid()); EXPECT_TRUE (p6.IsAbsolute()); EXPECT_FALSE (p6.IsNegation());
+  EXPECT_TRUE  (p7.IsValid()); EXPECT_TRUE (p7.IsAbsolute()); EXPECT_FALSE (p7.IsNegation());
 }
 
 
@@ -49,13 +49,13 @@ TEST(T_Pathspec, ParseSimpleNegation) {
   const Pathspec p6(" ! /foo/bar/baz.foo");
   const Pathspec p7("  ! foo/bar/baz.foo");
 
-  EXPECT_TRUE (p1.IsValid()); EXPECT_TRUE  (p1.IsAbsolute()); EXPECT_TRUE (p1.IsNegation());
-  EXPECT_TRUE (p2.IsValid()); EXPECT_TRUE  (p2.IsAbsolute()); EXPECT_TRUE (p2.IsNegation());
-  EXPECT_TRUE (p3.IsValid()); EXPECT_FALSE (p3.IsAbsolute()); EXPECT_TRUE (p3.IsNegation());
-  EXPECT_TRUE (p4.IsValid()); EXPECT_FALSE (p4.IsAbsolute()); EXPECT_TRUE (p4.IsNegation());
-  EXPECT_TRUE (p5.IsValid()); EXPECT_FALSE (p5.IsAbsolute()); EXPECT_TRUE (p5.IsNegation());
-  EXPECT_TRUE (p6.IsValid()); EXPECT_TRUE  (p6.IsAbsolute()); EXPECT_TRUE (p6.IsNegation());
-  EXPECT_TRUE (p7.IsValid()); EXPECT_FALSE (p7.IsAbsolute()); EXPECT_TRUE (p7.IsNegation());
+  EXPECT_FALSE (p1.IsValid()); EXPECT_TRUE  (p1.IsAbsolute()); EXPECT_TRUE (p1.IsNegation());
+  EXPECT_TRUE  (p2.IsValid()); EXPECT_TRUE  (p2.IsAbsolute()); EXPECT_TRUE (p2.IsNegation());
+  EXPECT_TRUE  (p3.IsValid()); EXPECT_FALSE (p3.IsAbsolute()); EXPECT_TRUE (p3.IsNegation());
+  EXPECT_TRUE  (p4.IsValid()); EXPECT_FALSE (p4.IsAbsolute()); EXPECT_TRUE (p4.IsNegation());
+  EXPECT_TRUE  (p5.IsValid()); EXPECT_FALSE (p5.IsAbsolute()); EXPECT_TRUE (p5.IsNegation());
+  EXPECT_TRUE  (p6.IsValid()); EXPECT_TRUE  (p6.IsAbsolute()); EXPECT_TRUE (p6.IsNegation());
+  EXPECT_TRUE  (p7.IsValid()); EXPECT_FALSE (p7.IsAbsolute()); EXPECT_TRUE (p7.IsNegation());
 }
 
 
