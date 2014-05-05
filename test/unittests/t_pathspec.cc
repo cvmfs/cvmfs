c5b9d3826c208776b8c9d5738e6acb826be6c9dc
Rene Meusel <rene@renemeusel.de>
2014-05-05 08:19:12 +0000

    TEST: unit tests for pathspec pattern parsing

--- test/unittests/t_pathspec.cc
+++ test/unittests/t_pathspec.cc
@@ -57,3 +57,33 @@ TEST(T_Pathspec, ParseSimpleNegation) {
   EXPECT_TRUE (p6.IsValid()); EXPECT_TRUE  (p6.IsAbsolute()); EXPECT_TRUE (p6.IsNegation());
   EXPECT_TRUE (p7.IsValid()); EXPECT_FALSE (p7.IsAbsolute()); EXPECT_TRUE (p7.IsNegation());
 }
+
+
+TEST(T_Pathspec, ParsePlaceholders) {
+  const Pathspec p1("!/hallo/??test/test");
+  const Pathspec p2("foo?bar");
+  const Pathspec p3("!bar.???");
+  const Pathspec p4("/foo/bar/ba?");
+  const Pathspec p5("/fo?/b?r/?az");
+
+  EXPECT_TRUE (p1.IsValid()); EXPECT_TRUE  (p1.IsAbsolute()); EXPECT_TRUE  (p1.IsNegation());
+  EXPECT_TRUE (p2.IsValid()); EXPECT_FALSE (p2.IsAbsolute()); EXPECT_FALSE (p2.IsNegation());
+  EXPECT_TRUE (p3.IsValid()); EXPECT_FALSE (p3.IsAbsolute()); EXPECT_TRUE  (p3.IsNegation());
+  EXPECT_TRUE (p4.IsValid()); EXPECT_TRUE  (p4.IsAbsolute()); EXPECT_FALSE (p4.IsNegation());
+  EXPECT_TRUE (p5.IsValid()); EXPECT_TRUE  (p5.IsAbsolute()); EXPECT_FALSE (p5.IsNegation());
+}
+
+
+TEST(T_Pathspec, ParseWildcards) {
+  const Pathspec p1("!/hallo/*/test");
+  const Pathspec p2("foo/*bar");
+  const Pathspec p3("!bar.*");
+  const Pathspec p4("/foo/b*r");
+  const Pathspec p5("/foo/*/bar");
+
+  EXPECT_TRUE (p1.IsValid()); EXPECT_TRUE  (p1.IsAbsolute()); EXPECT_TRUE  (p1.IsNegation());
+  EXPECT_TRUE (p2.IsValid()); EXPECT_FALSE (p2.IsAbsolute()); EXPECT_FALSE (p2.IsNegation());
+  EXPECT_TRUE (p3.IsValid()); EXPECT_FALSE (p3.IsAbsolute()); EXPECT_TRUE  (p3.IsNegation());
+  EXPECT_TRUE (p4.IsValid()); EXPECT_TRUE  (p4.IsAbsolute()); EXPECT_FALSE (p4.IsNegation());
+  EXPECT_TRUE (p5.IsValid()); EXPECT_TRUE  (p5.IsAbsolute()); EXPECT_FALSE (p5.IsNegation());
+}
