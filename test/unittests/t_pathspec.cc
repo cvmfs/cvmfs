a83c25b12f9d0c38c6d8568ef3032b0569117ab8
Rene Meusel <rene@renemeusel.de>
2014-05-06 08:10:51 +0000

    remove unnecessary whitespace skipping

--- test/unittests/t_pathspec.cc
+++ test/unittests/t_pathspec.cc
@@ -9,15 +9,11 @@ TEST(T_Pathspec, ParseSimpleRelative) {
   const Pathspec p2("foo/bar");
   const Pathspec p3("foo/bar.baz");
   const Pathspec p4("foo/bar/baz.foo");
-  const Pathspec p5(" foo");
-  const Pathspec p6("  bar/baz/foo.bar");
 
   EXPECT_TRUE (p1.IsValid()); EXPECT_FALSE (p1.IsAbsolute());
   EXPECT_TRUE (p2.IsValid()); EXPECT_FALSE (p2.IsAbsolute());
   EXPECT_TRUE (p3.IsValid()); EXPECT_FALSE (p3.IsAbsolute());
   EXPECT_TRUE (p4.IsValid()); EXPECT_FALSE (p4.IsAbsolute());
-  EXPECT_TRUE (p5.IsValid()); EXPECT_FALSE (p5.IsAbsolute());
-  EXPECT_TRUE (p6.IsValid()); EXPECT_FALSE (p6.IsAbsolute());
 }
 
 
@@ -27,16 +23,12 @@ TEST(T_Pathspec, ParseSimpleAbsolute) {
   const Pathspec p3("/foo/bar");
   const Pathspec p4("/foo/bar.baz");
   const Pathspec p5("/foo/bar/baz.foo");
-  const Pathspec p6(" /foo/bar/baz.foo");
-  const Pathspec p7("  /foo/bar/baz.foo");
 
   EXPECT_FALSE (p1.IsValid()); EXPECT_TRUE (p1.IsAbsolute());
   EXPECT_TRUE  (p2.IsValid()); EXPECT_TRUE (p2.IsAbsolute());
   EXPECT_TRUE  (p3.IsValid()); EXPECT_TRUE (p3.IsAbsolute());
   EXPECT_TRUE  (p4.IsValid()); EXPECT_TRUE (p4.IsAbsolute());
   EXPECT_TRUE  (p5.IsValid()); EXPECT_TRUE (p5.IsAbsolute());
-  EXPECT_TRUE  (p6.IsValid()); EXPECT_TRUE (p6.IsAbsolute());
-  EXPECT_TRUE  (p7.IsValid()); EXPECT_TRUE (p7.IsAbsolute());
 }
 
 
