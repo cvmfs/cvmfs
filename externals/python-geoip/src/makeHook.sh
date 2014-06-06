888accff5a48f4f8a034514c511071ed1e8db9b9
Dave Dykstra <dwd@fnal.gov>
2014-06-06 09:33:30 -0500

    go back to extracting GeoIP.so from .egg file for older python

--- externals/python-geoip/src/makeHook.sh
+++ externals/python-geoip/src/makeHook.sh
@@ -8,5 +8,9 @@ mkdir -p .tmp
 PYTHONPATH=$PWD/.tmp python setup.py install --install-lib=$PWD/.tmp
 rm -rf dist
 mkdir dist
+if [ -f .tmp/*.egg ]; then
+    # older python versions like 2.4 install a .egg; extract GeoIP.so
+    (cd .tmp; unzip -o *.egg GeoIP.so)
+fi
 cp .tmp/GeoIP.so dist
 rm -rf .tmp
