6538a5f611aa6546fbeb0a8f508e313de73ba1d0
Dave Dykstra <dwd@fnal.gov>
2014-06-06 09:01:28 -0500

    GeoIP.py isn't generated for newer versions of python and isn't needed on any

--- externals/python-geoip/src/makeHook.sh
+++ externals/python-geoip/src/makeHook.sh
@@ -2,11 +2,11 @@
 
 set -e
 LIBGEOIP="$PWD/../build_libgeoip/libGeoIP"
-rm -rf build dist
+rm -rf build
 CFLAGS="-I$LIBGEOIP" LDFLAGS="-L$LIBGEOIP/.libs" python setup.py build
 mkdir -p .tmp
 PYTHONPATH=$PWD/.tmp python setup.py install --install-lib=$PWD/.tmp
+rm -rf dist
+mkdir dist
+cp .tmp/GeoIP.so dist
 rm -rf .tmp
-cd dist
-unzip -o *.egg GeoIP.so GeoIP.py
-rm *.egg
