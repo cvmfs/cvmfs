0ee57684fed9252f729630fa71850b14f05b2b16
Rene Meusel <rene@renemeusel.de>
2014-07-11 11:55:17 +0000

    TEST: CatalogTraversal<> can ignore 404 on root catalogs

--- test/unittests/t_catalog_traversal.cc
+++ test/unittests/t_catalog_traversal.cc
@@ -2210,3 +2210,145 @@ TEST_F(T_CatalogTraversal, TraverseRepositoryTagListFirstLevelHistoryTraversePru
 
   CheckVisitedCatalogs(catalogs, TraverseRepositoryTagListFirstLevelHistoryTraversePrunedNoRepeat_visited_catalogs);
 }
+
+
+//
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//
+
+
+CatalogIdentifiers TraverseUntilUnavailableRevisionNoRepeat_visited_catalogs;
+void TraverseUntilUnavailableRevisionNoRepeatCallback(
+                             const MockedCatalogTraversal::CallbackData &data) {
+  TraverseUntilUnavailableRevisionNoRepeat_visited_catalogs.push_back(
+    std::make_pair(data.catalog->GetRevision(), data.catalog->path().ToString()));
+}
+
+TEST_F(T_CatalogTraversal, TraverseUntilUnavailableRevisionNoRepeat) {
+  TraverseUntilUnavailableRevisionNoRepeat_visited_catalogs.clear();
+  EXPECT_EQ (0u, TraverseUntilUnavailableRevisionNoRepeat_visited_catalogs.size());
+
+  std::set<shash::Any> deleted_catalogs;
+  deleted_catalogs.insert(GetRootHash(1));
+  deleted_catalogs.insert(GetRootHash(2));
+  deleted_catalogs.insert(GetRootHash(3));
+  deleted_catalogs.insert(GetRootHash(4));
+  MockObjectFetcher::deleted_catalogs = &deleted_catalogs;
+
+  CatalogIdentifiers catalogs;
+
+  CatalogTraversalParams params;
+  params.history             = 4;
+  params.no_repeat_history   = true;
+  params.ignore_load_failure = true;
+  MockedCatalogTraversal traverse(params);
+  traverse.RegisterListener(&TraverseUntilUnavailableRevisionNoRepeatCallback);
+
+  const bool successful_traversal = traverse.Traverse();
+  EXPECT_TRUE (successful_traversal);
+
+  catalogs.push_back(std::make_pair(6, ""));
+  catalogs.push_back(std::make_pair(5, "/00/13"));
+  catalogs.push_back(std::make_pair(5, "/00/13/29"));
+  catalogs.push_back(std::make_pair(5, "/00/13/28"));
+  catalogs.push_back(std::make_pair(4, "/00/12"));
+  catalogs.push_back(std::make_pair(4, "/00/12/27"));
+  catalogs.push_back(std::make_pair(4, "/00/12/26"));
+  catalogs.push_back(std::make_pair(4, "/00/12/26/38"));
+  catalogs.push_back(std::make_pair(4, "/00/12/26/37"));
+  catalogs.push_back(std::make_pair(4, "/00/12/26/36"));
+  catalogs.push_back(std::make_pair(4, "/00/12/26/35"));
+  catalogs.push_back(std::make_pair(4, "/00/12/25"));
+  catalogs.push_back(std::make_pair(4, "/00/11"));
+  catalogs.push_back(std::make_pair(4, "/00/11/24"));
+  catalogs.push_back(std::make_pair(4, "/00/11/23"));
+  catalogs.push_back(std::make_pair(4, "/00/11/22"));
+  catalogs.push_back(std::make_pair(4, "/00/11/22/34"));
+  catalogs.push_back(std::make_pair(4, "/00/11/22/34/43"));
+  catalogs.push_back(std::make_pair(4, "/00/11/22/34/42"));
+  catalogs.push_back(std::make_pair(4, "/00/11/22/34/41"));
+  catalogs.push_back(std::make_pair(4, "/00/11/22/33"));
+  catalogs.push_back(std::make_pair(5, ""));
+  catalogs.push_back(std::make_pair(2, "/00/10"));
+  catalogs.push_back(std::make_pair(2, "/00/10/21"));
+  catalogs.push_back(std::make_pair(2, "/00/10/20"));
+  catalogs.push_back(std::make_pair(2, "/00/10/20/32"));
+  catalogs.push_back(std::make_pair(2, "/00/10/20/31"));
+  catalogs.push_back(std::make_pair(2, "/00/10/20/30"));
+  catalogs.push_back(std::make_pair(2, "/00/10/20/30/40"));
+
+  CheckVisitedCatalogs(catalogs, TraverseUntilUnavailableRevisionNoRepeat_visited_catalogs);
+
+  MockObjectFetcher::deleted_catalogs = NULL;
+}
+
+
+//
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//
+
+
+CatalogIdentifiers TraverseWithUnavailableNestedNoRepeat_visited_catalogs;
+void TraverseWithUnavailableNestedNoRepeatCallback(
+                             const MockedCatalogTraversal::CallbackData &data) {
+  TraverseWithUnavailableNestedNoRepeat_visited_catalogs.push_back(
+    std::make_pair(data.catalog->GetRevision(), data.catalog->path().ToString()));
+}
+
+TEST_F(T_CatalogTraversal, TraverseWithUnavailableNestedNoRepeat) {
+  TraverseWithUnavailableNestedNoRepeat_visited_catalogs.clear();
+  EXPECT_EQ (0u, TraverseWithUnavailableNestedNoRepeat_visited_catalogs.size());
+
+  MockCatalog* doomed_nested_catalog = GetCatalog(2, "/00/10/20");
+  ASSERT_NE (static_cast<MockCatalog*>(NULL), doomed_nested_catalog);
+
+  std::set<shash::Any> deleted_catalogs;
+  deleted_catalogs.insert(doomed_nested_catalog->catalog_hash());
+  MockObjectFetcher::deleted_catalogs = &deleted_catalogs;
+
+  CatalogIdentifiers catalogs;
+
+  CatalogTraversalParams params;
+  params.history             = 4;
+  params.quiet               = true;
+  params.no_repeat_history   = true;
+  params.ignore_load_failure = true; // even though load failures should be
+                                     // ignored, traversal is supposed to fail
+                                     // because a missing nested catalog is con-
+                                     // sidered an error!
+  MockedCatalogTraversal traverse(params);
+  traverse.RegisterListener(&TraverseWithUnavailableNestedNoRepeatCallback);
+
+  const bool successful_traversal = traverse.Traverse();
+  EXPECT_FALSE (successful_traversal);
+
+  // the doomed catalog is part of revision 5, thus, all catalog of revision 6
+  // should still be hit (+ a couple more from revision 5)
+  catalogs.push_back(std::make_pair(6, ""));
+  catalogs.push_back(std::make_pair(5, "/00/13"));
+  catalogs.push_back(std::make_pair(5, "/00/13/29"));
+  catalogs.push_back(std::make_pair(5, "/00/13/28"));
+  catalogs.push_back(std::make_pair(4, "/00/12"));
+  catalogs.push_back(std::make_pair(4, "/00/12/27"));
+  catalogs.push_back(std::make_pair(4, "/00/12/26"));
+  catalogs.push_back(std::make_pair(4, "/00/12/26/38"));
+  catalogs.push_back(std::make_pair(4, "/00/12/26/37"));
+  catalogs.push_back(std::make_pair(4, "/00/12/26/36"));
+  catalogs.push_back(std::make_pair(4, "/00/12/26/35"));
+  catalogs.push_back(std::make_pair(4, "/00/12/25"));
+  catalogs.push_back(std::make_pair(4, "/00/11"));
+  catalogs.push_back(std::make_pair(4, "/00/11/24"));
+  catalogs.push_back(std::make_pair(4, "/00/11/23"));
+  catalogs.push_back(std::make_pair(4, "/00/11/22"));
+  catalogs.push_back(std::make_pair(4, "/00/11/22/34"));
+  catalogs.push_back(std::make_pair(4, "/00/11/22/34/43"));
+  catalogs.push_back(std::make_pair(4, "/00/11/22/34/42"));
+  catalogs.push_back(std::make_pair(4, "/00/11/22/34/41"));
+  catalogs.push_back(std::make_pair(4, "/00/11/22/33"));
+
+  const bool dont_check_catalog_count = false;
+  CheckVisitedCatalogs(catalogs, TraverseWithUnavailableNestedNoRepeat_visited_catalogs,
+                       dont_check_catalog_count);
+
+  MockObjectFetcher::deleted_catalogs = NULL;
+}
