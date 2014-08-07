dcc2dca0d2ad22974e4459863fe73e2f9dbfb698
Rene Meusel <rene@renemeusel.de>
2014-08-07 07:01:18 +0000

    Documentation: Pathspec class

--- cvmfs/pathspec/pathspec.cc
+++ cvmfs/pathspec/pathspec.cc
@@ -70,6 +70,9 @@ Pathspec& Pathspec::operator=(const Pathspec &other) {
 
 
 void Pathspec::Parse(const std::string &spec) {
+  // parsing is done using std::string iterators to walk through th entire
+  // pathspec parameter. Thus, all parsing methods receive references to these
+  // iterators and increment itr as they pass along.
         std::string::const_iterator itr = spec.begin();
   const std::string::const_iterator end = spec.end();
 
@@ -85,11 +88,14 @@ void Pathspec::Parse(const std::string &spec) {
 
 void Pathspec::ParsePathElement(      std::string::const_iterator  &itr,
                                 const std::string::const_iterator  &end) {
+  // find the end of the current pattern element (next directory boundary)
   const std::string::const_iterator begin_element = itr;
   while (itr != end && *itr != kSeparator) {
     ++itr;
   }
   const std::string::const_iterator end_element = itr;
+
+  // create a PathspecElementPattern out of this directory description
   patterns_.push_back(PathspecElementPattern(begin_element, end_element));
 }
 
