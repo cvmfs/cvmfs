985c094517babe9f6741f74503b56c0f022482bb
Rene Meusel <rene@renemeusel.de>
2014-10-28 12:49:31 +0000

    TEST: unit tests for `History::Remove()`

--- test/unittests/t_history.cc
+++ test/unittests/t_history.cc
@@ -238,3 +238,202 @@ TEST_F(T_History, ListTags) {
 
   delete history;
 }
+
+
+TEST_F(T_History, InsertAndRemoveTag) {
+  const std::string hp = GetHistoryFilename();
+  History *history = History::Create(hp, fqrn);
+  ASSERT_NE (static_cast<History*>(NULL), history);
+  EXPECT_EQ (fqrn, history->fqrn());
+
+  const unsigned int dummy_count = 40;
+  const TagVector dummy_tags = GetDummyTags(dummy_count);
+  ASSERT_TRUE (history->BeginTransaction());
+        TagVector::const_iterator i    = dummy_tags.begin();
+  const TagVector::const_iterator iend = dummy_tags.end();
+  for (; i != iend; ++i) {
+    ASSERT_TRUE (history->Insert(*i));
+  }
+  EXPECT_TRUE (history->CommitTransaction());
+  EXPECT_EQ (dummy_count, history->GetNumberOfTags());
+
+  const std::string to_be_deleted = dummy_tags[5].name;
+  History::Tag deleted_tag;
+  EXPECT_TRUE (history->Find(to_be_deleted, &deleted_tag));
+  ASSERT_TRUE (history->Remove(dummy_tags[5].name));
+  EXPECT_EQ (dummy_count - 1, history->GetNumberOfTags());
+  EXPECT_FALSE (history->Find(to_be_deleted, &deleted_tag));
+
+  TagVector tags;
+  ASSERT_TRUE (history->List(&tags));
+  EXPECT_EQ (dummy_count - 1, tags.size());
+
+                                          i    = dummy_tags.begin();
+        TagVector::const_reverse_iterator j    = tags.rbegin();
+  const TagVector::const_reverse_iterator jend = tags.rend();
+  for (; j != jend; ++j, ++i) {
+    if (i->name == to_be_deleted) {
+      --j;
+      continue;
+    }
+    CompareTags(*i, *j);
+  }
+
+  delete history;
+}
+
+
+TEST_F(T_History, RemoveNonExistentTag) {
+  const std::string hp = GetHistoryFilename();
+  History *history = History::Create(hp, fqrn);
+  ASSERT_NE (static_cast<History*>(NULL), history);
+  EXPECT_EQ (fqrn, history->fqrn());
+
+  const unsigned int dummy_count = 40;
+  const TagVector dummy_tags = GetDummyTags(dummy_count);
+  ASSERT_TRUE (history->BeginTransaction());
+        TagVector::const_iterator i    = dummy_tags.begin();
+  const TagVector::const_iterator iend = dummy_tags.end();
+  for (; i != iend; ++i) {
+    ASSERT_TRUE (history->Insert(*i));
+  }
+  EXPECT_TRUE (history->CommitTransaction());
+  EXPECT_EQ (dummy_count, history->GetNumberOfTags());
+
+  ASSERT_TRUE (history->Remove("doesnt_exist"));
+  EXPECT_EQ (dummy_count, history->GetNumberOfTags());
+
+  TagVector tags;
+  ASSERT_TRUE (history->List(&tags));
+  EXPECT_EQ (dummy_count, tags.size());
+
+                                          i    = dummy_tags.begin();
+        TagVector::const_reverse_iterator j    = tags.rbegin();
+  const TagVector::const_reverse_iterator jend = tags.rend();
+  for (; j != jend; ++j, ++i) {
+    CompareTags(*i, *j);
+  }
+
+  delete history;
+}
+
+
+TEST_F(T_History, RemoveMultipleTags) {
+  const std::string hp = GetHistoryFilename();
+  History *history = History::Create(hp, fqrn);
+  ASSERT_NE (static_cast<History*>(NULL), history);
+  EXPECT_EQ (fqrn, history->fqrn());
+
+  const unsigned int dummy_count = 40;
+  const TagVector dummy_tags = GetDummyTags(dummy_count);
+  ASSERT_TRUE (history->BeginTransaction());
+        TagVector::const_iterator i    = dummy_tags.begin();
+  const TagVector::const_iterator iend = dummy_tags.end();
+  for (; i != iend; ++i) {
+    ASSERT_TRUE (history->Insert(*i));
+  }
+  EXPECT_TRUE (history->CommitTransaction());
+  EXPECT_EQ (dummy_count, history->GetNumberOfTags());
+
+  std::vector<std::string> to_be_deleted;
+  to_be_deleted.push_back(dummy_tags[2].name);
+  to_be_deleted.push_back(dummy_tags[5].name);
+  to_be_deleted.push_back(dummy_tags[10].name);
+  to_be_deleted.push_back(dummy_tags[15].name);
+
+        std::vector<std::string>::const_iterator j    = to_be_deleted.begin();
+  const std::vector<std::string>::const_iterator jend = to_be_deleted.end();
+  for (; j != jend; ++j) {
+    EXPECT_TRUE (history->Remove(*j));
+  }
+  EXPECT_EQ (dummy_count - to_be_deleted.size(), history->GetNumberOfTags());
+
+  TagVector tags;
+  ASSERT_TRUE (history->List(&tags));
+  EXPECT_EQ (dummy_count - to_be_deleted.size(), tags.size());
+
+                                          i    = dummy_tags.begin();
+        TagVector::const_reverse_iterator k    = tags.rbegin();
+  const TagVector::const_reverse_iterator kend = tags.rend();
+  for (; k != kend; ++k, ++i) {
+    bool should_exist = true;
+          std::vector<std::string>::const_iterator l    = to_be_deleted.begin();
+    const std::vector<std::string>::const_iterator lend = to_be_deleted.end();
+    for (; l != lend; ++l) {
+      if (i->name == *l) {
+        --k;
+        should_exist = false;
+        break;
+      }
+    }
+    if (should_exist) {
+      CompareTags(*i, *k);
+    }
+  }
+
+  delete history;
+}
+
+
+TEST_F(T_History, RemoveTagsWithReOpen) {
+  const std::string hp = GetHistoryFilename();
+  History *history1 = History::Create(hp, fqrn);
+  ASSERT_NE (static_cast<History*>(NULL), history1);
+  EXPECT_EQ (fqrn, history1->fqrn());
+
+  const unsigned int dummy_count = 40;
+  const TagVector dummy_tags = GetDummyTags(dummy_count);
+  ASSERT_TRUE (history1->BeginTransaction());
+        TagVector::const_iterator i    = dummy_tags.begin();
+  const TagVector::const_iterator iend = dummy_tags.end();
+  for (; i != iend; ++i) {
+    ASSERT_TRUE (history1->Insert(*i));
+  }
+  EXPECT_TRUE (history1->CommitTransaction());
+  EXPECT_EQ (dummy_count, history1->GetNumberOfTags());
+  delete history1;
+
+  History *history2 = History::OpenWritable(hp);
+  ASSERT_NE (static_cast<History*>(NULL), history2);
+  EXPECT_EQ (fqrn, history2->fqrn());
+
+  std::vector<std::string> to_be_deleted;
+  to_be_deleted.push_back(dummy_tags[2].name);
+  to_be_deleted.push_back(dummy_tags[5].name);
+  to_be_deleted.push_back(dummy_tags[10].name);
+  to_be_deleted.push_back(dummy_tags[15].name);
+
+        std::vector<std::string>::const_iterator j    = to_be_deleted.begin();
+  const std::vector<std::string>::const_iterator jend = to_be_deleted.end();
+  for (; j != jend; ++j) {
+    EXPECT_TRUE (history2->Remove(*j));
+  }
+  EXPECT_EQ (dummy_count - to_be_deleted.size(), history2->GetNumberOfTags());
+  delete history2;
+
+  History *history3 = History::Open(hp);
+  TagVector tags;
+  ASSERT_TRUE (history3->List(&tags));
+  EXPECT_EQ (dummy_count - to_be_deleted.size(), tags.size());
+
+                                          i    = dummy_tags.begin();
+        TagVector::const_reverse_iterator k    = tags.rbegin();
+  const TagVector::const_reverse_iterator kend = tags.rend();
+  for (; k != kend; ++k, ++i) {
+    bool should_exist = true;
+          std::vector<std::string>::const_iterator l    = to_be_deleted.begin();
+    const std::vector<std::string>::const_iterator lend = to_be_deleted.end();
+    for (; l != lend; ++l) {
+      if (i->name == *l) {
+        --k;
+        should_exist = false;
+        break;
+      }
+    }
+    if (should_exist) {
+      CompareTags(*i, *k);
+    }
+  }
+
+  delete history3;
+}
