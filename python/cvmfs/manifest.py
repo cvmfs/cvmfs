f804b0551e70cdfdfbc07b830b069151ac185924
Rene Meusel <rene@renemeusel.de>
2014-12-08 18:15:39 +0000

    add 'G' manifest field

--- python/cvmfs/manifest.py
+++ python/cvmfs/manifest.py
@@ -51,25 +51,27 @@ class Manifest:
         key_char = line[0]
         data     = line[1:-1]
         if   key_char == "C":
-            self.root_catalog      = data
+            self.root_catalog        = data
         elif key_char == "R":
-            self.root_hash         = data
+            self.root_hash           = data
         elif key_char == "B":
-            self.root_catalog_size = int(data)
+            self.root_catalog_size   = int(data)
         elif key_char == "X":
-            self.certificate       = data
+            self.certificate         = data
         elif key_char == "H":
-            self.history_database  = data
+            self.history_database    = data
         elif key_char == "T":
-            self.last_modified     = datetime.fromtimestamp(int(data), tz=tzutc())
+            self.last_modified       = datetime.fromtimestamp(int(data), tz=tzutc())
         elif key_char == "D":
-            self.ttl               = int(data)
+            self.ttl                 = int(data)
         elif key_char == "S":
-            self.revision          = int(data)
+            self.revision            = int(data)
         elif key_char == "N":
-            self.repository_name   = data
+            self.repository_name     = data
         elif key_char == "L":
-            self.micro_catalog     = data
+            self.micro_catalog       = data
+        elif key_char == "G":
+            self.garbage_collectable = (data == "yes")
         else:
             raise UnknownManifestField(key_char)
 
