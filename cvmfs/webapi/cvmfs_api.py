0106108b2bb4f9ad5eaa4e02981293d5f5613aaf
Dave Dykstra <dwd@fnal.gov>
2014-12-26 11:06:20 -0600

    Add Content-Length to message in the body of a bad request

--- cvmfs/webapi/cvmfs_api.py
+++ cvmfs/webapi/cvmfs_api.py
@@ -6,7 +6,8 @@ negative_expire_secs = 60*5;        # 5 minutes
 def bad_request(start_response, reason):
     response_body = 'Bad Request: ' + reason + "\n"
     start_response('400 Bad Request',
-                  [('Cache-control', 'max-age=' + str(negative_expire_secs))])
+                  [('Cache-control', 'max-age=' + str(negative_expire_secs)),
+                   ('Content-Length', str(len(response_body)))])
     return [response_body]
 
 def dispatch(api_func, path_info, repo_name, version, start_response, environ):
