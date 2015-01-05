5e2502c8ea31d5eed4fdd9caac5187ec15554819
Jakob Blomer <jblomer@cern.ch>
2015-01-05 17:26:49 +0100

    FIX: download of local files into memory

--- test/unittests/t_download.cc
+++ test/unittests/t_download.cc
@@ -44,10 +44,28 @@ TEST_F(T_Download, File) {
   UnlinkGuard unlink_guard(dest_path);
 
   JobInfo info(&foo_url, false /* compressed */, false /* probe hosts */,
-               fdest, NULL);
+               fdest,  NULL);
   download_mgr.Fetch(&info);
   EXPECT_EQ(info.error_code, kFailOk);
   fclose(fdest);
+}
+
+
+TEST_F(T_Download, LocalFile2Mem) {
+  string dest_path;
+  FILE *fdest = CreateTempFile("/tmp/cvmfstest", 0600, "w+", &dest_path);
+  ASSERT_TRUE(fdest != NULL);
+  UnlinkGuard unlink_guard(dest_path);
+  char buf = '1';
+  fwrite(&buf, 1, 1, fdest);
+  fclose(fdest);
+
+  string url = "file://" + dest_path;
+  JobInfo info(&url, false /* compressed */, false /* probe hosts */, NULL);
+  download_mgr.Fetch(&info);
+  ASSERT_EQ(info.error_code, kFailOk);
+  ASSERT_EQ(info.destination_mem.size, 1U);
+  EXPECT_EQ(info.destination_mem.data[0], '1');
 }
 
 
