eeb561fbf1053f7080793b4fa8164f46abf0ea54
Rene Meusel <rene@renemeusel.de>
2015-01-22 17:15:55 +0000

    FIX: use apt-get purge for removal

--- test/migration_tests/common.sh
+++ test/migration_tests/common.sh
@@ -143,7 +143,7 @@ uninstall_package() {
   if has_binary yum; then
     sudo yum -y erase $pkg_name
   elif has_binary apt-get; then
-    sudo apt-get --assume-yes remove $pkg_name
+    sudo apt-get --assume-yes purge $pkg_name
   else
     return 1
   fi
