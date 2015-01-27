883a0df62f18582ff24c4c16bdaf41a26e70a870
Rene Meusel <rene@renemeusel.de>
2015-01-27 10:45:18 +0000

    create cache partition on SLC6 x64 (special tests)

--- test/cloud_testing/platforms/slc6_x86_64-NFS_setup.sh
+++ test/cloud_testing/platforms/slc6_x86_64-NFS_setup.sh
@@ -4,6 +4,16 @@
 script_location=$(dirname $(readlink --canonicalize $0))
 . ${script_location}/common_setup.sh
 
+echo -n "creating additional disk partitions... "
+disk_to_partition=/dev/vda
+free_disk_space=$(get_unpartitioned_space $disk_to_partition)
+cache_partition_size=16106127360 # 15 GiB
+if [ $free_disk_space -lt $cache_partition_size ]; then
+  die "fail (not enough unpartitioned disk space - $free_disk_space bytes)"
+fi
+create_partition $disk_to_partition $cache_partition_size || die "fail (creating partition)"
+echo "done"
+
 # install CernVM-FS RPM packages
 echo "installing RPM packages... "
 install_rpm "$CONFIG_PACKAGES"
@@ -26,3 +36,7 @@ echo "installing additional RPM packages..."
 install_from_repo gcc
 install_from_repo gcc-c++
 
+# rebooting the system (returning 0 value)
+echo "sleep 1 && reboot" > killme.sh
+sudo nohup sh < killme.sh &
+exit 0
