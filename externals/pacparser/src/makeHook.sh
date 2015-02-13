1d69b0386007ccfa7d5ada17d208718df012eb8d
Rene Meusel <rene@renemeusel.de>
2015-02-13 10:27:25 +0000

    add printf-debugging print outs

--- externals/pacparser/src/makeHook.sh
+++ externals/pacparser/src/makeHook.sh
@@ -4,10 +4,13 @@ set -e
 
 static_result_dir=src/static
 
+echo "make clean && make for libpacparser..."
 [ -d $static_result_dir ] && rm -fR $static_result_dir
 make -C src clean
 make -C src
+echo "finished internal build of libpacparser"
 
+echo "creating static link library for libpacparser..."
 mkdir src/static
 cp src/pacparser.o src/libjs.a $static_result_dir
 
@@ -16,4 +19,8 @@ ar x libjs.a
 rm -f libjs.a
 ar rsc libpacparser.a *.o
 rm -f *.o
+echo "finished creating static link library for libpacparser"
+
+echo "stripping debug symbols for libpacparser..."
 strip -S libpacparser.a
+echo "finished building libpacparser.a"
