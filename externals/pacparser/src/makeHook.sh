ed68f221dc4bb0312499f188902563c8f9478744
Rene Meusel <rene@renemeusel.de>
2015-02-13 09:34:47 +0000

    FIX: clean pacparser before (re-)build

--- externals/pacparser/src/makeHook.sh
+++ externals/pacparser/src/makeHook.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
-make -C src
-mkdir src/static
-cp src/pacparser.o src/libjs.a src/static
+static_result_dir=src/static
 
-cd src/static
+[ -d $static_result_dir ] && rm -fR $static_result_dir
+make -C src clean
+make -C src
+
+mkdir src/static
+cp src/pacparser.o src/libjs.a $static_result_dir
+
+cd $static_result_dir
 ar x libjs.a
 rm -f libjs.a
 ar rsc libpacparser.a *.o
 rm -f *.o
-strip -S libpacparser.a 
+strip -S libpacparser.a
