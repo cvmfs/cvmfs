161942f5252efe45f85d1a57323e8ab6641a7b1a
Jose Molina <jose.molina@cern.ch>
2015-02-19 16:45:31 +0100

    TEST: update T_Options class using typed tests

--- test/unittests/t_options.cc
+++ test/unittests/t_options.cc
@@ -10,7 +10,7 @@
 
 using namespace std;  // NOLINT
 
-
+template <class OptionsT>
 class T_Options : public ::testing::Test {
  protected:
   virtual void SetUp() {
@@ -34,71 +34,77 @@ class T_Options : public ::testing::Test {
   }
 
  protected:
-  BashOptionsManager    bash_options_manager_;
-  FastOptionsManager    fast_options_manager_;
-  UnlinkGuard           unlink_guard_;
-  string                config_file_;
+  // type-based overlaoded instantiation of History object wrapper
+  // Inspired from here:
+  //   http://stackoverflow.com/questions/5512910/
+  //          explicit-specialization-of-template-class-member-function
+  template <typename T> struct type {};
+
+  unsigned ExpectedValues(const type<BashOptionsManager>  type_specifier) {
+    return 9u;
+  }
+
+  unsigned ExpectedValues(const type<FastOptionsManager>  type_specifier) {
+    return 6u;
+  }
+
+  unsigned ExpectedValues() {
+    return ExpectedValues(type<OptionsT>());
+  }
+
+ protected:
+  OptionsT     options_manager_;
+  UnlinkGuard  unlink_guard_;
+  string       config_file_;
 };  // class T_Options
 
+typedef ::testing::Types<BashOptionsManager, FastOptionsManager> OptionsManagers;
+TYPED_TEST_CASE(T_Options, OptionsManagers);
 
-void Parse(OptionsManager* options_manager, string config_file,
-    unsigned expected_number_elements) {
+
+TYPED_TEST(T_Options, ParsePath) {
   string container;
-  options_manager->ParsePath(config_file, false);
-  ASSERT_EQ(expected_number_elements, options_manager->GetAllKeys().size());
+  OptionsManager &options_manager = TestFixture::options_manager_;
+  const string &config_file = TestFixture::config_file_;
+  const unsigned expected_number_elements = TestFixture::ExpectedValues();
+  options_manager.ParsePath(config_file, false);
 
-  EXPECT_TRUE(options_manager->GetValue("CVMFS_CACHE_BASE", &container));
+  ASSERT_EQ(expected_number_elements, options_manager.GetAllKeys().size());
+
+  EXPECT_TRUE(options_manager.GetValue("CVMFS_CACHE_BASE", &container));
   EXPECT_EQ("/root/cvmfs_testing/cache", container);
-  EXPECT_TRUE(options_manager->GetSource("CVMFS_CACHE_BASE", &container));
+  EXPECT_TRUE(options_manager.GetSource("CVMFS_CACHE_BASE", &container));
   EXPECT_EQ(config_file, container);
 
-  EXPECT_TRUE(options_manager->GetValue("CVMFS_RELOAD_SOCKETS", &container));
+  EXPECT_TRUE(options_manager.GetValue("CVMFS_RELOAD_SOCKETS", &container));
   EXPECT_EQ("/root/cvmfs_testing/cache", container);
-  EXPECT_TRUE(options_manager->GetSource("CVMFS_RELOAD_SOCKETS", &container));
+  EXPECT_TRUE(options_manager.GetSource("CVMFS_RELOAD_SOCKETS", &container));
   EXPECT_EQ(config_file, container);
 
-  EXPECT_TRUE(options_manager->GetValue("CVMFS_SERVER_URL", &container));
+  EXPECT_TRUE(options_manager.GetValue("CVMFS_SERVER_URL", &container));
   EXPECT_EQ("http://volhcb28:3128/data", container);
-  EXPECT_TRUE(options_manager->GetSource("CVMFS_SERVER_URL", &container));
+  EXPECT_TRUE(options_manager.GetSource("CVMFS_SERVER_URL", &container));
   EXPECT_EQ(config_file, container);
 
-  EXPECT_TRUE(options_manager->GetValue("CVMFS_SHARED_CACHE", &container));
+  EXPECT_TRUE(options_manager.GetValue("CVMFS_SHARED_CACHE", &container));
   EXPECT_EQ("no", container);
-  EXPECT_TRUE(options_manager->GetSource("CVMFS_SHARED_CACHE", &container));
+  EXPECT_TRUE(options_manager.GetSource("CVMFS_SHARED_CACHE", &container));
   EXPECT_EQ(config_file, container);
 
-  EXPECT_TRUE(options_manager->GetValue("CVMFS_HTTP_PROXY", &container));
+  EXPECT_TRUE(options_manager.GetValue("CVMFS_HTTP_PROXY", &container));
   EXPECT_EQ("DIRECT", container);
-  EXPECT_TRUE(options_manager->GetSource("CVMFS_HTTP_PROXY", &container));
+  EXPECT_TRUE(options_manager.GetSource("CVMFS_HTTP_PROXY", &container));
   EXPECT_EQ(config_file, container);
 
-  EXPECT_TRUE(options_manager->GetValue("value", &container));
+  EXPECT_TRUE(options_manager.GetValue("value", &container));
   EXPECT_EQ("", container);
-  EXPECT_TRUE(options_manager->GetSource("value", &container));
+  EXPECT_TRUE(options_manager.GetSource("value", &container));
   EXPECT_EQ(config_file, container);
 }
 
-
-void ParseNoValidFile(OptionsManager* options_manager) {
+TYPED_TEST(T_Options, ParsePathNoFile) {
   string fileName = "somethingThatDoesntExists";
-  options_manager->ParsePath(fileName, false);
-  ASSERT_EQ(0u, options_manager->GetAllKeys().size());
-}
-
-
-TEST_F(T_Options, ParsePathSimple) {
-  Parse(&fast_options_manager_, config_file_, 6u);
-}
-
-TEST_F(T_Options, ParsePathSimpleNoFile) {
-  ParseNoValidFile(&fast_options_manager_);
-}
-
-TEST_F(T_Options, ParsePathBash) {
-  Parse(&bash_options_manager_, config_file_, 9u);
-}
-
-TEST_F(T_Options, ParsePathBashNoFile) {
-  ParseNoValidFile(&bash_options_manager_);
+  TestFixture::options_manager_.ParsePath(fileName, false);
+  ASSERT_EQ(0u, TestFixture::options_manager_.GetAllKeys().size());
 }
 
