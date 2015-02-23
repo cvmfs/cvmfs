173bfbd3122154bd64768664ae2289c6176e22bc
Jakob Blomer <jblomer@cern.ch>
2015-02-23 22:41:21 +0100

    more style fixes

--- cvmfs/swissknife_check.h
+++ cvmfs/swissknife_check.h
@@ -5,9 +5,11 @@
 #ifndef CVMFS_SWISSKNIFE_CHECK_H_
 #define CVMFS_SWISSKNIFE_CHECK_H_
 
-#include "swissknife.h"
-#include "hash.h"
+#include <string>
+
 #include "catalog.h"
+#include "hash.h"
+#include "swissknife.h"
 
 namespace download {
 class DownloadManager;
@@ -17,19 +19,19 @@ namespace swissknife {
 
 class CommandCheck : public Command {
  public:
-  ~CommandCheck() { };
-  std::string GetName() { return "check"; };
+  ~CommandCheck() { }
+  std::string GetName() { return "check"; }
   std::string GetDescription() {
     return "CernVM File System repository sanity checker\n"
       "This command checks the consisteny of the file catalogs of a "
         "cvmfs repository.";
-  };
+  }
   ParameterList GetParams() {
     ParameterList r;
     r.push_back(Parameter::Mandatory('r', "repository directory / url"));
-    r.push_back(Parameter::Optional ('t', "check specific repository tag"));
-    r.push_back(Parameter::Optional ('l', "log level (0-4, default: 2)"));
-    r.push_back(Parameter::Switch   ('c', "check availability of data chunks"));
+    r.push_back(Parameter::Optional('t', "check specific repository tag"));
+    r.push_back(Parameter::Optional('l', "log level (0-4, default: 2)"));
+    r.push_back(Parameter::Switch('c', "check availability of data chunks"));
     return r;
   }
   int Main(const ArgumentList &args);
@@ -56,6 +58,6 @@ class CommandCheck : public Command {
                       const bool is_transition_point = false);
 };
 
-}
+}  // namespace swissknife
 
 #endif  // CVMFS_SWISSKNIFE_CHECK_H_
