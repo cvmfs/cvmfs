25ea504fc1c63f9ac2c0526550043c03d71608c0
Pranav Goswami <pranavgeekme@gmail.com>
2015-03-13 12:28:49 +0530

    Update t_smalloc.cc

--- test/unittests/t_smalloc.cc
+++ test/unittests/t_smalloc.cc
@@ -4,7 +4,28 @@
 #include "../../cvmfs/smalloc.h"
 
 class T_Smalloc : public ::testing::Test {
-	protected : 
-
+	protected: 
+		virtual void Setup() {
+	static const unsigned kNumSmall = 10;
+    static const unsigned kNumBig = 10000000
 	}
+	}
+
+
+TEST_F(T_Smalloc,Test_smalloc) {
+unsigned N = kNumSmall;
+  for (unsigned i = 0; i < N; ++i)
+  {
+   	void *r_mem = smalloc(N);
+  	EXPECT_EQ(r_mem, NULL);
+  	
+  }
+  
+  EXPECT_EQ(smalloc(10000000000, NULL);
+  EXPECT_EQ(smalloc(8),NULL);
+  }
+
+
+
+
 
