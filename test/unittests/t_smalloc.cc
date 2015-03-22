b28f5ea96aa09d11c1d53f12f689548ebf53ac75
Weiwei Jia <harryxiyou@gmail.com>
2015-03-22 13:51:02 +0800

    add part of smalloc unittest

---
+++ test/unittests/t_smalloc.cc
@@ -0,0 +1,90 @@
+/**
+ * This file is part of the CernVM File System.
+ */
+
+#include <gtest/gtest.h>
+
+#include "../../cvmfs/smalloc.h"
+//#include "../../cvmfs/util.h"
+
+// Debug switcher
+#define SMALLOC_DEBUG_
+
+// Test smalloc
+TEST(T_Smalloc, Smalloc) {
+	/* we need not test size 0 because smalloc returns either NULL, or a
+	 * unique pointer value that can later be successfully passed to free.
+	 * The largest malloc size is depending on the specific OS and
+	 * configuration so here we set 100MB is the largest test size.
+	 * */
+  for (unsigned int size = 1u; size <= 1024u * 1024u * 100u; ) {
+#ifdef SMALLOC_DEBUG_
+		printf("size is %u.\n", size);
+#endif
+		void *mem = smalloc(size);
+		EXPECT_TRUE(mem != NULL);
+		free(mem);
+		size = size * 512u;
+	}
+}
+
+// Test srealloc
+TEST(T_Smalloc, Srealloc) {
+	/* Srealloc(ptr, size) changes the size of memory block pointed to ptr to size
+	 * bytes. Size may be larger or smaller than old size. If ptr is NULL,
+	 * then it is the same as smalloc(). If size is zero and ptr is not
+	 * NULL, it is equivalent to free().*/
+	for (unsigned int size = 1u; size <= 1024u * 1024u * 100u; ) {
+#ifdef SMALLOC_DEBUG_
+		printf("size is %u.\n", size);
+#endif
+		//Test ptr is NULL
+		void *mem = srealloc(NULL, size);
+		EXPECT_TRUE(mem != NULL);
+#ifdef SMALLOC_DEBUG_
+		printf("999 %p\n", mem);
+#endif
+		// Test size is larger than old size
+		mem = srealloc(mem, size + 1u);
+		EXPECT_TRUE(mem != NULL);
+#ifdef SMALLOC_DEBUG_
+		printf("999 %p\n", mem);
+#endif
+		// Test size is smaller than old size 
+		// mem = srealloc(mem, size - 1u); TODO
+#ifdef SMALLOC_DEBUG_
+		printf("999 %p\n", mem);
+#endif
+		//if (size != 1u)
+			free(mem);
+		size = size * 1024u;
+	}
+}
+
+// Test scalloc
+TEST(T_Smalloc, Scalloc) {
+	/* Scalloc(nmemb, size) allocates memory space for an array of nmemb elements of
+	 * size bytes each. If nmemb or size is 0, then returns NULL or a
+	 * unique pointer value. We cannot test the condition of nmemb or size
+	 * is 0.
+	 */
+	for (unsigned int size = 1u, num = 1u; size <= 1024u * 1024u; num++) {
+#ifdef SMALLOC_DEBUG_
+    printf("size is %u.\n", size);
+		printf("num is %u.\n", num);
+#endif
+    int *mem = static_cast<int *>(scalloc(num, size));
+		EXPECT_FALSE(mem == NULL);
+    for (const int *tmp = mem; tmp < mem + num; tmp++) {
+			EXPECT_EQ(*tmp, 0);
+		}
+		free(mem);
+		size = size * 512u;
+	}
+}
+
+TEST(T_Smalloc, Smmap) {
+}
+
+TEST(T_Smalloc, Smunmap) {
+}
