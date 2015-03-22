346f0dbaa8a16fc0043edc3b0eb2eb5b21bc7257
Weiwei Jia <harryxiyou@gmail.com>
2015-03-22 19:51:04 +0800

    fix coding style

--- test/unittests/t_smalloc.cc
+++ test/unittests/t_smalloc.cc
@@ -7,24 +7,26 @@
 #include "../../cvmfs/smalloc.h"
 
 // Debug switcher
-//#define SMALLOC_DEBUG_
+// #define SMALLOC_DEBUG_
+
+// Max size is 10MB
+static const unsigned int max_size = 1024u * 1024u * 10u;
 
 // Test smalloc
 TEST(T_Smalloc, Smalloc) {
 	/* we need not test size 0 because smalloc returns either NULL, or a
 	 * unique pointer value that can later be successfully passed to free.
 	 * The largest malloc size is depending on the specific OS and
-	 * configuration so here we set 100MB is the largest test size.
+	 * configuration so here we set 10MB is the largest test size.
 	 * */
-  for (unsigned int size = 1u; size <= 1024u * 1024u * 100u; ) {
+  for (unsigned int size = 1u; size <= max_size; size = size * 512u) {
 #ifdef SMALLOC_DEBUG_
-		printf("size is %u.\n", size);
+    printf("size is %u.\n", size);
 #endif
-		void *mem = smalloc(size);
-		EXPECT_TRUE(mem != NULL);
-		free(mem);
-		size = size * 512u;
-	}
+    void *mem = smalloc(size);
+    EXPECT_TRUE(mem != NULL);
+    free(mem);
+  }
 }
 
 // Test srealloc
@@ -33,31 +35,37 @@ TEST(T_Smalloc, Srealloc) {
 	 * bytes. Size may be larger or smaller than old size. If ptr is NULL,
 	 * then it is the same as smalloc(). If size is zero and ptr is not
 	 * NULL, it is equivalent to free().*/
-	for (unsigned int size = 1u; size <= 1024u * 1024u * 100u; ) {
+  for (unsigned int size = 1u; size <= max_size; size = size * 1024) {
 #ifdef SMALLOC_DEBUG_
-		printf("size is %u.\n", size);
+    printf("size is %u.\n", size);
 #endif
-		//Test ptr is NULL
-		void *mem = srealloc(NULL, size);
-		EXPECT_TRUE(mem != NULL);
+    // Test ptr is NULL
+    void *mem = srealloc(NULL, size);
+    EXPECT_TRUE(mem != NULL);
 #ifdef SMALLOC_DEBUG_
-		printf("999 %p\n", mem);
+    printf("999 %p\n", mem);
 #endif
-		// Test size is larger than old size
-		mem = srealloc(mem, size + 1u);
-		EXPECT_TRUE(mem != NULL);
+    // Test size is larger than old size
+    mem = srealloc(mem, size + 1u);
+    EXPECT_TRUE(mem != NULL);
 #ifdef SMALLOC_DEBUG_
-		printf("999 %p\n", mem);
+    printf("999 %p\n", mem);
 #endif
-		// Test size is smaller than old size 
-		// mem = srealloc(mem, size - 1u); TODO
+    // Test size is smaller than old size
+    if (size != 1u) {
+			/*TODO: When size is 1u, srealloc would be asserted inside
+			 * beecause it is not allowed the second parameter of srealloc is
+       * zero. However, standard realloc(ptr, _size) permits _size is zero.
+			 * When _size is zero, it is free. Therefore, this maybe a bug for
+			 * current smalloc*/
+      mem = srealloc(mem, size - 1u);
+      EXPECT_TRUE(mem != NULL);
+    }
 #ifdef SMALLOC_DEBUG_
-		printf("999 %p\n", mem);
+    printf("999 %p\n", mem);
 #endif
-		//if (size != 1u)
-			free(mem);
-		size = size * 1024u;
-	}
+    free(mem);
+  }
 }
 
 // Test scalloc
@@ -67,37 +75,38 @@ TEST(T_Smalloc, Scalloc) {
 	 * unique pointer value. We cannot test the condition of nmemb or size
 	 * is 0.
 	 */
-	for (unsigned int size = 1u, num = 1u; size <= 1024u * 1024u; num++) {
+  for (unsigned int size = 1u, num = 1u; size <= max_size; num++) {
 #ifdef SMALLOC_DEBUG_
     printf("size is %u.\n", size);
-		printf("num is %u.\n", num);
+    printf("num is %u.\n", num);
 #endif
     int *mem = static_cast<int *>(scalloc(num, size));
-		EXPECT_FALSE(mem == NULL);
+    EXPECT_FALSE(mem == NULL);
     for (const int *tmp = mem; tmp < mem + num; tmp++) {
-			EXPECT_EQ(*tmp, 0);
-		}
-		free(mem);
-		size = size * 512u;
-	}
+      EXPECT_EQ(*tmp, 0);
+    }
+    free(mem);
+    size = size * 512u;
+  }
 }
 
-//Test smmap and smunmap
+// Test smmap and smunmap
 TEST(T_Smalloc, SmmapAndSunmap) {
-  for (unsigned int size = sizeof(size_t) * 2u; size <= 1024u * 1024u; size = size + 4096u) {
+  for (unsigned int size = sizeof(size_t) * 2u; size <= 1024u * 1024u; ) {
 #ifdef SMALLOC_DEBUG_
-		printf("size is %u.\n", size);
+    printf("size is %u.\n", size);
 #endif
-		size_t pages = ((size + 2 * sizeof(size_t)) + 4095) / 4096; 
-		void *mem = smmap(size);
-		EXPECT_FALSE(mem == NULL);
-		unsigned char *area = static_cast<unsigned char *>(mem);
-		area = area - sizeof(size_t);
-		size_t _pages = *(reinterpret_cast<size_t *>(area));
+    size_t pages = ((size + 2 * sizeof(size_t)) + 4095) / 4096;
+    void *mem = smmap(size);
+    EXPECT_FALSE(mem == NULL);
+    unsigned char *area = static_cast<unsigned char *>(mem);
+    area = area - sizeof(size_t);
+    size_t _pages = *(reinterpret_cast<size_t *>(area));
     EXPECT_EQ(_pages, pages);
-		area = area - sizeof(size_t);
+    area = area - sizeof(size_t);
     EXPECT_EQ(*(reinterpret_cast<size_t *>(area)), kMemMarker);
-		smunmap(mem);
-	}
+    smunmap(mem);
+    size = size + 4096u;
+  }
 }
 
