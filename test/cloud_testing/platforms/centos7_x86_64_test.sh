7e8f5394a91d51a7aaaf5b6a04fc6b145497afab
Rene Meusel <rene@renemeusel.de>
2015-04-14 14:48:37 +0000

    Cloud test adaption: CentOS 7

--- test/cloud_testing/platforms/centos7_x86_64_test.sh
+++ test/cloud_testing/platforms/centos7_x86_64_test.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
 # source the common platform independent functionality and option parsing
-script_location=$(dirname $(readlink --canonicalize $0))
+script_location=$(cd "$(dirname "$0")"; pwd)
 . ${script_location}/common_test.sh
 
-ut_retval=0
-it_retval=0
-s3_retval=0
-mg_retval=0
+retval = 0
 
 # allow apache access to the mounted server file system
 echo -n "setting SELinux labels for apache... "
@@ -21,19 +18,22 @@ echo "OK"
 
 # running unit test suite
 run_unittests --gtest_shuffle \
-              --gtest_death_test_use_fork || ut_retval=$?
+              --gtest_death_test_use_fork || retval=1
 
-echo "running CernVM-FS test cases..."
 cd ${SOURCE_DIRECTORY}/test
-export CVMFS_TEST_SERVER_CACHE='/srv/cache' &&         \
-./run.sh $TEST_LOGFILE -x src/005-asetup               \
-                          src/004-davinci              \
-                          src/007-testjobs             \
-                          src/024-reload-during-asetup \
-                          src/5* || it_retval=$?
+echo "running CernVM-FS client test cases..."
+CVMFS_TEST_CLASS_NAME=ClientIntegrationTests                                  \
+./run.sh $CLIENT_TEST_LOGFILE -o ${CLIENT_TEST_LOGFILE}${XUNIT_OUTPUT_SUFFIX} \
+                              -x src/005-asetup                               \
+                                 src/004-davinci                              \
+                                 src/007-testjobs                             \
+                                 src/024-reload-during-asetup                 \
+                                 --                                           \
+                                 src/0*                                       \
+                              || retval=1
 
 # TODO: enable this as soon as 2.1.20 is released
 echo "skipping CernVM-FS migration test case (no EL7 package for 2.1.19)"
 #./run.sh $MIGRATIONTEST_LOGFILE migration_tests/001-hotpatch || mg_retval=$?
 
-[ $ut_retval -eq 0 ] && [ $it_retval -eq 0 ] && [ $s3_retval -eq 0 ] && [ $mg_retval -eq 0 ]
+exit $retval
