29f39807f63feec6b9e1b8c40f3c7a212da2a5a2
Rene Meusel <rene@renemeusel.de>
2015-04-16 14:03:18 +0000

    Cosmetics: using naming scheme for tmp file

--- test/unittests/t_xattr.cc
+++ test/unittests/t_xattr.cc
@@ -46,7 +46,7 @@ TEST_F(T_Xattr, CreateFromFile) {
 
   // Create extended attributes
   string tmp_path;
-  FILE *f = CreateTempFile("/tmp/cvmfs-test", 0600, "w", &tmp_path);
+  FILE *f = CreateTempFile("/tmp/cvmfs_ut_xattr", 0600, "w", &tmp_path);
   ASSERT_TRUE(f != NULL);
   fclose(f);
   UnlinkGuard unlink_guard(tmp_path);
