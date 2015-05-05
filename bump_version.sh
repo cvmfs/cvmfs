f3c292d06a2fc81ce97018f12db84a8e2598a522
Jakob Blomer <jblomer@cern.ch>
2015-05-05 08:52:11 +0000

    bump version to 2.2.0, adjust bump_version script

--- bump_version.sh
+++ bump_version.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 set -e
 
+NEW_MINOR=$1
+NEW_PATCH=$2
+[[ -z "$NEW_MINOR" ]] && exit 1
+[[ -z "$NEW_PATCH" ]] && exit 1
+
 VERSION=$(grep "^## CVMFS_VERSION" CMakeLists.txt | cut -d" " -f3)
-MINOR_VERSION=$(echo $VERSION | cut -d. -f3)
 echo "Current version: $VERSION"
 
-MINOR_VERSION=$(($MINOR_VERSION+1))
-VERSION="$(echo $VERSION | cut -d. -f1).$(echo $VERSION | cut -d. -f2).$MINOR_VERSION"
+VERSION="$(echo $VERSION | cut -d. -f1).${NEW_MINOR}.${NEW_PATCH}"
 echo "New version: $VERSION"
 
 echo "Patching CMakeLists.txt"
 sed -i -e "s/^## CVMFS_VERSION \(.*\)/## CVMFS_VERSION $VERSION/" CMakeLists.txt
-sed -i -e "s/^set (CernVM-FS_VERSION_PATCH \(.*\)/set (CernVM-FS_VERSION_PATCH $MINOR_VERSION)/" CMakeLists.txt
+sed -i -e "s/^set (CernVM-FS_VERSION_MINOR \(.*\)/set (CernVM-FS_VERSION_MINOR $NEW_MINOR)/" CMakeLists.txt
+sed -i -e "s/^set (CernVM-FS_VERSION_PATCH \(.*\)/set (CernVM-FS_VERSION_PATCH $NEW_PATCH)/" CMakeLists.txt
 grep VERSION CMakeLists.txt
 
 echo "Patching RPM"
@@ -23,5 +27,4 @@ sed -i -e "s/\(\s*\)<f n=\"cvmfs-[0-9\.]*\(.*\)/\1<f n=\"cvmfs-$VERSION\2/" pack
 sed -i -e "s/\(\s*\)<f n=\"libcvmfs_fuse\.[0-9\.]\+\(.*\)/\1<f n=\"libcvmfs_fuse.$VERSION.\2/" packaging/mac/cvmfs.pmdoc.template/01cvmfs-contents.xml
 sed -i -e "s/\(\s*\)<f n=\"libcvmfs_fuse_debug\.[0-9\.]\+\(.*\)/\1<f n=\"libcvmfs_fuse_debug.$VERSION.\2/" packaging/mac/cvmfs.pmdoc.template/01cvmfs-contents.xml
 grep $VERSION packaging/mac/cvmfs.pmdoc.template/01cvmfs-contents.xml
-
 
