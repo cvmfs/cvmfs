3fb3df701fa6f4b48647069da9f1001a3ab0a34e
Jakob Blomer <jblomer@cern.ch>
2015-05-07 11:04:06 +0000

    add stubs for backoff and wpad unit tests

---
+++ test/unittests/t_backoff.cc
@@ -0,0 +1,24 @@
+/**
+ * This file is part of the CernVM File System.
+ */
+
+#include <gtest/gtest.h>
+
+#include "../../cvmfs/libcvmfs.h"
+
+using namespace std;  // NOLINT
+
+class T_Backoff : public ::testing::Test {
+ protected:
+  virtual void SetUp() {
+  }
+
+  virtual void TearDown() {
+  }
+
+  BackoffThrottle throttle_;
+};
+
+
+TEST_F(T_Backoff, Create) {
+}
---
+++ test/unittests/t_wpad.cc
@@ -0,0 +1,23 @@
+/**
+ * This file is part of the CernVM File System.
+ */
+
+#include <gtest/gtest.h>
+
+#include "../../cvmfs/libcvmfs.h"
+
+using namespace std;  // NOLINT
+
+class T_Wpad : public ::testing::Test {
+ protected:
+  virtual void SetUp() {
+  }
+
+
+  virtual void TearDown() {
+  }
+};
+
+
+TEST_F(T_Wpad, Init) {
+}
