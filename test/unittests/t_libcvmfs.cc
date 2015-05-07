1412dd5f1550d815d5f4c85cf8fce7da250a28bf
Jakob Blomer <jblomer@cern.ch>
2015-05-07 09:49:45 +0000

    add libcvmfs unit test infrastructure

---
+++ test/unittests/t_libcvmfs.cc
@@ -0,0 +1,23 @@
+/**
+ * This file is part of the CernVM File System.
+ */
+
+#include <gtest/gtest.h>
+
+#include "../../cvmfs/libcvmfs.h"
+
+using namespace std;  // NOLINT
+
+class T_Libcvmfs : public ::testing::Test {
+ protected:
+  virtual void SetUp() {
+  }
+
+
+  virtual void TearDown() {
+  }
+};
+
+
+TEST_F(T_Libcvmfs, Init) {
+}
