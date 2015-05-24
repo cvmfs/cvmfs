e18c5c7109b12bb35a517d8add5ba09c3824b20d
Jakob Blomer <jblomer@cern.ch>
2015-05-24 15:23:05 +0200

    add cache manager unit test stub

---
+++ test/unittests/t_cache.cc
@@ -0,0 +1,18 @@
+/**
+ * This file is part of the CernVM File System.
+ */
+
+#include <gtest/gtest.h>
+
+#include "../../cvmfs/cache.h"
+
+class T_Cache : public ::testing::Test {
+ protected:
+  virtual void SetUp() {
+  }
+
+ protected:
+};
+
+TEST_F(T_Cache, Noop) {
+}
