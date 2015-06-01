e1308e25cf766927c1e86a6975ea679b36420d28
Rene Meusel <rene@renemeusel.de>
2015-06-01 13:45:52 +0000

    TEST: PositiveIntegerSanitizer

--- test/unittests/t_sanitizer.cc
+++ test/unittests/t_sanitizer.cc
@@ -43,13 +43,29 @@ TEST_F(T_Sanitizer, Space) {
   EXPECT_TRUE(test_sanitizer.IsValid("abc ABC"));
 }
 
+TEST_F(T_Sanitizer, PositiveIntegerSanitizer) {
+  sanitizer::PositiveIntegerSanitizer test_sanitizer;
+
+  EXPECT_TRUE(test_sanitizer.IsValid("123"));
+  EXPECT_TRUE(test_sanitizer.IsValid("0"));
+
+  EXPECT_FALSE(test_sanitizer.IsValid("a"));
+  EXPECT_FALSE(test_sanitizer.IsValid(""));
+  EXPECT_FALSE(test_sanitizer.IsValid("?"));
+  EXPECT_FALSE(test_sanitizer.IsValid("5-0"));
+  EXPECT_FALSE(test_sanitizer.IsValid("-50"));
+
+  EXPECT_EQ(test_sanitizer.Filter("1+2"), "12");
+  EXPECT_EQ(test_sanitizer.Filter("0abc1243"), "01243");
+  EXPECT_EQ(test_sanitizer.Filter("-4-1243"), "41243");
+}
+
 TEST_F(T_Sanitizer, IntegerSanitizer) {
   sanitizer::IntegerSanitizer test_sanitizer;
 
   EXPECT_TRUE(test_sanitizer.IsValid("123"));
   EXPECT_TRUE(test_sanitizer.IsValid("0"));
   EXPECT_TRUE(test_sanitizer.IsValid("-50"));
-
 
   EXPECT_FALSE(test_sanitizer.IsValid("a"));
   EXPECT_FALSE(test_sanitizer.IsValid(""));
