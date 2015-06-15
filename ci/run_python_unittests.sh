c5f46a40d60672c56178c823d2f034105c9556ac
Rene Meusel <rene@renemeusel.de>
2015-06-15 15:55:27 +0200

    FIX: unittest2 check on Python 2.7

--- ci/run_python_unittests.sh
+++ ci/run_python_unittests.sh
@@ -23,11 +23,14 @@ PY_UT_XML_PREFIX=$1
 # check if python is capable of running the unit tests on the current platform
 which python > /dev/null 2>&1                    || complain "no python available"
 compare_versions "$(python_version)" -gt "2.6.0" || complain "ancient python version"
-check_python_module "unittest2"                  || complain "no unittest2 installed"
 check_python_module "xmlrunner"                  || complain "no python-xmlrunner installed"
 check_python_module "dateutil"                   || complain "no python-dateutil installed"
 check_python_module "requests"                   || complain "no python-requests installed"
 check_python_module "M2Crypto"                   || complain "no m2crypto installed"
+if compare_versions "$(python_version)" -lt "2.7.0"; then
+  # unittest2 is a back-port of the Python 2.7 unittest features
+  check_python_module "unittest2" || complain "python 2.6 but no unittest2 installed"
+fi
 
 # run the unit tests
 UNITTEST_RUNNER="${SCRIPT_LOCATION}/../python/run_unittests.py"
