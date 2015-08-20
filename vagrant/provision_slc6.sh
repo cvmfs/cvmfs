cdfc48f67b789ae0ebbb46e320f0e31ff962e384
Rene Meusel <rene@renemeusel.de>
2015-08-20 17:11:39 +0200

    Vagrant: add FakeS3 for SLC6

--- vagrant/provision_slc6.sh
+++ vagrant/provision_slc6.sh
@@ -48,7 +48,8 @@ yum -y install libuuid-devel gcc gcc-c++ glibc-common cmake fuse fuse-devel  \
                curl attr httpd
 
 # install convenience packages for development
-yum -y install git tig iftop htop jq
+yum -y install git tig iftop htop jq rubygems
+gem install fakes3 --version 0.2.0
 
 # link the CernVM-FS source directory in place
 if [ ! -L $CVMFS_SOURCE_DIR ]; then
