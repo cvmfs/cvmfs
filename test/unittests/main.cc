2bf9d36d4b93eb8b4251e0ec0a40b2f9fb2e75aa
Rene Meusel <rene@renemeusel.de>
2015-08-24 13:45:16 +0200

    use CvmfsEnvironment class

--- test/unittests/main.cc
+++ test/unittests/main.cc
@@ -3,9 +3,11 @@
  */
 
 #include <gtest/gtest.h>
+#include "env.h"
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   ::testing::FLAGS_gtest_death_test_style = "threadsafe";
+  ::testing::AddGlobalTestEnvironment(new CvmfsEnvironment(argc, argv));
   return RUN_ALL_TESTS();
 }
