7d26ed2409e3ef5662bfa8de357beb5b438383b3
Rene Meusel <rene@renemeusel.de>
2015-08-24 13:58:10 +0200

    FIX: create CvmfsEnvironment before init of gtest

--- test/unittests/main.cc
+++ test/unittests/main.cc
@@ -6,8 +6,9 @@
 #include "env.h"
 
 int main(int argc, char **argv) {
+  CvmfsEnvironment* env = new CvmfsEnvironment(argc, argv);
   ::testing::InitGoogleTest(&argc, argv);
   ::testing::FLAGS_gtest_death_test_style = "threadsafe";
-  ::testing::AddGlobalTestEnvironment(new CvmfsEnvironment(argc, argv));
+  ::testing::AddGlobalTestEnvironment(env);
   return RUN_ALL_TESTS();
 }
