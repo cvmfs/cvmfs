e5fbe82ef37af7662f3af10f1bdf609422fccda0
Jakob Blomer <jblomer@cern.ch>
2015-09-24 16:28:45 +0200

    add unit tests for key handling

--- test/unittests/t_encrypt.cc
+++ test/unittests/t_encrypt.cc
@@ -4,18 +4,52 @@
 
 #include <gtest/gtest.h>
 
+#include <unistd.h>
+
+#include <algorithm>
+#include <cstdio>
+#include <cstring>
 #include <string>
 
 #include "../../cvmfs/encrypt.h"
 #include "../../cvmfs/util.h"
+#include "testutil.h"
 
 using namespace std;  // NOLINT
 
 namespace cipher {
 
+TEST(T_Encrypt, KeyFiles) {
+  CipherNone cipher;
+  UniquePtr<Key> k(Key::CreateRandomly(cipher.key_size()));
+  ASSERT_TRUE(k.IsValid());
+
+  string tmp_path;
+  FILE *f = CreateTempFile("./key", 0600, "w+", &tmp_path);
+  ASSERT_TRUE(f != NULL);
+  fclose(f);
+  EXPECT_FALSE(k->SaveToFile("/no/such/file"));
+  EXPECT_TRUE(k->SaveToFile(tmp_path));
+
+  UniquePtr<Key> k_restore1(Key::CreateFromFile(tmp_path));
+  ASSERT_TRUE(k_restore1.IsValid());
+  EXPECT_EQ(k->size(), k_restore1->size());
+  EXPECT_EQ( 0, memcmp(k->data(), k_restore1->data(),
+                       std::min(k->size(), k_restore1->size())) );
+
+  EXPECT_EQ(0, truncate(tmp_path.c_str(), 0));
+  UniquePtr<Key> k_restore2(Key::CreateFromFile(tmp_path));
+  EXPECT_FALSE(k_restore2.IsValid());
+
+  unlink(tmp_path.c_str());
+  UniquePtr<Key> k_restore3(Key::CreateFromFile(tmp_path));
+  EXPECT_FALSE(k_restore3.IsValid());
+}
+
+
 TEST(T_Encrypt, None) {
   CipherNone cipher;
-  UniquePtr<Key>k(Key::CreateRandomly(cipher.key_size()));
+  UniquePtr<Key> k(Key::CreateRandomly(cipher.key_size()));
   ASSERT_TRUE(k.IsValid());
 
   string empty;
@@ -40,9 +74,9 @@ TEST(T_Encrypt, None) {
 
 TEST(T_Encrypt, DecryptWrongEnvelope) {
   CipherNone cipher;
-  UniquePtr<Key>k(Key::CreateRandomly(cipher.key_size()));
+  UniquePtr<Key> k(Key::CreateRandomly(cipher.key_size()));
   ASSERT_TRUE(k.IsValid());
-  UniquePtr<Key>k_bad(Key::CreateRandomly(1));
+  UniquePtr<Key> k_bad(Key::CreateRandomly(1));
   ASSERT_TRUE(k_bad.IsValid());
 
   string ciphertext;
