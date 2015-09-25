c89da238eb9e20bc393427655aa17a9d9db31a5a
Jakob Blomer <jblomer@cern.ch>
2015-09-25 10:04:06 +0200

    add more documentation, testing to the encryption routines

--- test/unittests/t_encrypt.cc
+++ test/unittests/t_encrypt.cc
@@ -12,12 +12,22 @@
 #include <string>
 
 #include "../../cvmfs/encrypt.h"
+#include "../../cvmfs/hash.h"
 #include "../../cvmfs/util.h"
 #include "testutil.h"
 
 using namespace std;  // NOLINT
 
 namespace cipher {
+
+TEST(T_Encrypt, Entropy) {
+  // Enough entropy for 100,000 256 bit keys?
+  for (unsigned i = 0; i < 100000; ++i) {
+    UniquePtr<Key> k(Key::CreateRandomly(32));
+    ASSERT_TRUE(k.IsValid());
+  }
+}
+
 
 TEST(T_Encrypt, KeyFiles) {
   CipherNone cipher;
@@ -44,6 +54,34 @@ TEST(T_Encrypt, KeyFiles) {
   unlink(tmp_path.c_str());
   UniquePtr<Key> k_restore3(Key::CreateFromFile(tmp_path));
   EXPECT_FALSE(k_restore3.IsValid());
+}
+
+
+TEST(T_Encrypt, DecryptWrongEnvelope) {
+  CipherNone cipher;
+  UniquePtr<Key> k(Key::CreateRandomly(cipher.key_size()));
+  ASSERT_TRUE(k.IsValid());
+  UniquePtr<Key> k_bad(Key::CreateRandomly(1));
+  ASSERT_TRUE(k_bad.IsValid());
+
+  string ciphertext;
+  string plaintext;
+  int retval = Cipher::Decrypt(ciphertext, *k, &plaintext);
+  EXPECT_FALSE(retval);
+
+  ciphertext = "X";
+  ciphertext[0] = 0xF0;
+  retval = Cipher::Decrypt(ciphertext, *k, &plaintext);
+  EXPECT_FALSE(retval);
+  ciphertext[0] = 0x0F;
+  retval = Cipher::Decrypt(ciphertext, *k, &plaintext);
+  EXPECT_FALSE(retval);
+
+  ciphertext[0] = cipher.algorithm() << 4;
+  retval = Cipher::Decrypt(ciphertext, *k_bad, &plaintext);
+  EXPECT_FALSE(retval);
+  retval = Cipher::Decrypt(ciphertext, *k, &plaintext);
+  EXPECT_TRUE(retval);
 }
 
 
@@ -111,31 +149,15 @@ TEST(T_Encrypt, Aes_256_Cbc) {
 }
 
 
-TEST(T_Encrypt, DecryptWrongEnvelope) {
-  CipherNone cipher;
-  UniquePtr<Key> k(Key::CreateRandomly(cipher.key_size()));
-  ASSERT_TRUE(k.IsValid());
-  UniquePtr<Key> k_bad(Key::CreateRandomly(1));
-  ASSERT_TRUE(k_bad.IsValid());
-
-  string ciphertext;
-  string plaintext;
-  int retval = Cipher::Decrypt(ciphertext, *k, &plaintext);
-  EXPECT_FALSE(retval);
-
-  ciphertext = "X";
-  ciphertext[0] = 0xF0;
-  retval = Cipher::Decrypt(ciphertext, *k, &plaintext);
-  EXPECT_FALSE(retval);
-  ciphertext[0] = 0x0F;
-  retval = Cipher::Decrypt(ciphertext, *k, &plaintext);
-  EXPECT_FALSE(retval);
-
-  ciphertext[0] = cipher.algorithm() << 4;
-  retval = Cipher::Decrypt(ciphertext, *k_bad, &plaintext);
-  EXPECT_FALSE(retval);
-  retval = Cipher::Decrypt(ciphertext, *k, &plaintext);
-  EXPECT_TRUE(retval);
+TEST(T_Encrypt, Aes_256_Cbc_Iv) {
+  CipherAes256Cbc cipher;
+  // Many Iv requests in a short time should still return unique IVs
+  shash::Md5 md5;
+  for (unsigned i = 0; i < 100000; ++i) {
+    shash::Md5 next_iv = cipher.GenerateIv();
+    ASSERT_NE(md5, next_iv);
+    md5 = next_iv;
+  }
 }
 
 }  // namespace cipher
