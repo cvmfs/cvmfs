2d791185619f2f340840393458edc83a5fa21a3f
Brian Bockelman <bbockelm@cse.unl.edu>
2015-09-28 10:13:30 -0500

    Report information about external data through xattr and swissknife info.

--- cvmfs/cvmfs.cc
+++ cvmfs/cvmfs.cc
@@ -1490,6 +1490,10 @@ static void cvmfs_getxattr(fuse_req_t req, fuse_ino_t ino, const char *name,
     } else {
       attribute_value = "DIRECT";
     }
+  } else if (attr == "user.external_file") {
+    attribute_value = d.IsExternalFile() ? "1" : "0";
+  } else if (attr == "user.external_data") {
+    attribute_value = catalog_manager_->GetExternalDataRepository() ? "1" : "0";
   } else if (attr == "user.external_host") {
     attribute_value = download_manager_->GetExternalHost();
   } else if (attr == "user.host") {
@@ -1602,7 +1606,7 @@ static void cvmfs_listxattr(fuse_req_t req, fuse_ino_t ino, size_t size) {
     "user.host\0user.proxy\0user.uptime\0user.nclg\0user.nopen\0"
     "user.ndownload\0user.timeout\0user.timeout_direct\0user.rx\0user.speed\0"
     "user.fqrn\0user.ndiropen\0user.inode_max\0user.tag\0user.host_list\0"
-    "user.external_host\0";
+    "user.external_host\0user.external_data\0user.external_file\0";
   string attribute_list;
   if (hide_magic_xattrs_) {
     LogCvmfs(kLogCvmfs, kLogDebug, "Hiding extended attributes");
