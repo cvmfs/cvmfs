f58522ec8166536272b80b3a32876a632151f744
Jose Molina <jose.molina@cern.ch>
2015-10-20 14:33:10 +0200

    Add the rest of the dependencies when packaging the cvmfs_preload binary

--- cvmfs/cvmfs_preload.sh
+++ cvmfs/cvmfs_preload.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-set -e
-set -x
-
 export TMPDIR=`mktemp -d /tmp/cvmfs_preload_selfextracted.XXXXXX`
 
 ARCHIVE=`awk '/^__ARCHIVE_BELOW__/ {print NR + 1; exit 0; }' $0`
 
-tail -n+"$ARCHIVE" "$0" | tar xzv -C "$TMPDIR"
+tail -n+"$ARCHIVE" "$0" | tar xzv -C "$TMPDIR" || echo "Couldn't extract the tar file"
 
 CDIR=$(pwd)
 cd "$TMPDIR"
 
 # execute the command
-LD_PRELOAD="$TMPDIR/libtbb_cvmfs.so.2 $TMPDIR/libtbbmalloc_cvmfs.so.2" ${TMPDIR}/cvmfs_preload $@
+LD_PRELOAD="$TMPDIR/libtbb_cvmfs.so.2 $TMPDIR/libtbbmalloc_cvmfs.so.2 $TMPDIR/libssl.so $TMPDIR/libcrypto.so $TMPDIR/libgssapi_krb5.so $TMPDIR/libkrb5.so $TMPDIR/libk5crypto.so $TMPDIR/libkrb5support.so $TMPDIR/libkeyutils.so" ${TMPDIR}/cvmfs_preload $@ || echo "Failed to execute cvmfs_preload"
 
 cd "$CDIR"
 rm -rf "$TMPDIR"
