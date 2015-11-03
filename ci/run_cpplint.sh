1a07497f140db8d5472532f900417620ebfb9478
Jakob Blomer <jblomer@cern.ch>
2015-11-03 16:23:17 +0100

    check for python version before running linter

--- ci/run_cpplint.sh
+++ ci/run_cpplint.sh
@@ -20,6 +20,18 @@ CPPLINT="${REPO_ROOT}/cpplint.py"
 [ -d $REPO_ROOT ] || die "$REPO_ROOT is malformed"
 [ -f $CPPLINT ]   || die "$CPPLINT missing"
 
+PYTHON_VERSION_STR=$(python -V 2>&1)
+if [ $? -ne 0 ]; then
+  echo "WARNING: python missing, skipping linter"
+  exit 0
+fi
+PYTHON_MAJOR=$(echo $PYTHON_VERSION_STR | awk '{print $2}' | cut -d. -f1)
+PYTHON_MINOR=$(echo $PYTHON_VERSION_STR | awk '{print $2}' | cut -d. -f2)
+if [ $PYTHON_MAJOR -lt 2 -o $PYTHON_MAJOR -eq 2 -a $PYTHON_MINOR -lt 4 ]; then
+  echo "WARNING: python too old (< 2.4), skipping linter"
+  exit 0
+fi
+
 # define locations and file extensions of source files
 SOURCE_DIRS="cvmfs mount test/unittests"
 
