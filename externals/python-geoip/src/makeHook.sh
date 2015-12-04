602b6f5d44c71cb15f11fd269437530a04d784bd
Rene Meusel <rene@renemeusel.de>
2015-12-04 11:49:49 +0100

    FIX: GeoIP external build on ArchLinux

--- externals/python-geoip/src/makeHook.sh
+++ externals/python-geoip/src/makeHook.sh
@@ -11,10 +11,12 @@ mkdir dist
 if [ -f .tmp/*.egg ]; then
     # older python versions like 2.4 install a .egg; extract GeoIP.so
     (cd .tmp; unzip -o *.egg GeoIP.so)
-fi
-if [ -d .tmp/*.egg ]; then
+elif [ -d .tmp/*.egg ]; then
 	# for some reason SLC6 produces an uncompressed *.egg directory containing GeoIP.so
 	cp .tmp/*.egg/GeoIP.so .tmp
+elif [ -f .tmp/GeoIP*.so ]; then
+  # on ArchLinux no *.egg is created but a GeoIP.<system tag>.so file
+  cp .tmp/GeoIP*.so .tmp/GeoIP.so
 fi
 cp .tmp/GeoIP.so dist
 rm -rf .tmp
