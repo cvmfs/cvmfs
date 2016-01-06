388d110a8655807443bf02ac261e77268e780bf7
Jakob Blomer <jblomer@cern.ch>
2016-01-06 18:39:39 +0100

    fix overlooked invocations of MkdirDeep

--- cvmfs/util.cc
+++ cvmfs/util.cc
@@ -518,20 +518,20 @@ bool MakeCacheDirectories(const string &path, const mode_t mode) {
   const string canonical_path = MakeCanonicalPath(path);
 
   string this_path = canonical_path + "/quarantaine";
-  if (!MkdirDeep(this_path, mode)) return false;
+  if (!MkdirDeep(this_path, mode, false)) return false;
 
   this_path = canonical_path + "/ff";
 
   platform_stat64 stat_info;
   if (platform_stat(this_path.c_str(), &stat_info) != 0) {
     this_path = canonical_path + "/txn";
-    if (!MkdirDeep(this_path, mode))
+    if (!MkdirDeep(this_path, mode, false))
       return false;
     for (int i = 0; i <= 0xff; i++) {
       char hex[3];
       snprintf(hex, sizeof(hex), "%02x", i);
       this_path = canonical_path + "/" + string(hex);
-      if (!MkdirDeep(this_path, mode))
+      if (!MkdirDeep(this_path, mode, false))
         return false;
     }
   }
