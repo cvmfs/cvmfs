9aea832502ef78079b78bc99a1e302d50f6e4557
Rene Meusel <rene@renemeusel.de>
2016-01-12 17:04:09 +0100

    add Fedora 23 as a testing platform

---
+++ test/cloud_testing/platforms/fedora23_x86_64_setup.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+# source the common platform independent functionality and option parsing
+script_location=$(dirname $(readlink --canonicalize $0))
+. ${script_location}/common_setup.sh
+
+# update packages installed on the system
+echo "updating installed RPM packages..."
+sudo dnf -y update || die "fail (dnf update)"
+
+# disable SELinux (OverlayFS doesn't support it)
+echo -n "set SELinux into permissive mode..."
+sudo setenforce 0 || die "fail"
+echo "done"
+
+# install CernVM-FS RPM packages
+echo "installing RPM packages... "
+install_rpm "$CONFIG_PACKAGES"
+install_rpm $CLIENT_PACKAGE
+install_rpm $SERVER_PACKAGE
+install_rpm $DEVEL_PACKAGE
+install_rpm $UNITTEST_PACKAGE
+
+# installing WSGI apache module
+echo "installing python WSGI module..."
+install_from_repo mod_wsgi || die "fail (installing mod_wsgi)"
+sudo systemctl start httpd || die "fail (starting apache)"
+
+# setup environment
+echo -n "setting up CernVM-FS environment..."
+sudo cvmfs_config setup                          || die "fail (cvmfs_config setup)"
+sudo mkdir -p /var/log/cvmfs-test                || die "fail (mkdir /var/log/cvmfs-test)"
+sudo chown sftnight:sftnight /var/log/cvmfs-test || die "fail (chown /var/log/cvmfs-test)"
+sudo cvmfs_config chksetup > /dev/null           || die "fail (cvmfs_config chksetup)"
+echo "done"
+
+# install additional stuff (needed for perl testing tools)
+echo "installing additional RPM packages..."
+install_from_repo gcc
+install_from_repo gcc-c++
+install_from_repo rubygems
+install_from_repo java
+
+# increase open file descriptor limits
+echo -n "increasing ulimit -n ... "
+set_nofile_limit 65536 || die "fail"
+echo "done"
---
+++ test/cloud_testing/platforms/fedora23_x86_64_test.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+# source the common platform independent functionality and option parsing
+script_location=$(cd "$(dirname "$0")"; pwd)
+. ${script_location}/common_test.sh
+
+retval=0
+
+# running unit test suite
+run_unittests --gtest_shuffle \
+              --gtest_death_test_use_fork || retval=1
+
+
+cd ${SOURCE_DIRECTORY}/test
+echo "running CernVM-FS client test cases..."
+CVMFS_TEST_CLASS_NAME=ClientIntegrationTests                                  \
+./run.sh $CLIENT_TEST_LOGFILE -o ${CLIENT_TEST_LOGFILE}${XUNIT_OUTPUT_SUFFIX} \
+                                 --                                           \
+                                 src/0*                                       \
+                              || retval=1
+
+
+echo "running CernVM-FS server test cases..."
+CVMFS_TEST_SERVER_CACHE='/srv/cache'                                          \
+CVMFS_TEST_CLASS_NAME=ServerIntegrationTests                                  \
+./run.sh $SERVER_TEST_LOGFILE -o ${SERVER_TEST_LOGFILE}${XUNIT_OUTPUT_SUFFIX} \
+                              -x src/518-hardlinkstresstest                   \
+                                 src/523-corruptchunkfailover                 \
+                                 src/524-corruptmanifestfailover              \
+                                 src/577-garbagecollecthiddenstratum1revision \
+                                 src/579-garbagecollectstratum1legacytag      \
+                                 src/585-xattrs                               \
+                                 --                                           \
+                                 src/5*                                       \
+                              || retval=1
+
+
+echo "NOT running CernVM-FS migration test cases (disabled)..."
+# TODO(rmeusel): enable this when there is a 'previous version' for FC23
+# CVMFS_TEST_CLASS_NAME=MigrationTests                                              \
+# ./run.sh $MIGRATIONTEST_LOGFILE -o ${MIGRATIONTEST_LOGFILE}${XUNIT_OUTPUT_SUFFIX} \
+#                                    migration_tests/*                              \
+#                                 || retval=1
+
+exit $retval
