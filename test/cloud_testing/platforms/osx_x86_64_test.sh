51aa5fbd20cd14d7bd689e73271f0eb7b7ef6411
Jose Molina <jose.molina@cern.ch>
2016-01-13 16:30:41 +0100

    TEST: Add more integration tests to mac

--- test/cloud_testing/platforms/osx_x86_64_test.sh
+++ test/cloud_testing/platforms/osx_x86_64_test.sh
@@ -13,10 +13,23 @@ cd "$workdir"
 
 # everything will be placed in the home folder
 echo "running CernVM-FS client test cases..."
-CVMFS_TEST_CLASS_NAME=ClientIntegrationTests                                  \
-sudo ./run.sh "$logfile"              src/000-dummy                                \
-                                 src/001-chksetup                             \
-                              || retval=1
+CVMFS_TEST_CLASS_NAME=ClientIntegrationTests
+sudo ./run.sh "$logfile"  000-dummy                 \
+                          001-chksetup              \
+                          002-probe                 \
+                          003-nested                \
+                          004-davinci               \
+                          005-asetup                \
+                          007-testjobs              \
+                          008-default_domain        \
+                          009-tar                   \
+                          010-du                    \
+                          012-ls-s                  \
+                          013-certificate_cache     \
+                          014-corrupt_lru           \
+                          017-dnstimeout            \
+                          018-httpunreachable       \
+                          019-httptimeout
 
 
 exit $retval
