70a4a4f9d2e2cfcc9d20ecc43733bfd28ee7de7c
Jose Molina <jose.molina@cern.ch>
2016-01-15 16:51:48 +0100

    TEST: adapt integration test 023 to mac

--- test/cloud_testing/platforms/osx_x86_64_test.sh
+++ test/cloud_testing/platforms/osx_x86_64_test.sh
@@ -14,21 +14,23 @@ cd "$workdir"
 # everything will be placed in the home folder
 echo "running CernVM-FS client test cases..."
 CVMFS_TEST_CLASS_NAME=ClientIntegrationTests
-sudo ./run.sh "$logfile"  src/000-dummy                 \
-                          src/001-chksetup              \
-                          src/002-probe                 \
-                          src/003-nested                \
-                          src/009-tar                   \
-                          src/010-du                    \
-                          src/012-ls-s                  \
-                          src/013-certificate_cache     \
-                          src/014-corrupt_lru           \
-                          src/015-rebuild_on_crash      \
-                          src/017-dnstimeout            \
-                          src/018-httpunreachable       \
-                          src/019-httptimeout           \
-                          src/020-emptyrepofailover     \
-                          src/021-stacktrace
+sudo ./run.sh "$logfile"  src/000-dummy                           \
+                          src/001-chksetup                        \
+                          src/002-probe                           \
+                          src/003-nested                          \
+                          src/009-tar                             \
+                          src/010-du                              \
+                          src/012-ls-s                            \
+                          src/013-certificate_cache               \
+                          src/014-corrupt_lru                     \
+                          src/015-rebuild_on_crash                \
+                          src/017-dnstimeout                      \
+                          src/018-httpunreachable                 \
+                          src/019-httptimeout                     \
+                          src/020-emptyrepofailover               \
+                          src/021-stacktrace                      \
+                          src/022-tacktrace_private_mnt           \
+                          stc/023-reload_safe_path_traversal
 
 
 exit $retval
