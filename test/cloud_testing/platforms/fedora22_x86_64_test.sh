a71bb0ccd6b15ef69db107df7a9aeb309bc8cfbc
Rene Meusel <rene@renemeusel.de>
2016-01-15 14:51:21 +0100

    disable incompatible client tests on Fedora

--- test/cloud_testing/platforms/fedora22_x86_64_test.sh
+++ test/cloud_testing/platforms/fedora22_x86_64_test.sh
@@ -15,6 +15,10 @@ cd ${SOURCE_DIRECTORY}/test
 echo "running CernVM-FS client test cases..."
 CVMFS_TEST_CLASS_NAME=ClientIntegrationTests                                  \
 ./run.sh $CLIENT_TEST_LOGFILE -o ${CLIENT_TEST_LOGFILE}${XUNIT_OUTPUT_SUFFIX} \
+                              -x src/004-davinci                              \
+                                 src/005-asetup                               \
+                                 src/007-testjobs                             \
+                                 src/024-reload-during-asetup                 \
                                  --                                           \
                                  src/0*                                       \
                               || retval=1
