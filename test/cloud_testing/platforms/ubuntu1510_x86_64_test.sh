88a770fbd30ae72d0a54233f2e9b051ec013f4e7
Rene Meusel <rene@renemeusel.de>
2016-01-20 15:19:40 +0100

    use OverlayFS on Ubuntu 15.10

--- test/cloud_testing/platforms/ubuntu1510_x86_64_test.sh
+++ test/cloud_testing/platforms/ubuntu1510_x86_64_test.sh
@@ -26,6 +26,7 @@ CVMFS_TEST_CLASS_NAME=ClientIntegrationTests                                  \
 
 echo "running CernVM-FS server test cases..."
 CVMFS_TEST_CLASS_NAME=ServerIntegrationTests                                  \
+CVMFS_TEST_UNIONFS=overlayfs                                                  \
 ./run.sh $SERVER_TEST_LOGFILE -o ${SERVER_TEST_LOGFILE}${XUNIT_OUTPUT_SUFFIX} \
                               -x src/523-corruptchunkfailover                 \
                                  src/524-corruptmanifestfailover              \
