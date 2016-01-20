c0b9943e3d0baebee92ab8184a5ba19f894647c9
Jose Molina <jose.molina@cern.ch>
2016-01-20 18:01:22 +0100

    TEST: update available integration tests for Mac OS X

--- test/cloud_testing/platforms/osx_x86_64_test.sh
+++ test/cloud_testing/platforms/osx_x86_64_test.sh
@@ -29,7 +29,40 @@ sudo ./run.sh "$logfile"  src/000-dummy                           \
                           src/020-emptyrepofailover               \
                           src/021-stacktrace                      \
                           src/022-tacktrace_private_mnt           \
-                          src/023-reload_safe_path_traversal
+                          src/023-reload_safe_path_traversal      \
+                          src/025-proxyfailover                   \
+                          src/026-tightcache                      \
+                          src/027-automount                       \
+                          src/028-negativecache                   \
+                          src/029-requeststorm                    \
+                          src/030-missingrootcatalog              \
+                          src/034-cachecleanup                    \
+                          src/035-unpinumount                     \
+                          src/036-cacheoverload                   \
+                          src/037-strictmount                     \
+                          src/038-maxttl                          \
+                          src/041-rocache                         \
+                          src/042-cleanuppipes                    \
+                          src/043-highinodes                      \
+                          src/044-unpinonmount                    \
+                          src/046-defaultd                        \
+                          src/047-blacklist                       \
+                          src/048-exportfqrn                      \
+                          src/049-cdconf                          \
+                          src/050-configrepo                      \
+                          src/051-failonbrokenpubkey              \
+                          src/053-uuid                            \
+                          src/054-geoapi                          \
+                          src/058-keysdir                         \
+                          src/059-fallbackproxy                   \
+                          src/060-hidexattrs                      \
+                          src/062-loadtag                         \
+                          src/063-uidmap                          \
+                          src/064-fsck                            \
+                          src/065-http-400                        \
+                          src/066-killall                         \
+                          src/067-wpad                            \
+                          src/068-rocache
 
-
+retval=$?
 exit $retval
