007c267e55db9308e3b5ae1dd9742b12a0b4ffa5
Jakob Blomer <jblomer@cern.ch>
2016-01-20 21:22:39 +0100

    add warning comment

--- cvmfs/quota.cc
+++ cvmfs/quota.cc
@@ -505,6 +505,9 @@ bool PosixQuotaManager::DoCleanup(const uint64_t leave_size) {
       pid_t pid;
       int statloc;
       if ((pid = fork()) == 0) {
+        // TODO(jblomer): eviciting files in the cache should perhaps become a
+        // thread.  This would also allow to block the chunks and prevent the
+        // race with re-insertion.  Then again, a thread can block umount.
 #ifndef DEBUGMSG
         int max_fd = sysconf(_SC_OPEN_MAX);
         for (int i = 0; i < max_fd; ++i)
