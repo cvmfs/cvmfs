9f3f97f77cb47184d02bb58bb38aa7b1fb2325fd
Rene Meusel <rene@renemeusel.de>
2016-01-22 14:53:46 +0100

    disable several tests for S3

--- test/cloud_testing/platforms/slc6_x86_64_test.sh
+++ test/cloud_testing/platforms/slc6_x86_64_test.sh
@@ -118,6 +118,10 @@ if [ $s3_retval -eq 0 ]; then
                                src/591-importrepo                           \
                                src/594-backendoverwrite                     \
                                src/595-geoipdbupdate                        \
+                               src/600-securecvmfs                          \
+                               src/605-resurrectancientcatalog              \
+                               src/622-gracefulrmfs                         \
+                               src/626-cacheexpiry                          \
                                --                                           \
                                src/5*                                       \
                                src/6*                                       \
