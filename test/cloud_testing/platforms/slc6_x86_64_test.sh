7ca2e916da544b11ee96e0127c9bb9c28c1f3967
Rene Meusel <rene@renemeusel.de>
2016-01-22 17:21:52 +0100

    disable 607 for S3 tests

--- test/cloud_testing/platforms/slc6_x86_64_test.sh
+++ test/cloud_testing/platforms/slc6_x86_64_test.sh
@@ -120,6 +120,7 @@ if [ $s3_retval -eq 0 ]; then
                                src/595-geoipdbupdate                        \
                                src/600-securecvmfs                          \
                                src/605-resurrectancientcatalog              \
+                               src/607-noapache                             \
                                src/608-infofile                             \
                                src/614-geoservice                           \
                                src/622-gracefulrmfs                         \
