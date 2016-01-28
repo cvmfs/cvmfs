7f167d4942038c728d5dcd422f6e079857b182f9
Jose Molina <jose.molina@cern.ch>
2016-01-28 10:37:02 +0100

    TRAVIS: fix script for displaying the results properly

--- ci/run_travis.sh
+++ ci/run_travis.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 report_error() {
-  log_file="$1"
+  local log_file="$1"
+
   FAILURES=$(( FAILURES+1 ))
   echo "Failed!"
   echo ""
@@ -11,62 +12,78 @@ report_error() {
   echo ""
 }
 
+running_on_linux() {
+  [[ "$TRAVIS_OS_NAME" == "linux" ]]
+}
+
 check_failure() {
-  code=$1
-  if [ $code -eq 0 ]; then
+  local result=$1
+
+  if $result ; then
     echo "FAILED"
   else
     echo "PASSED"
   fi
 }
 
-CPPLINT_FAILED=1
-BUILD_FAILED=1
-UNITTESTS_FAILED=1
+CPPLINT_FAILED=false
+BUILD_FAILED=false
+UNITTESTS_FAILED=false
 FAILURES=0
 
-# create the build directory
-mkdir -p build
 
+echo ""
+echo ""
 # run the cpplint first
 echo -n "RUNNING CPPLINT ......................................................................................... "
-ci/run_cpplint.sh > cpplint.log 2>&1                                      || CPPLINT_FAILED=0 report_error "$(pwd)/cpplint.log"
-echo "Done"
+ci/run_cpplint.sh > cpplint.log 2>&1                                      || CPPLINT_FAILED=true report_error "$(pwd)/cpplint.log"
+if ! $CPPLINT_FAILED ; then
+  echo "Done"
+fi
 
-# Build CVMFS (make -j stresses the memory of travis)
+# Build CVMFS (make -j stresses the memory of travis machines)
 echo -n "BUILDING CernVM-FS ...................................................................................... "
-cd build
-cmake -DBUILD_UNITTESTS=yes -DBUILD_PRELOADER=yes .. > build.log 2>&1     || BUILD_FAILED=1 report_error "$(pwd)/build.log"
-make >> build.log 2>&1                                                    || BUILD_FAILED=1 report_error "$(pwd)/build.log"
-echo "Done"
+mkdir -p build && cd build
+cmake -DBUILD_UNITTESTS=yes -DBUILD_PRELOADER=yes .. > build.log 2>&1     || BUILD_FAILED=true report_error "$(pwd)/build.log"
+make >> build.log 2>&1                                                    || BUILD_FAILED=true report_error "$(pwd)/build.log"
+if ! $BUILD_FAILED ; then
+  echo "Done"
+fi
 
 #running the unit tests on mac fails because travis osx machines have limited resources
 echo -n "RUNNING UNIT TESTS ...................................................................................... "
-if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-  test/unittests/cvmfs_unittests --gtest_shuffle --gtest_filter="-*Slow:T_Dns.CaresResolverLocalhost:T_Dns.NormalResolverCombined:T_Dns.CaresResolverMany" > unittests.log 2>&1  || UNITTESTS_FAILED=1 report_error "$(pwd)/unittests.log"
-  echo "Done"
+if running_on_linux; then
+  test/unittests/cvmfs_unittests --gtest_shuffle --gtest_filter="-*Slow:T_Dns.CaresResolverLocalhost:T_Dns.NormalResolverCombined:T_Dns.CaresResolverMany" > unittests.log 2>&1  || UNITTESTS_FAILED=true report_error "$(pwd)/unittests.log"
+  if ! $UNITTESTS_FAILED ; then
+    echo "Done"
+  fi
 else
   echo "Skipped"
 fi
 
+
 ################################################################################
+
 if [ $FAILURES -ne 0 ]; then
+  echo ""
   echo ""
   echo ""
   echo "SUMMARY"
   echo "-------"
+  echo ""
   echo -n "CPPLINT ------------ "
   check_failure $CPPLINT_FAILED
   echo -n "BUILD -------------- "
   check_failure $BUILD_FAILED
-  if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+  if running_on_linux; then
     echo -n "UNITTESTS ---------- "
     check_failure $UNITTESTS_FAILED
   fi
 
   echo ""
   echo ""
-  echo "Please check the corresponding logs and fix the errors"
+  echo "Please check the corresponding logs and fix the error(s)"
 fi
 
+exit $FAILURES
 
