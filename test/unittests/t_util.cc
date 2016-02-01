21eae6674310e15649ff694cd37bb068844ee8d7
Jose Molina <jose.molina@cern.ch>
2016-02-01 16:55:06 +0100

    FIX: coverity 63533

--- test/unittests/t_util.cc
+++ test/unittests/t_util.cc
@@ -437,7 +437,8 @@ TEST_F(T_Util, WritePipe) {
   void *buffer_output = scalloc(20, sizeof(char));
   MakePipe(fd);
   WritePipe(fd[1], to_write.c_str(), to_write.length());
-  read(fd[0], buffer_output, to_write.length());
+  int bytes_read = read(fd[0], buffer_output, to_write.length());
+  EXPECT_EQ(bytes_read, to_write.length());
 
   EXPECT_STREQ(to_write.c_str(), static_cast<const char*>(buffer_output));
   ASSERT_DEATH(WritePipe(-1, to_write.c_str(), to_write.length()),
