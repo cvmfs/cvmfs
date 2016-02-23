fb541be5903fb58c93857ea668d2f5eeda125135
Rene Meusel <rene@renemeusel.de>
2016-02-23 16:01:20 +0100

    FIX: enable PIC for libnghttp2

--- externals/nghttp2/src/configureHook.sh
+++ externals/nghttp2/src/configureHook.sh
@@ -5,4 +5,5 @@ SCRIPT_LOCATION=$(cd "$(dirname "$0")"; pwd)
 autoreconf -i
 automake
 autoconf
-CFLAGS="-fPIC" CXXFLAGS="-fPIC" ./configure --prefix=${SCRIPT_LOCATION}
+./configure --prefix=${SCRIPT_LOCATION} \
+            --with-pic=yes
