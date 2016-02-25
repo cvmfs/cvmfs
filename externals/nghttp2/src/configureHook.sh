0f63d9c3a8bcac40022304409afe46de8e7e4bd5
Jose Molina <jose.molina@cern.ch>
2016-02-25 14:35:44 +0100

    FIX: remove inconsistencies when building nghttp2

--- externals/nghttp2/src/configureHook.sh
+++ externals/nghttp2/src/configureHook.sh
@@ -2,9 +2,6 @@
 
 SCRIPT_LOCATION=$(cd "$(dirname "$0")"; pwd)
 
-autoreconf -i
-automake
-autoconf
 ./configure --prefix=${SCRIPT_LOCATION} \
             --with-pic=yes              \
             --enable-lib-only
