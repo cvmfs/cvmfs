dbed5c872d555f7dc370180557efe6c8a3e500e7
Jakob Blomer <jblomer@cern.ch>
2016-05-04 10:27:35 +0200

    install x509 helper on sl6/64 test vm

--- test/cloud_testing/platforms/slc6_x86_64_setup.sh
+++ test/cloud_testing/platforms/slc6_x86_64_setup.sh
@@ -57,6 +57,16 @@ install_rpm $SERVER_PACKAGE
 install_rpm $DEVEL_PACKAGE
 install_rpm $UNITTEST_PACKAGE
 
+# TODO(jblomer): when we get support on more platforms, we might want to get the
+# helper package in a more general way than hard-coding it into the setup script
+x509_helper="https://ecsft.cern.ch/dist/cvmfs/cvmfs-x509-helper/cvmfs-x509-helper-0.9-1.el6.x86_64.rpm"
+echo -n "download X509 helper $x509_helper... "
+wget --no-check-certificate --quiet "$x509_helper" || die "download failed"
+echo "OK"
+echo -n "install x509 helper... "
+sudo rpm -ivh $(basename $x509_helper) > /dev/null || die "install failed"
+echo "OK"
+
 # installing WSGI apache module
 echo "installing python WSGI module..."
 install_from_repo mod_wsgi || die "fail (installing mod_wsgi)"
