12ae34acc5a00d626ae1d075b8a412770adfed58
Jakob Blomer <jblomer@cern.ch>
2016-05-19 15:22:28 +0200

    fix libcvmfs version

--- cvmfs/libcvmfs.h
+++ cvmfs/libcvmfs.h
@@ -11,7 +11,7 @@
  */
 #define LIBCVMFS_VERSION 2
 #define LIBCVMFS_VERSION_MAJOR LIBCVMFS_VERSION
-#define LIBCVMFS_VERSION_MINOR 3
+#define LIBCVMFS_VERSION_MINOR 2
 // Revision Changelog
 // 13: revision introduced
 // 14: fix expand_path for absolute paths, add mountpoint to cvmfs_context
