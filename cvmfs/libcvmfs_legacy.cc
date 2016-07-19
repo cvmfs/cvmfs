1a4eb1c1d58b0779353e962b45e6d0ac9172cb1a
Jakob Blomer <jblomer@cern.ch>
2016-07-19 12:01:26 +0200

    handle manual CVMFS_CACHE_DIR in cvmfs_config

---
+++ cvmfs/libcvmfs_legacy.cc
@@ -0,0 +1,11 @@
+/**
+ * This file is part of the CernVM File System.
+ *
+ * Translates cvmfs_init and cvmfs_repo_attach calls into their contemporary
+ * counterparts.
+ */
+
+#include "cvmfs_config.h"
+#include "libcvmfs.h"
+
+using namespace std;  // NOLINT
