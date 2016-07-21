a13e6650c83df0b29b6b6d012c72331dc4d72114
Jakob Blomer <jblomer@cern.ch>
2016-07-21 10:51:30 +0200

    update documentation of public libcvmfs header

--- cvmfs/libcvmfs.h
+++ cvmfs/libcvmfs.h
@@ -57,7 +57,8 @@ typedef struct OptionsManager cvmfs_option_map;
 #endif
 
 /**
- * Error codes for cvmfs_init_v2.  Mirrors the failure codes in loader.h
+ * Error codes for cvmfs_init_v2() and cvmfs_attach_repo_v2().  Mirrors the
+ * failure codes in loader.h
  */
 typedef enum {
   LIBCVMFS_ERR_OK = 0,
@@ -97,28 +98,61 @@ void cvmfs_set_log_fn( void (*log_fn)(const char *msg) );
 
 
 /**
- * An option map must be created an populated before calling cvmfs_init_v2.
+ * An option map must be created an populated before calling cvmfs_init_v2().
  */
 cvmfs_option_map *cvmfs_options_init();
+/**
+ * Creates a new option map based on an existing one.  Should be used to add
+ * repository specific options passed to cvmfs_attach_repo_v2() based on the
+ * global options that were passed to cvmfs_init_v2().
+ */
 cvmfs_option_map *cvmfs_options_clone(cvmfs_option_map *opts);
+/**
+ * Frees the resources of a cvmfs_options_map, which was created either by a
+ * call to cvmfs_options_init() or by a call to cvmfs_options_clone().
+ */
 void cvmfs_options_fini(cvmfs_option_map *opts);
+/**
+ * Fills a cvmfs_options_map.  Use the same key/value pairs as the configuration
+ * parameters used by the fuse module in /etc/cvmfs/...
+ */
 void cvmfs_options_set(cvmfs_option_map *opts,
                        const char *key, const char *value);
+/**
+ * Removes a key-value pair from a cvmfs_options_map.  The key may or may not
+ * exist before the call.
+ */
 void cvmfs_options_unset(cvmfs_option_map *opts, const char *key);
+/**
+ * Retrieves the value for a given key or NULL of the key does not exist.  If
+ * the result is not NULL, it must be freed by a call to cvmfs_options_free().
+ */
 char *cvmfs_options_get(cvmfs_option_map *opts, const char *key);
+/**
+ * Prints the key-value pairs of cvmfs_option_map line-by-line.  The resulting
+ * string needs to be freed by a call to cvmfs_options_free().
+ */
 char *cvmfs_options_dump(cvmfs_option_map *opts);
+/**
+ * Frees a string returned from cvmfs_options_get() or cvmfs_options_dump().
+ */
 void cvmfs_options_free(char *value);
 
 
 /**
- * Initialize global CVMFS library structures
- * Note: this _must_ be called before anything else in the library.
+ * Initialize global CVMFS library structures.  Legacy.  Use cvmfs_init_v2().
+ * Note: this _must_ be called before any of the following functions.
  *
  * @param[in] options, option1,option2,...
  * \return 0 on success
  */
 int cvmfs_init(char const *options);
 
+
+/**
+ * Initializes the global cvmfs state based on a cvmfs_options_map.  The
+ * cvmfs_fini() function works for both cvmfs_init() and cvmfs_init_v2()
+ */
 cvmfs_errors cvmfs_init_v2(cvmfs_option_map *opts);
 
 
@@ -130,9 +164,7 @@ cvmfs_errors cvmfs_init_v2(cvmfs_option_map *opts);
 void cvmfs_fini();
 
 /**
- * Initialize a CVMFS remote repository.
- *
- * See libcvmfs usage() for possible options.
+ * Initialize a CVMFS remote repository.  Legacy.  Use cvmfs_attach_repo_v2().
  *
  * @param[in] options, option1,option2,...
  * \return 0 on success
@@ -140,42 +172,56 @@ void cvmfs_fini();
 cvmfs_context* cvmfs_attach_repo(char const *options);
 
 /**
- * Creates a new ctx.  On successful return, *ctx is not NULL and must be freed
- * by a call to cvmfs_detach_repo.  Otherwise *ctx is NULL.
+ * Creates a new repository context.  On successful return, *ctx is not NULL and
+ * must be freed by a call to cvmfs_detach_repo().  Otherwise *ctx is NULL.
+ *
+ * The cvmfs_option_map can be the global cvmfs_option_map pointer or a new
+ * pointer retrieved from cvmfs_options_clone().  If it is a new pointer,
+ * ownership can be transferred to the context by a call to
+ * cvmfs_adopt_options() so that the options map is free by cvmfs_detach_repo().
  */
 cvmfs_errors cvmfs_attach_repo_v2(const char *fqrn, cvmfs_option_map *opts,
                                   cvmfs_context **ctx);
 
+/**
+ * Transfer ownership of opts to the context ctx.  The option map is
+ * automatically freed by cvmfs_detach_repo.
+ */
 void cvmfs_adopt_options(cvmfs_context *ctx, cvmfs_option_map *opts);
 
+
 /**
- * Uninitialize a CVMFS remote repository and release all resources for it.
+ * Uninitialize a cvmfs remote repository and release all resources for it.
+ * Works with both cvmfs_attach_repo() and cvmfs_attach_repo_v2().
  */
 void cvmfs_detach_repo(cvmfs_context *ctx);
 
+
 /**
- * Load a new catalog if there is one
+ * Load a new catalog if there is one.  Currently not implemented.
  * \return 0 on success
  */
 int cvmfs_remount(cvmfs_context *ctx);
 
+
 /**
- * Open a file in the CVMFS cache.
+ * Open a file in the cvmfs cache.
  *
  * @param[in] path, path to open (e.g. /dir/file, not /cvmfs/repo/dir/file)
  * \return read-only file descriptor, -1 on failure (sets errno)
  */
 int cvmfs_open(cvmfs_context *ctx, const char *path);
 
+
 /**
- * Reads from a file descriptor returned by cvmfs_open.  File descriptors that
+ * Read from a file descriptor returned by cvmfs_open().  File descriptors that
  * have bit 30 set indicate chunked files.
  */
 ssize_t cvmfs_pread(cvmfs_context *ctx,
                     int fd, void *buf, size_t size, off_t off);
 
 /**
- * Closes a file previously opened with cvmfs_open().
+ * Close a file previously opened with cvmfs_open().
  *
  * @param[in] fd, file descriptor to close
  * \return 0 on success, -1 on failure (sets errno)
@@ -183,10 +229,10 @@ ssize_t cvmfs_pread(cvmfs_context *ctx,
 int cvmfs_close(cvmfs_context *ctx, int fd);
 
 /**
- * Reads a symlink from the catalog.  Environment variables in the
- * symlink that are of the form $(varname) are expanded.  If the
- * provided buffer is not long enough for the null-terminated value of
- * the symlink, -1 is returned and errno is set to ERANGE.
+ * Read a symlink from the catalog.  Environment variables in the symlink that
+ * are of the form $(varname) are expanded according to the process' environment
+ * variables.  If the provided buffer is not long enough for the null-terminated
+ * value of the symlink, -1 is returned and errno is set to ERANGE.
  *
  * @param[in] path, path of symlink (e.g. /dir/file, not /cvmfs/repo/dir/file)
  * @param[out] buf, buffer in which to write the null-terminated value of the symlink
@@ -199,8 +245,8 @@ int cvmfs_readlink(cvmfs_context *ctx,
   size_t size);
 
 /**
- * Get information about a file.  If the file is a symlink,
- * return info about the file it points to, not the symlink itself.
+ * Get information about a file.  If the file is a symlink, return info about
+ * the file it points to, not the symlink itself.
  *
  * @param[in] path, path of file (e.g. /dir/file, not /cvmfs/repo/dir/file)
  * @param[out] st, stat buffer in which to write the result
@@ -209,8 +255,8 @@ int cvmfs_readlink(cvmfs_context *ctx,
 int cvmfs_stat(cvmfs_context *ctx, const char *path, struct stat *st);
 
 /**
- * Get information about a file.  If the file is a symlink,
- * return info about the link, not the file it points to.
+ * Get information about a file.  If the file is a symlink, return info about
+ * the link, not the file it points to.
  *
  * @param[in] path, path of file (e.g. /dir/file, not /cvmfs/repo/dir/file)
  * @param[out] st, stat buffer in which to write the result
