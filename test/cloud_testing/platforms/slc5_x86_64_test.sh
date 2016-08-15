62222aedb5a9ddcf684da9d40fcda843c6c885a4
Jakob Blomer <jblomer@cern.ch>
2016-08-15 13:15:32 +0200

    exclude test that triggers aufs panic on SL5

--- test/cloud_testing/platforms/slc5_x86_64_test.sh
+++ test/cloud_testing/platforms/slc5_x86_64_test.sh
@@ -84,12 +84,14 @@ CVMFS_TEST_CLASS_NAME=ClientIntegrationTests                                  \
 
 
 echo "running CernVM-FS server test cases..."
+# TODO(jblomer): 551-openfds triggers an aufs panic
 CVMFS_TEST_SERVER_CACHE='/srv/cache/server'                                   \
 CVMFS_TEST_CLASS_NAME=ServerIntegrationTests                                  \
 ./run.sh $SERVER_TEST_LOGFILE -o ${SERVER_TEST_LOGFILE}${XUNIT_OUTPUT_SUFFIX} \
                               -x src/518-hardlinkstresstest                   \
                                  src/523-corruptchunkfailover                 \
                                  src/524-corruptmanifestfailover              \
+                                 src/551-openfds                              \
                                  src/585-xattrs                               \
                                  src/600-securecvmfs                          \
                                  src/608-infofile                             \
