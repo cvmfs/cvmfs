a0b6eb44be27a8f5bbd3fb5a108dae7f973e0b01
Dave Dykstra <dwd@fnal.gov>
2016-08-18 09:12:19 -0500

    fix external host set, add external proxy commands

--- cvmfs/talk.cc
+++ cvmfs/talk.cc
@@ -104,6 +104,34 @@ static std::string GenerateHostInfo(download::DownloadManager *manager) {
   return host_str;
 }
 
+static std::string GenerateProxyInfo(download::DownloadManager *manager) {
+  vector< vector<download::DownloadManager::ProxyInfo> > proxy_chain;
+  unsigned active_group;
+  unsigned fallback_group;
+
+  manager->GetProxyInfo(&proxy_chain, &active_group, &fallback_group);
+  string proxy_str;
+  if (proxy_chain.size()) {
+    proxy_str += "Load-balance groups:\n";
+    for (unsigned i = 0; i < proxy_chain.size(); ++i) {
+      vector<string> urls;
+      for (unsigned j = 0; j < proxy_chain[i].size(); ++j) {
+        urls.push_back(proxy_chain[i][j].Print());
+      }
+      proxy_str +=
+        "[" + StringifyInt(i) + "] " + JoinStrings(urls, ", ") + "\n";
+    }
+    proxy_str += "Active proxy: [" + StringifyInt(active_group) + "] " +
+                 proxy_chain[active_group][0].url + "\n";
+    if (fallback_group < proxy_chain.size())
+      proxy_str += "First fallback group: [" +
+                   StringifyInt(fallback_group) + "]\n";
+  } else {
+    proxy_str = "No proxies defined\n";
+  }
+  return proxy_str;
+}
+
 static void *MainTalk(void *data __attribute__((unused))) {
   LogCvmfs(kLogTalk, kLogDebug, "talk thread started");
 
@@ -292,11 +320,11 @@ static void *MainTalk(void *data __attribute__((unused))) {
       } else if (line == "host switch") {
         cvmfs::download_manager_->SwitchHost();
         Answer(con_fd, "OK\n");
-      } else if (line.substr(0, 16) == "external host set") {
-        if (line.length() < 18) {
+      } else if (line.substr(0, 17) == "external host set") {
+        if (line.length() < 19) {
           Answer(con_fd, "Usage: external host set <URL>\n");
         } else {
-          const std::string host = line.substr(17);
+          const std::string host = line.substr(18);
           cvmfs::external_download_manager_->SetHostChain(host);
           Answer(con_fd, "OK\n");
         }
@@ -308,40 +336,25 @@ static void *MainTalk(void *data __attribute__((unused))) {
           cvmfs::download_manager_->SetHostChain(hosts);
           Answer(con_fd, "OK\n");
         }
+      } else if (line == "external proxy info") {
+        Answer(con_fd, GenerateProxyInfo(cvmfs::external_download_manager_));
       } else if (line == "proxy info") {
-        vector< vector<download::DownloadManager::ProxyInfo> > proxy_chain;
-        unsigned active_group;
-        unsigned fallback_group;
-        cvmfs::download_manager_->GetProxyInfo(
-          &proxy_chain, &active_group, &fallback_group);
-
-        string proxy_str;
-        if (proxy_chain.size()) {
-          proxy_str += "Load-balance groups:\n";
-          for (unsigned i = 0; i < proxy_chain.size(); ++i) {
-            vector<string> urls;
-            for (unsigned j = 0; j < proxy_chain[i].size(); ++j) {
-              urls.push_back(proxy_chain[i][j].Print());
-            }
-            proxy_str +=
-              "[" + StringifyInt(i) + "] " + JoinStrings(urls, ", ") + "\n";
-          }
-          proxy_str += "Active proxy: [" + StringifyInt(active_group) + "] " +
-                       proxy_chain[active_group][0].url + "\n";
-          if (fallback_group < proxy_chain.size())
-            proxy_str += "First fallback group: [" +
-                         StringifyInt(fallback_group) + "]\n";
-        } else {
-          proxy_str = "No proxies defined\n";
-        }
-
-        Answer(con_fd, proxy_str);
+        Answer(con_fd, GenerateProxyInfo(cvmfs::download_manager_));
       } else if (line == "proxy rebalance") {
         cvmfs::download_manager_->RebalanceProxies();
         Answer(con_fd, "OK\n");
       } else if (line == "proxy group switch") {
         cvmfs::download_manager_->SwitchProxyGroup();
         Answer(con_fd, "OK\n");
+      } else if (line.substr(0, 18) == "external proxy set") {
+        if (line.length() < 20) {
+          Answer(con_fd, "Usage: external proxy set <proxy list>\n");
+        } else {
+          string external_proxies = line.substr(19);
+          cvmfs::external_download_manager_->SetProxyChain(
+            external_proxies, "", download::DownloadManager::kSetProxyRegular);
+          Answer(con_fd, "OK\n");
+        }
       } else if (line.substr(0, 9) == "proxy set") {
         if (line.length() < 11) {
           Answer(con_fd, "Usage: proxy set <proxy list>\n");
