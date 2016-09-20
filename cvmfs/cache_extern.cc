d91c402b9e67afad67f334c7ed221bade378c578
Jakob Blomer <jblomer@cern.ch>
2016-09-20 14:27:25 +0200

    add external cache manager stubs

---
+++ cvmfs/cache_extern.cc
@@ -0,0 +1,8 @@
+/**
+ * This file is part of the CernVM File System.
+ */
+#include "cvmfs_config.h"
+#include "cache_extern.h"
+
+namespace cache {
+}  // namespace cache
---
+++ cvmfs/cache_extern.h
@@ -0,0 +1,10 @@
+/**
+ * This file is part of the CernVM File System.
+ */
+#ifndef CVMFS_CACHE_EXTERN_H_
+#define CVMFS_CACHE_EXTERN_H_
+
+namespace cache {
+}  // namespace cache
+
+#endif  // CVMFS_CACHE_EXTERN_H_
