1d4192a98458e2df6fe1bfd710485a23eb33e3ab
Radu Popescu <mail@radupopescu.net>
2016-09-29 19:14:49 +0200

    Updating cvmfs_lease_path_util tests

--- gateway/apps/cvmfs_lease/src/cvmfs_lease_path_util.erl
+++ gateway/apps/cvmfs_lease/src/cvmfs_lease_path_util.erl
@@ -9,17 +9,21 @@
 
 -module(cvmfs_lease_path_util).
 
--export([is_subpath/2]).
+-export([are_overlapping/2]).
 
 %%--------------------------------------------------------------------
 %% @doc
-%% Checks if Path1 is a subpath of Path2
+%% Checks if Path1 and Path2 overlap.
 %%
-%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
+%% Two paths overlap if one is a subpath of the other(foo/bar/baz and
+%% foo/bar)
+%%
+%% @spec are_overlapping(Path1, Path2) -> boolean().
 %% @end
 %%--------------------------------------------------------------------
--spec is_subpath(binary(), binary()) -> boolean().
-is_subpath(Path1, Path2) when is_binary(Path1), is_binary(Path2),
-                              size(Path1) > 0, size(Path2) > 0 ->
-    binary:longest_common_prefix([Path1, Path2]) =:= size(Path2).
+
+are_overlapping(Path1, Path2) when is_binary(Path1), is_binary(Path2),
+                                   size(Path1) > 0, size(Path2) > 0 ->
+    CommonPrefixLength = binary:longest_common_prefix([Path1, Path2]),
+    (CommonPrefixLength =:= size(Path1)) orelse (CommonPrefixLength =:= size(Path2)).
 
