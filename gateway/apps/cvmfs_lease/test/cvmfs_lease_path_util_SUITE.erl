91c1e1686cc2c41ac8f026914f9962a07c696e7d
Radu Popescu <mail@radupopescu.net>
2016-09-29 21:59:16 +0200

    Place cvmfs_lease_path_util tests under ct harness

--- gateway/apps/cvmfs_lease/test/cvmfs_lease_path_util_SUITE.erl
+++ gateway/apps/cvmfs_lease/test/cvmfs_lease_path_util_SUITE.erl
@@ -7,15 +7,28 @@
 %%%
 %%%-------------------------------------------------------------------
 
--module(prop_cvmfs_lease_path_util).
+-module(cvmfs_lease_path_util_SUITE).
 
+-include_lib("common_test/include/ct.hrl").
 -include_lib("proper/include/proper.hrl").
 -include_lib("eunit/include/eunit.hrl").
+
+-export([all/0, run_eunit/1]).
+
+%% Tests description
+
+all() ->
+    [run_eunit].
+
+run_eunit(_Config) ->
+    ok = eunit:test(?MODULE).
+
+%% Test cases
 
 non_empty_binary() ->
     non_empty(binary()).
 
-%% Two paths of the form P1 and P2P1 (e.g. foo and foo/bar) overlap
+%% Two paths of the form P2 and P2P1 (e.g. foo and foo/bar) overlap
 prop_are_overlapping_p2_p2p1() ->
     ?FORALL({Path1, Path2}
            ,{non_empty_binary(), non_empty_binary()}
@@ -26,3 +39,7 @@ prop_are_overlapping_p2p1_p2() ->
     ?FORALL({Path1, Path2}
            ,{non_empty_binary(), non_empty_binary()}
            ,cvmfs_lease_path_util:are_overlapping(<<Path2/binary,Path1/binary>>,Path2)).
+
+% Test cases
+overlap_test() ->
+    ?assertEqual(true, proper:quickcheck(prop_are_overlapping_p2_p2p1())).
