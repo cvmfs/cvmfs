8cc98b4fa603c03bc6c5a373a8db7c9e5a3b1b18
Radu Popescu <mail@radupopescu.net>
2016-09-29 19:14:49 +0200

    Updating cvmfs_lease_path_util tests

--- apps/cvmfs_lease/test/prop_cvmfs_lease_path_util.erl
+++ apps/cvmfs_lease/test/prop_cvmfs_lease_path_util.erl
@@ -10,13 +10,19 @@
 -module(prop_cvmfs_lease_path_util).
 
 -include_lib("proper/include/proper.hrl").
+-include_lib("eunit/include/eunit.hrl").
 
 non_empty_binary() ->
     non_empty(binary()).
 
-%% For any pair of paths of the form P1 and P2P1 (e.g. foo and foo/bar) P1 is a subpath of P2P1
-prop_is_subpath_p1_p2p1() ->
-    io:format("Describe test case"),
+%% Two paths of the form P1 and P2P1 (e.g. foo and foo/bar) overlap
+prop_are_overlapping_p2_p2p1() ->
     ?FORALL({Path1, Path2}
            ,{non_empty_binary(), non_empty_binary()}
-           ,cvmfs_lease_path_util:is_subpath(<<Path1/binary,Path2/binary>>,Path1)).
+           ,cvmfs_lease_path_util:are_overlapping(Path2, <<Path2/binary,Path1/binary>>)).
+
+%% Two paths of the form P2P1 and P2 (e.g. foo/bar and foo) overlap
+prop_are_overlapping_p2p1_p2() ->
+    ?FORALL({Path1, Path2}
+           ,{non_empty_binary(), non_empty_binary()}
+           ,cvmfs_lease_path_util:are_overlapping(<<Path2/binary,Path1/binary>>,Path2)).
