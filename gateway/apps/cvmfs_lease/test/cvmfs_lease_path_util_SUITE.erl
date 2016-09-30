f10568c1ad802b085fdb4ec84a68425688119deb
Radu Popescu <mail@radupopescu.net>
2016-09-30 09:33:54 +0200

    cvmfs_lease_path_util_SUITE - Simplify generation of input data

--- gateway/apps/cvmfs_lease/test/cvmfs_lease_path_util_SUITE.erl
+++ gateway/apps/cvmfs_lease/test/cvmfs_lease_path_util_SUITE.erl
@@ -13,14 +13,14 @@
 -include_lib("proper/include/proper.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
--export([all/0, run_eunit/1]).
+-export([all/0, qc_path_overlap_test/1]).
 
 %% Tests description
 
 all() ->
-    [run_eunit].
+    [qc_path_overlap_test].
 
-run_eunit(_Config) ->
+qc_path_overlap_test(_Config) ->
     ok = eunit:test(?MODULE).
 
 %% Test cases
@@ -28,18 +28,18 @@ run_eunit(_Config) ->
 non_empty_binary() ->
     non_empty(binary()).
 
-%% Two paths of the form P2 and P2P1 (e.g. foo and foo/bar) overlap
-prop_are_overlapping_p2_p2p1() ->
-    ?FORALL({Path1, Path2}
-           ,{non_empty_binary(), non_empty_binary()}
-           ,cvmfs_lease_path_util:are_overlapping(Path2, <<Path2/binary,Path1/binary>>)).
+%% Generate a pair of non_empty binaries of the form {B1, B1B2} or {B1B2, B1}
+ne_binary_pair() ->
+    ?LET({B1, B2},
+         {non_empty_binary(), non_empty_binary()},
+         proper_types:oneof([{<<B1/binary,B2/binary>>, B1} ,{B1, <<B1/binary,B2/binary>>}])).
 
-%% Two paths of the form P2P1 and P2 (e.g. foo/bar and foo) overlap
-prop_are_overlapping_p2p1_p2() ->
+%% Two paths of the form P2 and P2P1 (e.g. foo and foo/bar) overlap
+prop_are_overlapping() ->
     ?FORALL({Path1, Path2}
-           ,{non_empty_binary(), non_empty_binary()}
-           ,cvmfs_lease_path_util:are_overlapping(<<Path2/binary,Path1/binary>>,Path2)).
+           ,ne_binary_pair()
+           ,cvmfs_lease_path_util:are_overlapping(Path1, Path2)).
 
 % Test cases
 overlap_test() ->
-    ?assertEqual(true, proper:quickcheck(prop_are_overlapping_p2_p2p1())).
+    ?assertEqual(true, proper:quickcheck(prop_are_overlapping())).
