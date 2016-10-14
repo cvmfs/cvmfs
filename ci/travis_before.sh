9b70f57a9d6b0d5a334ca55af0ab309c2e56e663
Radu Popescu <mail@radupopescu.net>
2016-10-14 16:48:57 +0200

    Updating Travis-CI configuration

--- ci/travis_before.sh
+++ ci/travis_before.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 if [ ! -f ~/bin/rebar3 ]; then
+    # Rebar3
     mkdir -p ~/bin
     wget -O ~/bin/rebar3 https://s3.amazonaws.com/rebar3/rebar3
     chmod +x ~/bin/rebar3
+
+    # Libsodium
+    wget https://github.com/jedisct1/libsodium/releases/download/1.0.11/libsodium-1.0.11.tar.gz
+    tar xzf libsodium-1.0.11.tar.gz && cd libsodium-1.0.1 && ./configure && make && make install
 fi
 
