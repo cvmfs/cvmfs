2e2506a94b922c8603aea16e66e192a393a5bc85
Radu Popescu <mail@radupopescu.net>
2016-10-19 22:20:39 +0200

    Fix flycheck warning re: unknown parse transform

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,5 @@
   (flycheck-erlang-include-path . ("../include"
                                    "../../../_build/test/lib"))
   (flycheck-erlang-library-path . ("../../../_build/default/lib/lager/ebin"
+                                   "../../../_build/test/lib/proper/ebin"
                                    "../../../_build/test/lib"))))
