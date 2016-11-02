1d8523a60c66d855bbe80f0000c8f5b352bcdb1b
Jakob Blomer <jblomer@cern.ch>
2016-11-02 18:50:31 +0100

    add compilation unit for creating virtual tag folder

---
+++ cvmfs/history_fs.cc
@@ -0,0 +1,5 @@
+/**
+ * This file is part of the CernVM File System.
+ */
+ #include "cvmfs_config.h"
+ #include "history_fs.h"
---
+++ cvmfs/history_fs.h
@@ -0,0 +1,7 @@
+/**
+ * This file is part of the CernVM File System.
+ */
+#ifndef CVMFS_HISTORY_FS_H_
+#define CVMFS_HISTORY_FS_H_
+
+#endif  // CVMFS_HISTORY_FS_H_
