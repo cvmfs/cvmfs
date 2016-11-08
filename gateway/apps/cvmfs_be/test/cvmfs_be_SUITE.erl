e0a52b8e79e17ab48e38f1112758a543ea169ef8
Radu Popescu <mail@radupopescu.net>
2016-11-08 13:22:18 +0100

    Rename cvmfs_proc -> cvmfs_be

--- gateway/apps/cvmfs_be/test/cvmfs_be_SUITE.erl
+++ gateway/apps/cvmfs_be/test/cvmfs_be_SUITE.erl
@@ -7,7 +7,7 @@
 %%%
 %%%-------------------------------------------------------------------
 
--module(cvmfs_proc_SUITE).
+-module(cvmfs_be_SUITE).
 
 -include_lib("common_test/include/ct.hrl").
 -include_lib("proper/include/proper.hrl").
@@ -66,15 +66,15 @@ init_per_suite(Config) ->
     ok = application:set_env(cvmfs_lease, max_lease_time, MaxLeaseTime),
     {ok, _} = application:ensure_all_started(cvmfs_lease),
 
-    ok = application:load(cvmfs_proc),
-    {ok, _} = application:ensure_all_started(cvmfs_proc),
+    ok = application:load(cvmfs_be),
+    {ok, _} = application:ensure_all_started(cvmfs_be),
 
     lists:flatten([[{max_lease_time, MaxLeaseTime}]
                   ,Config]).
 
 end_per_suite(_Config) ->
-    application:stop(cvmfs_proc),
-    application:unload(cvmfs_proc),
+    application:stop(cvmfs_be),
+    application:unload(cvmfs_be),
     application:stop(cvmfs_lease),
     application:unload(cvmfs_lease),
     application:stop(cvmfs_auth),
@@ -95,22 +95,22 @@ end_per_testcase(_TestCase, _Config) ->
 % Valid user and valid path should be accepted
 valid_user_valid_path(_Config) ->
     {VUser, VPath} = valid_user_and_path(),
-    {ok, _} = cvmfs_proc:new_lease(VUser, VPath).
+    {ok, _} = cvmfs_be:new_lease(VUser, VPath).
 % Invalid user and valid path should be rejected
 invalid_user_valid_path(_Config) ->
     {VUser, VPath} = valid_user_and_path(),
     {IUser, _} = invalid_user_and_path(VUser, VPath),
-    {error, invalid_user} = cvmfs_proc:new_lease(IUser, VPath).
+    {error, invalid_user} = cvmfs_be:new_lease(IUser, VPath).
 % Valid user and invalid path should be rejected
 valid_user_invalid_path(_Config) ->
     {VUser, VPath} = valid_user_and_path(),
     {_, IPath} = invalid_user_and_path(VUser, VPath),
-    {error, invalid_path} = cvmfs_proc:new_lease(VUser, IPath).
+    {error, invalid_path} = cvmfs_be:new_lease(VUser, IPath).
 % Invalid user and invalid path should be rejected with {error, invalid_user}
 invalid_user_invalid_path(_Config) ->
     {VUser, VPath} = valid_user_and_path(),
     {IUser, IPath} = invalid_user_and_path(VUser, VPath),
-    {error, invalid_user} = cvmfs_proc:new_lease(IUser, IPath).
+    {error, invalid_user} = cvmfs_be:new_lease(IUser, IPath).
 
 % Submit payload
 % Normal lease check
@@ -118,35 +118,35 @@ lease_success(_Config) ->
     % Start with a valid user and path and receive a valid lease token
     {User, Path} = valid_user_and_path(),
     Payload = <<"placeholder_for_a_real_payload">>,
-    {ok, Token} = cvmfs_proc:new_lease(User, Path),
+    {ok, Token} = cvmfs_be:new_lease(User, Path),
     % Followup with a payload submission (final = false)
-    {ok, payload_added} = cvmfs_proc:submit_payload(User, Token, Payload, false),
+    {ok, payload_added} = cvmfs_be:submit_payload(User, Token, Payload, false),
     % Submit final payload
-    {ok, payload_added, lease_ended} = cvmfs_proc:submit_payload(User, Token, Payload, true),
+    {ok, payload_added, lease_ended} = cvmfs_be:submit_payload(User, Token, Payload, true),
     % After the lease has been closed, the token should be rejected
-    {error, lease_not_found} = cvmfs_proc:submit_payload(User, Token, Payload, true).
+    {error, lease_not_found} = cvmfs_be:submit_payload(User, Token, Payload, true).
 
 % Attempt to submit a payload without first obtaining a token
 submission_with_invalid_token_fails(_Config) ->
     {User, _} = valid_user_and_path(),
     Token = <<"invalid_token">>,
     Payload = <<"placeholder">>,
-    {error, invalid_macaroon} = cvmfs_proc:submit_payload(User, Token, Payload, false).
+    {error, invalid_macaroon} = cvmfs_be:submit_payload(User, Token, Payload, false).
 
 % Start a valid lease, make submission after the token has expired
 submission_with_expired_token_fails(Config) ->
     {User, Path} = valid_user_and_path(),
     Payload = <<"placeholder">>,
-    {ok, Token} = cvmfs_proc:new_lease(User, Path),
+    {ok, Token} = cvmfs_be:new_lease(User, Path),
     ct:sleep(?config(max_lease_time, Config)),
-    {error, lease_expired} = cvmfs_proc:submit_payload(User, Token, Payload, false).
+    {error, lease_expired} = cvmfs_be:submit_payload(User, Token, Payload, false).
 
 submission_with_different_user_name(_Config) ->
     {VUser, VPath} = valid_user_and_path(),
     {IUser, _} = invalid_user_and_path(VUser, VPath),
     Payload = <<"placeholder">>,
-    {ok, Token} = cvmfs_proc:new_lease(VUser, VPath),
-    {error, invalid_user} = cvmfs_proc:submit_payload(IUser, Token, Payload, false).
+    {ok, Token} = cvmfs_be:new_lease(VUser, VPath),
+    {error, invalid_user} = cvmfs_be:submit_payload(IUser, Token, Payload, false).
 
 
 %% Properties
