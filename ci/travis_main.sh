76fc9ee6596dd47f1b623ed38102835f598cf25e
Radu Popescu <mail@radupopescu.net>
2016-11-08 17:11:55 +0100

    Do "rebar3 dialyzer" in CI

--- ci/travis_main.sh
+++ ci/travis_main.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -it -v "${PWD}":/mnt radupopescu/erlang-libsodium:19 sh -c "cd /mnt && rebar3 ct"
+docker run --rm -it -v "${PWD}":/mnt radupopescu/erlang-libsodium:19 sh -c "cd /mnt && rebar3 dialyzer && rebar3 ct"
