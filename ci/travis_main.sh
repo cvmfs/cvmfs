b53d544c06488bb77792313427d540a7361a7314
Radu Popescu <mail@radupopescu.net>
2016-11-09 10:18:16 +0100

    Updating Travis-CI config to match Gitlab-CI's

--- ci/travis_main.sh
+++ ci/travis_main.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker run --rm -it -v "${PWD}":/mnt radupopescu/erlang-libsodium:19 sh -c "cd /mnt && rebar3 dialyzer && rebar3 ct"
+docker run --rm -it -v "${PWD}":/mnt radupopescu/erlang-libsodium:19 \
+       sh -c "cd /mnt && rebar3 release && rebar3 as test dialyzer,ct && rebar3 as prod tar"
