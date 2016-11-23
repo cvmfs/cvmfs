5c077771410f41f5ab2fbe35449b5b03e94da9c1
Radu Popescu <mail@radupopescu.net>
2016-11-23 19:15:38 +0100

    Adding `make_cvmfs_server.sh` builder script
    This script produces the final `cvmfs_server` script that can be
    packaged.

---
+++ cvmfs/make_cvmfs_server.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+#
+# This file is part of the CernVM File System
+# This script takes care of creating, removing, and maintaining repositories
+# on a Stratum 0/1 server
+
+# Create the `cvmfs_server` script
+
+
+OUTPUT_FILE="cvmfs_server"
+
+# Make an fresh OUTPUT_FILE to begin
+echo '
+#!/bin/sh
+#
+# This file is part of the CernVM File System
+# This script takes care of creating, removing, and maintaining repositories
+# on a Stratum 0/1 server
+
+
+' > $OUTPUT_FILE
+
+# List the components that make up the resulting file
+COMPONENTS="\
+    server/cvmfs_server_prelude.sh
+    server/cvmfs_server_util.sh
+    server/cvmfs_server_common.sh
+    server/cvmfs_server_health_check.sh
+    server/cvmfs_server_compat.sh
+    server/cvmfs_server_transaction.sh
+    server/cvmfs_server_abort.sh
+    server/cvmfs_server_publish.sh
+    server/cvmfs_server_coda.sh
+    " 
+
+# Includes COMPONENTS in OUTPUT_FILE
+for c in $COMPONENTS ; do
+    cat $c >> $OUTPUT_FILE
+done
+
+# Make OUTPUT_FILE executable
+chmod +x $OUTPUT_FILE
