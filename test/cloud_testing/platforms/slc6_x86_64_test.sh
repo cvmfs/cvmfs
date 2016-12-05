213c449bf1ca93c47d335c4cea795163f0f7a7fa
Jakob Blomer <jblomer@cern.ch>
2016-12-05 15:40:48 +0100

    host failover on corrupted data (after no-cache attempt)

--- test/cloud_testing/platforms/slc6_x86_64_test.sh
+++ test/cloud_testing/platforms/slc6_x86_64_test.sh
@@ -71,7 +71,6 @@ CVMFS_TEST_SERVER_CACHE='/srv/cache'                                          \
 CVMFS_TEST_CLASS_NAME=ServerIntegrationTests                                  \
 ./run.sh $SERVER_TEST_LOGFILE -o ${SERVER_TEST_LOGFILE}${XUNIT_OUTPUT_SUFFIX} \
                               -x src/518-hardlinkstresstest                   \
-                                 src/523-corruptchunkfailover                 \
                                  src/524-corruptmanifestfailover              \
                                  src/585-xattrs                               \
                                  --                                           \
