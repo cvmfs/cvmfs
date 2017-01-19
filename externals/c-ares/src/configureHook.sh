8402f94ff9c4773d5b466dd94e8cb8cb38cb8690
Radu Popescu <mail@radupopescu.net>
2017-01-19 10:41:47 +0100

    Revert to using "-fvisibility=hidden" when building c-ares
    * c-ares is now build with the "-fvisibility=hidden" flag, as before
    * in the configure step, specifying "--enable-shared=no", to only build
      the static library
    * In the build step, only building the `libcares.la` target

--- externals/c-ares/src/configureHook.sh
+++ externals/c-ares/src/configureHook.sh
@@ -4,4 +4,4 @@
 #              Please install XCode and run `xcode-select --install` afterwards.
 #              Also remember to start XCode at least once and agree to the EULA.
 
-sh configure LDFLAGS="$LDFLAGS -rdynamic" CPPFLAGS="$CPPFLAGS -D_FILE_OFFSET_BITS=64" CFLAGS="$CFLAGS -fno-strict-aliasing -fasynchronous-unwind-tables -fno-omit-frame-pointer -fno-optimize-sibling-calls -fPIC"
+sh configure --enable-shared=no LDFLAGS="$LDFLAGS -rdynamic" CPPFLAGS="$CPPFLAGS -D_FILE_OFFSET_BITS=64" CFLAGS="$CFLAGS -fno-strict-aliasing -fasynchronous-unwind-tables -fno-omit-frame-pointer -fno-optimize-sibling-calls -fvisibility=hidden -fPIC"
