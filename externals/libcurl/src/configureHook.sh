59a8b3eb21dcfda7d433d014c405821f4a60ae34
Radu Popescu <rpopescu@cern.ch>
2017-01-23 14:12:59 +0100

    Fixing build and install of libressl

--- externals/libcurl/src/configureHook.sh
+++ externals/libcurl/src/configureHook.sh
@@ -6,6 +6,11 @@ cd $cares_location
 sh makeHook.sh
 cd $cdir
 
+ssl_location="$(pwd)/../build_ssl"
+cd $ssl_location
+sh makeHook.sh
+cd $cdir
+
 sh configure CPPFLAGS="$CPPFLAGS -I${PWD}/${cares_location} -D_FILE_OFFSET_BITS=64" \
   LDFLAGS="$LDFLAGS -L${PWD}/${cares_location}/.libs -rdynamic" \
   CFLAGS="$CFLAGS $CVMFS_BASE_C_FLAGS -fvisibility=hidden -fPIC" \
@@ -39,7 +44,7 @@ sh configure CPPFLAGS="$CPPFLAGS -I${PWD}/${cares_location} -D_FILE_OFFSET_BITS=
   --disable-tls-srp \
   --disable-ntlm-wb \
   --disable-unix-sockets \
-  --with-ssl \
+  --with-ssl=${ssl_location}/ssl_install \
   --without-winssl \
   --without-darwinssl \
   --without-gnutls \
--- externals/ssl/src/makeHook.sh
+++ externals/ssl/src/makeHook.sh
@@ -2,5 +2,6 @@
 
 cd build
 make clean
-CFLAGS="$CVMFS_BASE_C_FLAGS -fPIC" CXXFLAGS="$CVMFS_BASE_CXX_FLAGS -fPIC" make
-strip -S libssl.a
+CFLAGS="$CVMFS_BASE_C_FLAGS -fPIC" CXXFLAGS="$CVMFS_BASE_CXX_FLAGS -fPIC" make install
+strip -S ../ssl_install/lib/libssl.a
+strip -S ../ssl_install/lib/libcrypto.a
