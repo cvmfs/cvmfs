5b75a82b3d0faf1cb40c337acaa4874fa0155bb0
Radu Popescu <mail@radupopescu.net>
2017-01-26 16:36:37 +0100

    Enable selective rebuild of external libraries

--- bootstrap.sh
+++ bootstrap.sh
@@ -38,10 +38,7 @@ externals_build_dir=$EXTERNALS_BUILD_LOCATION
 externals_install_dir=$EXTERNALS_INSTALL_LOCATION
 repo_root=$(pwd)
 
-# check if bootstrapping already happened
-if [ -f "$externals_install_dir/.bootstrapDone" ]; then
-  exit 0
-fi
+
 
 print_hint() {
   local msg="$1"
@@ -131,103 +128,135 @@ replace_in_external() {
   cd $cdir
 }
 
+build_lib() {
+  local lib_name=$1
+  rm -rf $externals_build_dir/build_${lib_name}
+  case ${lib_name} in
+    libcurl)
+      # (only on Mac)
+      if [ x"$(uname)" = x"Darwin" ]; then
+          rm -rf $externals_build_dir/build_ssl
+          do_extract "ssl" "libressl-${SSL_VERSION}.tar.gz"
+          do_build "ssl"
+      fi
+
+      rm -rf $externals_build_dir/build_c-ares
+      do_extract "c-ares" "c-ares-${CARES_VERSION}.tar.gz"
+      do_build "c-ares"      
+
+      do_extract "libcurl" "curl-${CURL_VERSION}.tar.gz"
+      patch_external "libcurl" "reenable_poll_darwin.patch"
+      do_build "libcurl"
+      ;;
+    pacparser)
+      do_extract "pacparser"    "pacparser-${PACPARSER_VERSION}.tar.gz"
+      patch_external "pacparser"   "fix_find_proxy_ex.patch"            \
+                                   "fix_cflags.patch"
+      do_build "pacparser"
+      ;;
+    zlib)
+      do_extract "zlib"         "zlib-${ZLIB_VERSION}.tar.gz"
+      do_build "zlib"
+      ;;
+    sparsehash)
+      do_extract "sparsehash"   "sparsehash-${SPARSEHASH_VERSION}.tar.gz"
+      patch_external "sparsehash"  "fix_sl4_compilation.patch"          \
+                                  "fix_warning_gcc48.patch"
+      replace_in_external "sparsehash"  "config.guess.latest" "config.guess"
+      replace_in_external "sparsehash"  "config.sub.latest" "config.sub"
+      do_build "sparsehash"
+      ;;
+    leveldb)
+      do_extract "leveldb"      "leveldb-${LEVELDB_VERSION}.tar.gz"
+      patch_external "leveldb"     "dont_search_snappy.patch"           \
+                                  "dont_search_tcmalloc.patch"         \
+                                  "arm64_memory_barrier.patch"
+      do_build "leveldb"
+      ;;
+    googletest)
+      do_extract "googletest"   "gtest-${GOOGLETEST_VERSION}.tar.gz"
+      replace_in_external "googletest"  "config.guess.latest" "build-aux/config.guess"
+      replace_in_external "googletest"  "config.sub.latest" "build-aux/config.sub"
+      do_build "googletest"
+      ;;
+    libgeoip)
+      do_extract "libgeoip" "GeoIP-${LIBGEOIP_VERSION}.tar.gz"
+      replace_in_external "libgeoip" "config.guess.latest" "config.guess"
+      replace_in_external "libgeoip" "config.sub.latest" "config.sub"
+      do_build "libgeoip"
+
+      rm -rf $externals_build_dir/build_python-geoip
+      do_extract "python-geoip" "GeoIP-${PYTHON_GEOIP_VERSION}.tar.gz"
+      do_build "python-geoip"
+      ;;
+    tbb)
+      do_extract "tbb"          "tbb-${TBB_VERSION}.tar.gz"
+      patch_external "tbb"         "custom_library_suffix.patch"        \
+                                  "symlink_to_build_directories.patch" \
+                                  "32bit_mock.patch"
+      do_build "tbb"
+      ;;
+    protobuf)
+      do_extract "protobuf"     "protobuf-${PROTOBUF_VERSION}.tar.bz2"
+      do_build "protobuf"
+      ;;
+    googlebench)
+      do_copy "googlebench"
+      do_build "googlebench"
+      ;;
+    sqlite3)
+      do_copy "sqlite3"
+      do_build "sqlite3"
+      ;;
+    vjson)
+      do_copy "vjson"
+      patch_external "vjson"       "missing_include.patch"
+      do_build "vjson"
+      ;;
+    sha2)
+      do_copy "sha2"
+      do_build "sha2"
+      ;;
+    sha3)
+      do_copy "sha3"
+      do_build "sha3"
+      ;;
+    *)
+      echo "Unknown library name. Exiting."
+      exit 1
+  esac
+}
+
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-rm -rf $externals_build_dir/* && mkdir -p $externals_build_dir
-rm -rf $externals_install_dir/*
+# Build a list of libs that need to be built
+missing_libs="libcurl pacparser zlib sparsehash leveldb googletest libgeoip tbb protobuf googlebench sqlite3 vjson sha2 sha3"
+
+if [ -f $externals_install_dir/.bootstrapDone ]; then
+  existing_libs=$(cat $externals_install_dir/.bootstrapDone)
+  for l in $existing_libs; do
+    if [ x"$l" != x ]; then
+      missing_libs=$(echo $missing_libs | sed -e "s/$l//")
+    fi
+  done
+fi
+
+mkdir -p $externals_build_dir
 mkdir -p $externals_install_dir/include
 mkdir -p $externals_install_dir/lib
 
-# Configure, build and install external libraries. The library order matters!
+rm -f $externals_install_dir/.bootstrapDone
+for l in $existing_libs; do
+  echo $l >> $externals_install_dir/.bootstrapDone
+done
 
-# SSL (only on Mac)
-if [ x"$(uname)" = x"Darwin" ]; then
-    do_extract "ssl" "libressl-${SSL_VERSION}.tar.gz"
-    do_build "ssl"
+if [ x"$missing_libs" != x ]; then
+  echo "Bulding libraries: $missing_libs"
 fi
 
-# c-ares
-do_extract "c-ares" "c-ares-${CARES_VERSION}.tar.gz"
-do_build "c-ares"
-
-# libcurl
-do_extract "libcurl" "curl-${CURL_VERSION}.tar.gz"
-patch_external "libcurl" "reenable_poll_darwin.patch"
-do_build "libcurl"
-
-# pacparser
-do_extract "pacparser"    "pacparser-${PACPARSER_VERSION}.tar.gz"
-patch_external "pacparser"   "fix_find_proxy_ex.patch"            \
-                             "fix_cflags.patch"
-do_build "pacparser"
-
-# zlib                             
-do_extract "zlib"         "zlib-${ZLIB_VERSION}.tar.gz"
-do_build "zlib"
-
-# # sparsehash
-do_extract "sparsehash"   "sparsehash-${SPARSEHASH_VERSION}.tar.gz"
-patch_external "sparsehash"  "fix_sl4_compilation.patch"          \
-                             "fix_warning_gcc48.patch"
-replace_in_external "sparsehash"  "config.guess.latest" "config.guess"
-replace_in_external "sparsehash"  "config.sub.latest" "config.sub"
-do_build "sparsehash"
-
-# leveldb
-do_extract "leveldb"      "leveldb-${LEVELDB_VERSION}.tar.gz"
-patch_external "leveldb"     "dont_search_snappy.patch"           \
-                             "dont_search_tcmalloc.patch"         \
-                             "arm64_memory_barrier.patch"
-do_build "leveldb"
-
-# googletest                             
-do_extract "googletest"   "gtest-${GOOGLETEST_VERSION}.tar.gz"
-replace_in_external "googletest"  "config.guess.latest" "build-aux/config.guess"
-replace_in_external "googletest"  "config.sub.latest" "build-aux/config.sub"
-do_build "googletest"
-
-# libgeoip
-do_extract "libgeoip" "GeoIP-${LIBGEOIP_VERSION}.tar.gz"
-replace_in_external "libgeoip" "config.guess.latest" "config.guess"
-replace_in_external "libgeoip" "config.sub.latest" "config.sub"
-do_build "libgeoip"
-
-# python-geoip
-do_extract "python-geoip" "GeoIP-${PYTHON_GEOIP_VERSION}.tar.gz"
-do_build "python-geoip"
-
-# tbb
-do_extract "tbb"          "tbb-${TBB_VERSION}.tar.gz"
-patch_external "tbb"         "custom_library_suffix.patch"        \
-                             "symlink_to_build_directories.patch" \
-                             "32bit_mock.patch"
-do_build "tbb"
-
-# protobuf                             
-do_extract "protobuf"     "protobuf-${PROTOBUF_VERSION}.tar.bz2"
-do_build "protobuf"
-
-# googlebench
-do_copy "googlebench"
-do_build "googlebench"
-
-# sqlite
-do_copy "sqlite3"
-do_build "sqlite3"
-
-# vjson
-do_copy "vjson"
-patch_external "vjson"       "missing_include.patch"
-do_build "vjson"
-
-# sha2
-do_copy "sha2"
-do_build "sha2"
-
-# sha3
-do_copy "sha3"
-do_build "sha3"
+for l in $missing_libs; do
+  build_lib $l
+  echo $l >> $externals_install_dir/.bootstrapDone
+done
 
 ## Done!
-# create a hint that bootstrapping is already done
-touch "$externals_install_dir/.bootstrapDone"
