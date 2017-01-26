51d3326a22050550bb9c23176d40576b338d532e
Radu Popescu <mail@radupopescu.net>
2017-01-26 18:11:59 +0100

    vjson/makeHook.sh - installing missing header

--- externals/vjson/src/makeHook.sh
+++ externals/vjson/src/makeHook.sh
@@ -4,5 +4,5 @@ make clean
 make CVMFS_BASE_CXX_FLAGS="$CVMFS_BASE_CXX_FLAGS"
 strip -S libvjson.a
 
-cp -rv json.h $EXTERNALS_INSTALL_LOCATION/include/
+cp -rv json.h block_allocator.h $EXTERNALS_INSTALL_LOCATION/include/
 cp -rv libvjson.a $EXTERNALS_INSTALL_LOCATION/lib/
