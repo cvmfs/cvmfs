fdff10208a36713b3b5dfb0f8096d08ebaef5626
Radu Popescu <mail@radupopescu.net>
2017-02-13 20:04:02 +0100

    Extend SpoolerDefinition for a new HTTP uploader

--- cvmfs/upload_spooler_definition.cc
+++ cvmfs/upload_spooler_definition.cc
@@ -13,24 +13,21 @@
 namespace upload {
 
 SpoolerDefinition::SpoolerDefinition(
-                      const std::string&       definition_string,
-                      const shash::Algorithms  hash_algorithm,
-                      const zlib::Algorithms   compression_algorithm,
-                      const bool               use_file_chunking,
-                      const size_t             min_file_chunk_size,
-                      const size_t             avg_file_chunk_size,
-                      const size_t             max_file_chunk_size) :
-  driver_type(Unknown),
-  hash_algorithm(hash_algorithm),
-  compression_alg(compression_algorithm),
-  use_file_chunking(use_file_chunking),
-  min_file_chunk_size(min_file_chunk_size),
-  avg_file_chunk_size(avg_file_chunk_size),
-  max_file_chunk_size(max_file_chunk_size),
-  number_of_threads(tbb::task_scheduler_init::default_num_threads()),
-  number_of_concurrent_uploads(number_of_threads * 100),
-  valid_(false)
-{
+    const std::string& definition_string,
+    const shash::Algorithms hash_algorithm,
+    const zlib::Algorithms compression_algorithm, const bool use_file_chunking,
+    const size_t min_file_chunk_size, const size_t avg_file_chunk_size,
+    const size_t max_file_chunk_size)
+    : driver_type(Unknown),
+      hash_algorithm(hash_algorithm),
+      compression_alg(compression_algorithm),
+      use_file_chunking(use_file_chunking),
+      min_file_chunk_size(min_file_chunk_size),
+      avg_file_chunk_size(avg_file_chunk_size),
+      max_file_chunk_size(max_file_chunk_size),
+      number_of_threads(tbb::task_scheduler_init::default_num_threads()),
+      number_of_concurrent_uploads(number_of_threads * 100),
+      valid_(false) {
   // check if given file chunking values are sane
   if (use_file_chunking && (min_file_chunk_size >= avg_file_chunk_size ||
                             avg_file_chunk_size >= max_file_chunk_size)) {
@@ -46,25 +43,26 @@ SpoolerDefinition::SpoolerDefinition(
   }
 
   // recognize and configure the spooler driver
-  if (upstream[0]        == "local") {
+  if (upstream[0] == "local") {
     driver_type = Local;
   } else if (upstream[0] == "S3") {
     driver_type = S3;
+  } else if (upstream[0] == "http") {
+    driver_type = HTTP;
   } else if (upstream[0] == "mock") {
     driver_type = Mock;  // for unit testing purpose only!
   } else {
     driver_type = Unknown;
     LogCvmfs(kLogSpooler, kLogStderr, "unknown spooler driver: %s",
-      upstream[0].c_str());
+             upstream[0].c_str());
     return;
   }
 
   // save data
-  temporary_path        = upstream[1];
+  temporary_path = upstream[1];
   spooler_configuration = upstream[2];
   valid_ = true;
 }
-
 
 SpoolerDefinition SpoolerDefinition::Dup2DefaultCompression() const {
   SpoolerDefinition result(*this);
