935e0d2982de916b2d1688db9c878e6f10e296fd
Radu Popescu <mail@radupopescu.net>
2017-02-18 23:55:49 +0100

    HTTP: Supply SHA1 of payload when submitting

--- gateway/apps/cvmfs_services/src/cvmfs_payloads_handler.erl
+++ gateway/apps/cvmfs_services/src/cvmfs_payloads_handler.erl
@@ -36,8 +36,9 @@ init(Req0 = #{method := <<"GET">>}, State) ->
 %% A "POST" request to /api/payloads, which can return either 200 OK
 %% or in 400 - Bad Request
 %%
-%% The use and session_token parameters should be passed in the query string,
-%% while the payload should be passed in the request body.
+%% The user, payload hash and session_token parameters should be passed
+%% in the query string, while the payload should be passed in the request
+%% body.
 %%
 %% The body of the reply, for a valid request contains the fields:
 %% "status" - either "ok", "error"
@@ -47,10 +48,10 @@ init(Req0 = #{method := <<"GET">>}, State) ->
 init(Req0 = #{method := <<"POST">>}, State) ->
     {URI, T0} = cvmfs_fe_util:tick(Req0, micro_seconds),
 
-    {Status, Reply, Req2} = case cowboy_req:match_qs([user, session_token], Req0) of
-                                #{user := User, session_token := Token} ->
+    {Status, Reply, Req2} = case cowboy_req:match_qs([user, session_token, hash], Req0) of
+                                #{user := User, session_token := Token, hash := Hash} ->
                                     {ok, Payload, Req1} = cvmfs_fe_util:read_body(Req0),
-                                    Rep = p_submit_payload(User, Token, Payload),
+                                    Rep = p_submit_payload(User, Token, Payload, Hash),
                                     {200, Rep, Req1};
                                 _ ->
                                     {400, #{}, Req0}
@@ -67,11 +68,18 @@ init(Req0 = #{method := <<"POST">>}, State) ->
 %% Private functions
 
 
-p_submit_payload(User, Token, Payload) ->
-    case cvmfs_be:submit_payload(User, Token, Payload) of
-        {ok, payload_added} ->
-            #{<<"status">> => <<"ok">>};
-        {error, Reason} ->
-            #{<<"status">> => <<"error">>, <<"reason">> => atom_to_binary(Reason, latin1)}
+p_submit_payload(User, Token, Payload, Hash) ->
+    HashBinary = base64:decode(Hash),
+    case crypto:hash(sha, Payload) of
+        HashBinary ->
+            case cvmfs_be:submit_payload(User, Token, Payload) of
+                {ok, payload_added} ->
+                    #{<<"status">> => <<"ok">>};
+                {error, Reason} ->
+                    #{<<"status">> => <<"error">>, <<"reason">> => atom_to_binary(Reason, latin1)}
+            end;
+        _ ->
+            #{<<"status">> => <<"error">>, <<"reason">> => <<"invalid_payload_hash">>}
     end.
+
 
