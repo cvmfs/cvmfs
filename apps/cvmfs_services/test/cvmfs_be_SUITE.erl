f4de06d9bd2f5935c1c44477aafac9e585fea46e
Radu Popescu <mail@radupopescu.net>
2017-03-15 18:17:01 +0100

    Update cvmfs_be API to use shared secrets

--- apps/cvmfs_services/test/cvmfs_be_SUITE.erl
+++ apps/cvmfs_services/test/cvmfs_be_SUITE.erl
@@ -18,11 +18,11 @@
 
 -export([api_qc/1]).
 
--export([valid_user_valid_path/1
-        ,valid_user_busy_path/1
-        ,invalid_user_valid_path/1
-        ,valid_user_invalid_path/1
-        ,invalid_user_invalid_path/1]).
+-export([valid_key_valid_path/1
+        ,valid_key_busy_path/1
+        ,invalid_key_valid_path/1
+        ,valid_key_invalid_path/1
+        ,invalid_key_invalid_path/1]).
 
 -export([end_valid_lease/1
         ,end_invalid_lease/1
@@ -43,11 +43,11 @@ groups() ->
     [{specifications, [], [{group, new_lease}
                           ,{group, end_lease}
                           ,{group, submit_payload}]}
-    ,{new_lease, [], [valid_user_valid_path
-                     ,valid_user_busy_path
-                     ,invalid_user_valid_path
-                     ,valid_user_invalid_path
-                     ,invalid_user_invalid_path]}
+    ,{new_lease, [], [valid_key_valid_path
+                     ,valid_key_busy_path
+                     ,invalid_key_valid_path
+                     ,valid_key_invalid_path
+                     ,invalid_key_invalid_path]}
     ,{end_lease, [], [end_valid_lease
                      ,end_invalid_lease
                      ,end_lease_invalid_macaroon]}
@@ -64,10 +64,10 @@ init_per_suite(Config) ->
 
     ok = application:load(cvmfs_services),
     ok = ct:require(repos),
-    ok = ct:require(acl),
+    ok = ct:require(keys),
     ok = application:set_env(cvmfs_services, enabled_services, [cvmfs_auth, cvmfs_lease, cvmfs_be]),
     ok = application:set_env(cvmfs_services, repo_config, #{repos => ct:get_config(repos)
-                                                           ,acl => ct:get_config(acl)}),
+                                                           ,keys => ct:get_config(keys)}),
 
     MaxLeaseTime = 50, % milliseconds
     ok = application:set_env(cvmfs_services, max_lease_time, MaxLeaseTime),
@@ -93,44 +93,34 @@ end_per_testcase(_TestCase, _Config) ->
 %% Specs
 
 % New lease
-% Valid user and valid path should be accepted
-valid_user_valid_path(_Config) ->
-    {VUser, VPath} = valid_user_and_path(),
-    {ok, Token} = cvmfs_be:new_lease(VUser, VPath),
+% Valid key and valid path should be accepted
+valid_key_valid_path(_Config) ->
+    {ok, Token} = cvmfs_be:new_lease(<<"key1">>, <<"repo1.domain1.org">>),
     ok = cvmfs_be:end_lease(Token).
-% Valid user and busy path should be rejected with remaining time
-valid_user_busy_path(_Config) ->
-    {VUser, VPath} = valid_user_and_path(),
-    {ok, Token} = cvmfs_be:new_lease(VUser, VPath),
-    {path_busy, _} = cvmfs_be:new_lease(VUser, VPath),
+% Valid key and busy path should be rejected with remaining time
+valid_key_busy_path(_Config) ->
+    {ok, Token} = cvmfs_be:new_lease(<<"key1">>, <<"repo1.domain1.org">>),
+    {path_busy, _} = cvmfs_be:new_lease(<<"key1">>, <<"repo1.domain1.org">>),
     ok = cvmfs_be:end_lease(Token).
-% Invalid user and valid path should be rejected
-invalid_user_valid_path(_Config) ->
-    {VUser, VPath} = valid_user_and_path(),
-    {IUser, _} = invalid_user_and_path(VUser, VPath),
-    {error, invalid_user} = cvmfs_be:new_lease(IUser, VPath).
-% Valid user and invalid path should be rejected
-valid_user_invalid_path(_Config) ->
-    {VUser, VPath} = valid_user_and_path(),
-    {_, IPath} = invalid_user_and_path(VUser, VPath),
-    {error, invalid_path} = cvmfs_be:new_lease(VUser, IPath).
-% Invalid user and invalid path should be rejected with {error, invalid_user}
-invalid_user_invalid_path(_Config) ->
-    {VUser, VPath} = valid_user_and_path(),
-    {IUser, IPath} = invalid_user_and_path(VUser, VPath),
-    {error, invalid_user} = cvmfs_be:new_lease(IUser, IPath).
+% Invalid key and valid path should be rejected
+invalid_key_valid_path(_Config) ->
+    {ok, false} = cvmfs_be:new_lease(<<"key2">>, <<"repo1.domain1.org">>).
+% Valid key and invalid path should be rejected
+valid_key_invalid_path(_Config) ->
+    {error, invalid_repo} = cvmfs_be:new_lease(<<"key1">>, <<"repo1.domain1.com">>).
+% Invalid key and invalid path should be rejected with {error, invalid_key}
+invalid_key_invalid_path(_Config) ->
+    {error, invalid_repo} = cvmfs_be:new_lease(<<"key2">>, <<"repo1.domain1.com">>).
 
 % End lease
 % End valid lease
 end_valid_lease(_Config) ->
-    {VUser, VPath} = valid_user_and_path(),
-    {ok, Token} = cvmfs_be:new_lease(VUser, VPath),
+    {ok, Token} = cvmfs_be:new_lease(<<"key1">>, <<"repo1.domain1.org">>),
     ok = cvmfs_be:end_lease(Token).
 
 % End invalid lease
 end_invalid_lease(_Config) ->
-    {VUser, VPath} = valid_user_and_path(),
-    {ok, Token} = cvmfs_be:new_lease(VUser, VPath),
+    {ok, Token} = cvmfs_be:new_lease(<<"key1">>, <<"repo1.domain1.org">>),
     ok = cvmfs_be:end_lease(Token),
     ok = cvmfs_be:end_lease(Token).
 
@@ -142,10 +132,10 @@ end_lease_invalid_macaroon(_Config) ->
 % Submit payload
 % Normal lease check
 lease_success(_Config) ->
-    % Start with a valid user and path and receive a valid lease token
-    {User, Path} = valid_user_and_path(),
+    % Start with a valid key and path and receive a valid lease token
+    {Key, Path} = {<<"key1">>, <<"repo1.domain1.org">>},
     Payload = <<"placeholder_for_a_real_payload">>,
-    {ok, Token} = cvmfs_be:new_lease(User, Path),
+    {ok, Token} = cvmfs_be:new_lease(Key, Path),
     % Followup with a payload submission
     {ok, payload_added} = cvmfs_be:submit_payload(Token, Payload),
     % Submit final payload and end the lease
@@ -162,9 +152,9 @@ submission_with_invalid_token_fails(_Config) ->
 
 % Start a valid lease, make submission after the token has expired
 submission_with_expired_token_fails(Config) ->
-    {User, Path} = valid_user_and_path(),
+    {Key, Path} = {<<"key1">>, <<"repo1.domain1.org">>},
     Payload = <<"placeholder">>,
-    {ok, Token} = cvmfs_be:new_lease(User, Path),
+    {ok, Token} = cvmfs_be:new_lease(Key, Path),
     ct:sleep(?config(max_lease_time, Config)),
     {error, lease_expired} = cvmfs_be:submit_payload(Token, Payload).
 
@@ -173,16 +163,3 @@ submission_with_expired_token_fails(Config) ->
 api_qc(_Config) ->
     ?assert(true).
 
-%% Private functions
-valid_user_and_path() ->
-    [U | _ ] = lists:filter(fun(U1) ->
-                                    {ok, Ps} = cvmfs_auth:get_user_permissions(U1),
-                                    length(Ps) > 0
-                            end,
-                            cvmfs_auth:get_users()),
-    {ok, [P | _]} = cvmfs_auth:get_user_permissions(U),
-    {U, P}.
-
-%% Produces an invalid {User, Path} pair
-invalid_user_and_path(VUser, VPath) ->
-    {<<VUser/binary,"_invalid">>, <<VPath/binary,"_invalid">>}.
