ccd655d3faf2c50c1e204d6f638ec4d58634081b
Jakob Blomer <jblomer@cern.ch>
2017-03-29 10:20:28 +0200

    prevent build errors with old versions of libfuse

--- cvmfs/duplex_fuse.h
+++ cvmfs/duplex_fuse.h
@@ -23,6 +23,16 @@ static int __attribute__((used)) fuse_lowlevel_notify_inval_entry(
 #else
 #define FUSE_USE_VERSION 26
 #include <fuse/fuse_lowlevel.h>
+#if(FUSE_VERSION < 28)
+#include <cstdlib>
+extern "C" {
+static int __attribute__((used)) fuse_lowlevel_notify_inval_entry(
+  void *, unsigned long, const char *, size_t)  // NOLINT
+{
+  abort();
+}
+}
+#endif
 #endif
 
 #endif  // CVMFS_DUPLEX_FUSE_H_
