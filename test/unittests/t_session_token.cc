6d20c6af594ebef56660b290dad2a1c8421bd75f
Radu Popescu <mail@radupopescu.net>
2017-04-04 17:38:52 +0200

    Expand receiver/session_token tests suite

--- test/unittests/t_session_token.cc
+++ test/unittests/t_session_token.cc
@@ -14,10 +14,29 @@ TEST_F(T_SessionToken, GenerateBasic) {
   std::string session_token;
   std::string public_token_id;
   std::string token_secret;
-  ASSERT_TRUE(generate_session_token("some_key_id", "some_path", 1,
-                                     &session_token, &public_token_id,
-                                     &token_secret));
+  ASSERT_EQ(
+      generate_session_token("some_key_id", "some_path", 1, &session_token,
+                             &public_token_id, &token_secret),
+      0);
   ASSERT_FALSE(session_token.empty());
   ASSERT_FALSE(public_token_id.empty());
   ASSERT_FALSE(token_secret.empty());
+}
+
+TEST_F(T_SessionToken, GenerateWithInvalidParameters) {
+  ASSERT_EQ(generate_session_token("key_id", "some_path", 1, NULL, NULL, NULL),
+            1);
+}
+
+TEST_F(T_SessionToken, GetTokenId) {
+  std::string session_token;
+  std::string public_token_id;
+  std::string token_secret;
+  ASSERT_EQ(
+      generate_session_token("some_key_id", "some_path", 1, &session_token,
+                             &public_token_id, &token_secret),
+      0);
+  std::string recovered_token_id;
+  ASSERT_EQ(get_token_public_id(session_token, &recovered_token_id), 0);
+  ASSERT_EQ(public_token_id, recovered_token_id);
 }
