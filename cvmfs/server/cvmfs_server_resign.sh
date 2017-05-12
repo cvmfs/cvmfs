b8e7c35a8c9fd6d74a5f5cdf5c5b17f1db1a8f59
Dave Dykstra <dwd@fnal.gov>
2017-05-12 11:53:52 -0500

    add -n option

--- cvmfs/server/cvmfs_server_resign.sh
+++ cvmfs/server/cvmfs_server_resign.sh
@@ -12,11 +12,27 @@
 cvmfs_server_resign() {
   local names
   local retcode=0
+  local require_repo_config=1
+
+  # parameter handling
+  OPTIND=1
+  while getopts "n" option; do
+    case $option in
+      n)
+        require_repo_config=0
+      ;;
+      ?)
+        shift $(($OPTIND-2))
+        usage "Command resign: Unrecognized option: $1"
+      ;;
+    esac
+  done
 
   # get repository names
+  shift $(($OPTIND-1))
   check_parameter_count_for_multiple_repositories $#
   names=$(get_or_guess_multiple_repository_names "$@")
-  check_multiple_repository_existence "$names"
+  [ $require_repo_config -eq 0 ] || check_multiple_repository_existence "$names"
 
   # sanity checks
   is_root || die "Only root can resign repositories"
@@ -24,19 +40,35 @@ cvmfs_server_resign() {
   for name in $names; do
 
     # sanity checks
-    is_stratum0 $name  || { echo "Repository $name is not a stratum 0 repository"; retcode=1; continue; }
-    health_check $name || { echo "Repository $name is not healthy"; retcode=1; continue; }
+    if [ $require_repo_config -eq 1 ]; then
+      is_stratum0 $name  || { echo "Repository $name is not a stratum 0 repository"; retcode=1; continue; }
+      health_check $name || { echo "Repository $name is not healthy"; retcode=1; continue; }
 
-    # get repository information
-    load_repo_config $name
+      # get repository information
+      load_repo_config $name
 
-    # check if repository is compatible to the installed CernVM-FS version
-    check_repository_compatibility $name
+      # check if repository is compatible to the installed CernVM-FS version
+      check_repository_compatibility $name
 
-    # do it!
-    create_whitelist $name $CVMFS_USER \
-        ${CVMFS_UPSTREAM_STORAGE} \
-        ${CVMFS_SPOOL_DIR}/tmp
+      # do it!
+      create_whitelist $name $CVMFS_USER \
+          ${CVMFS_UPSTREAM_STORAGE} \
+          ${CVMFS_SPOOL_DIR}/tmp
+    else
+      # do not require repository configuration, just the whitelist file
+      local whitelist_path=${DEFAULT_LOCAL_STORAGE}/$name/.cvmfswhitelist
+      [ -f $whitelist_path ] || { echo "$whitelist_path does not exist!"; retcode=1; continue; }
+
+      local user tmpdir
+      user="`stat --format=%U $whitelist_path`"
+      tmpdir="`mktemp -d`"
+      trap "rm -rf $tmpdir" EXIT HUP INT TERM
+
+      create_whitelist $name $user "" $tmpdir 1
+
+      rm -rf $tmpdir
+      trap - EXIT HUP INT TERM
+    fi
 
   done
 
