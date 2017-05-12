32383f642cbac5778c41b9cb65b0f9144c5fc9fe
Dave Dykstra <dwd@fnal.gov>
2017-05-12 11:56:08 -0500

    add create_whitelist option to re-create locally, reusing old fingerprint

--- cvmfs/server/cvmfs_server_ssl.sh
+++ cvmfs/server/cvmfs_server_ssl.sh
@@ -53,14 +53,15 @@ create_whitelist() {
   local user=$2
   local spooler_definition=$3
   local temp_dir=$4
-  local masterkey
+  local local_recreate=${5:-0}
   local usemasterkeycard=0
+  local hash_algorithm
+  local local_repo_storage
 
   local whitelist
   whitelist=${temp_dir}/whitelist.$name
-  local hash_algorithm="${CVMFS_HASH_ALGORITHM-sha1}"
 
-  masterkey=/etc/cvmfs/keys/${name}.masterkey
+  local masterkey=/etc/cvmfs/keys/${name}.masterkey
   if cvmfs_sys_file_is_regular $masterkey; then
     echo -n "Signing 30 day whitelist with master key... "
   elif masterkeycard_cert_available >/dev/null; then
@@ -72,8 +73,18 @@ create_whitelist() {
   echo `date -u "+%Y%m%d%H%M%S"` > ${whitelist}.unsigned
   echo "E`date -u --date='+30 days' "+%Y%m%d%H%M%S"`" >> ${whitelist}.unsigned
   echo "N$name" >> ${whitelist}.unsigned
-  openssl x509 -in /etc/cvmfs/keys/${name}.crt -outform der | \
-    __swissknife hash -a $hash_algorithm -f >> ${whitelist}.unsigned
+  if [ $local_recreate -eq 1 ]; then
+    local_repo_storage="${DEFAULT_LOCAL_STORAGE}/$name"
+    local fingerprint
+    fingerprint="`cat -v $local_repo_storage/.cvmfswhitelist | awk '/^N/{getline;print;exit}'`"
+    echo "$fingerprint" >> ${whitelist}.unsigned
+    hash_algorithm="`echo "$fingerprint"|sed -n 's/.*-//p'|tr '[A-Z]' '[a-z]'`"
+    hash_algorithm="${hash_algorithm:-sha1}"
+  else
+    hash_algorithm="${CVMFS_HASH_ALGORITHM:-sha1}"
+    openssl x509 -in /etc/cvmfs/keys/${name}.crt -outform der | \
+      __swissknife hash -a $hash_algorithm -f >> ${whitelist}.unsigned
+  fi
 
   local hash;
   hash="`cat ${whitelist}.unsigned | __swissknife hash -a $hash_algorithm`"
@@ -89,7 +100,14 @@ create_whitelist() {
   chown $user $whitelist
 
   rm -f ${whitelist}.unsigned ${whitelist}.signature ${whitelist}.hash
-  __swissknife upload -i $whitelist -o .cvmfswhitelist -r $spooler_definition
+  if [ $local_recreate -eq 1 ]; then
+    # copy first to a new name in case the filesystem is full
+    cp -f $whitelist $local_repo_storage/.cvmfswhitelist.new
+    chown $user $local_repo_storage/.cvmfswhitelist.new
+    mv -f $local_repo_storage/.cvmfswhitelist.new $local_repo_storage/.cvmfswhitelist
+  else
+    __swissknife upload -i $whitelist -o .cvmfswhitelist -r $spooler_definition
+  fi
   rm -f $whitelist
   echo "done"
 }
