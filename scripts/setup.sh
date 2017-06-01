8bc7034ae448a3b53a2c947530d342dd60d016b8
Radu Popescu <mail@radupopescu.net>
2017-06-01 10:23:48 +0200

    Add some feedback to the setup.sh script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,8 +7,12 @@
 #-------------------------------------------------------------------
 
 # Setup Mnesia
+echo "Setting up the Mnesia schema"
 bin/cvmfs_services escript scripts/setup_mnesia.escript
 
 # Install syslog configuration file
+echo "Installing the syslog configuration file"
 cp -v scripts/90-cvmfs_services.conf /etc/rsyslog.d/
+
+echo "  - restarting rsyslog"
 systemctl restart rsyslog
