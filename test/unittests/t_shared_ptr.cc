53e41775c8736f674e075716c8eae4a3a9152574
Radu Popescu <mail@radupopescu.net>
2017-06-13 12:02:25 +0200

    Expanding t_shared_ptr.cc

--- test/unittests/t_shared_ptr.cc
+++ test/unittests/t_shared_ptr.cc
@@ -42,19 +42,26 @@ class T_SharedPtr : public ::testing::Test {
  protected:
   // At the beginning of each test case, there should be no living instances of
   // TestData
-  void SetUp() { counter_ = 0; }
+  void SetUp() {
+    counter1_ = 0;
+    counter2_ = 0;
+  }
   // After each test case, there should be no more living instances of TestData.
-  void TearDown() { ASSERT_EQ(counter_, 0); }
+  void TearDown() {
+    ASSERT_EQ(counter1_, 0);
+    ASSERT_EQ(counter2_, 0);
+  }
 
-  int counter_;
+  int counter1_;
+  int counter2_;
 };
 
 // Basic usage of shared_ptr
 TEST_F(T_SharedPtr, SharedPtr) {
   // New instance of TestData managed by shared_ptr
-  SharedPtr<TestData> shared_data1(new TestData(&counter_));
+  SharedPtr<TestData> shared_data1(new TestData(&counter1_));
   // Counter should indicated the new instance
-  EXPECT_EQ(counter_, 1);
+  EXPECT_EQ(counter1_, 1);
   // The reference should be unique at this point
   EXPECT_TRUE(shared_data1.Unique());
   {
@@ -62,7 +69,7 @@ TEST_F(T_SharedPtr, SharedPtr) {
     SharedPtr<TestData> shared_data2 = shared_data1;
     // Counter hasn't been incremented, since we are still dealing with a single
     // TestData instance
-    EXPECT_EQ(counter_, 1);
+    EXPECT_EQ(counter1_, 1);
     // The shared_ptr reference is no longer unique...
     EXPECT_FALSE(shared_data1.Unique());
     // And the use count of the shared references is 2
@@ -76,18 +83,54 @@ TEST_F(T_SharedPtr, SharedPtr) {
 // Working with classes which contain shared_ptr members
 TEST_F(T_SharedPtr, SharedPtrMembers) {
   // Create an instance of ClassSharedMember
-  ClassSharedMember data1(&counter_, 1234);
-  EXPECT_EQ(counter_, 1);
+  ClassSharedMember data1(&counter1_, 1234);
+  EXPECT_EQ(counter1_, 1);
 
   {
     // Create a new instance of ClassSharedMember using the copy-constructor
     ClassSharedMember data2(data1);
-    EXPECT_EQ(counter_, 2);
+    EXPECT_EQ(counter1_, 2);
     // The shared reference has 2 uses, by data1 and data2
     EXPECT_EQ(data1.value_use_count(), 2);
   }
 
   // data2 has been destroyed.
-  EXPECT_EQ(counter_, 1);
+  EXPECT_EQ(counter1_, 1);
   EXPECT_EQ(data1.value_use_count(), 1);
+}
+
+TEST_F(T_SharedPtr, Resetting) {
+  // New instance of TestData managed by shared_ptr
+  SharedPtr<TestData> shared_data1(new TestData(&counter1_));
+  // Counter should indicated the new instance
+  EXPECT_EQ(counter1_, 1);
+  // The reference should be unique at this point
+  EXPECT_TRUE(shared_data1.Unique());
+
+  SharedPtr<TestData> shared_data2(shared_data1);
+
+  EXPECT_EQ(1, counter1_);
+  EXPECT_FALSE(shared_data1.Unique());
+  EXPECT_EQ(2, shared_data1.UseCount());
+
+  shared_data1.Reset();
+
+  EXPECT_EQ(1, counter1_);
+  EXPECT_TRUE(shared_data2.Unique());
+  EXPECT_EQ(1, shared_data2.UseCount());
+
+  shared_data1 = shared_data2;
+
+  EXPECT_EQ(1, counter1_);
+  EXPECT_FALSE(shared_data1.Unique());
+  EXPECT_EQ(2, shared_data1.UseCount());
+
+  shared_data2.Reset(new TestData(&counter2_));
+
+  EXPECT_EQ(1, counter1_);
+  EXPECT_EQ(1, counter2_);
+  EXPECT_TRUE(shared_data1.Unique());
+  EXPECT_EQ(1, shared_data1.UseCount());
+  EXPECT_TRUE(shared_data2.Unique());
+  EXPECT_EQ(1, shared_data2.UseCount());
 }
