96ffcfafa5ea1d3d78e91aafa8e562134ea61eb9
Radu Popescu <mail@radupopescu.net>
2017-06-13 15:29:29 +0200

    Adding a simple unit test for the platform_spinlock_*** functions

--- test/unittests/t_platforms.cc
+++ test/unittests/t_platforms.cc
@@ -8,3 +8,14 @@
 
 // TODO(Radu): Could add some unit tests for all the functions in
 //             platform_{linux,osx}.h
+
+class T_Platform : public ::testing::Test {};
+
+TEST_F(T_Platform, Spinlock) {
+  platform_spinlock lock;
+  platform_spinlock_init(&lock, PTHREAD_PROCESS_PRIVATE);
+  ASSERT_EQ(0, platform_spinlock_trylock(&lock));
+  ASSERT_EQ(-1, platform_spinlock_trylock(&lock));
+  platform_spinlock_unlock(&lock);
+  ASSERT_EQ(0, platform_spinlock_trylock(&lock));
+}
