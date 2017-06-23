9ff1e63fcbf88397721d5ba7563f3a61b4380291
Radu Popescu <mail@radupopescu.net>
2017-06-23 17:07:33 +0200

    Use higher default values for HTTP request reading time and request size

--- apps/cvmfs_services/src/cvmfs_fe_util.erl
+++ apps/cvmfs_services/src/cvmfs_fe_util.erl
@@ -18,7 +18,7 @@ read_body(Req0) ->
 
 
 read_body_rec(Req0, Acc) ->
-    case cowboy_req:read_body(Req0) of
+    case cowboy_req:read_body(Req0,#{length => 1000000000, period => 36000000}) of
         {ok, Data, Req1} ->
             DataSize = size(Data),
             {ok, <<Data:DataSize/binary,Acc/binary>>, Req1};
