150bcf003de36a7045e8535a2e78d51aa75411ab
Radu Popescu <mail@radupopescu.net>
2017-06-23 11:27:46 +0200

    Update Docker image version used for CI

--- gateway/ci/travis_main.sh
+++ gateway/ci/travis_main.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run --rm -it -v "${PWD}":/mnt radupopescu/erlang-libsodium:19 \
+docker run --rm -it -v "${PWD}":/mnt radupopescu/erlang-libsodium:20.0 \
        sh -c "cd /mnt && rebar3 release && rebar3 as test dialyzer,ct && rebar3 as prod tar"
