11422c9860a8e859a930789698590ed70c8dc4b7
Radu Popescu <mail@radupopescu.net>
2017-07-10 16:23:44 +0200

    Simplify application parameter handling

--- apps/cvmfs_services/src/cvmfs_services_app.erl
+++ apps/cvmfs_services/src/cvmfs_services_app.erl
@@ -17,33 +17,24 @@
 %%====================================================================
 
 start(_StartType, _StartArgs) ->
-    Vars = case application:get_env(repo_config) of
-               {ok, {file, RepoConfigFile}} ->
-                   {ok, VarList} = file:consult(RepoConfigFile),
-                   maps:from_list(VarList);
-               {ok, RepoConfigMap} ->
-                   RepoConfigMap;
-               undefined ->
-                   #{repos => [], keys => []}
-           end,
-    ReceiverPoolConfig1 = case application:get_env(receiver_config) of
-                             {ok, PoolConfig} ->
-                                 PoolConfig;
-                             undefined ->
-                                 []
-                         end,
+    RepoVars = read_vars(repo_config, #{repos => [], keys => []}),
+
+    UserVars = read_vars(user_config,
+                         #{max_lease_time => 7200000,
+                           receiver_config => [{size, 1}, {max_overflow, 1}],
+                           receiver_worker_config => [{executable_path, "/usr/bin/cvmfs_receiver"}]}),
+
+    application:set_env(cvmfs_services, max_lease_time, maps:get(max_lease_time, UserVars)),
+
+    ReceiverPoolConfig1 = maps:get(receiver_config, UserVars),
     ReceiverPoolConfig2 = case lists:keyfind(worker_module, 1, ReceiverPoolConfig1) of
                               false ->
                                   [{worker_module, cvmfs_receiver} | ReceiverPoolConfig1];
                               _ ->
                                   ReceiverPoolConfig1
                           end,
-    ReceiverWorkerConfig = case application:get_env(receiver_worker_config) of
-                               {ok, WorkerConfig} ->
-                                   maps:from_list(WorkerConfig);
-                               undefined ->
-                                   #{}
-                           end,
+    ReceiverWorkerConfig = maps:from_list(maps:get(receiver_worker_config, UserVars)),
+
     {ok, Services} = application:get_env(enabled_services),
 
     case lists:member(cvmfs_fe, Services) of
@@ -56,8 +47,8 @@ start(_StartType, _StartArgs) ->
     Services2 = lists:delete(cvmfs_fe, Services),
 
     cvmfs_services_sup:start_link({Services2,
-                                   maps:get(repos, Vars),
-                                   maps:get(keys, Vars),
+                                   maps:get(repos, RepoVars),
+                                   maps:get(keys, RepoVars),
                                    ReceiverPoolConfig2,
                                    ReceiverWorkerConfig}).
 
@@ -68,3 +59,13 @@ stop(_State) ->
 %%====================================================================
 %% Internal functions
 %%====================================================================
+read_vars(VarName, Defaults) ->
+    case application:get_env(VarName) of
+        {ok, {file, ConfigFile}} ->
+            {ok, VarList} = file:consult(ConfigFile),
+            maps:from_list(VarList);
+        {ok, ConfigMap} ->
+            ConfigMap;
+        undefined ->
+            Defaults
+    end.
