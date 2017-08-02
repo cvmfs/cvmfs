73e6702844318fc313174e67b20ec45e5bbd0037
Jakob Blomer <jblomer@cern.ch>
2017-08-02 09:19:43 +0200

    increase memory size for ram-cache tests

--- test/cloud_testing/platforms/centos7_x86_64-RAMCACHE_setup.sh
+++ test/cloud_testing/platforms/centos7_x86_64-RAMCACHE_setup.sh
@@ -18,7 +18,7 @@ sudo sh -c "echo CVMFS_CACHE_ram_extern_TYPE=external >> /etc/cvmfs/default.d/90
 sudo sh -c "echo CVMFS_CACHE_ram_extern_CMDLINE=/usr/libexec/cvmfs/cache/cvmfs_cache_ram,/etc/cvmfs/default.d/90-ramcache.conf >> /etc/cvmfs/default.d/90-ramcache.conf"
 sudo sh -c "echo CVMFS_CACHE_ram_extern_LOCATOR=unix=/var/lib/cvmfs/cvmfs-cache.socket >> /etc/cvmfs/default.d/90-ramcache.conf"
 sudo sh -c "echo CVMFS_CACHE_PLUGIN_LOCATOR=unix=/var/lib/cvmfs/cvmfs-cache.socket >> /etc/cvmfs/default.d/90-ramcache.conf"
-sudo sh -c "echo CVMFS_CACHE_PLUGIN_SIZE=2000 >> /etc/cvmfs/default.d/90-ramcache.conf"
+sudo sh -c "echo CVMFS_CACHE_PLUGIN_SIZE=3500 >> /etc/cvmfs/default.d/90-ramcache.conf"
 sudo cvmfs_config setup                          || die "fail (cvmfs_config setup)"
 sudo mkdir -p /var/log/cvmfs-test                || die "fail (mkdir /var/log/cvmfs-test)"
 sudo chown sftnight:sftnight /var/log/cvmfs-test || die "fail (chown /var/log/cvmfs-test)"
