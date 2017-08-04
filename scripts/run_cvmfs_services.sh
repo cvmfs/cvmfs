41bd3f2ef0b696fdde9dac00eb4ca23ebbca3516
Radu Popescu <mail@radupopescu.net>
2017-08-04 14:06:17 +0200

    Adding "status" command to run_cvmfs_services.sh

--- scripts/run_cvmfs_services.sh
+++ scripts/run_cvmfs_services.sh
@@ -12,8 +12,10 @@ if [ x"$action" = xstart ]; then
 elif [ x"$action" = xstop ]; then
     RUNNER_LOG_DIR=/tmp $SCRIPT_LOCATION/../bin/cvmfs_services stop
     echo "CVMFS gateway services stopped."
+elif [ x"$action" = xstatus ]; then
+    RUNNER_LOG_DIR=/tmp $SCRIPT_LOCATION/../bin/cvmfs_services status
 else
     echo "Unknown action: $action"
-    echo "Usage: run_cvmfs_services.sh <start|stop>"
+    echo "Usage: run_cvmfs_services.sh <start|stop|status>"
     exit 1
 fi
