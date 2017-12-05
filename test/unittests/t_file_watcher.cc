33f2948c09f9ce864360ae4243a51e20475836f3
Radu Popescu <mail@radupopescu.net>
2017-12-05 14:13:33 +0100

    Making FileWatcher unittest crossplatform

--- test/unittests/t_file_watcher.cc
+++ test/unittests/t_file_watcher.cc
@@ -4,43 +4,65 @@
 
 #include <gtest/gtest.h>
 
-#include "file_watcher.h"
+#include <map>
+
 #include "logging.h"
+#include "platform.h"
 #include "util/pointer.h"
+#include "util/posix.h"
+
+typedef std::map<file_watcher::Event, int> Counters;
 
 class TestEventHandler : public file_watcher::EventHandler {
  public:
-  TestEventHandler() {}
+  explicit TestEventHandler(Counters* ctrs) : counters_(ctrs) {}
   virtual ~TestEventHandler() {}
 
-  virtual bool Handle(const std::string& file_path,
+  virtual bool Handle(const std::string& /*file_path*/,
                       file_watcher::Event event,
                       bool* clear_handler) {
-    LogCvmfs(kLogCvmfs, kLogStdout,
-             "Handling %d event for file: %s",
-             event, file_path.c_str());
+    (*counters_)[event]++;
     *clear_handler = true;
     return true;
   }
+
+  Counters* counters_;
 };
 
-class TestFileWatcher : public file_watcher::FileWatcher {
- public:
-  TestFileWatcher() : FileWatcher() {}
-  virtual ~TestFileWatcher() {}
+class T_FileWatcher : public ::testing::Test {
+ protected:
+  void SetUp() { counters_.clear(); }
 
-  virtual bool RunEventLoop(const FileWatcher::HandlerMap&, int) {
-    return true;
-  }
+  Counters counters_;
 };
 
-class T_FileWatcher : public ::testing::Test {};
+TEST_F(T_FileWatcher, ModifyThenDelete) {
+  const std::string watched_file_name =
+      GetCurrentWorkingDirectory() + "/file_watcher_test.txt";
+  SafeWriteToFile("test", watched_file_name, 0600);
 
-TEST_F(T_FileWatcher, Dummy) {
-  UniquePtr<file_watcher::FileWatcher> watcher(new TestFileWatcher());
+  UniquePtr<file_watcher::FileWatcher> watcher(platform_file_watcher());
 
-  TestEventHandler* hd = new TestEventHandler;
-  watcher->RegisterHandler("/tmp/file_watcher_test.txt", hd);
+  TestEventHandler* hd = new TestEventHandler(&counters_);
+  watcher->RegisterHandler(watched_file_name, hd);
 
   EXPECT_TRUE(watcher->Start());
+
+  SafeSleepMs(100);
+
+  SafeWriteToFile("test", watched_file_name, 0600);
+
+  SafeSleepMs(100);
+
+  Counters::const_iterator it_mod = counters_.find(file_watcher::kModified);
+  const int num_modifications = it_mod->second;
+  EXPECT_EQ(1, num_modifications);
+
+  unlink(watched_file_name.c_str());
+
+  SafeSleepMs(100);
+
+  Counters::const_iterator it_del = counters_.find(file_watcher::kDeleted);
+  const int num_deletions = it_del->second;
+  EXPECT_EQ(1, num_deletions);
 }
