8a4441843162a910c9d624cd8f521b4634cd5bae
Radu Popescu <mail@radupopescu.net>
2018-02-02 16:07:50 +0100

    Remove test-specific config files and update tests

--- gateway/apps/cvmfs_services/test/cvmfs_test_util.erl
+++ gateway/apps/cvmfs_services/test/cvmfs_test_util.erl
@@ -9,7 +9,8 @@
 
 -module(cvmfs_test_util).
 
--export([make_test_user_vars/1]).
+-export([make_test_user_vars/1,
+         make_test_repo_config/0]).
 
 make_test_user_vars(MaxLeaseTime) ->
    #{max_lease_time => MaxLeaseTime,
@@ -19,3 +20,10 @@ make_test_user_vars(MaxLeaseTime) ->
                          {worker_module, cvmfs_test_receiver}],
      receiver_worker_config => []
     }.
+
+make_test_repo_config() ->
+    #{repos => [{<<"repo1.domain1.org">>, [<<"key1">>]},
+                {<<"repo2.domain2.org">>, [<<"key1">>]},
+                {<<"repo3.domain3.org">>, [<<"key1">>, <<"key2">>]}],
+      keys => [{<<"plain_text">>, <<"key1">>, <<"secret1">>, <<"/">>},
+               {<<"plain_text">>, <<"key2">>, <<"secret2">>, <<"/subpath">>}]}.
