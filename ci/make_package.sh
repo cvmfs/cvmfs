63bade47378d6f45642d76c9cb28b55d2dbd1937
Radu Popescu <mail@radupopescu.net>
2018-05-03 17:56:38 +0200

    Rename cvmfs_gateway -> cvmfs-gateway where possible

--- ci/make_package.sh
+++ ci/make_package.sh
@@ -22,7 +22,7 @@ cd "$CVMFS_BUILD_LOCATION"
 rebar3 as prod compile
 rebar3 as prod release,tar
 REPO_GATEWAY_VERSION=$(grep -o "[0-9]\+\.[0-9]\+\.[0-9]\+" apps/cvmfs_gateway/src/cvmfs_gateway.app.src)
-TARBALL_NAME=cvmfs_gateway_${REPO_GATEWAY_VERSION}_${CVMFS_BUILD_PLATFORM}_x86_64.tar.gz
+TARBALL_NAME=cvmfs-gateway_${REPO_GATEWAY_VERSION}_${CVMFS_BUILD_PLATFORM}_x86_64.tar.gz
 PKGMAP_FILE=${CVMFS_BUILD_LOCATION}/pkgmap/pkgmap.${CVMFS_BUILD_PLATFORM}_x86_64
 
 # Create an RPM or DEB package from the tarball
@@ -30,12 +30,13 @@ if [ x"${CVMFS_BUILD_PLATFORM}" = xubuntu1604 ]; then
     PACKAGE_TYPE=deb
     PACKAGE_NAME_SUFFIX="+ubuntu16.04_amd64"
     PACKAGE_LOCATION=DEBS
+    PACKAGE_NAME=cvmfs-gateway_${REPO_GATEWAY_VERSION}~1${PACKAGE_NAME_SUFFIX}.${PACKAGE_TYPE}
 elif [ x"${CVMFS_BUILD_PLATFORM}" = xslc6 ] || [ x"${CVMFS_BUILD_PLATFORM}" = xcc7 ]; then
     PACKAGE_TYPE=rpm
     PACKAGE_NAME_SUFFIX="$(rpm --eval "%{?dist}").x86_64"
     PACKAGE_LOCATION=RPMS
+    PACKAGE_NAME=cvmfs-gateway-${REPO_GATEWAY_VERSION}-1${PACKAGE_NAME_SUFFIX}.${PACKAGE_TYPE}
 fi
-PACKAGE_NAME=cvmfs_gateway_${REPO_GATEWAY_VERSION}~1${PACKAGE_NAME_SUFFIX}.${PACKAGE_TYPE}
 
 mkdir -p ${CVMFS_BUILD_LOCATION}/$PACKAGE_LOCATION
 
@@ -50,10 +51,10 @@ fi
 
 fpm -s tar \
     -t ${PACKAGE_TYPE} \
-    --prefix /opt/cvmfs_gateway \
+    --prefix /opt/cvmfs-gateway \
     --package $PACKAGE_LOCATION/${PACKAGE_NAME} \
     --version ${REPO_GATEWAY_VERSION} \
-    --name cvmfs_gateway \
+    --name cvmfs-gateway \
     --maintainer "Radu Popescu <radu.popescu@cern.ch>" \
     --description "CernVM-FS Repository Gateway" \
     --url "http://cernvm.cern.ch" \
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -13,7 +13,7 @@ SCRIPT_LOCATION=$(cd "$(dirname "$0")"; pwd)
 # Setup Mnesia
 echo "Setting up the Mnesia"
 
-cvmfs_mnesia_root=/opt/cvmfs_mnesia
+cvmfs_mnesia_root=/opt/cvmfs-mnesia
 echo "  - (re)creating schema directory at $cvmfs_mnesia_root"
 sudo rm -rf $cvmfs_mnesia_root && sudo mkdir -p $cvmfs_mnesia_root
 
@@ -22,7 +22,7 @@ sudo $SCRIPT_LOCATION/../bin/cvmfs_gateway escript scripts/setup_mnesia.escript 
 
 # Install syslog configuration file
 echo "Installing the syslog configuration file"
-sudo cp -v $SCRIPT_LOCATION/90-cvmfs_gateway.conf /etc/rsyslog.d/
+sudo cp -v $SCRIPT_LOCATION/90-cvmfs-gateway.conf /etc/rsyslog.d/
 
 # Symlink the configuration directory into /etc/cvmfs/gateway
 if [ ! -e /etc/cvmfs/gateway ]; then
@@ -34,13 +34,13 @@ fi
 if [ x"$(which systemctl)" != x"" ]; then
     echo "  - restarting rsyslog"
     sudo systemctl restart rsyslog
-    sudo cp -v $SCRIPT_LOCATION/90-cvmfs_gateway_rotate_systemd /etc/logrotate.d/
+    sudo cp -v $SCRIPT_LOCATION/90-cvmfs-gateway-rotate-systemd /etc/logrotate.d/
 
     echo "  - installing systemd service file"
-    sudo cp -v $SCRIPT_LOCATION/cvmfs_gateway.service /etc/systemd/system/cvmfs_gateway.service
+    sudo cp -v $SCRIPT_LOCATION/cvmfs-gateway.service /etc/systemd/system/cvmfs-gateway.service
 else
     echo "  - restarting rsyslog"
     sudo service rsyslog restart
-    sudo cp -v $SCRIPT_LOCATION/90-cvmfs_gateway_rotate /etc/logrotate.d/
+    sudo cp -v $SCRIPT_LOCATION/90-cvmfs-gateway-rotate /etc/logrotate.d/
 fi
 
