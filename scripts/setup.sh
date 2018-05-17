c8471e099c7805b12e04997984ec3ca598f621d0
Radu Popescu <mail@radupopescu.net>
2018-05-17 15:54:47 +0200

    setup.sh - ensure that /var/log/cvmfs-gateway is created by the rsyslog daemon

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -12,19 +12,22 @@ SCRIPT_LOCATION=$(cd "$(dirname "$0")"; pwd)
 
 export RUNNER_LOG_DIR=/var/log/cvmfs-gateway
 
-# Setup Mnesia
-echo "Setting up the Mnesia"
-
-cvmfs_mnesia_root=/var/lib/cvmfs-gateway
-echo "  - (re)creating schema directory at $cvmfs_mnesia_root"
-sudo rm -rf $cvmfs_mnesia_root && sudo mkdir -p $cvmfs_mnesia_root
-
-echo "  - creating Mnesia schema"
-sudo $SCRIPT_LOCATION/../bin/cvmfs_gateway escript scripts/setup_mnesia.escript $cvmfs_mnesia_root
-
 # Install syslog configuration file
 echo "Installing the syslog configuration file"
 sudo cp -v $SCRIPT_LOCATION/90-cvmfs-gateway.conf /etc/rsyslog.d/
+
+if [ x"$(which systemctl)" != x"" ]; then
+    echo "  - restarting rsyslog"
+    sudo systemctl restart rsyslog
+    sudo cp -v $SCRIPT_LOCATION/90-cvmfs-gateway-rotate-systemd /etc/logrotate.d/
+
+    echo "  - installing systemd service file"
+    sudo cp -v $SCRIPT_LOCATION/cvmfs-gateway.service /etc/systemd/system/cvmfs-gateway.service
+else
+    echo "  - restarting rsyslog"
+    sudo service rsyslog restart
+    sudo cp -v $SCRIPT_LOCATION/90-cvmfs-gateway-rotate /etc/logrotate.d/
+fi
 
 # Symlink the configuration directory into /etc/cvmfs/gateway
 if [ ! -e /etc/cvmfs/gateway ]; then
@@ -40,16 +43,12 @@ if [ ! -e /etc/cvmfs/gateway/user.json ]; then
     sudo cp -v $SCRIPT_LOCATION/../etc/user.json /etc/cvmfs/gateway/
 fi
 
-if [ x"$(which systemctl)" != x"" ]; then
-    echo "  - restarting rsyslog"
-    sudo systemctl restart rsyslog
-    sudo cp -v $SCRIPT_LOCATION/90-cvmfs-gateway-rotate-systemd /etc/logrotate.d/
+# Setup Mnesia
+echo "Setting up the Mnesia"
 
-    echo "  - installing systemd service file"
-    sudo cp -v $SCRIPT_LOCATION/cvmfs-gateway.service /etc/systemd/system/cvmfs-gateway.service
-else
-    echo "  - restarting rsyslog"
-    sudo service rsyslog restart
-    sudo cp -v $SCRIPT_LOCATION/90-cvmfs-gateway-rotate /etc/logrotate.d/
-fi
+cvmfs_mnesia_root=/var/lib/cvmfs-gateway
+echo "  - (re)creating schema directory at $cvmfs_mnesia_root"
+sudo rm -rf $cvmfs_mnesia_root && sudo mkdir -p $cvmfs_mnesia_root
 
+echo "  - creating Mnesia schema"
+sudo $SCRIPT_LOCATION/../bin/cvmfs_gateway escript scripts/setup_mnesia.escript $cvmfs_mnesia_root
