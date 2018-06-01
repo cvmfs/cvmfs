dd49a84791c096409f647a2a1bcd8b1a5dd67c4b
Radu Popescu <mail@radupopescu.net>
2018-06-01 11:50:38 +0200

    Ubuntu: limit integration tests to single S3 test (TO REVERT)

--- test/cloud_testing/platforms/ubuntu_test.sh
+++ test/cloud_testing/platforms/ubuntu_test.sh
@@ -7,8 +7,8 @@ script_location=$(cd "$(dirname "$0")"; pwd)
 retval=0
 
 # running unittests
-run_unittests --gtest_shuffle \
-              --gtest_death_test_use_fork || retval=1
+# run_unittests --gtest_shuffle \
+#              --gtest_death_test_use_fork || retval=1
 
 
 CVMFS_EXCLUDE=
@@ -40,36 +40,36 @@ if [ x"$(lsb_release -cs)" = x"precise" ]; then
 fi
 
 
-cd ${SOURCE_DIRECTORY}/test
-echo "running CernVM-FS client test cases..."
-CVMFS_TEST_CLASS_NAME=ClientIntegrationTests                                  \
-./run.sh $CLIENT_TEST_LOGFILE -o ${CLIENT_TEST_LOGFILE}${XUNIT_OUTPUT_SUFFIX} \
-                              -x src/004-davinci                              \
-                                 src/005-asetup                               \
-                                 src/007-testjobs                             \
-                                 src/024-reload-during-asetup                 \
-                                 src/050-configrepo                           \
-                                 $CVMFS_EXCLUDE                               \
-                                 --                                           \
-                                 src/0*                                       \
-                              || retval=1
+# cd ${SOURCE_DIRECTORY}/test
+# echo "running CernVM-FS client test cases..."
+# CVMFS_TEST_CLASS_NAME=ClientIntegrationTests                                  \
+# ./run.sh $CLIENT_TEST_LOGFILE -o ${CLIENT_TEST_LOGFILE}${XUNIT_OUTPUT_SUFFIX} \
+#                               -x src/004-davinci                              \
+#                                  src/005-asetup                               \
+#                                  src/007-testjobs                             \
+#                                  src/024-reload-during-asetup                 \
+#                                  src/050-configrepo                           \
+#                                  $CVMFS_EXCLUDE                               \
+#                                  --                                           \
+#                                  src/0*                                       \
+#                               || retval=1
 
 
-if [ x"$(uname -m)" = x"x86_64" ]; then
-  echo "running CernVM-FS server test cases..."
-  CVMFS_TEST_CLASS_NAME=ServerIntegrationTests                                  \
-  ./run.sh $SERVER_TEST_LOGFILE -o ${SERVER_TEST_LOGFILE}${XUNIT_OUTPUT_SUFFIX} \
-                                -x src/518-hardlinkstresstest                   \
-                                   src/585-xattrs                               \
-                                   src/600-securecvmfs                          \
-                                   $CVMFS_EXCLUDE                               \
-                                   --                                           \
-                                   src/5*                                       \
-                                   src/6*                                       \
-                                   src/7*                                       \
-                                   src/8*                                       \
-                                || retval=1
-fi
+# if [ x"$(uname -m)" = x"x86_64" ]; then
+#   echo "running CernVM-FS server test cases..."
+#   CVMFS_TEST_CLASS_NAME=ServerIntegrationTests                                  \
+#   ./run.sh $SERVER_TEST_LOGFILE -o ${SERVER_TEST_LOGFILE}${XUNIT_OUTPUT_SUFFIX} \
+#                                 -x src/518-hardlinkstresstest                   \
+#                                    src/585-xattrs                               \
+#                                    src/600-securecvmfs                          \
+#                                    $CVMFS_EXCLUDE                               \
+#                                    --                                           \
+#                                    src/5*                                       \
+#                                    src/6*                                       \
+#                                    src/7*                                       \
+#                                    src/8*                                       \
+#                                 || retval=1
+# fi
 
 
 echo -n "starting the test S3 provider... "
@@ -124,11 +124,11 @@ if [ $s3_retval -eq 0 ]; then
 fi
 
 
-# Ubuntu 12.04
-echo "running CernVM-FS migration test cases..."
-CVMFS_TEST_CLASS_NAME=MigrationTests \
-./run.sh $MIGRATIONTEST_LOGFILE -o ${MIGRATIONTEST_LOGFILE}${XUNIT_OUTPUT_SUFFIX} \
-                                   migration_tests/*                              \
-                                || retval=1
+# # Ubuntu 12.04
+# echo "running CernVM-FS migration test cases..."
+# CVMFS_TEST_CLASS_NAME=MigrationTests \
+# ./run.sh $MIGRATIONTEST_LOGFILE -o ${MIGRATIONTEST_LOGFILE}${XUNIT_OUTPUT_SUFFIX} \
+#                                    migration_tests/*                              \
+#                                 || retval=1
 
 exit $retval
