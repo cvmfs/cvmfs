76be5196535bc87a2de4be098a628b2ef7b79d4f
Radu Popescu <mail@radupopescu.net>
2018-06-12 10:17:08 +0200

    Create separate platform configs for S3-backed integration test

--- test/cloud_testing/platforms/slc6_x86_64_test.sh
+++ test/cloud_testing/platforms/slc6_x86_64_test.sh
@@ -77,58 +77,6 @@ CVMFS_TEST_CLASS_NAME=ServerIntegrationTests                                  \
                               || retval=1
 
 
-echo -n "starting the test S3 provider... "
-s3_retval=0
-test_s3_pid=$(start_test_s3 $TEST_S3_LOGFILE) || { s3_retval=1; retval=1; echo "fail"; }
-echo "done ($test_s3_pid)"
-create_test_s3_bucket
-
-if [ $s3_retval -eq 0 ]; then
-  echo "running CernVM-FS server test cases against the test S3 provider..."
-  CVMFS_TEST_S3_CONFIG=$TEST_S3_CONFIG                                      \
-  CVMFS_TEST_HTTP_BASE=$TEST_S3_URL                                         \
-  CVMFS_TEST_CLASS_NAME=S3ServerIntegrationTests                            \
-  ./run.sh $S3_TEST_LOGFILE -o ${S3_TEST_LOGFILE}${XUNIT_OUTPUT_SUFFIX}     \
-                            -x src/518-hardlinkstresstest                   \
-                               src/519-importlegacyrepo                     \
-                               src/522-missingchunkfailover                 \
-                               src/523-corruptchunkfailover                 \
-                               src/525-bigrepo                              \
-                               src/528-recreatespoolarea                    \
-                               src/530-recreatespoolarea_defaultkey         \
-                               src/537-symlinkedbackend                     \
-                               src/538-symlinkedstratum1backend             \
-                               src/542-storagescrubbing                     \
-                               src/543-storagescrubbing_scriptable          \
-                               src/550-livemigration                        \
-                               src/563-garbagecollectlegacy                 \
-                               src/568-migratecorruptrepo                   \
-                               src/571-localbackendumask                    \
-                               src/572-proxyfailover                        \
-                               src/583-httpredirects                        \
-                               src/585-xattrs                               \
-                               src/591-importrepo                           \
-                               src/594-backendoverwrite                     \
-                               src/595-geoipdbupdate                        \
-                               src/600-securecvmfs                          \
-                               src/605-resurrectancientcatalog              \
-                               src/607-noapache                             \
-                               src/608-infofile                             \
-                               src/610-altpath                              \
-                               src/614-geoservice                           \
-                               src/622-gracefulrmfs                         \
-                               src/626-cacheexpiry                          \
-                               --                                           \
-                               src/5*                                       \
-                               src/6*                                       \
-                               src/8*                                       \
-                               || retval=1
-
-  echo -n "Stopping the test S3 provider... "
-  sudo kill -2 $test_s3_pid && echo "done" || echo "fail"
-fi
-
-
 echo "running CernVM-FS migration test cases..."
 CVMFS_TEST_CLASS_NAME=MigrationTests                                              \
 ./run.sh $MIGRATIONTEST_LOGFILE -o ${MIGRATIONTEST_LOGFILE}${XUNIT_OUTPUT_SUFFIX} \
