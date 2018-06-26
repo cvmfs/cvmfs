2d7a9f9a98e33b18ea96f74fd851028da335230a
Samuel Teuber <s.teuber@cern.ch>
2018-06-26 14:27:42 +0200

    Current state of struct interface

--- cvmfs/export_plugin/fs_traversal_interface.h
+++ cvmfs/export_plugin/fs_traversal_interface.h
@@ -9,9 +9,9 @@
 #include "shortstring.h"
 
 enum fs_type {
-  FS_CVMFS;
-  FS_POSIX;
-  FS_SQUASH;
+  FS_CVMFS,
+  FS_POSIX,
+  FS_SQUASH,
 };
 
 struct fs_traversal_context {
@@ -25,33 +25,69 @@ struct fs_traversal_context {
   void * ctx;
 };
 
+enum fs_open_type {
+  fs_open_read = 1,
+  fs_open_write = 2,
+  fs_open_append = 4
+};
+
+struct fs_file {
+  uint64_t version;
+  uint64_t size;
+  void *ctx;
+
+  struct fs_stat *stat_info;
+
+  int (*open)(void *ctx, fs_open_type op_mode);
+  int (*close)(void *ctx);
+  int (*read)(void *ctx, char *buff, size_t len);
+  int (*write)(void *ctx, const char *buff);
+};
+
 struct fs_traversal {
 
   struct fs_traversal_context *ctx;
 
+  // NOTE(steuber): How does this work?
   struct fs_traversal *(*initialize(fs_type type, const char *repo, const char *data));
-  void (*finalize)();
+
+  void (*finalize)(struct fs_traversal_context *ctx);
 
   /**
-   * Method which returns an iterator over all files, symlinks and directories
-   * in the root destination directory
+   * Method which returns a list over the given directory
    * 
-   * @params[out] iterator Where the iterator should be saved
+   * @param[in] dir The directory over which should be iterated
+   * @param[out] buf The list of the paths to the elements in the directory
+   * @param[in] len @todo(steuber): what?
    */
-  void (*listdir)(const char *dir, char ***buf, size_t len);
+  void (*list_dir)(struct fs_traversal_context *ctx,
+                const char *dir,
+                char ***buf,
+                size_t len);
 
   /**
-   * Method which checks whether the file described by the given content hash
+   * Method which returns a stat struct given a file path.
+   * If the file doesn't exist, NULL is returned
+   * 
+   * @param[in] path The path of the object to stat
+   */
+  struct cvmfs_stat *(get_stat)(struct fs_traversal_context *ctx,
+                const char *path);
+
+  /**
+   * Method which checks whether the file described by the given content and metadata hash
    * exists in the destination file system
    * 
    * This should always be realised by a file system lookup since the all files
-   * should be hardlinked once by their content hash.
+   * should be hardlinked once by a combination of content and metadata hash.
    * 
-   * @param[in] hash The content hash of the file that should searchedc
+   * @param[in] content The content hash of the file
+   * @param[in] meta The meta hash of the file
    * @returns True if file was found, false if not
-   * 
    */
-  bool (*has_hash)(const shash::Any hash);
+  bool (*has_hash)(struct fs_traversal_context *ctx,
+                const void *content,
+                const void *meta);
 
   /**
    * Method which creates a hardlink from the given path to the file identified
@@ -62,59 +98,89 @@ struct fs_traversal {
    * 
    * Error if:
    * - Hash file does not exist
-   * - path already exists
    * 
-   * @param[in] path The path at which the file should be hardlinked.
-   * @param[in] hash The content hash of the file to hardlink
+   * @param[in] content The content hash of the file
+   * @param[in] meta The meta hash of the file
+   * @param[in] meta The meta hash of the file to hardlink
    */
-  int (*link)(const char *path, void *hash);
+  int (*link)(struct fs_traversal_context *ctx,
+                const char *path,
+                void *content,
+                void *meta);
 
   /**
    * Method removes the hardlink at the given path
    * 
    * Error if:
    * - unlink not successful
-   * GetRootIterator
+   * 
    * @param[in] path The path which should be removed
    */
-  int (*unlink)(const char *path);
+  int (*unlink)(struct fs_traversal_context *ctx,
+                const char *path);
 
   /**
    * Method which will create the given directory
    * 
+   * @todo(steuber): Fail if "deep" mkdir?
+   * 
    * @param[in] path The path to the directory that should be created
+   * @param[in] stat The stat containing the meta data for directory creation
    */
-  int (*mkdir)(const char *path);
+  int (*mkdir)(struct fs_traversal_context *ctx,
+                const char *path,
+                const struct cvmfs_stat *stat);
 
   /**
    * Method which removes the given directory
    * 
    * @param[in] path The path to the directory that should be removed
    */
-  int (*rmdir)(const char *path);
+  int (*rmdir)(struct fs_traversal_context *ctx,
+                const char *path);
 
   /**
-   * Method which creates a copy of the given file on the destination file
-   * system.
+   * Atomically creates the file representing
+   * the given content and meta data hash
    * 
-   * @param[in] fd A file descriptor to use for reading the file
-   * @param[in] hash A hash by which the file can be identified (content hash)
+   * @param[in] content The content hash of the file
+   * @param[in] meta The meta hash of the file
+   * @param[in] stat The stat containing the meta data for directory creation
    */
-  int (*copy)(const char *path, void *hash);  // TODO(steuber): shash FILE -> c?
+  int (*touch)(struct fs_traversal_context *ctx,
+                void *content,
+                void *meta,
+                const struct cvmfs_stat *stat);
+
+  /**
+   * Retrieves a method struct which allows the manipulation of the file
+   * defined by the given content and meta data hash
+   * 
+   * @param[in] content The content hash of the file
+   * @param[in] meta The meta hash of the file
+   */
+  struct fs_file *(*get_handle)(struct fs_traversal_context *ctx,
+                void *content,
+                void *meta);
+
 
   /**
    * Method which creates a symlink at src which points to dest
    * 
    * @param[in] The position at which the symlink should be saved
+   * (parent directory must exist)
    * @param[in] The position the symlink should point to
    */
-  int (*symlink)(const char *src, const char *dest);
+  int (*symlink)(struct fs_traversal_context *ctx,
+                const char *src,
+                const char *dest);
 
   /**
    * Method which executes a garbage collection on the destination file system.
    * This will remove all no longer linked content adressed files
    */
-  int (*garbage_collection)();
+  // NOTE(steuber): Shouldn't this maybe just be part of the finalize step?
+  int (*garbage_collection)(struct fs_traversal_context *ctx);
 };
 
 #endif  // CVMFS_EXPORT_PLUGIN_FS_TRAVERSAL_INTERFACE_H_
