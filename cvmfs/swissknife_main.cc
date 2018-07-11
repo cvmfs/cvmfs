c39464d464885ab285f0aeef7292e4194a023d1e
dosarudaniel@gmail.com <ddosaru@phsftpc2r20.dyndns.cern.ch>
2018-07-11 14:25:42 +0200

    Created a database file.

--- cvmfs/swissknife_main.cc
+++ cvmfs/swissknife_main.cc
@@ -33,6 +33,92 @@
 
 using namespace std;  // NOLINT
 
+struct RevisionFlags {
+  enum T {
+    kInitialRevision   = 1,
+    kUpdatableRevision = 2,
+    kUpdatedRevision   = 3,
+    kFailingRevision   = 4,
+  };
+};
+
+class StatisticsDatabase : public sqlite::Database<StatisticsDatabase> {
+ public:
+  // not const - needs to be adaptable!
+  static float        kLatestSchema;
+  // not const - needs to be adaptable!
+  static unsigned     kLatestSchemaRevision;
+  static const float  kLatestCompatibleSchema;
+  static bool         compacting_fails;
+  static unsigned int  instances;
+  unsigned int         create_empty_db_calls;
+  unsigned int         check_compatibility_calls;
+  unsigned int         live_upgrade_calls;
+  mutable unsigned int compact_calls;
+
+  bool CreateEmptyDatabase() {
+    ++create_empty_db_calls;
+
+  return sqlite::Sql(sqlite_db(),
+    "CREATE TABLE foobar (foo TEXT, bar TEXT, "
+    "  CONSTRAINT pk_foo PRIMARY KEY (foo))").Execute();
+  }
+
+  bool CheckSchemaCompatibility() {
+    ++check_compatibility_calls;
+    return (schema_version() > kLatestCompatibleSchema - 0.1 &&
+            schema_version() < kLatestCompatibleSchema + 0.1);
+  }
+
+  bool LiveSchemaUpgradeIfNecessary() {
+    ++live_upgrade_calls;
+    const unsigned int revision = schema_revision();
+
+    if (revision == RevisionFlags::kInitialRevision) {
+      return true;
+    }
+
+    if (revision == RevisionFlags::kUpdatableRevision) {
+      set_schema_revision(RevisionFlags::kUpdatedRevision);
+      StoreSchemaRevision();
+      return true;
+    }
+
+    if (revision == RevisionFlags::kFailingRevision) {
+      return false;
+    }
+
+    return false;
+  }
+
+  bool CompactDatabase() const {
+    ++compact_calls;
+    return !compacting_fails;
+  }
+
+  ~StatisticsDatabase() {
+    --StatisticsDatabase::instances;
+  }
+
+ protected:
+  // TODO(rmeusel): C++11 - constructor inheritance
+  friend class sqlite::Database<StatisticsDatabase>;
+  StatisticsDatabase(const std::string  &filename,
+                const OpenMode      open_mode) :
+    sqlite::Database<StatisticsDatabase>(filename, open_mode),
+    create_empty_db_calls(0),  check_compatibility_calls(0),
+    live_upgrade_calls(0), compact_calls(0)
+  {
+    ++StatisticsDatabase::instances;
+  }
+};
+
+const float    StatisticsDatabase::kLatestCompatibleSchema = 1.0f;
+float          StatisticsDatabase::kLatestSchema           = 1.0f;
+unsigned       StatisticsDatabase::kLatestSchemaRevision   = RevisionFlags::kInitialRevision;
+unsigned int   StatisticsDatabase::instances               = 0;
+bool           StatisticsDatabase::compacting_fails        = false;
+
 typedef vector<swissknife::Command *> Commands;
 Commands command_list;
 
@@ -187,10 +273,18 @@ int main(int argc, char **argv) {
   // run the command
   const int retval = command->Main(args);
   if (display_statistics) {
+    // get the repo name
+    string repo_name = *args.find('N')->second;
+    // LogCvmfs(kLogCvmfs, kLogStdout, "For repository [%s]", repo_name.c_str());
+    // create a new database file if is not already there
+    StatisticsDatabase *db = StatisticsDatabase::Create(repo_name + ".db");
+
     LogCvmfs(kLogCvmfs, kLogStdout, "Command statistics");
     LogCvmfs(kLogCvmfs, kLogStdout, "%s",
              command->statistics()
              ->PrintList(perf::Statistics::kPrintHeader).c_str());
+
+    delete db;
   }
 
   // delete the command list
