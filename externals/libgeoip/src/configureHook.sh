89c56c77f349a39a4e6c9dd4f8d4d9d54bbe2a43
Dave Dykstra <2129743+DrDaveD@users.noreply.github.com>
2018-07-24 17:33:31 -0500

    remove libgeoip, python-geoip

--- externals/libgeoip/src/configureHook.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-sh configure CFLAGS="$(echo $CVMFS_BASE_CXX_FLAGS | sed s/-fvisibility=hidden//) -fPIC" \
-    --with-pic \
-    --disable-shared \
-    --prefix=$EXTERNALS_INSTALL_LOCATION
-
