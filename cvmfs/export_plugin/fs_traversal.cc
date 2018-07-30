50cc29315a6476735e739571503e5c06b1123a14
Nicholas Hazekamp <nhazekam@nd.edu>
2018-07-30 17:23:31 +0200

    Updated libcvmfs and working on integration test

--- cvmfs/export_plugin/fs_traversal.cc
+++ cvmfs/export_plugin/fs_traversal.cc
@@ -395,14 +395,13 @@ bool handle_file(
   if (!dest->touch(dest->context_, src_st)
     || (dest_st->cvm_checksum != NULL
       && should_write_anyway(dest, src_st, dest_st)) ) {
-    char *src_ident = src->get_identifier(dest->context_, src_st);
-    if (num_parallel) {
+    char *src_ident = src->get_identifier(src->context_, src_st);
+    if (num_parallel_) {
       FileCopy next_copy(src_ident, strdup(dest_data));
       WritePipe(pipe_chunks[1], &next_copy, sizeof(next_copy));
       atomic_inc64(&copy_queue);
     } else {
       if (!copyFile(src, src_ident, dest, dest_data, pstats)) {
-        free(src_ident);
         LogCvmfs(kLogCvmfs, kLogStderr,
           "Failed to copy %s->%s : %d : %s",
           entry, dest_data, errno, strerror(errno));
