8323b7a1142ca746b8f97bfd8fe911896685c45f
Jakob Blomer <jblomer@cern.ch>
2018-08-14 11:41:47 +0200

    disable service rate limit for CC7 / autofs+httpd

--- test/cloud_testing/platforms/centos7_x86_64_setup.sh
+++ test/cloud_testing/platforms/centos7_x86_64_setup.sh
@@ -73,6 +73,19 @@ echo -n "increasing ulimit -n ... "
 set_nofile_limit 65536 || die "fail"
 echo "done"
 
+# Disable service start rate limiting for apache and autofs
+mkdir -p /lib/systemd/system/httpd.service.d
+cat << EOF > /lib/systemd/system/httpd.service.d/cvmfs-test.conf
+[Unit]
+StartLimitIntervalSec=0
+EOF
+mkdir -p /lib/systemd/system/autofs.service.d
+cat << EOF > /lib/systemd/system/autofs.service.d/cvmfs-test.conf
+[Unit]
+StartLimitIntervalSec=0
+EOF
+sudo systemctl daemon-reload
+
 # Ensure Apache is up and running after package update
 sudo systemctl restart httpd || die "failure in final Apache restart"
 
