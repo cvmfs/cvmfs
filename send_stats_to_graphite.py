7b8f1fc04a901b6425ac16aeaf1f13542c5d593e
dosarudaniel <dosarudaniel@gmail.com>
2018-09-10 16:50:33 +0200

    Commented debug print calls

--- send_stats_to_graphite.py
+++ send_stats_to_graphite.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python
 
-# usage:
-# python example-pickle-client.py [<NR_SECONDS_FOR_DELAY>]
-# the stats.db SQLite local file should be in the same directory
-
-# this is script is made to run continuously, at NR_SECONDS_FOR_DELAY interval (default 10s)
-
 import argparse
 import pickle
 import re
@@ -16,13 +10,11 @@ import sys
 import time
 from calendar import timegm
 
-
-
 def get_data_publish_stats(last_timestamp):
     # check in server.conf if CVMFS_STATISTICS_DB variable is set
     conn = sqlite3.connect('stats.db') # change path
     c = conn.cursor()
-    print "Last publish submitted:" + last_timestamp
+
     # order DESC by id ---> first element of the list has the last start_time
     c.execute('SELECT start_time, files_added, files_removed, files_changed \
                 FROM publish_statistics \
@@ -35,8 +27,6 @@ def get_data_gc_stats(last_timestamp):
     # check in server.conf if CVMFS_STATISTICS_DB variable is set
     conn = sqlite3.connect('stats.db') # change path
     c = conn.cursor()
-
-    print "Last gc submitted:" + last_timestamp
 
     # order DESC by id ---> first element of the list has the last start_time
     c.execute('SELECT start_time, n_preserved_catalogs, n_condemned_catalogs, n_condemned_objects, sz_condemned_bytes \
@@ -53,62 +43,59 @@ def run(sock):
     # the following file must have two timestamps (one per line):
     # last_start_time_sent_for_publish_statistics
     # last_start_time_sent_for_gc_statistics
-    
     f=open("aux.txt", "r+") # todo: change path
 
     # read first two lines
     publish_timestamp = f.readline()[:-1] # delete the newline character
     gc_timestamp = f.readline()
     
-    
     f.truncate() # delete file content
     f.seek(0)    # move file cursor
 
-    print publish_timestamp
-    print gc_timestamp
+    # # for debug
+    # print publish_timestamp
+    # print gc_timestamp
 
     publish_stats = get_data_publish_stats(publish_timestamp)
     gc_stats = get_data_gc_stats(gc_timestamp)
-    print "length stats publish=%d " % len(publish_stats)
-    print "length   stats    gc=%d " % len(gc_stats)
+
+    # # for debug
+    # print "length stats publish=%d " % len(publish_stats)
+    # print "length   stats    gc=%d " % len(gc_stats)
 
     if len(publish_stats) > 0:
         publish_timestamp = publish_stats[0][0]
         for x in xrange(0,len(publish_stats)):
-            print "x = %d" % x
-            print publish_stats[x][0]
             time_obj = time.strptime(publish_stats[x][0], "%Y-%m-%d %H:%M:%S")
             timestamp_epoch = timegm(time_obj)
             tuples.append(('cvmfs.publish.files_added', (timestamp_epoch, publish_stats[x][1])))
             tuples.append(('cvmfs.publish.files_removed', (timestamp_epoch, publish_stats[x][2])))
             tuples.append(('cvmfs.publish.files_changed', (timestamp_epoch, publish_stats[x][3])))
-            #just for DBG
-            lines.append("cvmfs.publish.files_added %s %s" % (publish_stats[x][1], publish_stats[x][0]))
-            lines.append("cvmfs.publish.files_removed %s %s" % (publish_stats[x][2], publish_stats[x][0]))
-            lines.append("cvmfs.publish.files_changed %s %s" % (publish_stats[x][3], publish_stats[x][0]))
+            # #just for DBG
+            # lines.append("cvmfs.publish.files_added %s %s" % (publish_stats[x][1], publish_stats[x][0]))
+            # lines.append("cvmfs.publish.files_removed %s %s" % (publish_stats[x][2], publish_stats[x][0]))
+            # lines.append("cvmfs.publish.files_changed %s %s" % (publish_stats[x][3], publish_stats[x][0]))
 
     if len(gc_stats) > 0:
         gc_timestamp=gc_stats[0][0]
         for x in xrange(0,len(gc_stats)):
-            print "x = %d" % x
-            print gc_stats[x][0]
             time_obj = time.strptime(gc_stats[x][0], "%Y-%m-%d %H:%M:%S")
             timestamp_epoch = timegm(time_obj)
             tuples.append(('cvmfs.gc.n_preserved_catalogs', (timestamp_epoch, gc_stats[x][1])))
             tuples.append(('cvmfs.gc.n_condemned_catalogs', (timestamp_epoch, gc_stats[x][2])))
             tuples.append(('cvmfs.gc.n_condemned_objects', (timestamp_epoch, gc_stats[x][3])))
             tuples.append(('cvmfs.gc.sz_condemned_bytes', (timestamp_epoch, gc_stats[x][4])))
-            #just for DBG
-            lines.append("cvmfs.gc.n_preserved_catalogs %s %s" % (gc_stats[x][1], gc_stats[x][0]))
-            lines.append("cvmfs.gc.n_condemned_catalogs %s %s" % (gc_stats[x][2], gc_stats[x][0]))
-            lines.append("cvmfs.gc.n_condemned_objects %s %s" % (gc_stats[x][3], gc_stats[x][0]))
-            lines.append("cvmfs.gc.sz_condemned_bytes %s %s" % (gc_stats[x][3], gc_stats[x][0]))
+            # #just for DBG
+            # lines.append("cvmfs.gc.n_preserved_catalogs %s %s" % (gc_stats[x][1], gc_stats[x][0]))
+            # lines.append("cvmfs.gc.n_condemned_catalogs %s %s" % (gc_stats[x][2], gc_stats[x][0]))
+            # lines.append("cvmfs.gc.n_condemned_objects %s %s" % (gc_stats[x][3], gc_stats[x][0]))
+            # lines.append("cvmfs.gc.sz_condemned_bytes %s %s" % (gc_stats[x][3], gc_stats[x][0]))
     
-    #just for DBG
-    message = '\n'.join(lines) + '\n' #all lines must end in a newline
-    print("sending message")
-    print('-' * 80)
-    print(message)
+    # #just for DBG
+    # message = '\n'.join(lines) + '\n' #all lines must end in a newline
+    # print("sending message")
+    # print('-' * 80)
+    # print(message)
 
     # build the package
     package = pickle.dumps(tuples, 1)
@@ -139,7 +126,7 @@ def main():
     except socket.error:
         raise SystemExit("Couldn't connect to %(server)s on port %(port)d, is graphite running in a docker environment?" % { 'server':CARBON_SERVER_IP, 'port':CARBON_PICKLE_PORT })
 
-    # send data
+    # send new stats to carbon server if available
     try:
         run(sock)
     except KeyboardInterrupt:
