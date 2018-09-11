a1aca7e7578b236ad2dfa926931cc6ab705486c7
dosarudaniel <dosarudaniel@gmail.com>
2018-09-11 10:21:01 +0200

    Fixed coding style and changed python script location.

--- python/send_stats_to_graphite.py
+++ python/send_stats_to_graphite.py
@@ -10,27 +10,31 @@ import sys
 import time
 from calendar import timegm
 
+
 def get_data_publish_stats(last_timestamp, db_file):
     conn = sqlite3.connect(db_file)
     c = conn.cursor()
 
     # order DESC by id ---> first element of the list has the last start_time
-    c.execute('SELECT start_time, files_added, files_removed, files_changed, duplicated_files, directories_added, \
-                      directories_removed, directories_changed, sz_bytes_added, sz_bytes_removed, sz_bytes_uploaded \
+    c.execute('SELECT start_time, files_added, files_removed, files_changed, \
+                    duplicated_files, directories_added, directories_removed, \
+                    directories_changed, sz_bytes_added, sz_bytes_removed, \
+                    sz_bytes_uploaded \
                 FROM publish_statistics \
                 WHERE start_time > ("%s") \
                 ORDER BY publish_id DESC' % last_timestamp)
-    return c.fetchall() 
+    return c.fetchall()
 
 
 def get_data_gc_stats(last_timestamp, db_file):
     conn = sqlite3.connect(db_file)
     c = conn.cursor()
-    c.execute('SELECT start_time, n_preserved_catalogs, n_condemned_catalogs, n_condemned_objects, sz_condemned_bytes \
+    c.execute('SELECT start_time, n_preserved_catalogs, n_condemned_catalogs,\
+                    n_condemned_objects, sz_condemned_bytes \
                 FROM gc_statistics \
                 WHERE start_time > ("%s") \
-                ORDER BY gc_id DESC' % last_timestamp )
-    return c.fetchall() 
+                ORDER BY gc_id DESC' % last_timestamp)
+    return c.fetchall()
 
 
 def run(sock, db_file):
@@ -40,21 +44,23 @@ def run(sock, db_file):
     # the following file must have two timestamps (one per line):
     # last_start_time_sent_for_publish_statistics
     # last_start_time_sent_for_gc_statistics
-    f=open("aux.txt", "r+") # todo: change path to `/var/spool/cvmfs/<REPO>/last_timestamp_sent`
+
+    # todo: change path to `/var/spool/cvmfs/<REPO>/last_timestamp_sent`
+    f = open("aux.txt", "r+")
 
     # read first two lines
-    publish_timestamp = f.readline()[:-1] # delete the newline character
+    publish_timestamp = f.readline()[:-1]  # delete the newline character
     gc_timestamp = f.readline()
-    
-    f.truncate() # delete file content
-    f.seek(0)    # move file cursor
+
+    f.truncate()  # delete file content
+    f.seek(0)     # move file cursor
 
     publish_stats = get_data_publish_stats(publish_timestamp, db_file)
     gc_stats = get_data_gc_stats(gc_timestamp, db_file)
 
     if len(publish_stats) > 0:
         publish_timestamp = publish_stats[0][0]
-        for x in xrange(0,len(publish_stats)):
+        for x in xrange(0, len(publish_stats)):
             time_obj = time.strptime(publish_stats[x][0], "%Y-%m-%d %H:%M:%S")
             timestamp_epoch = timegm(time_obj)
             tuples.append(('cvmfs.publish.files_added', (timestamp_epoch, publish_stats[x][1])))
@@ -80,8 +86,8 @@ def run(sock, db_file):
             # lines.append("cvmfs.publish.sz_bytes_uploaded %s %s" % (publish_stats[x][10], publish_stats[x][0]))
 
     if len(gc_stats) > 0:
-        gc_timestamp=gc_stats[0][0]
-        for x in xrange(0,len(gc_stats)):
+        gc_timestamp = gc_stats[0][0]
+        for x in xrange(0, len(gc_stats)):
             time_obj = time.strptime(gc_stats[x][0], "%Y-%m-%d %H:%M:%S")
             timestamp_epoch = timegm(time_obj)
             tuples.append(('cvmfs.gc.n_preserved_catalogs', (timestamp_epoch, gc_stats[x][1])))
@@ -93,7 +99,7 @@ def run(sock, db_file):
             # lines.append("cvmfs.gc.n_condemned_catalogs %s %s" % (gc_stats[x][2], gc_stats[x][0]))
             # lines.append("cvmfs.gc.n_condemned_objects %s %s" % (gc_stats[x][3], gc_stats[x][0]))
             # lines.append("cvmfs.gc.sz_condemned_bytes %s %s" % (gc_stats[x][3], gc_stats[x][0]))
-    
+
     # #just for DBG
     # message = '\n'.join(lines) + '\n' #all lines must end in a newline
     # print("sending message")
@@ -107,9 +113,10 @@ def run(sock, db_file):
     sock.sendall(package)
 
     # write the last finished_time into a file
-    f.write(publish_timestamp + "\n") # write puclish_statistics last finished_timestamp
-    f.write(gc_timestamp) # write gc_statistics last finished_timestamp
+    f.write(publish_timestamp + "\n")  # write last publish start_time
+    f.write(gc_timestamp)              # write last gc start_time
     f.close()
+
 
 def main():
     parser = argparse.ArgumentParser(description='Send stats to carbon server using pickle.')
@@ -121,16 +128,16 @@ def main():
                         help='carbon pickle port')
 
     args = parser.parse_args()
-    db_file=args.db_file
+    db_file = args.db_file
     CARBON_SERVER_IP = args.CARBON_SERVER_IP
     CARBON_PICKLE_PORT = args.CARBON_PICKLE_PORT
 
     # connect to graphite
     sock = socket.socket()
     try:
-        sock.connect( (CARBON_SERVER_IP, CARBON_PICKLE_PORT) )
+        sock.connect((CARBON_SERVER_IP, CARBON_PICKLE_PORT))
     except socket.error:
-        raise SystemExit("Couldn't connect to %(server)s on port %(port)d, is graphite running in a docker environment?" % { 'server':CARBON_SERVER_IP, 'port':CARBON_PICKLE_PORT })
+        raise SystemExit("Couldn't connect to %(server)s on port %(port)d, is graphite running in a docker environment?" % {'server': CARBON_SERVER_IP, 'port': CARBON_PICKLE_PORT})
 
     # send new stats to carbon server if available
     try:
@@ -138,6 +145,7 @@ def main():
     except KeyboardInterrupt:
         sys.stderr.write("\nExiting on CTRL-c\n")
         sys.exit(0)
+
 
 if __name__ == "__main__":
     main()
