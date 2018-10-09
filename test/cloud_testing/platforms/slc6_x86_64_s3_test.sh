41fca94d06b675cfde940f36763410a4a528bfa4
Jakob Blomer <jblomer@cern.ch>
2018-10-09 17:15:03 +0200

    fix S3 errors in quick integration tests

--- test/cloud_testing/platforms/slc6_x86_64_s3_test.sh
+++ test/cloud_testing/platforms/slc6_x86_64_s3_test.sh
@@ -73,6 +73,7 @@ if [ $s3_retval -eq 0 ]; then
                                src/519-importlegacyrepo                     \
                                src/522-missingchunkfailover                 \
                                src/523-corruptchunkfailover                 \
+                               src/524-corruptmanifestfailover              \
                                src/525-bigrepo                              \
                                src/528-recreatespoolarea                    \
                                src/530-recreatespoolarea_defaultkey         \
@@ -93,7 +94,7 @@ if [ $s3_retval -eq 0 ]; then
                                src/600-securecvmfs                          \
                                src/605-resurrectancientcatalog              \
                                src/607-noapache                             \
-                               src/608-infofile                             \
+                               src/609-metainfofile                         \
                                src/610-altpath                              \
                                src/614-geoservice                           \
                                src/622-gracefulrmfs                         \
