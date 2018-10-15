8cec5c8c84dd6b83a74ef49207ae7e7fb1d0fe8b
Radu Popescu <mail@radupopescu.net>
2018-10-15 18:27:56 +0200

    Updating Travis CI script

--- ci/travis_main.sh
+++ ci/travis_main.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run --rm -it -v "${PWD}":/mnt radupopescu/erlang-libsodium:20.2 \
+docker run --rm -it -v "${PWD}":/mnt radupopescu/erlang-libsodium:21 \
        sh -c "cd /mnt && rebar3 release && rebar3 as test dialyzer,ct && rebar3 as prod tar"
