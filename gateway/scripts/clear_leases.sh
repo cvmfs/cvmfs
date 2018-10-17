80bdef187984db7b6d04edcf21809e98151f9aea
Radu Popescu <mail@radupopescu.net>
2018-10-17 13:39:59 +0200

    Perform all logging under /var/log/cvmfs-gateway

--- gateway/scripts/clear_leases.sh
+++ gateway/scripts/clear_leases.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_LOCATION=$(cd "$(dirname "$0")"; pwd)
 
-RUNNER_LOG_DIR=/var/log/cvmfs-gateway-runner $SCRIPT_LOCATION/../bin/cvmfs_gateway escript scripts/clear_leases.escript
+RUNNER_LOG_DIR=/var/log/cvmfs-gateway $SCRIPT_LOCATION/../bin/cvmfs_gateway escript scripts/clear_leases.escript
