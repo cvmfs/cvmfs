8997dfc38ac885d9ffcafef375a0234c58bf6d4d
Radu Popescu <mail@radupopescu.net>
2018-11-02 09:52:31 +0100

    Integration tests: systemd < 230 uses StartLimitInterval

--- test/cloud_testing/platforms/common.sh
+++ test/cloud_testing/platforms/common.sh
@@ -189,9 +189,15 @@ disable_systemd_rate_limit() {
   sudo mkdir -p $apachedir
   sudo mkdir -p $autofsdir
 
+  local limit_interval_setting="StartLimitIntervalSec=0"
+  local systemd_version=$(systemctl --version | head -1 | cut -d' ' -f2)
+  if [ "$systemd_version" -lt "230" ]; then
+    limit_interval_setting="StartLimitInterval=0"
+  fi
+
   cat << EOF > cvmfs-test.conf
 [Unit]
-StartLimitIntervalSec=0
+$limit_interval_setting
 EOF
   sudo cp cvmfs-test.conf $apachedir/cvmfs-test.conf
   sudo cp cvmfs-test.conf $autofsdir/cvmfs-test.conf
