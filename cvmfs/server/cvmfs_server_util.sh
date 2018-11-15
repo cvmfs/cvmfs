da070be5e3d969c35a5a3a5a7f7b928357d753b4
Jakob Blomer <jblomer@cern.ch>
2018-11-15 11:24:29 +0100

    allow for syncfs levels none, default, cautious

--- cvmfs/server/cvmfs_server_util.sh
+++ cvmfs/server/cvmfs_server_util.sh
@@ -1023,11 +1023,18 @@ is_subcommand() {
 }
 
 
-# Flushes data to disk; we might at some point want to do more than just sync
+# Flushes data to disk; only flush if the enforced level allowed for the
+# provided level.  The order is
+#  'none' (never sync)
+#  'default' (sync for rare, important operations like mkfs, publish)
+#  'cautious' (always sync)
 syncfs() {
-  if [ "x$CVMFS_DISABLE_SYNCFS" != "xtrue" ]; then
-    sync
-  fi
+  local level="${1:-default}"
+  local enforced_level="${CVMFS_SYNCFS_LEVEL:-default}"
+  [ "x$enforced_level" = "xnone" ] && return || true
+  [ "x$enforced_level" = "xdefault" -a "x$level" = "xcautious" ] && return || true
+
+  sync
 }
 
 
