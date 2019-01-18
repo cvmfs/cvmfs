1437e58102372c964efdf08ec5b734abcc7af498
Jakob Blomer <jblomer@cern.ch>
2019-01-18 17:23:50 +0100

    add mkfs command ui

--- cvmfs/publish/cmd_zpipe.h
+++ cvmfs/publish/cmd_zpipe.h
@@ -2,8 +2,8 @@
  * This file is part of the CernVM File System.
  */
 
-#ifndef CVMFS_ZPIPE_CMD_HELP_H_
-#define CVMFS_ZPIPE_CMD_HELP_H_
+#ifndef CVMFS_PUBLISH_CMD_ZPIPE_H_
+#define CVMFS_PUBLISH_CMD_ZPIPE_H_
 
 #include <string>
 
@@ -36,4 +36,4 @@ class CmdZpipe : public Command {
 
 }  // namespace publish
 
-#endif  // CVMFS_ZPIPE_CMD_HELP_H_
+#endif  // CVMFS_PUBLISH_CMD_ZPIPE_H_
