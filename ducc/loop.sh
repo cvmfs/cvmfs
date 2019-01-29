d6ec69b69288ece377b8266e54b7dc23b099068d
Simone Mosciatti <simone.mosciatti@cern.ch>
2019-01-29 10:02:10 +0100

    add logic if gitlab does not collaborate

--- ducc/loop.sh
+++ ducc/loop.sh
@@ -8,12 +8,30 @@ do
         
         git --version > /dev/null 2>&1 || { echo "git is required but it is not installed. Aborting."; exit 1; }
         ducc version > /dev/null 2>&1 || { echo "ducc is required but it is not installed. Aborting."; exit 1; }
-        
-        git clone ssh://git@gitlab.cern.ch:7999/unpacked/sync.git unpacked > /dev/null
-        pushd unpacked
-        git pull origin master || { echo "error in pulling the repository"; exit 1;}
-        popd
 
+	update_error_count=0
+	while true
+	do
+
+		# if for 3 times was no possible to update the repository we send an email and we exit.
+		if [ $update_error_count -ge 3 ]; then
+			echo "" | mail -s "[unpacked.cern.ch] Error on contacting the gitlab repository" -t simone.mosciatti@cern.ch
+			exit 2;
+		fi
+
+	        (git clone ssh://git@gitlab.cern.ch:7999/unpacked/sync.git unpacked > /dev/null)
+
+	        pushd unpacked
+	        (git pull origin master)
+	        pull_status=$?
+		popd
+
+		if [ $pull_status -eq 0 ]; then
+			break;
+		else
+			$update_error_count=$(( update_error_count+1))
+		fi
+	done
 
         (ducc convert unpacked/recipe.yaml)
         conversion_status=$?
@@ -24,7 +42,7 @@ do
                 echo "Error: $conversion_status"
                 echo "error_count: $error_count"
                 if [ $error_count -ge 10 ]; then
-                        echo "" | mail -s "Error on unpacked.cern.ch $conversion_status" -t simone.mosciatti@cern.ch
+                        echo "" | mail -s "[unpacked.cern.ch] Error on unpacked.cern.ch $conversion_status" -t simone.mosciatti@cern.ch
                         exit 1
                 fi
         else
