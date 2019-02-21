ae6f772ffa93c6ca0e51f1f21e051cb40c1b4be6
Simone Mosciatti <simone.mosciatti@cern.ch>
2019-02-21 16:04:52 +0100

    remove use of (Un)LockMutex from cvmfs.cc

--- cvmfs/cvmfs.cc
+++ cvmfs/cvmfs.cc
@@ -1025,7 +1025,7 @@ static void cvmfs_read(fuse_req_t req, fuse_ino_t ino, size_t size, off_t off,
 
     // Lock chunk handle
     pthread_mutex_t *handle_lock = chunk_tables->Handle2Lock(chunk_handle);
-    LockMutex(handle_lock);
+    MutexLockGuard m(handle_lock);
     chunk_tables->Lock();
     retval = chunk_tables->handle2fd.Lookup(chunk_handle, &chunk_fd);
     assert(retval);
@@ -1064,7 +1064,6 @@ static void cvmfs_read(fuse_req_t req, fuse_ino_t ino, size_t size, off_t off,
           chunk_tables->Lock();
           chunk_tables->handle2fd.Insert(chunk_handle, chunk_fd);
           chunk_tables->Unlock();
-          UnlockMutex(handle_lock);
           fuse_reply_err(req, EIO);
           return;
         }
@@ -1091,7 +1090,6 @@ static void cvmfs_read(fuse_req_t req, fuse_ino_t ino, size_t size, off_t off,
         chunk_tables->Lock();
         chunk_tables->handle2fd.Insert(chunk_handle, chunk_fd);
         chunk_tables->Unlock();
-        UnlockMutex(handle_lock);
         fuse_reply_err(req, -bytes_fetched);
         return;
       }
@@ -1107,7 +1105,6 @@ static void cvmfs_read(fuse_req_t req, fuse_ino_t ino, size_t size, off_t off,
     chunk_tables->Lock();
     chunk_tables->handle2fd.Insert(chunk_handle, chunk_fd);
     chunk_tables->Unlock();
-    UnlockMutex(handle_lock);
     LogCvmfs(kLogCvmfs, kLogDebug, "released chunk file descriptor %d",
              chunk_fd.fd);
   } else {
