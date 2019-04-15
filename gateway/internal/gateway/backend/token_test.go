da46c3da38ce61cbf2887c1eb77aa94634da40bb
Radu Popescu <mail@radupopescu.net>
2019-04-15 14:52:24 +0200

    Pack return of NewLeaseToken into a struct

--- gateway/internal/gateway/backend/token_test.go
+++ gateway/internal/gateway/backend/token_test.go
@@ -7,11 +7,11 @@ import (
 
 func TestLeaseToken(t *testing.T) {
 	t.Run("identity", func(t *testing.T) {
-		token, secret, err := NewLeaseToken("test.repo.org/some/path", 10*time.Second)
+		token, err := NewLeaseToken("test.repo.org/some/path", 10*time.Second)
 		if err != nil {
 			t.Fatalf("token generation failed: %v", err)
 		}
-		repoPath, err := CheckToken(token, secret)
+		repoPath, err := CheckToken(token.TokenStr, token.Secret)
 		if err != nil {
 			t.Fatalf("valid token was rejected: %v", err)
 		}
@@ -20,11 +20,11 @@ func TestLeaseToken(t *testing.T) {
 		}
 	})
 	t.Run("reject expired", func(t *testing.T) {
-		token1, secret1, err := NewLeaseToken("test.repo.org/some/path", 1*time.Microsecond)
+		token1, err := NewLeaseToken("test.repo.org/some/path", 1*time.Microsecond)
 		if err != nil {
 			t.Fatalf("token generation failed: %v", err)
 		}
-		if _, err := CheckToken(token1, secret1); err == nil {
+		if _, err := CheckToken(token1.TokenStr, token1.Secret); err == nil {
 			t.Fatalf("expired token was accepted")
 		} else {
 			if _, ok := err.(ExpiredTokenError); !ok {
@@ -33,15 +33,15 @@ func TestLeaseToken(t *testing.T) {
 		}
 	})
 	t.Run("reject forged", func(t *testing.T) {
-		_, secret1, err := NewLeaseToken("test.repo.org/some/path", 10*time.Second)
+		token1, err := NewLeaseToken("test.repo.org/some/path", 10*time.Second)
 		if err != nil {
 			t.Fatalf("token generation failed: %v", err)
 		}
-		token2, _, err := NewLeaseToken("test.repo.org/some/path", 10*time.Second)
+		token2, err := NewLeaseToken("test.repo.org/some/path", 10*time.Second)
 		if err != nil {
 			t.Fatalf("token generation failed: %v", err)
 		}
-		if _, err := CheckToken(token2, secret1); err == nil {
+		if _, err := CheckToken(token2.TokenStr, token1.Secret); err == nil {
 			t.Fatalf("forget token was accepted")
 		} else {
 			if _, ok := err.(InvalidTokenError); !ok {
@@ -50,11 +50,11 @@ func TestLeaseToken(t *testing.T) {
 		}
 	})
 	t.Run("reject garbage", func(t *testing.T) {
-		_, secret1, err := NewLeaseToken("test.repo.org/some/path", 10*time.Second)
+		token, err := NewLeaseToken("test.repo.org/some/path", 10*time.Second)
 		if err != nil {
 			t.Fatalf("token generation failed: %v", err)
 		}
-		if _, err := CheckToken("gsjhfjdklshfjlksadhflkjsd", secret1); err == nil {
+		if _, err := CheckToken("gsjhfjdklshfjlksadhflkjsd", token.Secret); err == nil {
 			t.Fatalf("garbage string was accepted as real token")
 		}
 	})
