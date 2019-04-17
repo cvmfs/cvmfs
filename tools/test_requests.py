4c525b7a067c6ec67ec96d0999511ae38ef3fe03
Radu Popescu <mail@radupopescu.net>
2019-04-17 14:32:44 +0200

    Added lease creation/query/deletion through HTTP frontend

--- tools/test_requests.py
+++ tools/test_requests.py
@@ -6,27 +6,46 @@ import hmac
 import json
 import requests
 
+def errMissingArg(argument):
+    print('Missing argument: "{}"'.format(argument))
+
 def computeHMAC(msg, key):
     d = hmac.HMAC(key, msg, digestmod='sha1').digest()
     return base64.b64encode(d).decode('utf-8')
 
 parser = argparse.ArgumentParser(description='Test gateway API requests')
-parser.add_argument('--request', required=True, help='which command to perform (get_repos|new_lease|drop_lease|submit_payload|commit)')
+parser.add_argument(
+    '--request', required=True,
+    help='which command to perform (get_repos|get_leases|new_lease|cancel_lease|submit_payload|commit)')
+parser.add_argument('--path', required=False, help="lease path")
+parser.add_argument('--token', required=False, help='lease token string')
 args = parser.parse_args()
-
-print('Request: {}'.format(args.request))
 
 base_url = 'http://localhost:4929/api/v1'
 
 key_id = 'key1'
 secret = b'secret1'
 
-if args.request == 'repos':
+rep = None
+if args.request == 'get_repos':
     rep = requests.get(base_url + '/repos')
-    print('Reply: {}'.format(rep.json()))
+elif args.request == 'get_leases':
+    rep = requests.get(base_url + '/leases')
 elif args.request == 'new_lease':
-    req = {'path':'/','api_version':2}
-    hmac_msg = json.dumps(req).encode()
-    headers = {'authorization': key_id + ' ' + computeHMAC(hmac_msg, secret)}
-    rep = requests.post(base_url + '/leases', json=req, headers=headers)
-    print('Reply: {}'.format(rep.json()))
+    if args.path:
+        req = {'path':args.path,'api_version':2}
+        hmac_msg = json.dumps(req).encode()
+        headers = {'authorization': key_id + ' ' + computeHMAC(hmac_msg, secret)}
+        rep = requests.post(base_url + '/leases', json=req, headers=headers)
+    else:
+        errMissingArg('--path')
+elif args.request == 'cancel_lease':
+    if args.token:
+        token = args.token
+        hmac_msg = token.encode()
+        headers = {'authorization': key_id + ' ' + computeHMAC(hmac_msg, secret)}
+        rep = requests.delete(base_url + '/leases/' + token, headers=headers)
+    else:
+        errMissingArg('--token')
+
+print(json.dumps(rep.json()))
