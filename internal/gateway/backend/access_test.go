12f8782f721f837dd2825fb65691305d1e2d0130
Radu Popescu <mail@radupopescu.net>
2019-04-17 18:17:59 +0200

    Creating test suite for backend lease actions

--- internal/gateway/backend/access_test.go
+++ internal/gateway/backend/access_test.go
@@ -1,86 +1,9 @@
 package backend
 
 import (
-	"fmt"
 	"strings"
 	"testing"
 )
-
-const accessConfigV1 = `
-{
-	"repos": [
-		{
-			"domain": "test.repo.org",
-			"keys": ["keyid123"]
-		}
-	],
-	"keys": [
-		{
-			"type": "file",
-			"file_name": "/etc/cvmfs/keys/test.repo.org.gw",
-			"repo_subpath": "/"
-		},
-		{
-			"type": "plain_text",
-			"id": "keyid2",
-			"secret": "secret2",
-			"repo_subpath": "/"
-		}
-	]
-}
-`
-const accessConfigV2 = `
-{
-	"version": 2,
-	"repos" : [
-		"test1.repo.org",
-		{
-			"domain": "test2.repo.org",
-			"keys": [
-				{
-					"id": "keyid1",
-					"path": "/"
-				},
-				{
-					"id": "keyid2",
-					"path": "/restricted/to/subdir"
-				}
-			]
-		}
-	],
-	"keys": [
-		{
-			"type": "file",
-			"file_name": "/etc/cvmfs/keys/test2.repo.org.gw"
-		},
-		{
-			"type": "plain_text",
-			"id": "keyid2",
-			"secret": "secret2"
-		}
-	]
-}
-`
-
-const accessConfigV2NoKeys = `
-{
-	"version": 2,
-	"repos" : [
-		"test1.repo.org"
-	]
-}
-`
-
-func mockKeyImporter(ks KeySpec) (string, string, string, error) {
-	switch ks.KeyType {
-	case "plain_text":
-		return ks.ID, ks.Secret, ks.Path, nil
-	case "file":
-		return "keyid123", "secret123", "/", nil
-	default:
-		return "", "", "", fmt.Errorf("unknown key type")
-	}
-}
 
 func TestEmptyAccessConfig(t *testing.T) {
 	t.Run("empty no version", func(t *testing.T) {
---
+++ internal/gateway/backend/lease_actions_test.go
@@ -0,0 +1,59 @@
+package backend
+
+import (
+	"io/ioutil"
+	"os"
+	"strings"
+	"testing"
+	"time"
+)
+
+func startBackend() *Services {
+	tmp, err := ioutil.TempDir("", "test_lease_actions")
+	if err != nil {
+		os.Exit(1)
+	}
+	cfg := testConfig(tmp)
+
+	ac := emptyAccessConfig()
+	rd := strings.NewReader(accessConfigV2)
+	if err := ac.load(rd, mockKeyImporter); err != nil {
+		os.Exit(2)
+	}
+
+	ldb, err := NewLeaseDB("embedded", cfg)
+
+	return &Services{Access: ac, Leases: ldb, Config: *cfg}
+}
+
+func TestLeaseActionsNewLease(t *testing.T) {
+	backend := startBackend()
+	t.Run("new lease busy", func(t *testing.T) {
+		keyID := "keyid1"
+		leasePath := "test2.repo.org/some/path"
+		token1, err := NewLease(backend, keyID, leasePath)
+		if err != nil {
+			t.Fatalf("could not obtain new lease: %v", err)
+		}
+		defer CancelLease(backend, token1)
+		token2, err := NewLease(backend, keyID, leasePath)
+		if err == nil {
+			CancelLease(backend, token2)
+			t.Fatalf("new lease should not have been granted for busy path")
+		}
+	})
+	t.Run("new lease expired", func(t *testing.T) {
+		keyID := "keyid1"
+		leasePath := "test2.repo.org/some/path"
+		token1, err := NewLease(backend, keyID, leasePath)
+		if err != nil {
+			t.Fatalf("could not obtain new lease: %v", err)
+		}
+		defer CancelLease(backend, token1)
+		time.Sleep(backend.Config.MaxLeaseTime)
+		if _, err := NewLease(backend, keyID, leasePath); err != nil {
+			t.Fatalf("could not obtain new lease: %v", err)
+		}
+	})
+	defer backend.Close()
+}
