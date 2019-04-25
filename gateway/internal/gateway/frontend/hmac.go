3434e00fe6939d20d76fa7d05a9288c7b1d7a1f3
Radu Popescu <mail@radupopescu.net>
2019-04-25 14:51:00 +0200

    Change HMAC computation to match the cvmfs server tools

--- gateway/internal/gateway/frontend/hmac.go
+++ gateway/internal/gateway/frontend/hmac.go
@@ -3,13 +3,14 @@ package frontend
 import (
 	"crypto/hmac"
 	"crypto/sha1"
+	"encoding/hex"
 )
 
 // ComputeHMAC of a message using a specific key
 func ComputeHMAC(message []byte, key string) []byte {
 	mac := hmac.New(sha1.New, []byte(key))
 	mac.Write(message)
-	return mac.Sum(nil)
+	return []byte(hex.EncodeToString(mac.Sum(nil)))
 }
 
 // CheckHMAC of a message
