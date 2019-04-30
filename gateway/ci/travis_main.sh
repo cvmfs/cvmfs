406e4c2fca6e09cc813b6fde3adfc903424b8d30
Radu Popescu <mail@radupopescu.net>
2019-04-30 19:37:40 +0200

    Removing unused: travis_main.sh

--- gateway/ci/travis_main.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker run --rm -it -v "${PWD}":/mnt radupopescu/erlang-libsodium:21 \
-       sh -c "cd /mnt && rebar3 release && rebar3 as test dialyzer,eunit,ct && rebar3 as prod tar"
