c3525894d6c5061498dfa00c90e60320a87a1b11
Radu Popescu <mail@radupopescu.net>
2019-05-01 12:33:27 +0200

    Removing old implementation

--- gateway/old/.dir-locals.el
+++
@@ -1,6 +0,0 @@
-((erlang-mode
-  (flycheck-erlang-include-path . ("../include"
-                                   "../../../_build/test/lib"))
-  (flycheck-erlang-library-path . ("../../../_build/default/lib/lager/ebin"
-                                   "../../../_build/test/lib/proper/ebin"
-                                   "../../../_build/test/lib"))))
--- gateway/old/scripts/clear_leases.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-SCRIPT_LOCATION=$(cd "$(dirname "$0")"; pwd)
-
-RUNNER_LOG_DIR=/var/log/cvmfs-gateway-runner $SCRIPT_LOCATION/../bin/cvmfs_gateway escript scripts/clear_leases.escript
