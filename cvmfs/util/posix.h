8d82add0e542fb115da9780106c7f7a160618ff0
Jakob Blomer <jblomer@cern.ch>
2019-05-03 15:28:35 +0200

    first baby steps towards creating a publish namespace

--- cvmfs/util/posix.h
+++ cvmfs/util/posix.h
@@ -109,7 +109,7 @@ bool ExecuteBinary(int *fd_stdin,
                    int *fd_stdout,
                    int *fd_stderr,
                    const std::string &binary_path,
-                   const std::vector<std::string>  &argv,
+                   const std::vector<std::string> &argv,
                    const bool double_fork = true,
                    pid_t *child_pid = NULL);
 bool ManagedExec(const std::vector<std::string> &command_line,
