7abea74e3161ff5863db872cb6cf6d7ed236469b
Jakob Blomer <jblomer@cern.ch>
2019-05-03 11:41:58 +0200

    add stub for enter command

---
+++ cvmfs/publish/cmd_enter.cc
@@ -0,0 +1,17 @@
+/**
+ * This file is part of the CernVM File System.
+ */
+
+#include "cvmfs_config.h"
+#include "cmd_enter.h"
+
+using namespace std;  // NOLINT
+
+namespace publish {
+
+int CmdEnter::Main(const Options &options) {
+
+  return 0;
+}
+
+}  // namespace publish
---
+++ cvmfs/publish/cmd_enter.h
@@ -0,0 +1,29 @@
+/**
+ * This file is part of the CernVM File System.
+ */
+
+#ifndef CVMFS_PUBLISH_CMD_ENTER_H_
+#define CVMFS_PUBLISH_CMD_ENTER_H_
+
+#include <string>
+
+#include "publish/command.h"
+
+namespace publish {
+
+class CmdEnter : public Command {
+ public:
+  explicit CmdEnter() { }
+  virtual std::string GetName() const { return "enter"; }
+  virtual std::string GetBrief() const {
+    return "Opens an ephemeral namespace to publish content";
+  }
+  virtual std::string GetUsage() const { return "[repository name]"; }
+  virtual ParameterList GetParams() const { return ParameterList(); }
+
+  virtual int Main(const Options &options);
+};
+
+}  // namespace publish
+
+#endif  // CVMFS_PUBLISH_CMD_ENTER_H_
