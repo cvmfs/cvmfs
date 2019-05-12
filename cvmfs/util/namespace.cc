bc3c2f5b2f272a99488d20eb6ce10310301bca57
Jakob Blomer <jblomer@cern.ch>
2019-05-12 20:04:00 +0200

    [publish] prototype publish from 'enter sandbox'

--- cvmfs/util/namespace.cc
+++ cvmfs/util/namespace.cc
@@ -69,7 +69,7 @@ bool BindMount(const std::string &from, const std::string &to) {
 #ifdef __APPLE__
   return false;
 #else
-  int rvi = mount(from.c_str(), to.c_str(), "", MS_BIND, NULL);
+  int rvi = mount(from.c_str(), to.c_str(), "", MS_BIND | MS_REC, NULL);
   return rvi == 0;
 #endif
 }
