05ce1d7ac34831bef81bd6b50ae1679331180d7e
Radu Popescu <mail@radupopescu.net>
2019-05-22 11:52:42 +0200

    Fixing test_requests.py

--- tools/test_requests.py
+++ tools/test_requests.py
@@ -20,50 +20,44 @@ def get_repos(args):
     rep = requests.get(args.gw_url + '/repos')
     print(json.dumps(rep.json()))
 
-
-def toggle_repos(args):
+def toggle_repo(args):
     req = {'enable': bool(args.enable)}
     hmac_msg = json.dumps(req).encode()
     headers = {'authorization': args.key_id +
-                ' ' + computeHMAC(hmac_msg, args.secret)}
+                ' ' + computeHMAC(hmac_msg, args.secret.encode())}
     rep = requests.post(args.gw_url + '/repos/' +
                         args.repo_name, json=req, headers=headers)
     print(json.dumps(rep.json()))
-
 
 def get_leases(args):
     rep = requests.get(args.gw_url + '/leases')
     print(json.dumps(rep.json()))
 
-
 def get_lease(args):
     rep = requests.get(args.gw_url + '/leases/' + args.token)
     print(json.dumps(rep.json()))
-
 
 def new_lease(args):
     req = {'path': args.path, 'api_version': '2'}
     hmac_msg = json.dumps(req).encode()
     headers = {'authorization': args.key_id +
-                ' ' + computeHMAC(hmac_msg, args.secret)}
+                ' ' + computeHMAC(hmac_msg, args.secret.encode())}
     rep = requests.post(args.gw_url + '/leases', json=req, headers=headers)
     print(json.dumps(rep.json()))
-
 
 def cancel_lease(args):
     token = args.token
     hmac_msg = token.encode()
     headers = {'authorization': args.key_id +
-                ' ' + computeHMAC(hmac_msg, args.secret)}
+                ' ' + computeHMAC(hmac_msg, args.secret.encode())}
     rep = requests.delete(args.gw_url + '/leases/' + token, headers=headers)
     print(json.dumps(rep.json()))
-
 
 def commit_lease(args):
     token = args.token
     hmac_msg = token.encode()
     headers = {'authorization': args.key_id +
-                ' ' + computeHMAC(hmac_msg, args.secret)}
+                ' ' + computeHMAC(hmac_msg, args.secret.encode())}
     req = {'old_root_hash': args.old_hash,
            'new_root_hash': args.new_hash,
            'tag_name': 'mytag',
@@ -72,7 +66,6 @@ def commit_lease(args):
     rep = requests.post(args.gw_url + '/leases/' + token,
                         json=req, headers=headers)
     print(json.dumps(rep.json()))
-
 
 def submit_payload(args):
     hmac_msg = None
@@ -91,11 +84,10 @@ def submit_payload(args):
         hmac_msg = args.token.encode()
         req_url = args.gw_url + '/payloads' + args.token
 
-    headers = {'authorization': args.key_id + ' ' + computeHMAC(hmac_msg, args.secret),
+    headers = {'authorization': args.key_id + ' ' + computeHMAC(hmac_msg, args.secret.encode()),
                'message-size': str(len(json.dumps(req)))}
     rep = requests.post(req_url, json=req, headers=headers)
     print(json.dumps(rep.json()))
-
 
 def main():
     parser = argparse.ArgumentParser(description='Test gateway API requests')
@@ -105,14 +97,13 @@ def main():
                         help='Public ID of the secret key used to sign the request')
     parser.add_argument('--secret', required=True,
                         help='Secret key to be used to sign the request')
-    subparsers = parser.add_subparsers(help='sub-command help')
+    subparsers = parser.add_subparsers(help='subcommands')
+    subparsers.required = True
+    subparsers.dest = 'command'
 
-
-    parser_get_repos = subparsers.add_parser(
+    subparsers.add_parser(
         'get_repos', help='get a list of all repositories'
     )
-    parser_get_repos.set_defaults(func=get_repos)
-
 
     parser_toggle_repo = subparsers.add_parser(
         'toggle_repo', help='enable or disable a repository')
@@ -122,35 +113,25 @@ def main():
         '--wait', required=False, action='store_true', default=False, help='wait until the repository can be disabled')
     parser_toggle_repo.add_argument(
         '--repo_name', required=True, help='name of the concerned repository')
-    parser_toggle_repo.set_defaults(func=toggle_repos)
 
-
-    parser_get_leases = subparsers.add_parser(
+    subparsers.add_parser(
         'get_leases', help='get active leases'
     )
-    parser_get_leases.set_defaults(func=get_leases)
-
 
     parser_get_lease = subparsers.add_parser(
         'get_lease', help='get an active lease'
     )
     parser_get_lease.add_argument('--token', required=True, help='lease token string')
-    parser_get_lease.set_defaults(func=get_lease)
-
 
     parser_new_lease = subparsers.add_parser(
         'new_lease', help='request a new active lease'
     )
     parser_new_lease.add_argument('--path', required=True, help="lease path")
-    parser_new_lease.set_defaults(func=new_lease)
-
 
     parser_cancel_lease = subparsers.add_parser(
         'cancel_lease', help='request a new active lease'
     )
     parser_cancel_lease.add_argument('--token', required=True, help='lease token string')
-    parser_cancel_lease.set_defaults(func=cancel_lease)
-
 
     parser_commit_lease = subparsers.add_parser(
         'commit_lease', help='request a new active lease'
@@ -158,8 +139,6 @@ def main():
     parser_commit_lease.add_argument('--token', required=True, help='lease token string')
     parser_commit_lease.add_argument('--old_hash', required=True, help='old root hash')
     parser_commit_lease.add_argument('--new_hash', required=True, help='new root hash')
-    parser_commit_lease.set_defaults(func=commit_lease)
-
 
     parser_submit_payload = subparsers.add_parser(
         'submit_payload', help='request a new active lease'
@@ -169,9 +148,19 @@ def main():
     parser_submit_payload.add_argument('--header_size', required=True, help='payload object pack header size')
     parser_submit_payload.add_argument(
         '--legacy', required=False, default=False, action='store_true', help='use legacy request format')
-    parser_submit_payload.set_defaults(func=submit_payload)
 
-    parser.parse_args()
+    args = parser.parse_args()
+
+    {
+        'get_repos': get_repos,
+        'toggle_repo': toggle_repo,
+        'get_leases': get_leases,
+        'get_lease': get_lease,
+        'new_lease': new_lease,
+        'cancel_lease': cancel_lease,
+        'commit_lease': commit_lease,
+        'submit_payload': submit_payload,
+    }[args.command](args)
 
 
 if __name__ == '__main__':
