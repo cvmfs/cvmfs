abea84f9246b8da9535aefaebf72d9110e2d8a1d
Radu Popescu <mail@radupopescu.net>
2019-07-01 17:26:02 +0200

    Removing Websockets notification subscriber
    * SubscriberWS is replaced with SubscriberSSE - based on server-sent events
    * Removing libwebsockets - server-sent events can be implemented just with Curl

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,7 +16,6 @@ PROTOBUF_VERSION=2.6.1
 MONGOOSE_VERSION=3.8
 RAPIDCHECK_VERSION=0.0
 LIBARCHIVE_VERSION=3.3.2
-LIBWEBSOCKETS_VERSION=3.0.1
 
 if [ x"$EXTERNALS_LIB_LOCATION" = x"" ]; then
   echo "Bootstrap - Missing environment variable: EXTERNALS_LIB_LOCATION"
@@ -236,11 +235,6 @@ build_lib() {
       do_extract "libarchive" "libarchive-${LIBARCHIVE_VERSION}.tar.gz"
       do_build "libarchive"
       ;;
-    libwebsockets)
-      do_extract "libwebsockets" "libwebsockets-${LIBWEBSOCKETS_VERSION}.tar.gz"
-      patch_external "libwebsockets" "CMakeLists.patch" "private.patch" "poll.patch"
-      do_build "libwebsockets"
-      ;;
     *)
       echo "Unknown library name. Exiting."
       exit 1
@@ -251,7 +245,7 @@ build_lib() {
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 # Build a list of libs that need to be built
-missing_libs="libcurl pacparser zlib sparsehash leveldb googletest ipaddress maxminddb protobuf googlebench sqlite3 vjson sha2 sha3 mongoose libarchive libwebsockets"
+missing_libs="libcurl pacparser zlib sparsehash leveldb googletest ipaddress maxminddb protobuf googlebench sqlite3 vjson sha2 sha3 mongoose libarchive"
 if [ x"$BUILD_QC_TESTS" != x"" ]; then
     missing_libs="$missing_libs rapidcheck"
 fi
--- externals/libwebsockets/src/makeHook.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-make -C build
-
-cp -rv build/include/libwebsockets.h \
-       build/include/lws_config.h \
-       $EXTERNALS_INSTALL_LOCATION/include/
-cp -rv build/lib/libwebsockets.a $EXTERNALS_INSTALL_LOCATION/lib/
