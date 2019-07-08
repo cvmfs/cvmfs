d9be6afd8f93a6da8eeac3cd88b42df7f6edb684
Radu Popescu <mail@radupopescu.net>
2019-07-08 14:48:34 +0200

    Adding test for notification system late subscription

--- gateway/internal/gateway/backend/notify_test.go
+++ gateway/internal/gateway/backend/notify_test.go
@@ -43,3 +43,36 @@ func TestNotificationSystem(t *testing.T) {
 		t.Fatalf("Unexpected received message pattern: %v", messages)
 	}
 }
+
+func TestNotificationSystemLateSubscription(t *testing.T) {
+	tmp, err := ioutil.TempDir("", "test_notifications")
+	if err != nil {
+		t.Fatalf("could not create temp dir")
+	}
+	defer os.RemoveAll(tmp)
+
+	ns, err := NewNotificationSystem(tmp)
+	if err != nil {
+		t.Fatalf("could not create notification system")
+	}
+
+	hd := make(chan NotificationMessage, 1000)
+
+	ctx := context.TODO()
+	repo := "test.repo.org"
+
+	ns.Publish(ctx, repo, []byte("msg1"))
+	ns.Publish(ctx, repo, []byte("msg2"))
+
+	ns.Subscribe(ctx, repo, hd)
+	ns.Unsubscribe(ctx, repo, hd)
+
+	messages := make([][]byte, 0)
+	for m := range hd {
+		messages = append(messages, []byte(m))
+	}
+
+	if len(messages) != 1 || !bytes.Equal(messages[0], []byte("msg2")) {
+		t.Fatalf("Unexpected received message pattern: %v", messages)
+	}
+}
