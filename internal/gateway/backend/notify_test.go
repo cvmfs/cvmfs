533680153b64d5a2f2170895ff2214e00b1552c0
Radu Popescu <mail@radupopescu.net>
2019-07-08 14:35:06 +0200

    Add notification system test

---
+++ internal/gateway/backend/notify_test.go
@@ -0,0 +1,45 @@
+package backend
+
+import (
+	"bytes"
+	"context"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestNotificationSystem(t *testing.T) {
+	tmp, err := ioutil.TempDir("", "test_notifications")
+	if err != nil {
+		t.Fatalf("could not create temp dir")
+	}
+	defer os.RemoveAll(tmp)
+
+	ns, err := NewNotificationSystem(tmp)
+	if err != nil {
+		t.Fatalf("could not create notification system")
+	}
+
+	hd := make(chan NotificationMessage, 1000)
+
+	ctx := context.TODO()
+	repo := "test.repo.org"
+
+	ns.Subscribe(ctx, repo, hd)
+
+	ns.Publish(ctx, repo, []byte("msg1"))
+	ns.Publish(ctx, repo, []byte("msg2"))
+
+	ns.Unsubscribe(ctx, repo, hd)
+
+	ns.Publish(ctx, repo, []byte("msg3"))
+
+	messages := make([][]byte, 0)
+	for m := range hd {
+		messages = append(messages, []byte(m))
+	}
+
+	if len(messages) != 2 || !bytes.Equal(messages[0], []byte("msg1")) || !bytes.Equal(messages[1], []byte("msg2")) {
+		t.Fatalf("Unexpected received message pattern: %v", messages)
+	}
+}
