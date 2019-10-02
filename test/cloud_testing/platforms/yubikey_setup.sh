58381d1d290f073ec2c546f33c748c777a3da3ba
Jan Priessnitz <jan.priessnitz@cern.ch>
2019-10-02 13:11:49 +0200

    Add yubikey cloud testing platform-specific scripts

---
+++ test/cloud_testing/platforms/yubikey_setup.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# source the common platform independent functionality and option parsing
+script_location=$(dirname $(readlink --canonicalize $0))
+. ${script_location}/common_setup.sh
+
+# CernVM-FS server needs 'jq' from epel
+echo "enabling epel yum repository..."
+install_from_repo epel-release
+
+# install CernVM-FS RPM packages
+echo "installing RPM packages... "
+install_rpm "$CONFIG_PACKAGES"
+install_rpm $CLIENT_PACKAGE
+install_rpm $SERVER_PACKAGE
+install_rpm $DEVEL_PACKAGE
+
+# setup environment
+echo -n "setting up CernVM-FS environment..."
+sudo cvmfs_config setup                          || die "fail (cvmfs_config setup)"
+sudo mkdir -p /var/log/cvmfs-test                || die "fail (mkdir /var/log/cvmfs-test)"
+sudo chown sftnight:sftnight /var/log/cvmfs-test || die "fail (chown /var/log/cvmfs-test)"
+sudo systemctl start autofs                      || die "fail (systemctl start autofs)"
+sudo cvmfs_config chksetup > /dev/null           || die "fail (cvmfs_config chksetup)"
+echo "done"
+
+# install yubikey-related software
+install_from_repo opensc
+install_from_repo yubico-piv-tool
+sudo systemctl enable pcscd
+sudo systemctl start pcscd
