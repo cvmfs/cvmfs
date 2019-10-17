9e43c29702e418ca4611240c005aca2b70849feb
Simone 'siscia' Mosciatti <simone.mosciatti@cern.ch>
2019-10-17 12:09:08 +0200

    consiste naming of DUCC package

--- test/cloud_testing/platforms/common_setup.sh
+++ test/cloud_testing/platforms/common_setup.sh
@@ -25,7 +25,7 @@ script_location=$(portable_dirname $0)
 #  SHRINKWRAP_PACKAGE    location of the CernVM-FS shrinkwrap package
 #  LOG_DIRECTORY         location of the test log files to be created
 #  GATEWAY_BUILD_URL     location of the repository gateway build to install
-#  DUCC_BUILD_URL        location of the DUCC build to install
+#  DUCC_PACKAGE          location of the DUCC package
 #
 
 SERVER_PACKAGE=""
@@ -38,7 +38,7 @@ CONFIG_PACKAGES=""
 SOURCE_DIRECTORY=""
 LOG_DIRECTORY=""
 GATEWAY_BUILD_URL=""
-DUCC_BUILD_URL=""
+DUCC_PACKAGE=""
 
 # parse script parameters (same for all platforms)
 while getopts "s:c:d:k:t:g:l:w:n:p:f:D:" option; do
@@ -74,7 +74,7 @@ while getopts "s:c:d:k:t:g:l:w:n:p:f:D:" option; do
       GATEWAY_BUILD_URL=$OPTARG
       ;;
     D)
-      DUCC_BUILD_URL=$OPTARG
+      DUCC_PACKAGE=$OPTARG
       ;;
     n)
       echo "WARNING: the -n parameter is obsolete"
@@ -99,7 +99,6 @@ if [ "x$(uname -s)" != "xDarwin" ]; then
        [ "x$UNITTEST_PACKAGE"      = "x" ] ||
        [ "x$SHRINKWRAP_PACKAGE"    = "x" ] ||
        [ "x$GATEWAY_BUILD_URL"     = "x" ] ||
-       [ "x$DUCC_BUILD_URL"        = "x" ] ||
        [ "x$DEVEL_PACKAGE"         = "x" ]; then
       echo "missing parameter(s), cannot run platform dependent test script"
       exit 200
