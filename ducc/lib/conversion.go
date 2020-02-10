7e336340a48a6456c0189883870174ae5a5a1e99
Simone 'siscia' Mosciatti <simone.mosciatti@cern.ch>
2020-02-10 18:26:01 +0100

    check singularity version

--- ducc/lib/conversion.go
+++ ducc/lib/conversion.go
@@ -10,6 +10,7 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 	"sync"
 
@@ -32,7 +33,50 @@ const (
 
 var subDirInsideRepo = ".layers"
 
+func assureValidSingularity() error {
+	err, stdout, _ := ExecCommand("singularity", "--version").StartWithOutput()
+	if err != nil {
+		err := fmt.Errorf("No working version of Singularity: %s", err)
+		LogE(err).Error("No working version of Singularity")
+		return err
+	}
+	version := stdout.String()
+	sems := strings.Split(version, ".")
+	if len(sems) < 2 {
+		err := fmt.Errorf("Singularity version returned an unexpected format, unable to find Major and Minor number")
+		LogE(err).WithFields(log.Fields{"version": version}).Error("Not valid singularity")
+		return err
+	}
+	majorS := sems[0]
+	majorI, err := strconv.Atoi(majorS)
+	if err != nil {
+		errF := fmt.Errorf("Singularity version returned an unexpected format, unable to parse Major number: %s", err)
+		LogE(errF).WithFields(log.Fields{"version": version, "major number": majorS}).Error("Not valid singularity")
+		return errF
+	}
+	if majorI >= 4 {
+		return nil
+	}
+	minorS := sems[1]
+	minorI, err := strconv.Atoi(minorS)
+	if err != nil {
+		errF := fmt.Errorf("Singularity version returned an unexpected format, unable to parse Minor number: %s", err)
+		LogE(errF).WithFields(log.Fields{"version": version, "minor number": minorS}).Error("Not valid singularity")
+		return errF
+	}
+	if majorI >= 3 && minorI >= 5 {
+		return nil
+	}
+	errF := fmt.Errorf("Installed singularity is too old, we need at least 3.5: Installed version: %s", version)
+	LogE(errF).WithFields(log.Fields{"version": version}).Error("Too old singularity")
+	return errF
+}
+
 func ConvertWishSingularity(wish WishFriendly) (err error) {
+	err := assureValidSingularity()
+	if err != nil {
+		return err
+	}
 	tmpDir, err := UserDefinedTempDir("", "conversion")
 	if err != nil {
 		LogE(err).Error("Error when creating tmp singularity directory")
