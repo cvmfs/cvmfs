97b6276d52e5a7a6862a376f29dead1fd01c5497
Jan Priessnitz <jan.priessnitz@cern.ch>
2020-03-09 14:08:51 +0100

    Cloudtests: disable external cache test on MacOS
    External plugins are not installed on MacOS ->
    disable related tests.

--- test/cloud_testing/platforms/macos_test.sh
+++ test/cloud_testing/platforms/macos_test.sh
@@ -39,6 +39,7 @@ CVMFS_TEST_CLASS_NAME=ClientIntegrationTests
                                    src/082-shrinkwrap-cms                       \
                                    src/083-suid                                 \
                                    src/084-premounted                           \
+                                   src/089-external_cache_plugin                \
                                    --                                           \
                                    src/0*
 
