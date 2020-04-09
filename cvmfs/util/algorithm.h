5a9fe5ca71a05e40fb11a407a5348ecb6f9cba47
Simone Mosciatti <simone.mosciatti@cern.ch>
2020-04-09 18:28:34 +0200

    [LINT] minor fixes

--- cvmfs/util/algorithm.h
+++ cvmfs/util/algorithm.h
@@ -141,7 +141,9 @@ friend class UTLog2Histogram;
     atomic_inc32(&(this->bins_[0]));  // add to overflow bin.
   }
 
-  //compute the quantile of order n
+  /**
+   * compute the quantile of order n
+   */
   unsigned int q(float n);
 
   std::string ToString();
