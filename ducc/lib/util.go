fe989ae21cf6b225c149a7dfa4947c71c9d7b0fc
Mohitty <mohitt@iitk.ac.in>
2020-06-26 21:57:45 +0200

    Use google uuid lib to generate link dir name

--- ducc/lib/util.go
+++ ducc/lib/util.go
@@ -1,73 +1,23 @@
 package lib
 
 import (
-	"crypto/rand"
 	"encoding/base32"
 	"encoding/base64"
-	"fmt"
-	"io"
-	"os"
 	"strings"
-	"syscall"
-	"time"
-	"golang.org/x/sys/unix"
+
+	"github.com/google/uuid"
 )
 
+//generates the file name for link dir in podman store
 func generateID(l int) string {
-	const (
-		maxretries = 9
-		backoff    = time.Millisecond * 10
-	)
-
-	var (
-		totalBackoff time.Duration
-		count        int
-		retries      int
-		size         = (l*5 + 7) / 8
-		u            = make([]byte, size)
-	)
-
-	for {
-		b := time.Duration(retries) * backoff
-		time.Sleep(b)
-		totalBackoff += b
-
-		n, err := io.ReadFull(rand.Reader, u[count:])
-		if err != nil {
-			if retryOnError(err) && retries < maxretries {
-				count += n
-				retries++
-				fmt.Printf("error generating version 4 uuid, retrying: %v", err)
-				continue
-			}
-
-			panic(fmt.Errorf("error reading random number generator, retried for %v: %v", totalBackoff.String(), err))
-		}
-
-		break
-	}
-
-	s := base32.StdEncoding.EncodeToString(u)
+	id := uuid.New()
+	bytes, _ := id.MarshalText()
+	s := base32.StdEncoding.EncodeToString(bytes)
 
 	return s[:l]
 }
 
-// retryOnError tries to detect whether or not retrying would be fruitful.
-func retryOnError(err error) bool {
-	switch err := err.(type) {
-	case *os.PathError:
-		return retryOnError(err.Err) // unpack the target error
-	case syscall.Errno:
-		if err == unix.EPERM {
-			// EPERM represents an entropy pool exhaustion, a condition under
-			// which we backoff and retry.
-			return true
-		}
-	}
-
-	return false
-}
-
+//generates the file name for config file (compliant with libpod) in podman store.
 func generateConfigFileName(digest string) (fname string, err error) {
 	reader := strings.NewReader(digest)
 	for reader.Len() > 0 {
