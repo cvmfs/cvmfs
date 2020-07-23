7b3f8e3525fbd7aa3ce7151de95620d1da638231
Jakob Blomer <jblomer@cern.ch>
2020-07-23 10:50:37 +0200

    improve integration test logging

--- test/cloud_testing/platforms/centos8_x86_64-CONTAINER_test.sh
+++ test/cloud_testing/platforms/centos8_x86_64-CONTAINER_test.sh
@@ -7,7 +7,7 @@ script_location=$(cd "$(dirname "$0")"; pwd)
 retval=0
 
 cd ${SOURCE_DIRECTORY}/test
-echo "running CernVM-FS client test cases..."
+echo "running CernVM-FS container integration test cases..."
 CVMFS_TEST_CLASS_NAME=ContainerIntegrationTests                               \
 ./run.sh $CLIENT_TEST_LOGFILE -o ${CLIENT_TEST_LOGFILE}${XUNIT_OUTPUT_SUFFIX} \
                                  container/*                                  \
