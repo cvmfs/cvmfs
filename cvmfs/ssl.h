8b49baeca299bc6d085a7caf04ce1da05f035c14
Simone 'siscia' Mosciatti <simone.mosciatti@cern.ch>
2020-07-31 14:04:17 +0200

    move everything to use headers anc .cc files

--- cvmfs/ssl.h
+++ cvmfs/ssl.h
@@ -15,44 +15,9 @@
 namespace CVMFS_NAMESPACE_GUARD {
 #endif
 
-inline unsigned int count_ssl_certificates(std::string directory) {
-  if (!DirectoryExists(directory)) return 0;
+unsigned int count_ssl_certificates(std::string directory);
 
-  std::vector<std::string> dotpem = FindFilesBySuffix(directory, ".pem");
-  std::vector<std::string> dotcrt = FindFilesBySuffix(directory, ".crt");
-
-  return dotpem.size() + dotcrt.size();
-}
-
-inline bool AddSSLCertificates(CURL *handle) {
-  std::vector<std::string> cadirs;
-  const char *cadir = getenv("X509_CERT_DIR");
-  if (cadir != NULL) {
-    cadirs.push_back(std::string(cadir));
-  }
-  cadirs.push_back("/etc/grid-security/certificates");
-
-  // most systems store the certificates here
-  cadirs.push_back("/etc/ssl/certs/");
-
-  cadirs.push_back("/etc/pki/tls/certs/");
-  cadirs.push_back("/etc/ssl/");
-  cadirs.push_back("/etc/pki/tls/");
-  cadirs.push_back("/etc/pki/ca-trust/extracted/pem/");
-  cadirs.push_back("/etc/ssl/");
-
-  for (std::vector<std::string>::const_iterator cadir = cadirs.begin();
-       cadir != cadirs.end(); ++cadir) {
-    if (count_ssl_certificates(*cadir) > 0) {
-      CURLcode res = curl_easy_setopt(handle, CURLOPT_CAPATH, (*cadir).c_str());
-      if (CURLE_OK == res) {
-        return true;
-      }
-    }
-  }
-
-  return false;
-}
+bool AddSSLCertificates(CURL *handle);
 
 #ifdef CVMFS_NAMESPACE_GUARD
 }  // namespace CVMFS_NAMESPACE_GUARD
