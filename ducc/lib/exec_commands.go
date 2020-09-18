a08acf410a29a98277ab55c68a2697d56bc6cc4c
Simone 'siscia' Mosciatti <simone.mosciatti@cern.ch>
2020-09-18 14:59:47 +0200

    remove unused function

--- ducc/lib/exec_commands.go
+++ ducc/lib/exec_commands.go
@@ -114,64 +114,6 @@ func (e *execCmd) Start() error {
 	return nil
 }
 
-/*
-func (e *execCmd) Start() error {
-	if e == nil {
-		err := fmt.Errorf("Use of nil execCmd")
-		LogE(err).Error("Call start with nil cmd, maybe error in the constructor")
-		return err
-	}
-
-	err := e.cmd.Start()
-	if err != nil {
-		LogE(err).Error("Error in starting the command")
-		return err
-	}
-
-	if e.stdin != nil {
-		go func() {
-			defer (*e.stdin).Close()
-			defer e.in.Close()
-			n, err := io.Copy(e.in, *e.stdin)
-			Log().WithFields(log.Fields{"n": n}).Info("Copied n bytes to STDIN")
-			if err != nil {
-				LogE(err).Error("Error in copying the input into STDIN.")
-				return
-			}
-			for n > 0 {
-				n, err = io.Copy(e.in, *e.stdin)
-				if err != nil {
-					LogE(err).Error("Error in copying the input into STDIN")
-					return
-				}
-				Log().WithFields(log.Fields{"n": n}).Info("Copied additionally n bytes to STDIN")
-			}
-		}()
-	}
-
-	slurpOut, errOUT := ioutil.ReadAll(e.out)
-	if errOUT != nil {
-		LogE(errOUT).Warning("Impossible to read the STDOUT")
-		return err
-	}
-	slurpErr, errERR := ioutil.ReadAll(e.err)
-	if errERR != nil {
-		LogE(errERR).Warning("Impossible to read the STDERR")
-		return err
-	}
-
-	err = e.cmd.Wait()
-	if err != nil {
-
-		LogE(err).Error("Error in executing the command")
-		Log().WithFields(log.Fields{"pipe": "STDOUT"}).Info(string(slurpOut))
-		Log().WithFields(log.Fields{"pipe": "STDERR"}).Info(string(slurpErr))
-		return err
-	}
-	return nil
-}
-*/
-
 func (e *execCmd) Env(key, value string) *execCmd {
 	if e == nil {
 		err := fmt.Errorf("Use of nil execCmd")
