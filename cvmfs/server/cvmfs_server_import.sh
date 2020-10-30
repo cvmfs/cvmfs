27dbe4cee6346139369f356d759439b01cf6715d
Hugo Meiland <humeilan@microsoft.com>
2020-10-30 09:22:36 +0100

    finetune import script to allow s3 backed cvmfs to be imported

--- cvmfs/server/cvmfs_server_import.sh
+++ cvmfs/server/cvmfs_server_import.sh
@@ -143,11 +143,13 @@ cvmfs_server_import() {
   check_upstream_validity $upstream
   check_cvmfs2_client               || die "cvmfs client missing"
   check_autofs_on_cvmfs             && die "Autofs on /cvmfs has to be disabled"
-  check_apache                      || die "Apache must be installed and running"
-  is_local_upstream $upstream       || die "Import only works locally for the moment"
+  #is_local_upstream $upstream       || die "Import only works locally for the moment"
   ensure_swissknife_suid $unionfs   || die "Need CAP_SYS_ADMIN for cvmfs_swissknife"
   lower_hardlink_restrictions
-  ensure_enabled_apache_modules
+  if [ $configure_apache -eq 1 ]; then
+    check_apache                      || die "Apache must be installed and running"
+    ensure_enabled_apache_modules
+  fi
   [ x"$keys_location" = "x" ] && die "Please provide the location of the repository security keys (-k)"
 
   if [ $unionfs = "overlayfs" ]; then
@@ -166,21 +168,23 @@ cvmfs_server_import() {
   local cvmfs_uid=$(echo $file_ownership | cut -d: -f1)
   local cvmfs_gid=$(echo $file_ownership | cut -d: -f2)
 
-  # investigate the given repository storage for sanity
   local storage_location=$(get_upstream_config $upstream)
-  local needed_items="${storage_location}                 \
-                      ${storage_location}/.cvmfspublished \
-                      ${storage_location}/data            \
-                      ${storage_location}/data/txn"
-  local i=0
-  while [ $i -lt 256 ]; do
-    needed_items="$needed_items ${storage_location}/data/$(printf "%02x" $i)"
-    i=$(($i+1))
-  done
-  for item in $needed_items; do
-    [ -e $item ] || die "$item missing"
-    [ $chown_backend -ne 0 ] || [ x"$cvmfs_user" = x"$(stat -c%U $item)" ] || die "$item not owned by $cvmfs_user"
-  done
+  if [ $(is_local_upstream $upstream) -eq 1 ]; then
+    # investigate the given repository storage for sanity
+    local needed_items="${storage_location}                 \
+                        ${storage_location}/.cvmfspublished \
+                        ${storage_location}/data            \
+                        ${storage_location}/data/txn"
+    local i=0
+    while [ $i -lt 256 ]; do
+      needed_items="$needed_items ${storage_location}/data/$(printf "%02x" $i)"
+      i=$(($i+1))
+    done
+    for item in $needed_items; do
+      [ -e $item ] || die "$item missing"
+      [ $chown_backend -ne 0 ] || [ x"$cvmfs_user" = x"$(stat -c%U $item)" ] || die "$item not owned by $cvmfs_user"
+    done
+  fi
 
   # check availability of repository signing key and certificate
   local keys="$public_key"
