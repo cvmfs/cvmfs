ea8ee2d47621772a4073694f775ca8c6c1bb6b83
Jakob Blomer <jblomer@cern.ch>
2020-11-02 10:06:18 +0100

    exclude integration test 015 on Ubuntu 16 / 32bit

--- test/cloud_testing/platforms/ubuntu_test.sh
+++ test/cloud_testing/platforms/ubuntu_test.sh
@@ -13,8 +13,16 @@ run_unittests --gtest_shuffle \
 
 CVMFS_EXCLUDE=
 
+ubuntu_release="$(lsb_release -cs)"
+if [ "x$ubuntu_release" == "xxenial" ]; then
+   if dpkg --print-architecture | grep i386; then
+      # This test has shown to occasionally hang in the GDB attach on Ubuntu 16 / 32 bit
+      CVMFS_EXCLUDE="$CVMFS_EXCLUDE 015-rebuild_on_crash"
+   fi
+fi
+
 # Kernel sources too old for gcc, TODO
-CVMFS_EXCLUDE="src/006-buildkernel"
+CVMFS_EXCLUDE="$CVMFS_EXCLUDE src/006-buildkernel"
 # Expected failure, see test case
 CVMFS_EXCLUDE="$CVMFS_EXCLUDE src/628-pythonwrappedcvmfsserver"
 
