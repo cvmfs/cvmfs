b4315980b07bf961a05eaacad3397acdb8b36dfd
Jakob Blomer <jblomer@cern.ch>
2020-11-04 18:58:43 +0100

    exclude test 015 on Ubuntu 16 / all platforms

--- test/cloud_testing/platforms/ubuntu_test.sh
+++ test/cloud_testing/platforms/ubuntu_test.sh
@@ -15,10 +15,8 @@ CVMFS_EXCLUDE=
 
 ubuntu_release="$(lsb_release -cs)"
 if [ "x$ubuntu_release" == "xxenial" ]; then
-   if dpkg --print-architecture | grep i386; then
-      # This test has shown to occasionally hang in the GDB attach on Ubuntu 16 / 32 bit
-      CVMFS_EXCLUDE="$CVMFS_EXCLUDE 015-rebuild_on_crash"
-   fi
+   # This test has shown to occasionally hang in the GDB attach on Ubuntu 16
+   CVMFS_EXCLUDE="$CVMFS_EXCLUDE 015-rebuild_on_crash"
 fi
 
 # Kernel sources too old for gcc, TODO
