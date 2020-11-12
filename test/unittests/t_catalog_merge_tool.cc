803c67684b7eca7a1f76afe6b0643eaeba3d3f9f
Simone Mosciatti <simone.mosciatti@cern.ch>
2020-11-12 15:35:23 +0100

    fix misspelling and remove outdated note about .cvmfscatalog file

--- test/unittests/t_catalog_merge_tool.cc
+++ test/unittests/t_catalog_merge_tool.cc
@@ -160,8 +160,6 @@ TEST_F(T_CatalogMergeTool, Symlink) {
   // |- bar
   //    |- baz
   //       |- .cvmfscatalog
-  //
-  // the .cvmfscatalog is not really generated
   DirSpec base;
   EXPECT_TRUE(base.AddDirectory("foo", "", 4096));
   EXPECT_TRUE(base.AddDirectory("bar", "", 4096));
@@ -180,7 +178,7 @@ TEST_F(T_CatalogMergeTool, Symlink) {
   // .
   // |- foo
   // |- bar
-  //    |- bax -> /foo
+  //    |- baz -> /foo
   DirSpec update = base;
 
   update.RemoveItemRec("bar/baz");
