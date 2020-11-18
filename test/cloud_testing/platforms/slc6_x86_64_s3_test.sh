9890cacc712a252b9176193dc9e7e9f75cb30f18
Jakob Blomer <jblomer@cern.ch>
2020-11-18 10:06:59 +0100

    exclude cvmfs_server enter test on SLC6/S3

--- test/cloud_testing/platforms/slc6_x86_64_s3_test.sh
+++ test/cloud_testing/platforms/slc6_x86_64_s3_test.sh
@@ -78,6 +78,7 @@ if [ $s3_retval -eq 0 ]; then
                                src/647-bearercvmfs                          \
                                src/670-listreflog                           \
                                src/673-acl                                  \
+                               src/682-enter                                \
                                --                                           \
                                src/5*                                       \
                                src/6*                                       \
