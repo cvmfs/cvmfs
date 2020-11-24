0d7d87595c877df1232ba0b150a82b71249be26a
aandvalenzuela <aandvalenzuela@gmail.com>
2020-11-24 16:27:48 +0100

    Eliminate hardlink warning for overlayfs

--- cvmfs/server/cvmfs_server_mkfs.sh
+++ cvmfs/server/cvmfs_server_mkfs.sh
@@ -200,11 +200,7 @@ cvmfs_server_mkfs() {
   # sanity checks
   check_repository_existence $name  && die "The repository $name already exists"
   check_upstream_validity $upstream
-  if [ $unionfs = "overlayfs" ]; then
-    local msg
-    msg="`check_overlayfs_version`" || die "$msg"
-    echo "Warning: CernVM-FS filesystems using overlayfs may not enforce hard link semantics during publishing."
-  else
+  if [ $unionfs = "aufs" ]; then
     check_aufs                      || die "aufs kernel module missing"
   fi
   check_cvmfs2_client               || die "cvmfs client missing"
