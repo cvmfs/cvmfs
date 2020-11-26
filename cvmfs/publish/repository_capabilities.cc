a5f1b66e80b1cfdf9883014ddc58097e676248a9
Jakob Blomer <jblomer@cern.ch>
2020-11-26 10:58:12 +0100

    [publish] fix capability handling and user namespaces

--- cvmfs/publish/repository_capabilities.cc
+++ cvmfs/publish/repository_capabilities.cc
@@ -6,14 +6,25 @@
 #include "repository.h"
 
 #include <sys/capability.h>
+#include <sys/prctl.h>
 
 #include "publish/except.h"
 
 namespace publish {
 
 void Repository::DropCapabilities() {
+  int retval;
+
+  // Because the process has file capabilities, its dumpable state is set to
+  // false, which in turn makes the /proc/self/... files owned by root.  We
+  // need to reset this to have them owned by the effective UID in order to
+  // set, e.g., uid_map/gid_map of user namespaces.
+  retval = prctl(PR_SET_DUMPABLE, 1, 0, 0, 0);
+  if (retval != 0)
+    throw EPublish("cannot clear dumpable state");
+
   cap_t caps = cap_get_proc();
-  int retval = cap_clear(caps);
+  retval = cap_clear(caps);
   cap_free(caps);
   if (retval != 0)
     throw EPublish("cannot clear process capabilities");
