2d3e97cd5f25fa9fcc10b93a9be2eaa52e464218
Simone Mosciatti <simone.mosciatti@cern.ch>
2020-12-14 16:17:57 +0100

    add constanst for dir and file permissions

--- ducc/constants/constants.go
+++ ducc/constants/constants.go
@@ -1,4 +1,11 @@
 package constants
 
+import (
+	"os"
+)
+
 var SubDirInsideRepo = ".layers"
 var ChainSubDir = ".chains"
+
+var DirPermision = os.FileMode(0755)
+var filePermision = os.FileMode(0644)
