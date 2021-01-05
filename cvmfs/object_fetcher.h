738abe68b3bd80556717ea8223b504ac46baae8c
Jakob Blomer <jblomer@cern.ch>
2021-01-05 13:42:27 +0100

    improve network error logging during gc

--- cvmfs/object_fetcher.h
+++ cvmfs/object_fetcher.h
@@ -592,10 +592,6 @@ class HttpObjectFetcher :
     const bool success = (retval == download::kFailOk);
     fclose(f);
 
-    static int test_fail = 0;
-    if (test_fail++ == 6)
-      return BaseTN::kFailNetwork;
-
     // check if download worked and remove temporary file if not
     if (!success) {
       LogCvmfs(kLogDownload, kLogDebug, "failed to download file "
@@ -614,13 +610,18 @@ class HttpObjectFetcher :
         case download::kFailProxyResolve:
         case download::kFailHostResolve:
         case download::kFailUnsupportedProtocol:
+          LogCvmfs(kLogDownload, kLogDebug | kLogStderr,
+                   "unsupported protocol: %s", url.c_str());
           return BaseTN::kFailNetwork;
 
         case download::kFailProxyHttp:
         case download::kFailHostHttp:
-          return (download_job.http_code == 404)
-            ? BaseTN::kFailNotFound
-            : BaseTN::kFailNetwork;
+          if (download_job.http_code == 404)
+            return BaseTN::kFailNotFound;
+          LogCvmfs(kLogDownload, kLogDebug | kLogStderr,
+                   "HTTP protocol error %d: %s (%d)",
+                   download_job.http_code, url.c_str(), retval);
+          return BaseTN::kFailNetwork;
 
         case download::kFailBadData:
         case download::kFailTooBig:
@@ -628,7 +629,11 @@ class HttpObjectFetcher :
 
         default:
           if (download::IsProxyTransferError(retval) ||
-              download::IsHostTransferError(retval)) {
+              download::IsHostTransferError(retval))
+          {
+            LogCvmfs(kLogDownload, kLogDebug | kLogStderr,
+                     "HTTP transfer error %d (HTTP code %d): %s",
+                     retval, download_job.http_code, url.c_str());
             return BaseTN::kFailNetwork;
           }
           return BaseTN::kFailUnknown;
