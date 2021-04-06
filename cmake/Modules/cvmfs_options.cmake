6c628658b4d2780dd4c77b772097ef39b1c9dc3f
aandvalenzuela <aandvalenzuela@gmail.com>
2021-04-06 11:51:39 +0200

    CMakeLists creation and BUILD_CONTAINERD option

--- cmake/Modules/cvmfs_options.cmake
+++ cmake/Modules/cvmfs_options.cmake
@@ -42,6 +42,8 @@ option (BUILTIN_EXTERNALS       "Use built-in versions of all third-party librar
 
 option (BUILD_DUCC              "Build cvmfs_ducc, it requires a modern go compiler > 1.11.5"      OFF)
 
+option (BUILD_CONTAINERD        "Build cvmfs-snapshotter, it requires a modern go compiler"         ON)
+
 if (BUILD_ALL)
   set (BUILD_CVMFS ON)
   set (BUILD_UNITTESTS ON)
