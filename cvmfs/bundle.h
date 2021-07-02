850a2a66eb266247fbad880489ec08bcd14e1fa2
RohitTopi <rohittopi474@gmail.com>
2021-07-02 22:03:29 +0530

    Add Bundle class

--- cvmfs/bundle.h
+++ cvmfs/bundle.h
@@ -5,6 +5,17 @@
 #ifndef CVMFS_BUNDLE_H_
 #define CVMFS_BUNDLE_H_
 
+/**
+ * A bundle has the binary format of object packs used by the gateway as
+ * a form to concatenate multiple objects in a single BLOB.
+ * Bundle files can be chunked the same way regular large files get chunked.
+ * Chunked and non-chunked bundle files get a B as a hash suffix
+ *
+ * The bundle files are created according to the contents of
+ * .cvmfsbundles file.
+ */
+class Bundle {
 
+};
 
 #endif  // CVMFS_BUNDLE_H_
