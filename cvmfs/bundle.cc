d0097533597e68c98e90852fa466a03a327443e5
RohitTopi <rohittopi474@gmail.com>
2021-07-05 13:27:59 +0530

    Add CreateBundle() function

--- cvmfs/bundle.cc
+++ cvmfs/bundle.cc
@@ -1,3 +1,87 @@
 /**
  * This file is part of the CernVM File System.
  */
+
+#include "bundle.h"
+
+#include <string>
+
+#include "json_document.h"
+#include "pack.h"
+#include "util/exception.h"
+#include "util/posix.h"
+#include "util/string.h"
+
+ObjectPack* Bundle::CreateBundle(const JSON* json_obj)
+{
+  const JSON* value = json_obj;
+  if (value->type != JSON_OBJECT) {
+    PANIC(kLogStderr, "JSON object not found");
+  } else {
+    // create an ObjectPack
+    ObjectPack* op = new ObjectPack();
+
+    if (op == NULL) {
+      PANIC(kLogStderr, "Insufficient memory");
+    }
+
+    value = (value->first_child);  // bundleid
+    value = (value->next_sibling);  // JSON array
+    if (value->first_child) {
+      value = value->first_child;
+      std::string filepath = std::string(value->string_value);
+
+      // if file doesn't exist then bundle is not created
+      if (!FileExists(filepath)) {
+        PrintWarning("File not found: " + filepath);
+        delete op;
+        return NULL;
+      }
+
+      // if the file size exceeds kMaxFileSize then bundle is not created
+      int64_t currentFileSize = GetFileSize(filepath);
+      if (currentFileSize > kMaxFileSize) {
+        PrintWarning("Large file found: " + filepath + " of size: "
+          + StringifyInt(currentFileSize));
+        delete op;
+        return NULL;
+      }
+
+      if (!AddFileToObjectPack(op, filepath)) {
+        delete op;
+        return NULL;
+      }
+
+      while (value->next_sibling) {
+        value = value->next_sibling;
+        filepath = std::string(value->string_value);
+
+        // if file doesn't exist then bundle is not created
+        if (!FileExists(filepath)) {
+          PrintWarning("File not found: " + filepath);
+          delete op;
+          return NULL;
+        }
+
+        // if the file size exceeds kMaxFileSize then bundle is not created
+        currentFileSize = GetFileSize(filepath);
+        if (currentFileSize > kMaxFileSize) {
+          PrintWarning("Large file found: " + filepath + " of size: "
+                + StringifyInt(currentFileSize));
+          delete op;
+          return NULL;
+        }
+
+        if (!AddFileToObjectPack(op, filepath)) {
+          delete op;
+          return NULL;
+        }
+      }
+    } else {
+      PrintWarning("Empty bundle not created");
+      return NULL;
+    }
+
+    return op;
+  }
+}
--- cvmfs/bundle.h
+++ cvmfs/bundle.h
@@ -5,6 +5,11 @@
 #ifndef CVMFS_BUNDLE_H_
 #define CVMFS_BUNDLE_H_
 
+#include <string>
+
+#include "json_document.h"
+#include "pack.h"
+
 /**
  * A bundle has the binary format of object packs used by the gateway as
  * a form to concatenate multiple objects in a single BLOB.
@@ -15,7 +20,20 @@
  * .cvmfsbundles file.
  */
 class Bundle {
+ public:
+  const int64_t kMaxFileSize = 1024 * 1024 * 100;
 
+/**
+ * Creates a ObjectPack bundle of one or more regular files
+ * The bundle is not created if any of the files to be included is not found or
+ * if the file size of any of the files exceeds a maximum value kMaxFileSize
+ * The JSON Object json_obj passed has a bundle id and a JSON array of file
+ * paths
+ *
+ * Returns a pointer to the created ObjectPack on success, NULL
+ * otherwise
+ */
+  ObjectPack* CreateBundle(const JSON* json_obj);
 };
 
 #endif  // CVMFS_BUNDLE_H_
