a89505f65949ebc99b4565104ac7df15714dc9bb
RohitTopi <rohittopi474@gmail.com>
2021-07-20 12:30:58 +0530

    Add test for parsing bundle specification

--- test/unittests/t_bundle.cc
+++ test/unittests/t_bundle.cc
@@ -6,6 +6,22 @@
 
 #include "bundle.h"
 
-TEST(T_Bundle, Bundle) {
+TEST(T_Bundle, ParseBundleSpec) {
+  std::string json_string = "{"
+    "\"bundle_id\":\"abc\","
+    "\"filepaths\":["
+    "\"/cvmfs/test.cern.ch/file1.txt\","
+    "\"/cvmfs/test.cern.ch/file2.txt\","
+    "\"/cvmfs/test.cern.ch/file3.txt\" "
+    "]}";
 
+  std::set<std::string> exp_result;
+  exp_result.insert("/cvmfs/test.cern.ch/file1.txt");
+  exp_result.insert("/cvmfs/test.cern.ch/file2.txt");
+  exp_result.insert("/cvmfs/test.cern.ch/file3.txt");
+
+  const JsonDocument *json = JsonDocument::Create(json_string);
+  Bundle b;
+  std::set<std::string> result = b.ParseBundleSpec(json->root());
+  EXPECT_TRUE(result == exp_result);
 }
