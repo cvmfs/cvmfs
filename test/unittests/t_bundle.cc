15e59e4f47d9601b58c0de4caf168e24d9277a38
RohitTopi <rohittopi474@gmail.com>
2021-07-23 10:24:55 +0530

    Add test for creation of a bundle

--- test/unittests/t_bundle.cc
+++ test/unittests/t_bundle.cc
@@ -4,6 +4,13 @@
 
 #include <gtest/gtest.h>
 
+#include <set>
+#include <string>
+
+#include "pack.h"
+#include "util/pointer.h"
+#include "util/posix.h"
+
 #include "bundle.h"
 
 TEST(T_Bundle, ParseBundleSpec) {
@@ -24,4 +31,35 @@ TEST(T_Bundle, ParseBundleSpec) {
   Bundle b;
   std::set<std::string> result = b.ParseBundleSpec(json->root());
   EXPECT_TRUE(result == exp_result);
+}
+
+TEST(T_Bundle, CreateBundle) {
+  std::string filepath1, filepath2;
+  FILE *file1 = CreateTempFile("cvmfstest", 0600, "w+", &filepath1);
+  FILE *file2 = CreateTempFile("cvmfstest", 0600, "w+", &filepath2);
+
+  size_t object_size = 0;
+
+  std::string file_content = "abcxyza";
+  size_t nbytes = fwrite(file_content.data(), 1, file_content.size(), file1);
+  assert(nbytes == file_content.size());
+  object_size += file_content.size();
+
+  file_content = "abc1234567890";
+  nbytes = fwrite(file_content.data(), 1, file_content.size(), file2);
+  assert(nbytes == file_content.size());
+  object_size += file_content.size();
+
+  fclose(file1);
+  fclose(file2);
+
+  std::set<std::string> filepaths;
+  filepaths.insert(GetAbsolutePath(filepath1));
+  filepaths.insert(GetAbsolutePath(filepath2));
+
+  Bundle b;
+  UniquePtr<ObjectPack> *result = b.CreateBundle(filepaths);
+
+  EXPECT_TRUE(result->IsValid());
+  EXPECT_TRUE((*result)->size() == object_size);
 }
