929cb910e247dbd335426b4c4532e4d0c8d1524a
Jakob Blomer <jblomer@cern.ch>
2021-07-27 17:41:32 +0200

    add -diff option to run_tidy.sh

--- ci/run_tidy.sh
+++ ci/run_tidy.sh
@@ -10,9 +10,15 @@ set -e
 SCRIPT_LOCATION=$(cd "$(dirname "$0")"; pwd)
 . ${SCRIPT_LOCATION}/common.sh
 
+DIFF=0
 BUILD_DIR=$1
+if [ "x$BUILD_DIR" = "x-diff" ]; then
+  DIFF=1
+  shift
+  BUILD_DIR=$1
+fi
 if [ "x$BUILD_DIR" = "x" ]; then
-  die "build directory missing (usage: '$0 <build dir>')"
+  die "build directory missing (usage: '$0 [-diff] <build dir>')"
 fi
 SCRIPT_OUTPUT=$2
 
@@ -22,11 +28,20 @@ REPO_ROOT="$(get_repository_root)"
 clang-tidy --version >/dev/null 2>&1 || die "clang-tidy missing"
 
 SOURCE_FILES=$(find $REPO_ROOT/mount -name '*.cc')
-#SOURCE_FILES="$SOURCE_FILES $REPO_ROOT/cvmfs/util/exception.cc"
+SOURCE_FILES="$SOURCE_FILES $REPO_ROOT/cvmfs/util/exception.cc"
+
+cd $REPO_ROOT
+if [ $DIFF -eq 0 ]; then
+  CMD0="true"
+  CMD1="clang-tidy -p $BUILD_DIR $SOURCE_FILES"
+else
+  CMD0="git diff -U0 devel"
+  CMD1="/usr/share/clang/clang-tidy-diff.py -path $BUILD_DIR -p1"
+fi
 
 if [ "x$SCRIPT_OUTPUT" != "x" ]; then
-  clang-tidy -p $BUILD_DIR $SOURCE_FILES 2>&1 | tee ${SCRIPT_OUTPUT}
+  $CMD0 | $CMD1 2>&1 | tee ${SCRIPT_OUTPUT}
   exit ${PIPESTATUS[0]}
 else
-  clang-tidy -p $BUILD_DIR $SOURCE_FILES
+  $CMD0 | $CMD1
 fi
