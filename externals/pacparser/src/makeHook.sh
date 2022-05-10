278eea215a07f8ae45a6d0e407aa893e016572c6
Radu Popescu <mail@radupopescu.net>
2022-05-10 08:01:23 -0700

    Rollback to Pacparser 1.3.8

--- externals/pacparser/src/makeHook.sh
+++ externals/pacparser/src/makeHook.sh
@@ -9,23 +9,24 @@ if [ x"$(uname)" = x"Darwin" ]; then
   FIX_COMP="CC=/usr/bin/clang CXX=/usr/bin/clang++"
 fi
 
-FIX_PYTHON="PYTHON=python3"
-if ! python3 -V >/dev/null 2>&1; then
-  FIX_PYTHON="PYTHON=python2"
-  if ! python2 -V >/dev/null 2>&1; then
-    FIX_PYTHON="PYTHON=python"
+FIX_PYTHON=""
+if ! python -V >/dev/null 2>&1; then
+  if ! python3 -V >/dev/null 2>&1; then
+    FIX_PYTHON="PYTHON=python2"
+  else
+    FIX_PYTHON="PYTHON=python3"
   fi
 fi
 
 echo "make clean && make for libpacparser (omitting test execution)..."
 [ -d $static_result_dir ] && rm -fR $static_result_dir
 make $FIX_PYTHON -C src clean
-make $FIX_PYTHON $FIX_COMP CVMFS_BASE_C_FLAGS="$CVMFS_BASE_C_FLAGS" -j1 -C src pacparser.o spidermonkey/libjs.a # default target runs tests!
+make $FIX_PYTHON $FIX_COMP CVMFS_BASE_C_FLAGS="$CVMFS_BASE_C_FLAGS" -j1 -C src pacparser.o libjs.a # default target runs tests!
 echo "finished internal build of libpacparser"
 
 echo "creating static link library for libpacparser..."
 mkdir src/static
-cp src/pacparser.o src/spidermonkey/libjs.a $static_result_dir
+cp src/pacparser.o src/libjs.a $static_result_dir
 
 cd $static_result_dir
 ar x libjs.a
