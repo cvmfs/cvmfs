6da0abed0df905565ea226a6f2b15042fc8ed83c
Jakob Blomer <jblomer@cern.ch>
2022-05-23 16:19:18 +0200

    Add initial EL9 integration test scritps

---
+++ test/cloud_testing/platforms/centos9_x86_64_setup.sh
@@ -0,0 +1,92 @@
+#!/bin/sh
+
+# source the common platform independent functionality and option parsing
+script_location=$(dirname $(readlink --canonicalize $0))
+. ${script_location}/common_setup.sh
+
+# CernVM-FS server needs 'jq' from epel
+echo "enabling epel yum repository..."
+install_from_repo epel-release
+
+# install CernVM-FS RPM packages
+echo "installing RPM packages... "
+install_rpm "$CONFIG_PACKAGES"
+install_rpm $CLIENT_PACKAGE
+install_rpm $SERVER_PACKAGE
+install_rpm $DEVEL_PACKAGE
+install_rpm $UNITTEST_PACKAGE
+install_rpm $SHRINKWRAP_PACKAGE
+install_rpm $GATEWAY_PACKAGE
+
+# installing WSGI apache module
+echo "installing python WSGI module..."
+install_from_repo python3-mod_wsgi || die "fail (installing mod_wsgi)"
+sudo systemctl restart httpd       || die "fail (restarting apache)"
+
+echo "installing mod_ssl for Apache"
+install_from_repo mod_ssl || die "fail (installing mod_ssl)"
+
+# setup environment
+echo -n "setting up CernVM-FS environment..."
+sudo cvmfs_config setup                          || die "fail (cvmfs_config setup)"
+sudo mkdir -p /var/log/cvmfs-test                || die "fail (mkdir /var/log/cvmfs-test)"
+sudo chown sftnight:sftnight /var/log/cvmfs-test || die "fail (chown /var/log/cvmfs-test)"
+sudo systemctl start autofs                      || die "fail (systemctl start autofs)"
+sudo cvmfs_config chksetup > /dev/null           || die "fail (cvmfs_config chksetup)"
+echo "done"
+
+# install additional stuff (needed for perl testing tools)
+echo "installing additional RPM packages..."
+install_from_repo gcc
+install_from_repo gcc-c++
+install_from_repo rubygems
+install_from_repo wget
+install_from_repo java-1.8.0-openjdk
+install_from_repo redhat-lsb-core
+install_from_repo tree
+install_from_repo fuse-overlayfs
+
+# traffic shaping
+# install_from_repo trickle
+# TODO: uncomment once trickle is available for Centos 9
+#       and enable test 056-lowspeedlimit
+
+# Install test dependency for 647
+install_from_repo python2-pip
+sudo pip2 install flask                      || die "fail (installing python-flask)"
+install_from_repo python3-flask
+
+# Install test dependency for 604
+install_from_repo python3
+install_from_repo netcat
+
+# Install the test S3 provider
+install_test_s3
+
+# building preloader
+install_from_repo cmake
+install_from_repo zlib-devel
+install_from_repo libattr-devel
+install_from_repo openssl-devel
+install_from_repo libuuid-devel
+install_from_repo python2-devel
+install_from_repo unzip
+install_from_repo bzip2
+install_from_repo acl
+
+# Migration test needs lsb_release
+echo "install lsb_release..."
+install_from_repo redhat-lsb-core
+
+# increase open file descriptor limits
+echo -n "increasing ulimit -n ... "
+set_nofile_limit 65536 || die "fail"
+echo "done"
+
+disable_systemd_rate_limit
+
+# Allow for proxying pass-through repositories
+sudo setsebool -P httpd_can_network_connect on
+
+# Ensure Apache is up and running after package update
+sudo systemctl restart httpd || die "failure in final Apache restart"
---
+++ test/cloud_testing/platforms/centos9_x86_64_test.sh
@@ -0,0 +1,78 @@
+#!/bin/sh
+
+export CVMFS_PLATFORM_NAME="centos9-x86_64"
+export CVMFS_TIMESTAMP=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
+
+# source the common platform independent functionality and option parsing
+script_location=$(cd "$(dirname "$0")"; pwd)
+. ${script_location}/common_test.sh
+
+retval=0
+
+# allow apache access to the mounted server file system
+echo -n "setting SELinux labels for apache... "
+sudo chcon -Rv --type=httpd_sys_content_t /srv > /dev/null || die "fail"
+echo "done"
+
+# start apache
+echo -n "starting apache... "
+sudo systemctl start httpd > /dev/null 2>&1 || die "fail"
+echo "OK"
+
+# running unit test suite
+run_unittests --gtest_shuffle \
+              --gtest_death_test_use_fork || retval=1
+
+cd ${SOURCE_DIRECTORY}/test
+echo "running CernVM-FS client test cases..."
+CVMFS_TEST_CLASS_NAME=ClientIntegrationTests                                  \
+./run.sh $CLIENT_TEST_LOGFILE -o ${CLIENT_TEST_LOGFILE}${XUNIT_OUTPUT_SUFFIX} \
+                              -x src/005-asetup                               \
+                                 src/004-davinci                              \
+                                 src/007-testjobs                             \
+                                 src/056-lowspeedlimit                        \
+                                 src/084-premounted                           \
+                                 src/094-attachmount                          \
+                                 --                                           \
+                                 src/0*                                       \
+                              || retval=1
+
+
+echo "running CernVM-FS server test cases..."
+CVMFS_TEST_CLASS_NAME=ServerIntegrationTests                                  \
+CVMFS_TEST_UNIONFS=overlayfs                                                  \
+./run.sh $SERVER_TEST_LOGFILE -o ${SERVER_TEST_LOGFILE}${XUNIT_OUTPUT_SUFFIX} \
+                              -x src/518-hardlinkstresstest                   \
+                                 src/600-securecvmfs                          \
+                                 src/628-pythonwrappedcvmfsserver             \
+                                 src/672-publish_stats_hardlinks              \
+                                 src/673-acl                                  \
+                                 src/684-https_s3                             \
+                                 src/686-azureblob_s3                         \
+                                 src/687-import_s3                            \
+                                 --                                           \
+                                 src/5*                                       \
+                                 src/6*                                       \
+                                 src/7*                                       \
+                                 src/8*                                       \
+                                 src/9*                                       \
+                              || retval=1
+
+
+# TODO(jblomer): re-enable once there is a cvmfs package history for EL9
+#echo "running CernVM-FS client migration test cases..."
+#CVMFS_TEST_CLASS_NAME=ClientMigrationTests                        \
+#./run.sh $MIGRATIONTEST_CLIENT_LOGFILE                            \
+#         -o ${MIGRATIONTEST_CLIENT_LOGFILE}${XUNIT_OUTPUT_SUFFIX} \
+#            migration_tests/0*                                    \
+#         || retval=1
+#
+#
+#echo "running CernVM-FS server migration test cases..."
+#CVMFS_TEST_CLASS_NAME=ServerMigrationTests                        \
+#./run.sh $MIGRATIONTEST_SERVER_LOGFILE                            \
+#         -o ${MIGRATIONTEST_SERVER_LOGFILE}${XUNIT_OUTPUT_SUFFIX} \
+#            migration_tests/5*                                    \
+#         || retval=1
+
+exit $retval
