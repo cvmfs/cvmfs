cd3a53b7ceb6a5c340a4ec1bd05636c66359a4bd
Jakob Blomer <jblomer@cern.ch>
2022-06-11 18:05:12 +0200

    add class InterruptCue

---
+++ cvmfs/interrupt.h
@@ -0,0 +1,19 @@
+/**
+ * This file is part of the CernVM File System.
+ */
+
+#ifndef CVMFS_INTERRUPT_H_
+#define CVMFS_INTERRUPT_H_
+
+/**
+ * Allows to query for interrupts of active file system requests.  Used
+ * to hande canceled fuse requests.
+ */
+class InterruptCue {
+ public:
+  InterruptCue() { }
+  virtual ~InterruptCue() { }
+  virtual bool IsCanceled() { return false; }
+};
+
+#endif  // CVMFS_INTERRUPT_H_
