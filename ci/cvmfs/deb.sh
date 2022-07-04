19323792135b681f4bfc93a71ded748e4de8a1a6
Radu Popescu <mail@radupopescu.net>
2022-07-04 11:03:11 +0200

    Fixing deb packaging on Ubuntu 22.04 (#2987)
    * Fixing deb packaging on Ubuntu 22.04
    
    * Depend on python3-dev on Ubuntu 22.03
    
    * Call CMake directly instead of dh_auto_configure

--- ci/cvmfs/deb.sh
+++ ci/cvmfs/deb.sh
@@ -61,12 +61,17 @@ fi
 # Fuse3 is only available as of Debian 10 "buster" and Ubuntu 20.04
 if [ x"$(lsb_release -sc)" = x"bullseye" -o \
      x"$(lsb_release -sc)" = x"buster" -o \
-     x"$(lsb_release -sc)" = x"focal" ]; then
+     x"$(lsb_release -sc)" = x"focal" -o \
+     x"$(lsb_release -sc)" = x"jammy" ]; then
   sed -i -e "s/^Build-Depends:/Build-Depends: libfuse3-dev,/g" debian/control
   sed -i -e "s/^Recommends:/Recommends: cvmfs-fuse3,/g" debian/control
 else
   cat debian/control | awk '/#FUSE3-BEGIN/{flag=1;next}/#FUSE3-END/{flag=0;next}!flag' > debian/control.tmp
   mv debian/control.tmp debian/control
+fi
+# Depend on python3-dev instead of python-dev on Ubuntu 22.04
+if [ x"$(lsb_release -sc)" = x"jammy" ]; then
+  sed -i -e "s/python-dev/python3-dev/g" debian/control
 fi
 # The cvmfs-gateway requires a go compiler
 if ! go version >/dev/null 2>&1; then
