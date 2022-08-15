dd49ada13d43cd17fc355b2cf4590b869029272d
Jakob Blomer <jblomer@cern.ch>
2022-08-15 08:12:19 +0200

    add comment

--- externals/libcrypto/src/configureHook.sh
+++ externals/libcrypto/src/configureHook.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# We install libcrypto into ${EXTERNALS_INSTALL_LOCATION}/crypto in order
+# to not taint the standard include / library paths with it. The LibreSSL
+# libcrypto is only supposed to be picked up by libcvmfs_crypto
+
 mkdir build && cd build
 CFLAGS="${CVMFS_BASE_C_FLAGS} -fPIC" ../configure \
   --enable-static \
