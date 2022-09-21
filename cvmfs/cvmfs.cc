3ca71fc8d7abc6fb714a4d16d1f66271521e0c4d
HereThereBeDragons <HereThereBeDragons@users.noreply.github.com>
2022-09-21 16:39:31 +0200

    appease linter

--- cvmfs/cvmfs.cc
+++ cvmfs/cvmfs.cc
@@ -466,7 +466,7 @@ static void cvmfs_lookup(fuse_req_t req, fuse_ino_t parent, const char *name) {
   // 076 fails with the following line uncommented
   //
   // WARNING! ENABLING THIS BREAKS ANY TYPE OF MOUNTPOINT POINTING TO THIS INODE
-  if (mount_point_->cache_symlinks()) { // && dirent.IsLink()) {
+  if (mount_point_->cache_symlinks()) {  //  && dirent.IsLink()) {
     LogCvmfs(kLogCache, kLogDebug, "Dentry to evict: %s", name);
     mount_point_->dentry_tracker()->Add(parent_fuse, name, uint64_t(timeout));
   }
@@ -1644,7 +1644,7 @@ static void cvmfs_init(void *userdata, struct fuse_conn_info *conn) {
 #if FUSE_VERSION >= 310
     if ((conn->capable & FUSE_CAP_CACHE_SYMLINKS) == 0) {
       mount_point_->DisableCacheSymlinks();
-      LogCvmfs(kLogCvmfs, kLogDebug, //kLogDebug | kLogSyslogErr,
+      LogCvmfs(kLogCvmfs, kLogDebug,  // kLogDebug | kLogSyslogErr,
            "Symlink caching requested but missing fuse kernel support, "
            "falling back to no caching");
     }
