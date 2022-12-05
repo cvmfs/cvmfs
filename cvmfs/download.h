a448d85650136e05644f83c3bc0080beccf97ebc
MJ Harvey <mharvey@jumptrading.com>
2022-12-05 06:41:35 -0600

    add header

--- cvmfs/download.h
+++ cvmfs/download.h
@@ -153,6 +153,7 @@ struct Counters {
  * Contains all the information to specify a download job.
  */
 struct JobInfo {
+  std::string name;
   const std::string *url;
   bool compressed;
   bool probe_hosts;
