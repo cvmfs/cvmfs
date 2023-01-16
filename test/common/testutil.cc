b4a021927cef450eedfdc3330fc2c89dcf09c727
HereThereBeDragons <HereThereBeDragons@users.noreply.github.com>
2023-01-16 16:18:27 +0100

    getprocessname: use proper trim function

--- test/common/testutil.cc
+++ test/common/testutil.cc
@@ -93,20 +93,11 @@ std::string GetProcessname(const pid_t pid) {
     if (line.compare(0, label.size(), label) == 0) {
       const std::string line_without_label = line.substr(label.size());
 
-      // left trim whitespace 
-      size_t l_last_space = line_without_label.find_first_not_of(" ");
-      size_t l_last_tab = line_without_label.find_first_not_of("\t");
-      const std::string line_without_label_ltrim =
-            line_without_label.substr(
-                  l_last_space > l_last_tab ? l_last_space : l_last_tab);
-      // right trim whitespace
-      std::istringstream value(line_without_label_ltrim);
-      SkipWhitespace(&value);
-      return value.str();
+      return Trim(line_without_label);
     }
   }
   return NULL;
-#endif  
+#endif
 }
 
 
