2a37af41ff26c9b73b70418fb6fa463172a9fd20
HereThereBeDragons <HereThereBeDragons@users.noreply.github.com>
2023-01-27 18:30:15 +0100

    working version - needs maybe clean up

--- cvmfs/statistics_database.cc
+++ cvmfs/statistics_database.cc
@@ -99,6 +99,7 @@ struct GcStats {
   std::string n_condemned_catalogs;
   std::string n_condemned_objects;
   std::string sz_condemned_bytes;
+  std::string duplicate_delete_requests;
 
   explicit GcStats(const perf::Statistics *statistics) {
     perf::Counter *c = NULL;
@@ -110,6 +111,8 @@ struct GcStats {
     n_condemned_objects = c ? c->ToString() : "0";
     c = statistics->Lookup("gc.sz_condemned_bytes");
     sz_condemned_bytes = c ? c->ToString() : "0";
+    c = statistics->Lookup("gc.duplicate_delete_requests");
+    duplicate_delete_requests = c ? c->ToString() : "0";
   }
 };
 
@@ -197,6 +200,7 @@ std::string PrepareStatementIntoGc(const perf::Statistics *statistics,
       "n_condemned_catalogs,"
       "n_condemned_objects,"
       "sz_condemned_bytes,"
+      "duplicate_delete_requests,"
       "success)"
       " VALUES("
       "'" + start_time + "'," +
@@ -205,6 +209,7 @@ std::string PrepareStatementIntoGc(const perf::Statistics *statistics,
       stats.n_condemned_catalogs + ","+
       stats.n_condemned_objects + "," +
       stats.sz_condemned_bytes + "," +
+      stats.duplicate_delete_requests + "," +
       (success ? "1" : "0") + ");";
   } else {
     // insert values except sz_condemned_bytes
@@ -215,6 +220,7 @@ std::string PrepareStatementIntoGc(const perf::Statistics *statistics,
       "n_preserved_catalogs,"
       "n_condemned_catalogs,"
       "n_condemned_objects,"
+      "duplicate_delete_requests,"
       "success)"
       " VALUES("
       "'" + start_time + "'," +
@@ -222,6 +228,7 @@ std::string PrepareStatementIntoGc(const perf::Statistics *statistics,
       stats.n_preserved_catalogs + "," +
       stats.n_condemned_catalogs + "," +
       stats.n_condemned_objects + "," +
+      stats.duplicate_delete_requests + "," +
       (success ? "1" : "0") + ");";
   }
   return insert_statement;
@@ -264,6 +271,7 @@ bool StatisticsDatabase::CreateEmptyDatabase() {
     "n_condemned_catalogs INTEGER,"
     "n_condemned_objects INTEGER,"
     "sz_condemned_bytes INTEGER,"
+    "duplicate_delete_requests INTEGER,"
     "success INTEGER);").Execute();
   return ret1 & ret2;
 }
