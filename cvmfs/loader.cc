2c02ef4d2999ff2fe90f4632f32c6f98f42315ca
MJ Harvey <mharvey@jumptrading.com>
2023-03-20 06:00:41 -0500

    disable for macos, use cvmfs utils for string split, appease linter

--- cvmfs/loader.cc
+++ cvmfs/loader.cc
@@ -34,7 +34,6 @@
 #include <cstdlib>
 #include <cstring>
 #include <string>
-#include <sstream>
 #include <vector>
 
 #include "duplex_fuse.h"
@@ -401,19 +400,6 @@ static int ParseFuseOptions(void *data __attribute__((unused)), const char *arg,
   }
 }
 
-
-static vector<int> splitToInts (const string &s, char delim) {
-    vector<int> result;
-    stringstream ss (s);
-    string item;
-
-    while (getline (ss, item, delim)) {
-        result.push_back ( stoi(item) );
-    }
-
-    return result;
-}
-
 static fuse_args *ParseCmdLine(int argc, char *argv[]) {
   struct fuse_args *mount_options = new fuse_args();
   CvmfsOptions cvmfs_options;
@@ -747,19 +733,25 @@ int FuseMain(int argc, char *argv[]) {
     LogCvmfs(kLogCvmfs, kLogStdout, "CernVM-FS: running with suid support");
   }
 
- if( options_manager->GetValue("CVMFS_CPU_AFFINITY", &parameter) ) {
+  if (options_manager->GetValue("CVMFS_CPU_AFFINITY", &parameter)) {
+#ifndef __APPLE__
      cpu_set_t mask;
-     vector<int> cpus = splitToInts( parameter, ',' );
+     vector<string> cpus = SplitString(parameter, ',');
      CPU_ZERO(&mask);
-     for( int i : cpus ) {
-        CPU_SET( i, &mask );
+     for (vector<string>::iterator i = cpus.begin(); i != cpus.end(); i++) {
+        CPU_SET(String2Uint64(*i), &mask);
      }
-
-     LogCvmfs(kLogCvmfs, kLogStdout, "Setting CPU Affinity to %s", parameter.c_str() );
-     int err = sched_setaffinity( 0, sizeof(mask), &mask );
-     if (err!=0 ) {
-        LogCvmfs(kLogCvmfs, kLogStdout | kLogSyslogErr, "Setting CPU Affinity failed with error %d", err );
+     LogCvmfs(kLogCvmfs, kLogStdout,
+              "Setting CPU Affinity to %s", parameter.c_str());
+     int err = sched_setaffinity(0, sizeof(mask), &mask);
+     if (err != 0) {
+        LogCvmfs(kLogCvmfs, kLogStdout | kLogSyslogErr,
+                 "Setting CPU Affinity failed with error %d", err);
      }
+#else
+     LogCvmfs(kLogCvmfs, kLogStdout | kLogsyslogErr,
+              "CPU affinity setting not supported on macOS");
+#endif
   }
   loader_exports_ = new LoaderExports();
   loader_exports_->loader_version = PACKAGE_VERSION;
