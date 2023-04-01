dd9c94f9b64e57d58e0fa26fa19897ebdd3fa2ce
Valentin Volkl <valentin.volkl@cern.ch>
2023-04-02 00:02:05 +0200

    ducc: fix for gitlab registry

--- ducc/lib/recipe.go
+++ ducc/lib/recipe.go
@@ -3,6 +3,8 @@ package lib
 import (
 	"strings"
 	"sync"
+  "time"
+  "math/rand"
 
 	l "github.com/cvmfs/ducc/log"
 	log "github.com/sirupsen/logrus"
@@ -39,23 +41,37 @@ func ParseYamlRecipeV1(data []byte) (Recipe, error) {
 	if err != nil {
 		return recipe, err
 	}
+
+  registryMap := make(map[string][]Image)
 	for _, inputImage := range recipeYamlV1.Input {
-		wg.Add(1)
-		go func(inputImage string) {
-			defer wg.Done()
 			input, err := ParseImage(inputImage)
 			if err != nil {
 				l.LogE(err).WithFields(log.Fields{"image": inputImage}).Warning("Impossible to parse the image")
-				return
 			}
-			output := formatOutputImage(recipeYamlV1.OutputFormat, input)
-			wish, err := CreateWish(inputImage, output, recipeYamlV1.CVMFSRepo, recipeYamlV1.User, recipeYamlV1.User)
-			if err != nil {
-				l.LogE(err).Warning("Error in creating the wish")
-			} else {
-				recipe.Wishes <- wish
-			}
-		}(inputImage)
+      registryMap[input.Registry] = append(registryMap[input.Registry], input)
+  }
+  for reg, inputImages := range registryMap {
+		wg.Add(1)
+		go func(inputImages []Image, reg string) {
+		  l.Log().Info("Starting with", inputImages[0])
+			defer wg.Done()
+      for _, input := range inputImages {
+        if reg == "gitlab-registry.cern.ch" {
+            time.Sleep(500 * time.Millisecond + time.Duration(rand.Intn(500)) * time.Millisecond)
+        }
+
+        l.Log().Info(reg)
+        l.Log().Info(input)
+
+        output := formatOutputImage(recipeYamlV1.OutputFormat, input)
+        wish, err := CreateWish(input, output, recipeYamlV1.CVMFSRepo, recipeYamlV1.User, recipeYamlV1.User)
+        if err != nil {
+          l.LogE(err).Warning("Error in creating the wish")
+        } else {
+          recipe.Wishes <- wish
+        }
+    }
+		}(inputImages, reg)
 	}
 	return recipe, nil
 }
