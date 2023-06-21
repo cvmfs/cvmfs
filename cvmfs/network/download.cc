46803b1ff61739697c77fa76cd47c9c3210ae2a8
HereThereBeDragons <HereThereBeDragons@users.noreply.github.com>
2023-06-21 11:06:29 +0200

    another try..

--- cvmfs/network/download.cc
+++ cvmfs/network/download.cc
@@ -1167,7 +1167,7 @@ void DownloadManager::SetRegularCache(JobInfo *info) {
 void DownloadManager::ReleaseCredential(JobInfo *info) {
   if (info->cred_data()) {
     assert(credentials_attachment_ != NULL);  // Someone must have set it
-    credentials_attachment_->ReleaseCurlHandle(info->curl_handle(),
+    credentials_attachment_->ReleaseCurlHandle(info->GetCurlHandle(),
                                                info->GetCredDataPtr());
     info->SetCredData(NULL);
   }
