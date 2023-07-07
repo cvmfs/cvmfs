cvmfs/cvmfs

commit d67bfbdf7549d16148f040dca4581696a70ef352

Don't poison md5path_cache with stale file entry

cvmfs/cvmfs.cc
@@ -358,6 +358,7 @@ static bool GetDirentForPath(const PathString &path,
   if (retval) {
     if (file_system_->IsNfsSource()) {
       dirent->set_inode(file_system_->nfs_maps()->GetInode(path));
+      mount_point_->md5path_cache()->Insert(md5path, *dirent);
     } else if (live_inode != 0) {
       dirent->set_inode(live_inode);
       if (FixupOpenInode(path, dirent)) {
@@ -365,11 +366,14 @@ static bool GetDirentForPath(const PathString &path,
           "content of %s change, replacing inode %" PRIu64 " --> %" PRIu64,
           path.c_str(), live_inode, dirent->inode());
         // The new inode is put in the tracker with refcounter == 0
+        // TODO(jblomer): move the replacement to cvmfs_lookup
         mount_point_->inode_tracker()->ReplaceInode(
           live_inode, glue::InodeEx(dirent->inode(), dirent->mode()));
+        // Do not populate the md5path cache until the inode tracker is fixed
+      } else {
+        mount_point_->md5path_cache()->Insert(md5path, *dirent);
       }
     }
-    mount_point_->md5path_cache()->Insert(md5path, *dirent);
     return true;
   }
 

commit 3c87374623f66d23f5e1a38d4632b42988f23dff

Fix performance bug introduced by commit d67bfbd

cvmfs/cvmfs.cc
@@ -407,6 +407,9 @@ static uint64_t GetDirentForPath(const PathString &path,
       } else {
         mount_point_->md5path_cache()->Insert(md5path, *dirent);
       }
+    } else {
+      // First time encounter of the inode
+      mount_point_->md5path_cache()->Insert(md5path, *dirent);
     }
     return 1;
   }