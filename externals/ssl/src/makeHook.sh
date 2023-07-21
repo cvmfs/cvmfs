82ac79e2738994e892b849b607fd6bddbac8e9f2
Valentin Volkl <valentin.volkl@cern.ch>
2023-07-21 15:15:52 +0200

    fix build on macos13
    libarchive is not required there

--- externals/ssl/src/makeHook.sh
+++ externals/ssl/src/makeHook.sh
@@ -3,6 +3,9 @@
 cd build
 make clean
 CFLAGS="$CVMFS_BASE_C_FLAGS -fPIC" CXXFLAGS="$CVMFS_BASE_CXX_FLAGS -fPIC" make install
-strip -S ssl/libssl.a
-strip -S crypto/libcrypto.a
+
+if [[ "x$(uname -s)" != "xDarwin" ]]; then
+  strip -S ssl/libssl.a
+  strip -S crypto/libcrypto.a
+fi
 make install
