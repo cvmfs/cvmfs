8a51f67bc37761603f86896fa571ba922b679fe7
HereThereBeDragons <HereThereBeDragons@users.noreply.github.com>
2023-08-01 13:34:01 +0200

    appease linter

--- cvmfs/network/download.cc
+++ cvmfs/network/download.cc
@@ -72,13 +72,13 @@ namespace download {
 
 /**
  * Returns the status if an interrupted happened for a given repository.
- * 
+ *
  * Used only in case CVMFS_FAILOVER_INDEFINITELY (failover_indefinitely_) is set
  * where failed downloads are retried indefinitely, unless an interrupt occurred
- * 
+ *
  * @note If you use this functionality you need to change the source code of
  * e.g. cvmfs_config reload to create a sentinel file. See comment below.
- * 
+ *
  * @return true if an interrupt occurred
  *         false otherwise
  */
@@ -90,7 +90,7 @@ bool Interrupted(const std::string &fqrn, JobInfo *info) {
   if (!fqrn.empty()) {
     // see https://github.com/cvmfs/cvmfs/pull/3304#discussion_r1278991197
     // it is up to the user the create this sentinel file ("pause_file") if
-    // CVMFS_FAILOVER_INDEFINITELY is used. It must be created during 
+    // CVMFS_FAILOVER_INDEFINITELY is used. It must be created during
     // "cvmfs_config reload" and "cvmfs_config reload $fqrn"
     std::string pause_file = std::string("/var/run/cvmfs/interrupt.") + fqrn;
 
