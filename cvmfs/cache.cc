a20821f45702ca7cbef05f9aba0ccefe846571bc
Valentin Volkl <valentin.volkl@cern.ch>
2023-08-03 15:02:19 +0200

    revert mistaken braces

--- cvmfs/cache.cc
+++ cvmfs/cache.cc
@@ -197,10 +197,9 @@ int CacheManager::RestoreState(const int fd_progress, void *data) {
   // exception: we do allow switching from the non-refcounted
   // to the refcounted posix cache manager
   if (state->manager_type != id()) {
-    if (fd_progress >= 0) {
+    if (fd_progress >= 0)
       SendMsg2Socket(fd_progress, "switching cache manager unsupported!\n");
     abort();
-    }
   }
   int new_root_fd = DoRestoreState(state->concrete_state);
   if (new_root_fd < -1) {
