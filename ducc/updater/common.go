914c2a6e86e95faa1db14f806cd0b547c8d1a547
oystub <o_github@oystub.com>
2023-08-11 11:39:07 +0200

    feat: full update task

--- ducc/updater/common.go
+++ ducc/updater/common.go
@@ -156,6 +156,66 @@ func FullUpdate(image db.Image, manifest ManifestWithBytesAndDigest, outputOptio
 		task.WaitForStart()
 		task.Log(nil, db.LOG_SEVERITY_DEBUG, "Starting update")
 
+		// Wa can start createFlatTask and createLayersTask in parallel
+		if outputOptions.CreateLayers.Value {
+			createLayersTask.Start(nil)
+			task.Log(nil, db.LOG_SEVERITY_INFO, "Started creating layers")
+		}
+		if outputOptions.CreateFlat.Value {
+			createFlatTask.Start(nil)
+			task.Log(nil, db.LOG_SEVERITY_INFO, "Started creating flat image")
+		}
+		// Podman has to wait for createLayersTask to finish
+		if outputOptions.CreatePodman.Value {
+			go func() {
+				createLayersResult := createLayersTask.WaitUntilDone()
+				if !db.TaskResultSuccessful(createLayersResult) {
+					task.Log(nil, db.LOG_SEVERITY_ERROR, "Cancelling 'Create Podman' due to failed 'Create Layers' task")
+					createPodmanTask.Cancel(nil)
+					return
+				}
+				createPodmanTask.Start(nil)
+				task.Log(nil, db.LOG_SEVERITY_INFO, "Started updating Podman store")
+			}()
+		}
+
+		someTaskFailed := false
+		// Wait for all tasks to finish
+		if outputOptions.CreateLayers.Value {
+			result := createLayersTask.WaitUntilDone()
+			if db.TaskResultSuccessful(result) {
+				task.Log(nil, db.LOG_SEVERITY_INFO, "Successfully created layers")
+			} else {
+				task.Log(nil, db.LOG_SEVERITY_ERROR, "Failed to create layers")
+				someTaskFailed = true
+			}
+		}
+		if outputOptions.CreateFlat.Value {
+			result := createFlatTask.WaitUntilDone()
+			if db.TaskResultSuccessful(result) {
+				task.Log(nil, db.LOG_SEVERITY_INFO, "Successfully created flat image")
+			} else {
+				task.Log(nil, db.LOG_SEVERITY_ERROR, "Failed to create flat image")
+				someTaskFailed = true
+			}
+		}
+		if outputOptions.CreatePodman.Value {
+			result := createPodmanTask.WaitUntilDone()
+			if db.TaskResultSuccessful(result) {
+				task.Log(nil, db.LOG_SEVERITY_INFO, "Successfully updated Podman store")
+			} else {
+				task.Log(nil, db.LOG_SEVERITY_ERROR, "Failed to update Podman store")
+				someTaskFailed = true
+			}
+		}
+
+		if someTaskFailed {
+			task.Log(nil, db.LOG_SEVERITY_ERROR, "Not all tasks were successful")
+			task.SetTaskCompleted(nil, db.TASK_RESULT_FAILURE)
+			return
+		}
+		task.Log(nil, db.LOG_SEVERITY_INFO, "All tasks completed successfully")
+		task.SetTaskCompleted(nil, db.TASK_RESULT_SUCCESS)
 	}()
 
 	return ptr, nil
