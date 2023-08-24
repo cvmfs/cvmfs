145af499cbed27e3657aaf5bca82486bdf06ad66
oystub <o_github@oystub.com>
2023-08-24 15:22:43 +0200

    update commands to work with daemon

--- ducc/cmd/convert.go
+++ ducc/cmd/convert.go
@@ -1,24 +1,20 @@
 package cmd
 
 import (
-	"io/ioutil"
+	"database/sql"
+	"fmt"
 	"os"
+	"sync"
 
-	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 
+	"github.com/cvmfs/ducc/config"
 	cvmfs "github.com/cvmfs/ducc/cvmfs"
-	exec "github.com/cvmfs/ducc/exec"
-	"github.com/cvmfs/ducc/lib"
+	"github.com/cvmfs/ducc/daemon"
+	"github.com/cvmfs/ducc/db"
+	"github.com/cvmfs/ducc/errorcodes"
 	l "github.com/cvmfs/ducc/log"
-)
-
-// errors
-var (
-	NoPasswordError      = 101
-	GetRecipeFileError   = 102
-	ParseRecipeFileError = 103
-	RepoNotExistsError   = 104
+	"github.com/cvmfs/ducc/registry"
 )
 
 var (
@@ -40,9 +36,30 @@ var convertCmd = &cobra.Command{
 	Short: "Convert the wishes",
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		AliveMessage()
 
-		if (skipLayers == false) && (skipThinImage == false) {
+		outputOptions := db.WishOutputOptions{
+			CreateLayers:    db.ValueWithDefault[bool]{Value: config.DEFAULT_CREATELAYERS, IsDefault: true},
+			CreateFlat:      db.ValueWithDefault[bool]{Value: config.DEFAULT_CREATEFLAT, IsDefault: true},
+			CreatePodman:    db.ValueWithDefault[bool]{Value: config.DEFAULT_CREATEPODMAN, IsDefault: true},
+			CreateThinImage: db.ValueWithDefault[bool]{Value: config.DEFAULT_CREATETHINIMAGE, IsDefault: true},
+		}
+
+		if skipLayers {
+			l.Log().Info("Skipping the creation of the thin image and podman store since provided --skip-layers")
+			outputOptions.CreateLayers = db.ValueWithDefault[bool]{Value: false, IsDefault: false}
+			skipThinImage = true
+			skipPodman = true
+		}
+		if skipFlat {
+			l.Log().Info("Skipping the creation of the flat image since provided --skip-flat")
+			outputOptions.CreateFlat = db.ValueWithDefault[bool]{Value: false, IsDefault: false}
+		}
+		if skipPodman {
+			l.Log().Info("Skipping the creation of the podman store since provided --skip-podman")
+			outputOptions.CreatePodman = db.ValueWithDefault[bool]{Value: false, IsDefault: false}
+		}
+
+		/*if (skipLayers == false) && (skipThinImage == false) {
 			_, err := lib.GetPassword()
 			if err != nil {
 				l.LogE(err).Error("No password provide to upload the docker images")
@@ -51,50 +68,134 @@ var convertCmd = &cobra.Command{
 		}
 
 		defer exec.ExecCommand("docker", "system", "prune", "--force", "--all")
+		*/
 
-		data, err := ioutil.ReadFile(args[0])
+		data, err := os.ReadFile(args[0])
 		if err != nil {
 			l.LogE(err).Error("Impossible to read the recipe file")
-			os.Exit(GetRecipeFileError)
+			os.Exit(errorcodes.GetRecipeFileError)
 		}
-		recipe, err := lib.ParseYamlRecipeV1(data)
+		recipe, err := db.ParseYamlRecipeV1(data, "cmd")
 		if err != nil {
 			l.LogE(err).Error("Impossible to parse the recipe file")
-			os.Exit(ParseRecipeFileError)
+			os.Exit(errorcodes.ParseRecipeFileError)
 		}
-		if !cvmfs.RepositoryExists(recipe.Repo) {
+		if !cvmfs.RepositoryExists(recipe.CvmfsRepo) {
 			l.LogE(err).Error("The repository does not seems to exists.")
-			os.Exit(RepoNotExistsError)
+			os.Exit(errorcodes.RepoNotExistsError)
 		}
-		for wish := range recipe.Wishes {
-			fields := log.Fields{"input image": wish.InputName,
-				"repository":   wish.CvmfsRepo,
-				"output image": wish.OutputName}
-			l.Log().WithFields(fields).Info("Start conversion of wish")
-			if !skipLayers {
-				err = lib.ConvertWish(wish, convertAgain, overwriteLayer)
-				if err != nil {
-					l.LogE(err).WithFields(fields).Error("Error in converting wish (layers), going on")
-				}
-			}
-			if !skipThinImage {
-				err = lib.ConvertWishDocker(wish)
-				if err != nil {
-					l.LogE(err).WithFields(fields).Error("Error in converting wish (docker), going on")
-				}
-			}
-			if !skipPodman {
-				err = lib.ConvertWishPodman(wish, convertAgain)
-				if err != nil {
-					l.LogE(err).WithFields(fields).Error("Error in converting wish (podman), going on")
-				}
-			}
-			if !skipFlat {
-				err = lib.ConvertWishFlat(wish)
-				if err != nil {
-					l.LogE(err).WithFields(fields).Error("Error in converting wish (singularity), going on")
-				}
+
+		// Start an in-memory database
+		database, err := sql.Open("sqlite3", ":memory:")
+		if err != nil {
+			panic("Unable to open in-memory database")
+		}
+		defer database.Close()
+		db.Init(database)
+
+		fmt.Printf("Recipe contains %d wishes\n", len(recipe.Wishes))
+
+		wishes := make([]db.Wish, len(recipe.Wishes))
+		for i, wish := range recipe.Wishes {
+			// Create wishes with output options
+			wishes[i] = db.Wish{
+				Identifier:    wish,
+				OutputOptions: outputOptions,
 			}
 		}
+		dbWishes, err := db.CreateWishes(nil, wishes)
+		if err != nil {
+			panic(fmt.Sprintf("Unable to create wishes: %s", err))
+		}
+
+		failedExpandWildcards := make([]db.Wish, 0)
+		succededExpandWildcards := make([]db.Wish, 0)
+
+		// Expand all wildcards
+		for _, wish := range dbWishes {
+			_, _, _, err := registry.ExpandWildcardAndStoreImages(wish)
+			if !wish.Identifier.InputTagWildcard {
+				continue
+			}
+			if err != nil {
+				fmt.Printf("Error expanding wildcard: %s\n", err)
+				failedExpandWildcards = append(failedExpandWildcards, wish)
+				succededExpandWildcards = append(succededExpandWildcards, wish)
+			}
+		}
+		if len(succededExpandWildcards) > 0 {
+			fmt.Printf("Successfully expanded %d wildcard wish(es)\n", len(succededExpandWildcards))
+			if len(failedExpandWildcards) > 0 {
+				fmt.Printf("FAILED to expand %d wildcard wish(es):\n", len(failedExpandWildcards))
+				for _, wish := range failedExpandWildcards {
+					fmt.Printf("\t- %s\n", wish.Identifier.InputString())
+				}
+			}
+		}
+
+		// Convert all images
+		images, err := db.GetAllImages(nil)
+		if err != nil {
+			panic(fmt.Sprintf("Unable to get all images: %s", err))
+		}
+		fmt.Printf("Found %d images to convert\n", len(images))
+
+		resultMutex := sync.Mutex{}
+		succeded := make([]db.Image, 0)
+		failed := make([]db.Image, 0)
+		tasks := make([]db.TaskPtr, len(images))
+		for i, image := range images {
+			tasks[i], err = daemon.UpdateImageTask(nil, image)
+			if err != nil {
+				fmt.Printf("Error in creating the task for image %s: %s\n", image.GetSimpleName(), err)
+				failed = append(failed, image)
+			}
+		}
+
+		// TODO: Display some stats about the conversion. Bytes to download, number of layers, etc.
+
+		wg := sync.WaitGroup{}
+		for i, task := range tasks {
+			i := i
+			wg.Add(1)
+			go func(task db.TaskPtr) {
+				defer wg.Done()
+				task.Start(nil)
+				result := task.WaitUntilDone()
+				if !db.TaskResultSuccessful(result) {
+					fmt.Printf("Error converting image %s\n", images[i].GetSimpleName())
+					resultMutex.Lock()
+					failed = append(failed, images[i])
+					resultMutex.Unlock()
+					return
+				}
+				fmt.Printf("Successfully converted image %s\n", images[i].GetSimpleName())
+				resultMutex.Lock()
+				succeded = append(succeded, images[i])
+				resultMutex.Unlock()
+			}(task)
+		}
+		wg.Wait()
+
+		fmt.Printf("Operation complete!\n")
+		if len(succeded) > 0 {
+			fmt.Printf("Successfully converted %d image(s)\n", len(succeded))
+			for _, image := range succeded {
+				fmt.Printf("\t- %s\n", image.GetSimpleName())
+			}
+		}
+		if len(failed) > 0 {
+			fmt.Printf("FAILED to convert %d image(s):\n", len(failed))
+			for _, image := range failed {
+				fmt.Printf("\t- %s\n", image.GetSimpleName())
+			}
+		}
+
+		if len(failed) > 0 || len(failedExpandWildcards) > 0 {
+			os.Exit(1)
+		}
+
+		os.Exit(0)
+
 	},
 }
---
+++ ducc/cmd/daemon.go
@@ -0,0 +1,116 @@
+package cmd
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"syscall"
+	"time"
+
+	"github.com/cvmfs/ducc/daemon"
+	"github.com/cvmfs/ducc/db"
+	"github.com/cvmfs/ducc/errorcodes"
+	"github.com/cvmfs/ducc/rest"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/spf13/cobra"
+)
+
+var restAPI bool
+var restAPIPort int
+var restAPIInterface string
+
+var automaticScheduling bool
+var databasePath string
+
+func init() {
+	daemonCmd.Flags().BoolVarP(&restAPI, "rest-api", "", true, "Run the REST API server")
+	daemonCmd.Flags().IntVarP(&restAPIPort, "rest-api-port", "", 8080, "Port on which to bind the REST API server")
+	daemonCmd.Flags().StringVarP(&restAPIInterface, "rest-api-interface", "", "0.0.0.0", "Interface on which to bind the REST API server")
+
+	daemonCmd.Flags().BoolVarP(&automaticScheduling, "automatic-scheduling", "", true, "Automatic scheduling of operations")
+
+	daemonCmd.Flags().StringVarP(&databasePath, "database-path", "", "/var/lib/cvmfs/ducc/ducc.db", "Path to the DUCC database")
+
+	rootCmd.AddCommand(daemonCmd)
+}
+
+var daemonCmd = &cobra.Command{
+	Use:   "daemon [flags]",
+	Short: "Run DUCC in daemon mode",
+	Args:  cobra.ExactArgs(0),
+	Run: func(cmd *cobra.Command, args []string) {
+		if err := os.MkdirAll(filepath.Dir(databasePath), 0755); err != nil {
+			fmt.Printf("Error creating directory for database: %s\n", err)
+			os.Exit(errorcodes.OpenDatabaseFileError)
+		}
+		database, err := sql.Open("sqlite3", databasePath)
+		if err != nil {
+			fmt.Printf("Error opening database: %s\n", err)
+			os.Exit(errorcodes.OpenDatabaseFileError)
+		}
+
+		if err := db.Init(database); err != nil {
+			fmt.Printf("Error initializing database: %s\n", err)
+			os.Exit(errorcodes.DatabaseError)
+		}
+		defer db.Close()
+
+		var restServer *http.Server
+		var restServerCleanupDone chan any
+		if restAPI {
+			// Start the REST API server
+			restServerCleanupDone = make(chan any) // We create a new channel here, so we can close it later
+			restServer = rest.StartRestServer(restServerCleanupDone, restAPIPort, restAPIInterface)
+			fmt.Printf("Running REST API on http://%s:%d\n", restAPIInterface, restAPIPort)
+		}
+
+		// Start the daemon
+		err = daemon.Init(automaticScheduling)
+		if err != nil {
+			fmt.Printf("Error initializing daemon: %s\n", err)
+			os.Exit(errorcodes.InternalLogicError)
+		}
+		daemonCtx, daemonCancelFunc := context.WithCancel(context.Background())
+		daemonCleanupDone := make(chan any)
+		go daemon.Run(daemonCtx, daemonCleanupDone)
+
+		// Start RPC
+		rpcCleanupDone := make(chan any)
+		rpcCtx, rpcCancelFunc := context.WithCancel(context.Background())
+		go daemon.RunRpcServer(rpcCtx, rpcCleanupDone)
+
+		// Handle signals
+		signals := make(chan os.Signal, 1)
+		signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
+
+		for {
+			signal := <-signals
+			switch signal {
+			case syscall.SIGINT, syscall.SIGTERM:
+				fmt.Println("Received SIGINT or SIGTERM, shutting down...")
+				if restServer != nil {
+					// After 5 seconds, we give up and force the server to shut down
+					ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+					restServer.Shutdown(ctx)
+					<-restServerCleanupDone
+				}
+				daemonCancelFunc()
+				<-daemonCleanupDone
+
+				rpcCancelFunc()
+				<-rpcCleanupDone
+				return
+				/*
+					TODO: Implement reloading of configuration
+					case syscall.SIGHUP:
+					fmt.Println("Received SIGHUP, reloading configuration...")
+				*/
+			}
+		}
+	},
+}
