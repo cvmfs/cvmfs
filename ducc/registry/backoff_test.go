3d2215fff63d70b204f54a9ec039fd7ab1fb9504
oystub <o_github@oystub.com>
2023-08-29 21:51:49 +0200

    per-repository tokens

--- ducc/registry/backoff_test.go
+++ ducc/registry/backoff_test.go
@@ -3,78 +3,43 @@ package registry
 import (
 	"context"
 	"math"
-	"math/rand"
 	"net/http"
-	"sync/atomic"
 	"testing"
 	"time"
 )
 
-func TestBackoffTurnstile_New(t *testing.T) {
+func TestBackoffGuard_New(t *testing.T) {
 	ctx, cancelFunc := context.WithCancel(context.Background())
 	defer cancelFunc()
-	turnstile := NewBackoffTurnstile(ctx, 2, 1*time.Second, 10*time.Second)
-	if turnstile == nil {
-		t.Error("Failed to create a new BackoffTurnstile")
+	guard := NewBackoffGuard(ctx, 2, 1*time.Second, 10*time.Second)
+	if guard == nil {
+		t.Error("Failed to create a new BackoffGuard")
 	}
 }
 
-func TestBackoffTurnstile_EnterExit(t *testing.T) {
+func TestBackoffGuard_SetBackoffRetryAfterInt(t *testing.T) {
 	ctx, cancelFunc := context.WithCancel(context.Background())
 	defer cancelFunc()
-	turnstile := NewBackoffTurnstile(ctx, 2, 1*time.Second, 10*time.Second)
-
-	go turnstile.Enter()
-	turnstile.Exit()
-}
-
-func TestBackoffTurnstile_MultipleEnterExit(t *testing.T) {
-	ctx, cancelFunc := context.WithCancel(context.Background())
-	defer cancelFunc()
-	turnstile := NewBackoffTurnstile(ctx, 2, 1*time.Second, 10*time.Second)
-
-	var insideTurnstileCount int64 = 0
-
-	enterExit := func() {
-		turnstile.Enter()
-		curCount := atomic.AddInt64(&insideTurnstileCount, 1)
-		if curCount > 1 {
-			t.Errorf("Multiple goroutines inside turnstile at the same time: %d", curCount)
-		}
-		time.Sleep(time.Millisecond * time.Duration(rand.Intn(10)))
-		atomic.AddInt64(&insideTurnstileCount, -1)
-		turnstile.Exit()
-	}
-
-	for i := 0; i < 50; i++ {
-		go enterExit()
-	}
-}
-
-func TestBackoffTurnstile_SetBackoffRetryAfterInt(t *testing.T) {
-	ctx, cancelFunc := context.WithCancel(context.Background())
-	defer cancelFunc()
-	turnstile := NewBackoffTurnstile(ctx, 2, 1*time.Second, 10*time.Second)
+	guard := NewBackoffGuard(ctx, 2, 1*time.Second, 10*time.Second)
 
 	// Use a mock HTTP response
 	res := &http.Response{
 		Header: make(http.Header),
 	}
 	res.Header.Set("Retry-After", "1") // Retry after 1 second
-	turnstile.SetBackoff(res)
+	guard.SetBackoff(res)
 	startTime := time.Now()
-	turnstile.Enter()
+	guard.Enter()
 	endTime := time.Now()
 	if endTime.Sub(startTime) < 1*time.Second || endTime.Sub(startTime) > 1*time.Second+10*time.Millisecond {
 		t.Error("Did not backoff for the specified time")
 	}
-	turnstile.Exit()
 }
 
-func TestBackoffTurnstile_SetBackoffRetryAfterHttpDate(t *testing.T) {
+func TestBackoffGuard_SetBackoffRetryAfterHttpDate(t *testing.T) {
 	ctx, cancelFunc := context.WithCancel(context.Background())
 	defer cancelFunc()
-	turnstile := NewBackoffTurnstile(ctx, 2, 1*time.Second, 10*time.Second)
+	guard := NewBackoffGuard(ctx, 2, 1*time.Second, 10*time.Second)
 
 	// Use a mock HTTP response with a Retry-After header set to an HTTP date format.
 	futureTime := time.Now().Add(2 * time.Second).UTC().Format(http.TimeFormat)
@@ -83,126 +48,120 @@ func TestBackoffTurnstile_SetBackoffRetryAfterHttpDate(t *testing.T) {
 	}
 	res.Header.Set("Retry-After", futureTime)
 
-	turnstile.SetBackoff(res)
+	guard.SetBackoff(res)
 	startTime := time.Now()
-	turnstile.Enter()
+	guard.Enter()
 	endTime := time.Now()
 	if endTime.Sub(startTime) < 2*time.Second || endTime.Sub(startTime) > (2+1)*time.Second+10*time.Millisecond {
 		t.Error("Did not backoff for the specified time")
 	}
-	turnstile.Exit()
-
 }
 
-func TestBackoffTurnstile_SetBackoffExponential(t *testing.T) {
+func TestBackoffGuard_SetBackoffExponential(t *testing.T) {
 	ctx, cancelFunc := context.WithCancel(context.Background())
 	defer cancelFunc()
-	turnstile := NewBackoffTurnstile(ctx, 2, 10*time.Millisecond, 1*time.Second)
+	guard := NewBackoffGuard(ctx, 2, 10*time.Millisecond, 1*time.Second)
 
 	res := &http.Response{
 		Header: make(http.Header),
 	}
 
-	turnstile.SetBackoff(res)
+	guard.SetBackoff(res)
 	startTime := time.Now()
-	turnstile.Enter()
+	guard.Enter()
 	endTime := time.Now()
 	if endTime.Sub(startTime) < 10*time.Millisecond || endTime.Sub(startTime) > 10*time.Millisecond+10*time.Millisecond {
 		t.Error("Did not backoff for the specified time")
 	}
-	turnstile.Exit()
 }
 
-func TestBackoffTurnstile_MultipleExponentialBackoffs(t *testing.T) {
+func TestBackoffGuard_MultipleExponentialBackoffs(t *testing.T) {
 	const backoffBase = 1.5
 	const initialBackoff = 10 * time.Millisecond
 
 	ctx, cancelFunc := context.WithCancel(context.Background())
 	defer cancelFunc()
-	turnstile := NewBackoffTurnstile(ctx, backoffBase, initialBackoff, 1*time.Second)
+	guard := NewBackoffGuard(ctx, backoffBase, initialBackoff, 1*time.Second)
 
 	res := &http.Response{
 		Header: make(http.Header),
 	}
 
 	for i := 0; i < 5; i++ {
-		turnstile.SetBackoff(res)
+		guard.SetBackoff(res)
 	}
 
 	expectedTotalWait := initialBackoff * time.Duration(math.Pow(backoffBase, 4))
 
 	startTime := time.Now()
-	turnstile.Enter()
+	guard.Enter()
 	endTime := time.Now()
 	if endTime.Sub(startTime) < expectedTotalWait || endTime.Sub(startTime) > expectedTotalWait+10*time.Millisecond {
 		t.Errorf("Did not backoff for the specified time. Expected %s, got %s", expectedTotalWait, endTime.Sub(startTime))
 	}
-	turnstile.Exit()
 }
 
-func TestBackoffTurnstile_MaxBackoffExponential(t *testing.T) {
+func TestBackoffGuard_MaxBackoffExponential(t *testing.T) {
 	const backoffBase = 1.5
 	const initialBackoff = 10 * time.Millisecond
 	const maxBackoff = 100 * time.Millisecond
 
 	ctx, cancelFunc := context.WithCancel(context.Background())
 	defer cancelFunc()
-	turnstile := NewBackoffTurnstile(ctx, backoffBase, initialBackoff, maxBackoff)
+	guard := NewBackoffGuard(ctx, backoffBase, initialBackoff, maxBackoff)
 
 	res := &http.Response{
 		Header: make(http.Header),
 	}
 
 	for i := 0; i < 10; i++ {
-		turnstile.SetBackoff(res)
+		guard.SetBackoff(res)
 	}
 
 	expectedTotalWait := maxBackoff
 
 	startTime := time.Now()
-	turnstile.Enter()
+	guard.Enter()
 	endTime := time.Now()
 	if endTime.Sub(startTime) < expectedTotalWait || endTime.Sub(startTime) > expectedTotalWait+10*time.Millisecond {
 		t.Errorf("Did not backoff for the specified time. Expected %s, got %s", expectedTotalWait, endTime.Sub(startTime))
 	}
-	turnstile.Exit()
 }
 
-func TestBackoffTurnstile_MaxBackoffRetryAfter(t *testing.T) {
+func TestBackoffGuard_MaxBackoffRetryAfter(t *testing.T) {
 	const maxBackoff = 100 * time.Millisecond
 
 	ctx, cancelFunc := context.WithCancel(context.Background())
 	defer cancelFunc()
-	turnstile := NewBackoffTurnstile(ctx, 2, 1*time.Second, maxBackoff)
+	guard := NewBackoffGuard(ctx, 2, 1*time.Second, maxBackoff)
 
 	res := &http.Response{
 		Header: make(http.Header),
 	}
 	res.Header.Set("Retry-After", "5")
 
-	turnstile.SetBackoff(res)
+	guard.SetBackoff(res)
 	startTime := time.Now()
-	turnstile.Enter()
+	guard.Enter()
 	endTime := time.Now()
 	if endTime.Sub(startTime) < maxBackoff || endTime.Sub(startTime) > maxBackoff+10*time.Millisecond {
 		t.Errorf("Did not backoff for the specified time. Expected %s, got %s", maxBackoff, endTime.Sub(startTime))
 	}
-	turnstile.Exit()
 }
 
-func TestBackoffTurnstile_ContextCanceled(t *testing.T) {
+func TestBackoffGuard_ContextCanceled(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
-	turnstile := NewBackoffTurnstile(ctx, 2, 1*time.Second, 10*time.Second)
+	guard := NewBackoffGuard(ctx, 2, 1*time.Second, 10*time.Second)
 
 	res := &http.Response{
 		Header: make(http.Header),
 	}
 	res.Header.Set("Retry-After", "1")
 
-	turnstile.SetBackoff(res)
+	guard.SetBackoff(res)
 	cancel()
 	startTime := time.Now()
-	turnstile.Enter()
+	guard.Enter()
 	endTime := time.Now()
 	if endTime.Sub(startTime) > 10*time.Millisecond {
 		t.Error("Turnstile worker did not exit immediately after context cancellation")
