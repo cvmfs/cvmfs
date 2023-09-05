cvmfs/cvmfs

commit d67bfbdf7549d16148f040dca4581696a70ef352

Don't poison md5path_cache with stale file entry

cvmfs/cvmfs.cc
@@ -358,6 +358,7 @@ static bool GetDirentForPath(const PathString &path,
   if (retval) {
     if (file_system_->IsNfsSource()) {
       dirent->set_inode(file_system_->nfs_maps()->GetInode(path));
+      mount_point_->md5path_cache()->Insert(md5path, *dirent);
     } else if (live_inode != 0) {
       dirent->set_inode(live_inode);
       if (FixupOpenInode(path, dirent)) {
@@ -365,11 +366,14 @@ static bool GetDirentForPath(const PathString &path,
           "content of %s change, replacing inode %" PRIu64 " --> %" PRIu64,
           path.c_str(), live_inode, dirent->inode());
         // The new inode is put in the tracker with refcounter == 0
+        // TODO(jblomer): move the replacement to cvmfs_lookup
         mount_point_->inode_tracker()->ReplaceInode(
           live_inode, glue::InodeEx(dirent->inode(), dirent->mode()));
+        // Do not populate the md5path cache until the inode tracker is fixed
+      } else {
+        mount_point_->md5path_cache()->Insert(md5path, *dirent);
       }
     }
-    mount_point_->md5path_cache()->Insert(md5path, *dirent);
     return true;
   }
 

commit a4af134bfb05c09ec10fb4b1180eaa8b867e2e21

Merge pull request #3309 from cvmfs/fix-perf
Fix performance bug introduced by commit d67bfbd

cvmfs/cvmfs.cc
@@ -395,7 +395,6 @@ static uint64_t GetDirentForPath(const PathString &path,
   if (retval) {
     if (file_system_->IsNfsSource()) {
       dirent->set_inode(file_system_->nfs_maps()->GetInode(path));
-      mount_point_->md5path_cache()->Insert(md5path, *dirent);
     } else if (live_inode != 0) {
       dirent->set_inode(live_inode);
       if (FixupOpenInode(path, dirent)) {
@@ -404,10 +403,9 @@ static uint64_t GetDirentForPath(const PathString &path,
           path.c_str(), live_inode, dirent->inode());
         return live_inode;
         // Do not populate the md5path cache until the inode tracker is fixed
-      } else {
-        mount_point_->md5path_cache()->Insert(md5path, *dirent);
       }
     }
+    mount_point_->md5path_cache()->Insert(md5path, *dirent);
     return 1;
   }
 