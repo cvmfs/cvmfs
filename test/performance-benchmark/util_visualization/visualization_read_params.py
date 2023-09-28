e8e00c0c990ab5b8bcbc14755d08deadf6b82c47
HereThereBeDragons <HereThereBeDragons@users.noreply.github.com>
2023-09-28 10:10:36 +0200

    visualization: add ignore config checking for sections not being plotted/written

--- test/performance-benchmark/util_visualization/visualization_read_params.py
+++ test/performance-benchmark/util_visualization/visualization_read_params.py
@@ -85,6 +85,18 @@ def verifyYAML(config):
     if (not section in config.keys()):
       continue
 
+
+    if (not "plot" in config[section].keys()):
+      config[section]["plot"] = True
+    elif (type(True) != type(config[section]["plot"])):
+      print('Section: "' + section + '": "plot" is not a boolean',
+            file=sys.stderr)
+      exit(22)
+
+    # we skip checking this section if its not requested to be executed
+    if config[section]["plot"] == False:
+      continue
+
     for label in ["cvmfs_build_names", "client_configs"]:
       if (not label in config[section].keys()
           or len(config[section][label]) == 0):
@@ -117,12 +129,7 @@ def verifyYAML(config):
             file=sys.stderr)
         exit(22)
 
-    if (not "plot" in config[section].keys()):
-      config[section]["plot"] = True
-    elif (type(True) != type(config[section]["plot"])):
-      print('Section: "' + section + '": "plot" is not a boolean',
-            file=sys.stderr)
-      exit(22)
+
 
   # check for general labels
   for a_set, a_label in [(set_cvmfs_build_name_labels, "cvmfs_build_name_labels"),
@@ -134,50 +141,21 @@ def verifyYAML(config):
 
   # check there is something to compare
   if ("cvmfs_build_comparison" in config.keys()
-      and len(config["cvmfs_build_comparison"]["cvmfs_build_names"]) < 2):
+      and len(config["cvmfs_build_comparison"]["cvmfs_build_names"]) < 2
+      and config["cvmfs_build_comparison"]["plot"] != False):
     print('cvmfs_build_comparison: to few elements in "cvmfs_build_names" '
           + 'to perform comparison', file=sys.stderr)
     exit(22)
 
   if ("client_config_comparison" in config.keys()
-      and len(config["client_config_comparison"]["client_configs"]) < 2):
+      and len(config["client_config_comparison"]["client_configs"]) < 2
+      and config["client_config_comparison"]["plot"] != False):
     print('client_config_comparison: to few elements in "client_configs" '
           + 'to perform comparison', file=sys.stderr)
     exit(22)
 
   # check scatter plot
   if "scatter_plot" in config.keys():
-    for label in ["cvmfs_build_names", "client_configs"]:
-      if (label in config["scatter_plot"].keys()
-          and len(config["scatter_plot"][label]) == 0):
-        print('Section: "scatter_plot" defined but "cvmfs_build_names" or '
-              + ' "client_configs" are empty. Either remove them to create the '
-              + 'scatter plots for all files or restrict it', file=sys.stderr)
-        exit(22)
-
-    if ((not "time_metrics" in config["scatter_plot"].keys()
-         or len(config["scatter_plot"]["time_metrics"]) == 0)
-        and (not "internal_affairs_metrics" in config["scatter_plot"].keys()
-             or len(config["scatter_plot"]["internal_affairs_metrics"]) == 0)):
-        print('Section: "scatter_plot" defined but neither "time_metrics" or '
-              + ' "internal_affairs_metrics" defined or they are empty',
-              file=sys.stderr)
-        exit(22)
-
-    if ("internal_affairs_metrics" in config["scatter_plot"].keys()):
-      if (not "internal_affairs_repos" in config["scatter_plot"].keys()
-          or len(config["scatter_plot"]["internal_affairs_repos"]) < 1):
-        print('Section: "scatter_plot" and "internal_affairs_metrics defined '
-            + ' but "internal_affairs_repos" is missing or empty',
-            file=sys.stderr)
-        exit(22)
-      
-      if (len(config["scatter_plot"]["internal_affairs_metrics"]) == 1
-          and config["scatter_plot"]["internal_affairs_metrics"] == "all"):
-        config["scatter_plot"]["internal_affairs_metrics"] = ["_" + key for key in \
-                           visualization_time.measurement_cvmfs_internal_dict.keys()]
-        print(config["scatter_plot"]["internal_affairs_metrics"])
-
     if (not "plot" in config["scatter_plot"].keys()):
       config["scatter_plot"]["plot"] = True
     elif (type(True) != type(config["scatter_plot"]["plot"])):
@@ -185,46 +163,85 @@ def verifyYAML(config):
             file=sys.stderr)
       exit(22)
 
+    # we skip checking this section if its not requested to be executed
+    if config["scatter_plot"]["plot"] == True:
+      for label in ["cvmfs_build_names", "client_configs"]:
+        if (label in config["scatter_plot"].keys()
+            and len(config["scatter_plot"][label]) == 0):
+          print('Section: "scatter_plot" defined but "cvmfs_build_names" or '
+                + ' "client_configs" are empty. Either remove them to create the '
+                + 'scatter plots for all files or restrict it', file=sys.stderr)
+          exit(22)
+
+      if ((not "time_metrics" in config["scatter_plot"].keys()
+          or len(config["scatter_plot"]["time_metrics"]) == 0)
+          and (not "internal_affairs_metrics" in config["scatter_plot"].keys()
+              or len(config["scatter_plot"]["internal_affairs_metrics"]) == 0)):
+          print('Section: "scatter_plot" defined but neither "time_metrics" or '
+                + ' "internal_affairs_metrics" defined or they are empty',
+                file=sys.stderr)
+          exit(22)
+
+      if ("internal_affairs_metrics" in config["scatter_plot"].keys()):
+        if (not "internal_affairs_repos" in config["scatter_plot"].keys()
+            or len(config["scatter_plot"]["internal_affairs_repos"]) < 1):
+          print('Section: "scatter_plot" and "internal_affairs_metrics defined '
+              + ' but "internal_affairs_repos" is missing or empty',
+              file=sys.stderr)
+          exit(22)
+
+        if (len(config["scatter_plot"]["internal_affairs_metrics"]) == 1
+            and config["scatter_plot"]["internal_affairs_metrics"] == "all"):
+          config["scatter_plot"]["internal_affairs_metrics"] = ["_" + key for key in \
+                            visualization_time.measurement_cvmfs_internal_dict.keys()]
+          print(config["scatter_plot"]["internal_affairs_metrics"])
+
+
+
   # check append csv
   if "append_to_csv" in config.keys():
-    for label in ["full_out_name", "tag"]:
-      if (not label in config["append_to_csv"].keys()
-          or len(config["append_to_csv"][label]) == 0):
-        print('Mandatory parameter missing or too short: "append_to_csv": '
-              + label, file=sys.stderr)
-        exit(22)
-    
-    for label in ["cvmfs_build_names", "client_configs"]:
-      if (label in config["append_to_csv"].keys()
-          and len(config["append_to_csv"][label]) == 0):
-        print('Section: "append_to_csv" defined but "cvmfs_build_names" or '
-              + ' "client_configs" are empty. Either remove them to create the '
-              + 'scatter plots for all files or restrict it', file=sys.stderr)
-        exit(22)
-
-    if ((not "time_metrics" in config["append_to_csv"].keys()
-         or len(config["append_to_csv"]["time_metrics"]) == 0)
-        and (not "internal_affairs_metrics" in config["append_to_csv"].keys()
-             or len(config["append_to_csv"]["internal_affairs_metrics"]) == 0)):
-        print('Section: "append_to_csv" defined but neither "time_metrics" or '
-              + ' "internal_affairs_metrics" defined or they are empty',
-              file=sys.stderr)
-        exit(22)
-
-    if ("internal_affairs_metrics" in config["append_to_csv"].keys()):
-      if (not "internal_affairs_repos" in config["append_to_csv"].keys()
-          or len(config["append_to_csv"]["internal_affairs_repos"]) < 1):
-        print('Section: initializeConfig"append_to_csv" and "internal_affairs_metrics" defined '
-            + ' but "internal_affairs_repos" is missing or empty',
-            file=sys.stderr)
-        exit(22)
-
     if (not "write" in config["append_to_csv"].keys()):
       config["append_to_csv"]["write"] = True
     elif (type(True) != type(config["append_to_csv"]["write"])):
       print('Section: "append_to_csv": "write" is not a boolean',
             file=sys.stderr)
       exit(22)
+
+    # we skip checking this section if its not requested to be executed
+    if config["append_to_csv"]["write"] == True:
+      for label in ["full_out_name", "tag"]:
+        if (not label in config["append_to_csv"].keys()
+            or len(config["append_to_csv"][label]) == 0):
+          print('Mandatory parameter missing or too short: "append_to_csv": '
+                + label, file=sys.stderr)
+          exit(22)
+
+      for label in ["cvmfs_build_names", "client_configs"]:
+        if (label in config["append_to_csv"].keys()
+            and len(config["append_to_csv"][label]) == 0):
+          print('Section: "append_to_csv" defined but "cvmfs_build_names" or '
+                + ' "client_configs" are empty. Either remove them to create the '
+                + 'scatter plots for all files or restrict it', file=sys.stderr)
+          exit(22)
+
+      if ((not "time_metrics" in config["append_to_csv"].keys()
+          or len(config["append_to_csv"]["time_metrics"]) == 0)
+          and (not "internal_affairs_metrics" in config["append_to_csv"].keys()
+              or len(config["append_to_csv"]["internal_affairs_metrics"]) == 0)):
+          print('Section: "append_to_csv" defined but neither "time_metrics" or '
+                + ' "internal_affairs_metrics" defined or they are empty',
+                file=sys.stderr)
+          exit(22)
+
+      if ("internal_affairs_metrics" in config["append_to_csv"].keys()):
+        if (not "internal_affairs_repos" in config["append_to_csv"].keys()
+            or len(config["append_to_csv"]["internal_affairs_repos"]) < 1):
+          print('Section: initializeConfig"append_to_csv" and "internal_affairs_metrics" defined '
+              + ' but "internal_affairs_repos" is missing or empty',
+              file=sys.stderr)
+          exit(22)
+
+
 
 def initConfig(config):
   config["_files"] = glob.glob(config["in_dirname"] + "*.csv")
@@ -246,13 +263,17 @@ def initConfig(config):
 
   for ele in config["num_threads"]:
     if not ele in config["_num_threads"]:
-      print('"num_threads" set with illegal values. Valid values are', 
+      print('"num_threads" set with illegal values. Valid values are',
             config["_num_threads"], file=sys.stderr)
       exit(22)
-  
+
   for section in ["cvmfs_build_comparison", "client_config_comparison",
                   "scatter_plot", "append_to_csv"]:
     if section in config.keys():
+      create_output = "plot" if section != "append_to_csv" else "write"
+      if config[section][create_output] == False:
+        continue
+
       for param in ["cvmfs_build_names", "client_configs"]:
         if param in config[section].keys():
           for ele in config[section][param]:
