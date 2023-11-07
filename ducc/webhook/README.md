6bda271743101007b9735b80324db848f5beecb9
marcoverl <marco.verlato@pd.infn.it>
2023-11-07 11:40:24 +0100

    Fix ducc webhook (#3425)
    * Create .env
    
    * Update README.md
    
    * Update registry-listener.go
    
    made more robust against failed ingestions of some layers
    
    * Update and rename registry-webhook.py to registry_webhook.py
    
    modified to pass env vars through .wsgi file
    
    * Create registry_webhook.wsgi
    
    * Create registry-listener.service
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update registry-listener.go

--- ducc/webhook/README.md
+++ ducc/webhook/README.md
@@ -1,19 +1,34 @@
-# Webhook notification service
-
-This small script listens to the webhook notifications from docker and harbor registries and appends to a file the image that was pushed, deleted or replicated.
-
-Run with:
-
-```
-python3 registry-webhook.py -f "notifications.txt" -h 127.0.0.1 -p 8080
-```
-where the flags account for:
-* `--file`(`-f`): name of the file where the notifications are appended.
-* `--host`(`-h`): IP address of the interface to bind to.
-* `--port`(`-p`): port number to listen on.
-* `--rotation`(`-r`): maximum action id per file.
-
+# Webhook notification service interacting with cvmfs_ducc
+The registry_webhook.py script listens to the webhook notifications from docker and harbor registries and appends to a notification file the image that was pushed, deleted or replicated. 
 The notifications appended are of the form:
 ```
 {id}|{action}|{image}
+```
+The registry-listener.go script checks for updates in the notification file and if finds a pushed image executes the cvmfs_ducc program with the convert-single-image option, so that after a few minutes the image is available on the cvmfs repository in both .flat (for Singularity/Apptainer use) and .layers (for Containerd/Kubernetes use) structures.   
+
+Below an example of installation and run of both services on a centos7 server as centos user (with sudo privileges). It requires cvmfs_ducc and httpd already installed on the server.
+```
+sudo yum install -y python36-mod_wsgi python3-flask
+sudo pip3 install python-dotenv
+cd /home/centos
+git clone --branch devel --depth 1 https://github.com/cvmfs/cvmfs.git
+cd cvmfs/ducc/webhook/; mkdir logs/
+#
+# set variables in .env file, if you change the PROJECT_PATH var and/or the user, change registry-listener.service accordingly
+#
+mod_wsgi-express-3 setup-server registry_webhook.wsgi --port 8080 --user centos --server-root=mod_wsgi-express-8080/ --log-directory logs/ --access-log
+sudo cp registry-listener.service /etc/systemd/system/
+mod_wsgi-express-8080/apachectl start
+sudo systemctl start registry-listener
+#
+# check logs here:
+tail -f logs/error_log
+journalctl -f -u registry-listener -l
+```
+To enable https with self-signed certificate:
+```
+sudo yum install -y mod_ssl
+openssl genpkey -algorithm RSA -out server.key
+openssl req -x509 -new -key server.key -out server.crt -days 365
+mod_wsgi-express-3 setup-server registry_webhook.wsgi  --https-port 8080 --https-only --server-name <server_name> --ssl-certificate-file server.crt --ssl-certificate-key-file server.key --user centos --server-root=mod_wsgi-express-8080/ --log-directory logs/ --access-log
 ```
--- ducc/webhook/registry_webhook.py
+++ ducc/webhook/registry_webhook.py
@@ -1,25 +1,26 @@
 from flask import Flask
 from flask import request
 
-import argparse
-import pprint
 import os
 
 app = Flask(__name__)
 
-@app.route("/<path:p>", methods=["POST"])
-def catch_all(p):
+@app.route("/", methods=["POST"])
+def catch_all():
+    notifications_file = os.environ.get('NOTIFICATIONS_FILE', 'notifications.txt')
     print(request.json)
     try:
         for (action, image) in handle_harbor(request.json):
+
             publish_message(notifications_file, action, image)
+
         return "ok"
     except Exception as e:
         print("Fail to handle the harbor hook")
         print(e)
 
     try:
-        for (action, image) in handle_dockerhub(request.json, notifications_file):
+        for (action, image) in handle_dockerhub(request.json):
 
             publish_message(notifications_file, action, image)
 
@@ -28,10 +29,10 @@ def catch_all(p):
         print("Fail to handle the dockerhub hook")
         print(e)
 
-    pprint.pprint(request.json)
     return "ko", 500
 
 def publish_message(notifications_file, action, image):
+    rotation = int(os.environ.get('ROTATION', '100'))
     with open(notifications_file, 'a+') as f:
         if os.stat(notifications_file).st_size == 0:
             current_id = 0
@@ -43,8 +44,8 @@ def publish_message(notifications_file, action, image):
             last_line = lines[-1]
             last_line_id = int(last_line.split('|')[0])
             current_id = last_line_id + 1
-            if (last_line_id % int(args_dic['rotation']) == 0 and last_line_id != 0):
-                new_notifications_file = str(first_line_id)+'-'+str(last_line_id)+notifications_file.split('/')[-1]
+            if (last_line_id % rotation == 0 and last_line_id != 0):
+                new_notifications_file = os.path.dirname(notifications_file)+'/'+str(first_line_id)+'-'+str(last_line_id)+notifications_file.split('/')[-1]
                 os.rename(notifications_file, new_notifications_file)
                 open(notifications_file, 'a').close()
                 with open(new_notifications_file, 'a+') as f:
@@ -60,7 +61,7 @@ def publish_message(notifications_file, action, image):
     print('{}|{}'.format(action, image))
 
 
-def handle_dockerhub(rjson, notifications_file):
+def handle_dockerhub(rjson):
     for event in rjson['events']:
         action = event['action']
         # we need the protocol, the host, the repositor and the tag
@@ -103,14 +104,4 @@ def handle_harbor(rjson):
             print("The replicated artifact from {} already exists in {}".format(str(source), str(destination)))
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser = argparse.ArgumentParser(add_help=False)
-    parser.add_argument("-f", "--file")
-    parser.add_argument("-h", "--host")
-    parser.add_argument("-p", "--port")
-    parser.add_argument("-r", "--rotation")
-    args = parser.parse_args()
-    args_dic = vars(args)
-    notifications_file = args_dic["file"]
-
-    app.run(host=args_dic["host"], port=args_dic["port"])
+    app.run()
