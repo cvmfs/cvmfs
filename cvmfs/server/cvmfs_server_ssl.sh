218c2adaf63b8b8318e9c8111b2f2ea6dbdb4b19
Bob Dröge <b.e.droge@rug.nl>
2023-11-14 12:07:36 +0100

    always use 'openssl pkeyutl' instead of rsautl, and fix the actual verify command

--- cvmfs/server/cvmfs_server_ssl.sh
+++ cvmfs/server/cvmfs_server_ssl.sh
@@ -64,11 +64,7 @@ create_whitelist() {
   local usemasterkeycard=0
   local hash_algorithm
 
-  local openssl_keyutil_cmd="openssl rsautl"
-  local openssl_version=`openssl version | cut -d' ' -f2`
-  if compare_versions "${openssl_version}" -ge "3.0.0" ; then
-    openssl_keyutil_cmd="openssl pkeyutl"
-  fi
+  local openssl_keyutil_cmd="openssl pkeyutl"
 
   local whitelist
   whitelist=${temp_dir}/whitelist.$name
@@ -117,9 +113,9 @@ create_whitelist() {
     else
       masterkeycard_read_pubkey >${whitelist}.pub
     fi
-    local checkhash="`${openssl_keyutil_cmd} -verify -inkey ${whitelist}.pub -pubin -in ${whitelist}.signature 2>/dev/null`"
+    ${openssl_keyutil_cmd} -verify -inkey ${whitelist}.pub -pubin -sigfile ${whitelist}.signature -in ${whitelist}.hash > /dev/null 2>&1 \
+      || die "invalid masterkeycard signature"
     rm -f ${whitelist}.pub
-    [ "$hash" = "$checkhash" ] || die "invalid masterkeycard signature"
   else
     ${openssl_keyutil_cmd} -inkey $masterkey -sign -in ${whitelist}.hash -out ${whitelist}.signature
   fi
