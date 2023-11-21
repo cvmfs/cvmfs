b26c6ffa9173c52110bb7670ccb901f7a93ad311
HereThereBeDragons <HereThereBeDragons@users.noreply.github.com>
2023-11-21 10:26:24 +0100

    bugfix: broken timeme function due to enabling autofs

--- test/performance-benchmark/util_benchmark/benchmark_cvmfs.py
+++ test/performance-benchmark/util_benchmark/benchmark_cvmfs.py
@@ -4,13 +4,14 @@ import subprocess
 import os
 
 def clear_and_mount_direct(repos):
-  doit = subprocess.Popen("cvmfs_config killall; cvmfs_config reload; cvmfs_config wipecache",
+  doit = subprocess.Popen("sudo cvmfs_config killall; sudo cvmfs_config reload;"
+                          + " sudo cvmfs_config wipecache",
                           universal_newlines=True, shell=True,
                           stdout=subprocess.PIPE, stderr=subprocess.PIPE)
   (std_out, stderr) = doit.communicate()
 
   for repo in repos:
-    doit = subprocess.Popen("umount /cvmfs/" + repo,
+    doit = subprocess.Popen("sudo umount /cvmfs/" + repo,
                             universal_newlines=True, shell=True,
                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     (std_out, stderr) = doit.communicate()
@@ -18,14 +19,20 @@ def clear_and_mount_direct(repos):
     if os.path.isdir("/cvmfs/" + repo) == False:
       os.makedirs("/cvmfs/" + repo)
 
-    doit = subprocess.Popen("/usr/bin/cvmfs2 -o rw,system_mount,fsname=cvmfs2,allow_other,grab_mountpoint,uid=`id -u cvmfs`,gid=`id -g cvmfs`,libfuse=3 " + repo + " /cvmfs/" + repo,
-                            universal_newlines=True, shell=True,
-                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    doit = subprocess.Popen("sudo /usr/bin/cvmfs2 -o rw,system_mount,fsname=cvmfs2," 
+                  + "allow_other,grab_mountpoint,uid=`id -u cvmfs`,gid=`id -g cvmfs`,"
+                  + "libfuse=3 " + repo + " /cvmfs/" + repo,
+                  universal_newlines=True, shell=True,
+                  stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     (std_out, stderr) = doit.communicate()
 
 def clear_and_reload_autofs():
       # also works for broken autofs in EL9
-      doit = subprocess.Popen("systemctl stop autofs; cvmfs_config killall; cvmfs_config wipecache; systemctl start autofs; cvmfs_config reload",
+      doit = subprocess.Popen(  "sudo systemctl stop autofs; "
+                              + "sudo cvmfs_config killall; "
+                              + "sudo cvmfs_config wipecache; "
+                              + "sudo systemctl start autofs; "
+                              + "sudo cvmfs_config reload",
                               universal_newlines=True, shell=True,
                               stdout=subprocess.PIPE, stderr=subprocess.PIPE)
       (std_out, stderr) = doit.communicate()
@@ -38,7 +45,7 @@ def getCVMFSVersion():
   return std_out.splitlines()[1]
 
 def installCVMFS(cvmfs_build_dir):
-  doit = subprocess.Popen("cd " + cvmfs_build_dir + "; ninja install",
+  doit = subprocess.Popen("cd " + cvmfs_build_dir + "; sudo ninja install",
                           universal_newlines=True, shell=True,
                           stdout=subprocess.PIPE, stderr=subprocess.PIPE)
   (std_out, stderr) = doit.communicate()
