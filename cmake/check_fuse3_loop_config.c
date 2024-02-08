ce80dbd604d247041e00fe124924eb456f89d49d
Jakob Blomer <jblomer@cern.ch>
2024-02-08 16:32:27 +0100

    fix loop_cfg detection in build system

--- cmake/check_fuse3_loop_config.c
+++ cmake/check_fuse3_loop_config.c
@@ -2,7 +2,7 @@
 #include <fuse3/fuse.h>
 
 int main() {
-  struct fuse_loop_config *fuse_loop_cfg;
+  struct fuse_loop_config *fuse_loop_cfg = fuse_loop_cfg_create();
   (void) fuse_loop_cfg;
   return 0;
 }
