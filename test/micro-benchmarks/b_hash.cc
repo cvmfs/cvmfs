930261e7cb67aa0d332d8e10b41b4324a2c2b50c
Jakob Blomer <jblomer@cern.ch>
2024-04-09 00:01:17 +0200

    add micro benchmark for HMAC-SHA256

--- test/micro-benchmarks/b_hash.cc
+++ test/micro-benchmarks/b_hash.cc
@@ -99,3 +99,17 @@ BENCHMARK_DEFINE_F(BM_Hash, Sha256)(benchmark::State &st) {
 }
 BENCHMARK_REGISTER_F(BM_Hash, Sha256)->Repetitions(3)->Arg(100)->Arg(4096)->
   Arg(100*1024);
+
+BENCHMARK_DEFINE_F(BM_Hash, Hmac256)(benchmark::State &st) {
+  unsigned size = st.range(0);
+  std::string content;
+  content.resize(size);
+  const std::string key = "0123456789abcdefghij";
+  std::string hex_digest;
+  while (st.KeepRunning()) {
+    hex_digest = shash::Hmac256(key, content, false /* raw_output */);
+  }
+  st.SetItemsProcessed(st.iterations());
+}
+BENCHMARK_REGISTER_F(BM_Hash, Hmac256)->Repetitions(3)->Arg(100)->Arg(4096)->
+  Arg(100*1024);
