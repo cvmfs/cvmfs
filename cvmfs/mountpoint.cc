58bfc808cebaa3934eec71acb8f600228db591ad
HereThereBeDragons <HereThereBeDragons@users.noreply.github.com>
2024-04-10 11:19:47 +0200

    improve logging for download manager, mark if external or standard download manager and add full qualified domain names to the output in some areas where ips where only given before

--- cvmfs/mountpoint.cc
+++ cvmfs/mountpoint.cc
@@ -2125,7 +2125,7 @@ bool MountPoint::SetupExternalDownloadMgr(bool dogeosort) {
   string optarg;
   external_download_mgr_ =
     download_mgr_->Clone(perf::StatisticsTemplate("download-external",
-      statistics_));
+      statistics_), "external");
 
   unsigned timeout;
   unsigned timeout_direct;
