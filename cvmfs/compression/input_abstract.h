b6f7fd626e46bed56891f8ba5f733ea338d340f5
HereThereBeDragons <HereThereBeDragons@users.noreply.github.com>
2024-04-26 17:45:11 +0200

    WIP working version of zlib using InputAbstract and Sink. Unittest working for InputMem and MemSink

---
+++ cvmfs/compression/input_abstract.h
@@ -0,0 +1,43 @@
+/**
+ * This file is part of the CernVM File System.
+ */
+
+#ifndef CVMFS_COMPRESSION_INPUT_ABSTRACT_H_
+#define CVMFS_COMPRESSION_INPUT_ABSTRACT_H_
+
+#include <cstdio>
+
+namespace zlib {
+
+class InputAbstract {
+ protected:
+  InputAbstract(const bool is_owner, const size_t max_chunk_size,
+                unsigned char *chunk=NULL) :
+                                                is_owner_(is_owner),
+                                                has_chunk_left_(false),
+                                                max_chunk_size_(max_chunk_size),
+                                                chunk_(chunk),
+                                                chunk_size_(0) { }
+
+ public:
+  virtual ~InputAbstract() { };
+  virtual bool NextChunk() = 0;
+  virtual bool IsValid() = 0;
+
+  bool has_chunk_left() const { return has_chunk_left_;  }
+  size_t max_chunk_size() const { return max_chunk_size_; }
+  size_t chunk_size() const { return chunk_size_; }
+  unsigned char* chunk() const { return chunk_; }
+
+ 
+ protected:
+  const bool is_owner_;
+  bool has_chunk_left_;
+  const size_t max_chunk_size_;
+  unsigned char *chunk_;
+  size_t chunk_size_;  // must be <= chunk_size_
+};
+
+} // namespace zlib
+
+#endif // CVMFS_COMPRESSION_INPUT_ABSTRACT_H_
---
+++ cvmfs/compression/input_mem.cc
@@ -0,0 +1,64 @@
+/**
+ * This file is part of the CernVM File System.
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include "input_mem.h"
+
+namespace zlib {
+
+InputMem::InputMem(const unsigned char* src, const size_t src_size,
+                   size_t max_chunk_size, bool is_owner) :
+                                  InputAbstract(is_owner, max_chunk_size),
+                                  src_(src), src_size_(src_size) {
+  if (IsValid()) {
+    has_chunk_left_ = true;
+    idx_ = -1;
+  }
+}
+
+InputMem::~InputMem() {
+  if (IsValid()) {
+    if (is_owner_) {
+      // we need to ignore "const" here
+      free(const_cast<unsigned char*>(src_));
+    }
+  }
+}
+
+
+
+bool InputMem::NextChunk() {
+  if ((idx_ != -1ul && idx_ + max_chunk_size_ >= src_size_) || !has_chunk_left_) {
+    std::cerr << "NextChunk: return false" << std::endl;
+    return false;
+  }
+
+  chunk_size_ = max_chunk_size_;
+  if (idx_ == -1ul) {
+    idx_ = 0;
+  } else {
+    idx_ += max_chunk_size_; 
+  }
+
+  if (src_size_ - idx_ <= max_chunk_size_) {
+    has_chunk_left_ = false;
+    chunk_size_ = src_size_ - idx_;
+  }
+
+  // just moving pointer as "moving window". no need to create a copy
+  // we need to ignore "const" to be able to use InputAbstract protected vars
+  chunk_ = const_cast<unsigned char*>(src_) + idx_;
+
+  std::cerr << "NextChunk: return true - chunk size " <<  chunk_size_ <<
+               " idx " << idx_ << " has chunks left " << has_chunk_left_  << std::endl;
+
+  return true;
+}
+
+bool InputMem::IsValid() {
+  return src_ != NULL;
+}
+
+} // namespace zlib
