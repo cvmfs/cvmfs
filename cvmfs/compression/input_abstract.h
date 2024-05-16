4b6d161ece7d3d31ce434c667ba85212fe5b0af0
HereThereBeDragons <HereThereBeDragons@users.noreply.github.com>
2024-05-16 17:53:04 +0200

    input_<type>[.h/.cc]: bug fixes and better logic to check for valid source

--- cvmfs/compression/input_abstract.h
+++ cvmfs/compression/input_abstract.h
@@ -53,10 +53,8 @@ class InputAbstract {
    */
   virtual bool IsValid() = 0;
   /**
-   * Resets the reading progress of the source, the next call to NextChunk()
+   * Resets the reading progress of a valid source. The next call to NextChunk()
    * will start reading from the beginning.
-   *
-   * @note Reset does not work for empty sources.
    */
   virtual bool Reset() = 0;
 
