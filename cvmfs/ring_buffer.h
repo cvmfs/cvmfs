f4914d1b226f178d923f667c8b71be9a56fe32e3
Jakob Blomer <jblomer@cern.ch>
2024-07-10 16:32:29 +0200

    add RingBuffer class

--- cvmfs/ring_buffer.h
+++ cvmfs/ring_buffer.h
@@ -5,9 +5,88 @@
 #ifndef CVMFS_RING_BUFFER_H_
 #define CVMFS_RING_BUFFER_H_
 
+#include <cstddef>
 
-class RingBuffer {
+#include "util/single_copy.h"
 
+/**
+ * A ring buffer that allows appending objects to the front and removing
+ * objects from the back. In the memory area, we prepend the object data with
+ * the size of the object.
+ */
+class RingBuffer : SingleCopy {
+ public:
+  /**
+   * The offset in buffer_
+   */
+  typedef size_t ObjectHandle_t;
+  static const ObjectHandle_t kInvalidObjectHandle;
+
+  explicit RingBuffer(size_t total_size);
+  ~RingBuffer();
+
+  /**
+   * Returns kInvalidObjectHandle if there is not enough space to insert the
+   * object.
+   */
+  ObjectHandle_t PushFront(const void *obj, size_t size);
+
+  /**
+   * Returns the handle of the remove object or kInvalidObjectHandle if the
+   * ring buffer is empty
+   */
+  ObjectHandle_t RemoveBack();
+
+  /**
+   * The passed object handle must be valid
+   */
+  size_t GetObjectSize(ObjectHandle_t handle) const;
+
+  /**
+   * Note that we cannot just return a pointer because an object may be
+   * split between the end and the beginning of the backing memory area
+   */
+  void CopyObject(ObjectHandle_t handle, void *to) const;
+
+  size_t total_size() const { return total_size_; }
+  size_t free_space() const { return free_space_; }
+
+ private:
+  /**
+   * Writes data into the ring buffer at front_, taking care of a potential
+   * buffer wrap. Assumes that the buffer has enough space.
+   */
+  void Put(const void *data, size_t size);
+  /**
+   * Copies data from the buffer to the memory area "to". The memory area
+   * has to be big enough. Takes care of a potential buffer wrap.
+   */
+  void Get(size_t from, size_t size, void *to) const;
+  /**
+   * Moves the back_ pointer forward, taking care of potential buffer wraps
+   */
+  void Shrink(size_t by);
+
+  /**
+   * Size in bytes of the memory area backing the ring buffer
+   */
+  const size_t total_size_;
+  /**
+   * Size in bytes of the unused space in the buffer
+   */
+  size_t free_space_;
+  /**
+   * Pointer to the front of the memory buffer, in [0, size_ - 1]
+   */
+  size_t front_;
+  /**
+   * Pointer to the back of the memory buffer, in [0, size_ - 1]
+   */
+  size_t back_;
+  /**
+   * The memory area backing the ring buffer
+   */
+  unsigned char *buffer_;
 };  // class RingBuffer
 
 #endif  // CVMFS_RING_H_
--- test/unittests/t_ring_buffer.cc
+++ test/unittests/t_ring_buffer.cc
@@ -4,14 +4,63 @@
 
 #include <gtest/gtest.h>
 
+#include <cstring>
+
 #include "ring_buffer.h"
+#include "util/pointer.h"
 
 class T_RingBuffer : public ::testing::Test {
  protected:
-  virtual void SetUp() { }
+  static const size_t kSize;
 
-  RingBuffer ring_buffer;
+  virtual void SetUp() {
+    ring = new RingBuffer(kSize);
+  }
+
+  virtual void TearDown() {
+    ring.Destroy();
+  }
+
+  UniquePtr<RingBuffer> ring;
 };
 
+const size_t T_RingBuffer::kSize = 997;
+
 TEST_F(T_RingBuffer, Basics) {
+  unsigned char buf[kSize];
+  memset(buf, 0, kSize);
+
+  EXPECT_EQ(RingBuffer::kInvalidObjectHandle, ring->PushFront(buf, kSize));
+  EXPECT_EQ(0u, ring->PushFront(buf, kSize - sizeof(size_t)));
+  EXPECT_EQ(0u, ring->free_space());
+  EXPECT_EQ(0u, ring->RemoveBack());
+  EXPECT_EQ(kSize, ring->free_space());
+
+  auto handle_null = ring->PushFront(NULL, 0);
+  EXPECT_NE(RingBuffer::kInvalidObjectHandle, handle_null);
+  EXPECT_EQ(0U, ring->GetObjectSize(handle_null));
+  EXPECT_EQ(kSize - sizeof(size_t), ring->free_space());
+}
+
+TEST_F(T_RingBuffer, Wrap) {
+  unsigned buf[kSize / 3 / sizeof(int)];
+  memset(buf, 0, kSize / 3);
+
+  RingBuffer::ObjectHandle_t objects[2];
+  objects[0] = ring->PushFront(buf, kSize / 3);
+  buf[0] = 1;
+  objects[1] = ring->PushFront(buf, kSize / 3);
+
+  for (unsigned i = 2; i < 1000; ++i) {
+    EXPECT_EQ(kSize / 3, ring->GetObjectSize(objects[i % 2]));
+
+    unsigned verify[kSize / 3 / sizeof(int)];
+    ring->CopyObject(objects[i % 2], verify);
+
+    EXPECT_EQ(2 * ((i - 2) / 2) + (i % 2), verify[0]);
+    EXPECT_EQ(objects[i % 2], ring->RemoveBack());
+
+    buf[0] = 2 * (i / 2) + (i % 2);
+    objects[i % 2] = ring->PushFront(buf, kSize / 3);
+  }
 }
