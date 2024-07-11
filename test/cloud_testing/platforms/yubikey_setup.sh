79f5ffd86e0c7a5430fe0215a67de46130fcb90b
Valentin Volkl <valentin.volkl@cern.ch>
2024-07-11 14:52:57 +0200

    Fix tests on yubikey-test node, which was migrated to alma9

--- test/cloud_testing/platforms/yubikey_setup.sh
+++ test/cloud_testing/platforms/yubikey_setup.sh
@@ -4,9 +4,6 @@
 script_location=$(dirname $(readlink --canonicalize $0))
 . ${script_location}/common_setup.sh
 
-# CernVM-FS server needs 'jq' from epel
-echo "enabling epel yum repository..."
-install_from_repo epel-release
 
 # install CernVM-FS RPM packages
 echo "installing RPM packages... "
@@ -16,17 +13,24 @@ install_rpm $CLIENT_PACKAGE
 install_rpm $SERVER_PACKAGE
 install_rpm $DEVEL_PACKAGE
 
-# setup environment
-echo -n "setting up CernVM-FS environment..."
-sudo cvmfs_config setup                          || die "fail (cvmfs_config setup)"
-sudo mkdir -p /var/log/cvmfs-test                || die "fail (mkdir /var/log/cvmfs-test)"
-sudo chown sftnight:sftnight /var/log/cvmfs-test || die "fail (chown /var/log/cvmfs-test)"
-sudo systemctl start autofs                      || die "fail (systemctl start autofs)"
-sudo cvmfs_config chksetup > /dev/null           || die "fail (cvmfs_config chksetup)"
-echo "done"
+#NOTE: the yubikey machine is now setup to run tests on bare metal and does not need setup. 
+# This is the setup used to initially setup the machine:
 
-# install yubikey-related software
-install_from_repo opensc
-install_from_repo yubico-piv-tool
-sudo systemctl enable pcscd
-sudo systemctl start pcscd
+# CernVM-FS server needs 'jq' from epel
+#echo "enabling epel yum repository..."
+#install_from_repo epel-release
+
+# setup environment
+#echo -n "setting up CernVM-FS environment..."
+#sudo cvmfs_config setup                          || die "fail (cvmfs_config setup)"
+#sudo mkdir -p /var/log/cvmfs-test                || die "fail (mkdir /var/log/cvmfs-test)"
+#sudo chown sftnight:sftnight /var/log/cvmfs-test || die "fail (chown /var/log/cvmfs-test)"
+#sudo systemctl start autofs                      || die "fail (systemctl start autofs)"
+#sudo cvmfs_config chksetup > /dev/null           || die "fail (cvmfs_config chksetup)"
+#echo "done"
+#
+## install yubikey-related software
+#install_from_repo opensc
+#install_from_repo yubico-piv-tool
+#sudo systemctl enable pcscd
+#sudo systemctl start pcscd
