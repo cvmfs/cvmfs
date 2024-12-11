0c68a32957f360c8585ec2bb3bef020bae91cdfe
Valentin Volkl <valentin.volkl@cern.ch>
2024-12-11 15:14:30 +0100

    Revert "hotfix for cache corruption alarms"
    This reverts commit 64b1b5df32bee14fa2d294d50dfe12f0cd6eac3d.

--- cvmfs/manifest_fetch.cc
+++ cvmfs/manifest_fetch.cc
@@ -74,6 +74,11 @@ static Failures DoVerify(unsigned char *manifest_data, size_t manifest_size,
     goto cleanup;
   }
 
+  // Quick way out: hash matches base catalog
+  if (base_catalog && (ensemble->manifest->catalog_hash() == *base_catalog)) {
+    return kFailOk;
+  }
+
   // Load certificate
   certificate_hash = ensemble->manifest->certificate();
   ensemble->FetchCertificate(certificate_hash);
