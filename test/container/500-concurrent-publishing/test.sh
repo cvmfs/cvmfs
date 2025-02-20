c8d122da2569c9941b9809bcd38c12470d68985d
Valentin Volkl <valentin.volkl@cern.ch>
2025-02-20 17:33:33 +0100

    Fix for cvmfs_server transaction -t: update repository state after waiting for lease (#3771)
    This is an important fix relevant when using the timeout option to wait for a lease on a publisher connected to a gateway.
    Without this patch, the the transaction that is waited on can be lost
    (to be precise, it is reverted by the following transaction due to the wrong choice of the base of the catalog diff)
    
    This is a rather narrow fix that updates the current readonly mount with the latest revision. We do this only when we are waiting for a lease to avoid the overhead of the manifest download and remount -
    to do that is really not required when we change a subrepository that isn't currently in a transaction by another publisher.
    
    However, there might still be an unlikely race condition when the start of the transaction happens before an ongoing publication is concluded, but the check for the lease happens after. To be investigated.
    
    A test reproducing this issue is included, but has yet to be added to the CI - needs a good way to build an image with the current cvmfs installed.
    
    This issue was first reported in https://its.cern.ch/jira/projects/SPI/issues/SPI-2652

---
+++ test/container/500-concurrent-publishing/test.sh
@@ -0,0 +1,17 @@
+docker exec  cvmfs-pub1 cvmfs_server transaction
+docker exec  cvmfs-pub1 bash -c 'echo abc > /cvmfs/test.repo.org/testfile'
+docker exec  cvmfs-pub1 cvmfs_server publish
+docker exec  cvmfs-pub1 cvmfs_server transaction
+docker exec  cvmfs-pub1 bash -c 'echo important_change > /cvmfs/test.repo.org/testfile'
+docker exec  cvmfs-pub2 cvmfs_server transaction -t 5000 &
+sleep 1
+docker exec  cvmfs-pub1 cvmfs_server publish
+wait $(jobs -p) 
+sleep 1
+docker exec  cvmfs-pub2 bash -c 'echo unrelated_change > /cvmfs/test.repo.org/another_file'
+docker exec  cvmfs-pub2 cvmfs_server publish
+docker exec  cvmfs-gw1 cvmfs_server tag -l
+docker exec  cvmfs-gw1 cat /cvmfs/test.repo.org/another_file
+docker exec  cvmfs-gw1 cat /cvmfs/test.repo.org/testfile
+docker exec  cvmfs-gw1 cat /cvmfs/test.repo.org/testfile | tee | grep important_change ||  echo -e ERROR >&2; exit 1
+
