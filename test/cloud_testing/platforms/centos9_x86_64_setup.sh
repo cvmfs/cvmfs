9089ebc24c5ee5bb08292eaccb2b10757951f643
Valentin Volkl <valentin.volkl@cern.ch>
2025-04-26 13:36:30 +0200

    azurite also in el9 tests

--- test/cloud_testing/platforms/centos9_x86_64_setup.sh
+++ test/cloud_testing/platforms/centos9_x86_64_setup.sh
@@ -67,6 +67,19 @@ install_from_repo patch
 # Install the test S3 provider
 install_test_s3
 
+# Install azurite
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[azure-cli]
+name=Azure CLI
+baseurl=https://packages.microsoft.com/yumrepos/azure-cli
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/azure-cli.repo
+sudo yum install -y azure-cli
+curl -fsSL https://rpm.nodesource.com/setup_16.x | sudo bash -
+sudo yum install -y nodejs
+sudo npm install -g azurite
+
 # building kernel
 install_from_repo perl
 
