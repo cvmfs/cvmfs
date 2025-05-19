5dc85a8cfc50937b764d3d86a0dab982937d9620
Georgios Christodoulis <christge@cern.ch>
2025-05-19 13:35:04 +0200

    Revert "exclude tests 066, 647 from EL9"
    This reverts commit fc2d9c13de62efabadf549b9e666b0b51201a5fe.

--- test/cloud_testing/platforms/centos9_x86_64-FUSE3_test.sh
+++ test/cloud_testing/platforms/centos9_x86_64-FUSE3_test.sh
@@ -11,7 +11,6 @@ retval=0
 
 # Test exclusions
 # 095: not needed on EL9 as attach mounts are supported
-# 066: temporarily until automounter reload bug is fixed
 
 cd ${SOURCE_DIRECTORY}/test
 echo "running CernVM-FS client test cases..."
@@ -26,7 +25,6 @@ CVMFS_TEST_CLASS_NAME=ClientIntegrationTests                                  \
                                  src/030-missingrootcatalog                   \
                                  src/056-lowspeedlimit                        \
                                  src/065-http-400                             \
-                                 src/066-killall                              \
                                  src/084-premounted                           \
                                  src/095-fuser                                \
                                  src/096-cancelreq                            \
