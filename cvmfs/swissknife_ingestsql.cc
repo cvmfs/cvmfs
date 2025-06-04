d92bc0f0cd6964074597f985fe223c48774467c0
Andriy Utkin <hello@autkin.net>
2025-06-04 11:09:11 +0100

    ingestsql: lint: use a var which is already proven non-NULL
    Error: /home/runner/work/cvmfs/cvmfs/cvmfs/swissknife_ingestsql.cc:520:34: error: The parameter must not be null [clang-analyzer-cplusplus.StringChecker,-warnings-as-errors]
      520 |     dir_temp = MakeCanonicalPath(getenv("TMPDIR"));
          |                                  ^~~~~~~~~~~~~~~~

--- cvmfs/swissknife_ingestsql.cc
+++ cvmfs/swissknife_ingestsql.cc
@@ -514,10 +514,11 @@ int swissknife::IngestSQL::Main(const swissknife::ArgumentList &args) {
   }
 
   string dir_temp = "";
+  const char *env_tmpdir;
   if (args.find('t') != args.end()) {
     dir_temp = MakeCanonicalPath(*args.find('t')->second);
-  } else if (getenv("TMPDIR")) {
-    dir_temp = MakeCanonicalPath(getenv("TMPDIR"));
+  } else if (env_tmpdir = getenv("TMPDIR")) {
+    dir_temp = MakeCanonicalPath(env_tmpdir);
   } else {
     LogCvmfs(kLogCvmfs, kLogStderr, "-t or TMPDIR required");
     return 1;
