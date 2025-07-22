71f1be459c40e924c2738e070a6f0ece6eae0d80
Valentin Volkl <valentin.volkl@cern.ch>
2025-07-22 09:45:35 +0200

    remove ctx guard that was already moved in https://github.com/cvmfs/cvmfs/commit/ce3146beda1403a4861569f35d2ada207b60cf36

--- cvmfs/cvmfs.cc
+++ cvmfs/cvmfs.cc
@@ -1397,11 +1397,6 @@ static void cvmfs_read(fuse_req_t req, fuse_ino_t ino, size_t size, off_t off,
   uint64_t abs_fd = (fd < 0) ? -fd : fd;
   ClearBit(glue::PageCacheTracker::kBitDirectIo, &abs_fd);
 
-  const struct fuse_ctx *fuse_ctx = fuse_req_ctx(req);
-  FuseInterruptCue ic(&req);
-  const ClientCtxGuard ctx_guard(fuse_ctx->uid, fuse_ctx->gid, fuse_ctx->pid,
-                                 &ic);
-
   // Do we have a a chunked file?
   if (fd < 0) {
     const uint64_t chunk_handle = abs_fd;
