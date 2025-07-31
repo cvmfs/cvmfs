eb1faa54bc0b2166ed091b4d36bd8d6339569e57
Valentin Volkl <valentin.volkl@cern.ch>
2025-07-31 12:08:01 +0200

    up

--- cvmfs/duplex_testing.h
+++ cvmfs/duplex_testing.h
@@ -19,8 +19,7 @@
 // While the unittests don't re-use object files 
 // there's no reason though.
 //
-//#define FRIEND_TEST(test_case_name, test_name) \
-//  friend class test_case_name##_##test_name##_Test
+//#define FRIEND_TEST(test_case_name, test_name) friend class test_case_name##_##test_name##_Test
 #endif
 
 #endif  // CVMFS_DUPLEX_TESTING_H_
