5a9e354b0b1787edd511ad1ac034216b33f66212
Valentin Volkl <valentin.volkl@cern.ch>
2025-07-31 11:34:40 +0200

    up

--- cvmfs/duplex_testing.h
+++ cvmfs/duplex_testing.h
@@ -10,9 +10,17 @@
 #else
 // no unittests are being built, so FRIEND_TEST
 // is not needed. Make it a no-op
-//#define FRIEND_TEST(test_case_name, test_name)
-#define FRIEND_TEST(test_case_name, test_name) \
-  friend class test_case_name##_##test_name##_Test
+#define FRIEND_TEST(test_case_name, test_name)
+
+// This is the definition of FRIEND_TEST in gtest_prod.h
+// This compiles too, and just hardcoding it here 
+// instead of including gtest_prod.h
+// could help make the compilation faster in the future.
+// While the unittests don't re-use object files 
+// there's no reason though.
+//
+//#define FRIEND_TEST(test_case_name, test_name) \
+//  friend class test_case_name##_##test_name##_Test
 #endif
 
 #endif  // CVMFS_DUPLEX_TESTING_H_
