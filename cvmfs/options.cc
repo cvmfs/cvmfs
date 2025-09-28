d226173d8317c5e534c9f7c02f445bd0f66d1c62
Georgios Christodoulis <christge@cern.ch>
2025-09-28 21:36:51 +0200

    use SetEnv instead of setenv, so that the env vars are visible to cvmfs_talk

--- cvmfs/options.cc
+++ cvmfs/options.cc
@@ -278,10 +278,10 @@ bool OptionsManager::HasConfigRepository(const string &fqrn,
 }
 
 void OptionsManager::SetupGlobalEnvironmentParams() {
-  setenv("CVMFS_ARCH", GetArch().c_str(), 1 /* overwrite */);
+  SetValue("CVMFS_ARCH", GetArch());
 
   // Set CVMFS_VERSION environment variable
-  setenv("CVMFS_VERSION", CVMFS_VERSION, 1 /* overwrite */);
+  SetValue("CVMFS_VERSION", CVMFS_VERSION);
 
   // Calculate and set CVMFS_VERSION_NUMERIC
   // Format: major * 10000 + minor * 100 + patch
@@ -291,7 +291,7 @@ void OptionsManager::SetupGlobalEnvironmentParams() {
   char version_numeric_str[16];
   snprintf(version_numeric_str, sizeof(version_numeric_str), "%d",
            version_numeric);
-  setenv("CVMFS_VERSION_NUMERIC", version_numeric_str, 1 /* overwrite */);
+  SetValue("CVMFS_VERSION_NUMERIC", version_numeric_str);
 }
 
 void OptionsManager::ParseDefault(const string &fqrn) {
