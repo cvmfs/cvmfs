0e9ef5996fb036d10913ce2cd92872f5d43308c8
Georgios Christodoulis <christge@cern.ch>
2025-11-14 12:14:03 +0100

    [BungleFileManager] return labeled objects ready to be fetched from the Fetcher

--- cvmfs/file_bundle.h
+++ cvmfs/file_bundle.h
@@ -7,8 +7,9 @@
 #ifndef CVMFS_FILE_BUNDLE_H_
 #define CVMFS_FILE_BUNDLE_H_
 
-#include "shortstring.h"  // PathString
-
+#include "cache.h"         // LabeledObject
+#include "shortstring.h"   // PathString
+#include "util/pointer.h"  // UniquePtr
 
 /*
 
@@ -31,7 +32,21 @@ class BundleFileMgr {
   // TODO(christge): this is to be reverted. It's the basic interface needed to
   // interact with the file bundle. Now there are some mocks for prototyping
   BundleFileMgr(const PathString &bf) { }
-  PathString GetNext() { return PathString{}; };
+  CacheManager::LabeledObject *GetNext() {
+    // TODO(christge): return actual labled objects
+    CacheManager::Label label;
+    label.path = std::string{};
+    label.size = sizeof(shash::Any);
+    label.zip_algorithm = zlib::kZlibDefault;
+    return new CacheManager::LabeledObject(shash::Any{}, label);
+  };
+
+  size_t Size() const { return size_; }
+
+ private:
+  // TODO(christge): properly setup in construction to return the number of
+  // files in the bundle
+  size_t size_ = 42;
 };
 
 #endif
