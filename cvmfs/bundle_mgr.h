689827bcab3e08d3f490924afd6341d8dd2aab78
Georgios Christodoulis <christge@cern.ch>
2025-11-14 19:51:16 +0100

    [BundleMgr] Create the core of the BundleMgr.
    Create a pool of fetcher threads (size proportional to the number of
    dependencies). Parse the trigger file and assign a LabeledObject to a
    fetcher thread.
    
    Signed-off-by: Georgios Christodoulis <christge@cern.ch>

--- cvmfs/bundle_mgr.h
+++ cvmfs/bundle_mgr.h
@@ -4,14 +4,13 @@
 
 #ifndef CVMFS_BUNDLE_MGR_H_
 #define CVMFS_BUNDLE_MGR_H_
-#include <string>
+#include <tuple>
+#include <vector>
 
-#include "duplex_fuse.h"   // fuse_ino_t
-#include "file_bundle.h"   // BundleFileMgr
-#include "mountpoint.h"    //MountPoint*, FileSystem*
-#include "shortstring.h"   // GetParentPath, GetFileName
-#include "util/inode.h"    // GetPathForInode, GetDirentForInode
-#include "util/pointer.h"  // UniquePtr
+#include "file_bundle.h"
+#include "mountpoint.h"
+#include "shortstring.h"
+#include "util/pointer.h"
 
 class BundleMgr {
  public:
@@ -20,15 +19,37 @@ class BundleMgr {
   explicit operator bool() const { return is_valid_; }
 
  private:
-  static void *DoFetch(void *data);
+  static void *EstablishConnection(void *data);
+  void SpawnFetchers();
+  void JoinFetchers();
+  CacheManager::LabeledObject ReceiveLabeledObject(int fd) const;
+  void SendLabeledObject(const CacheManager::LabeledObject &obj) const;
 
+  MountPoint *mount_point_;
+#ifndef __TEST_CVMFS_MOCKFUSE
+  cvmfs::Fetcher *fetcher_;
+#endif
   catalog::DirectoryEntry dirent_;
   PathString path_;
   NameString fname_;
   PathString parent_path_;
+
+  // The file that contains the dependences
   PathString bundle_file_path_;
   UniquePtr<BundleFileMgr> bfm_;
-  bool is_valid_;
+
+  std::vector<std::tuple<pthread_t, int> > fetcher_pool_;
+
+  enum class Command {
+    kTerminate,
+    kFetch
+  };
+
+  /**
+   * Used to send RPCs to the BundleMgr by the fetcher threads
+   */
+  int pipe_bm_[2];
+  bool is_valid_ = true;
 };
 #endif  // CVMFS_BUNDLE_MGR_H_
 
