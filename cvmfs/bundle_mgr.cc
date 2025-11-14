ac5fa7ffa51d0f124ae1f259254395c4f0b8c2d5
Georgios Christodoulis <christge@cern.ch>
2025-11-14 19:51:16 +0100

    [BundleMgr] Create the core of the BundleMgr.
    Create a pool of fetcher threads (size proportional to the number of
    dependencies). Parse the trigger file and assign a LabeledObject to a
    fetcher thread.
    
    Signed-off-by: Georgios Christodoulis <christge@cern.ch>

--- cvmfs/bundle_mgr.cc
+++ cvmfs/bundle_mgr.cc
@@ -8,47 +8,131 @@
 
 #include <vector>
 
-BundleMgr::BundleMgr(MountPoint *mp, fuse_ino_t ino) : is_valid_(true) {
-    is_valid_ = cvmfs::GetPathForInode(mp, mp->file_system(), ino, &path_);
-    is_valid_ &= cvmfs::GetDirentForInode(mp, mp->file_system(), ino, &dirent_);
-    fname_ = GetFileName(path_);
-    parent_path_ = GetParentPath(path_);
+#include "fetch.h"
+#include "util/inode.h"
+#include "util/posix.h"
 
-    // There is a naming convention regarding the name of the file with the
-    // contents of the bundle
-    bundle_file_path_ = PathString(parent_path_.ToString() + "/.cvmfsbundle."
-                                   + fname_.ToString());
-
-    bfm_ = new BundleFileMgr(bundle_file_path_);
+BundleMgr::BundleMgr(MountPoint *mp, fuse_ino_t ino) : mount_point_(mp) {
+  is_valid_ = cvmfs::GetPathForInode(mp, mp->file_system(), ino, &path_);
+  is_valid_ &= cvmfs::GetDirentForInode(mp, mp->file_system(), ino, &dirent_);
+  if (not is_valid_) {
+    return;
   }
+  fname_ = GetFileName(path_);
+  parent_path_ = GetParentPath(path_);
+  fetcher_ = dirent_.IsExternalFile() ? mp->external_fetcher() : mp->fetcher();
+
+  // There is a naming convention regarding the name of the file with the
+  // contents of the bundle
+  bundle_file_path_ = PathString(parent_path_.ToString() + "/.cvmfsbundle."
+                                 + fname_.ToString());
+
+  bfm_ = new BundleFileMgr(bundle_file_path_);
+  pipe_bm_[0] = pipe_bm_[1] = -1;
+}
 
 void BundleMgr::Fetch() {
-  std::vector<pthread_t> fetcher_pool;
+  SpawnFetchers();
 
-  for (PathString next_f = bfm_->GetNext(); not next_f.IsEmpty();
-       next_f = bfm_->GetNext()) {
-    pthread_t fetcher_t;
-
-    // TODO(christge): DoFetch requires real arguments
-    pthread_create(&fetcher_t, nullptr, DoFetch, nullptr);
-    fetcher_pool.emplace_back(fetcher_t);
+  CacheManager::LabeledObject *obj;
+  while ((obj = bfm_->GetNext()) != nullptr) {
+    // TODO(christge): In this form the assignment of a fetcher is left to
+    // SendLabeledObject. This is incorrect. SendLabeledObject should only
+    // unpack the LabeledObject and send it over the pipe
+    SendLabeledObject(*obj);
   }
 
-  // Join fetcher threads
-  while (not fetcher_pool.empty()) {
-    for (auto it = fetcher_pool.begin(); it != fetcher_pool.end();) {
-      auto &thread = *it;
-      void *retval;
-      int is_joinable = pthread_tryjoin_np(thread, &retval);
-      if (is_joinable == 0) {
-        // fetch has finished
-        fetcher_pool.erase(it);
-      } else {
-        // still busy
-        ++it;
+  JoinFetchers();
+}
+
+void BundleMgr::JoinFetchers() {
+  bool detach_mode = false;
+  // Join fetcher threads.
+  // Give a fetcher thread 10 seconds to join.
+  for (auto it = fetcher_pool_.begin(); it != fetcher_pool_.end(); ++it) {
+    auto &tuple = *it;
+    auto thread = std::get<0>(tuple);
+    auto fd = std::get<1>(tuple);
+
+    struct timespec ts;
+    if (clock_gettime(CLOCK_REALTIME, &ts) == -1) {
+      LogCvmfs(kLogBungleMgr,
+               kLogDebug,
+               "Failed to read CLOCK_REALTIME. Detaching fetchers.");
+      detach_mode = true;
+    }
+
+    if (detach_mode) {
+      pthread_detach(thread);
+    } else {
+      ts.tv_sec += 10;
+      Command cmd = Command::kTerminate;
+      WritePipe(fd, &cmd, sizeof(Command));
+      int res = pthread_timedjoin_np(thread, nullptr, &ts);
+      if (res != 0) {
+        LogCvmfs(kLogBungleMgr,
+                 kLogDebug,
+                 "Fetcher is busy for too long. Detaching.");
+        pthread_detach(thread);
       }
     }
   }
+  ClosePipe(pipe_bm_);
 }
 
-void* BundleMgr::DoFetch(void* data){}
+void BundleMgr::SpawnFetchers() {
+  MakePipe(pipe_bm_);
+
+  size_t size = bfm_->Size() / 30;
+  for (size_t i = 0; i < size; ++i) {
+    pthread_t thread;
+    int res = pthread_create(&thread, nullptr, EstablishConnection, this);
+    if (res != 0) {
+      continue;
+    }
+    int fd;
+    ReadPipe(pipe_bm_[0], &fd, sizeof(int));
+    fetcher_pool_.push_back({thread, fd});
+  }
+}
+
+void *BundleMgr::EstablishConnection(void *data) {
+  pthread_setname_np(pthread_self(), "bm_fetcher");
+  BundleMgr *mgr = static_cast<BundleMgr *>(data);
+  int rfd = mgr->pipe_bm_[0];
+  int back_channel[2];
+  MakePipe(back_channel);
+
+  WritePipe(rfd, back_channel + 1, sizeof(int));
+
+  Command cmd;
+  while (read(rfd, &cmd, sizeof(Command)) == sizeof(Command)) {
+    bool terminate = false;
+    switch (cmd) {
+      case Command::kFetch: {
+        auto object = mgr->ReceiveLabeledObject(rfd);
+        mgr->fetcher_->Fetch(object);
+      } break;
+      case Command::kTerminate:
+      default:
+        terminate = true;
+        break;
+    }
+    if (terminate) {
+      break;
+    }
+  }
+
+  ClosePipe(back_channel);
+  pthread_exit(nullptr);
+}
+
+CacheManager::LabeledObject BundleMgr::ReceiveLabeledObject(int fd) const {
+  return CacheManager::LabeledObject(shash::Any{}, CacheManager::Label());
+}
+
+void BundleMgr::SendLabeledObject(
+    const CacheManager::LabeledObject &obj) const {
+  (void)obj;
+}
+
