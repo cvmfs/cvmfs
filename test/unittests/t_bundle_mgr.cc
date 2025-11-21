49a8b239a63df3c6bb4c3a13d43121f92bc61e92
Georgios Christodoulis <christge@cern.ch>
2025-11-21 14:25:28 +0100

    mock the real Fetcher for testing

--- test/unittests/t_bundle_mgr.cc
+++ test/unittests/t_bundle_mgr.cc
@@ -9,6 +9,7 @@
 
 #include "bundle_mgr.h"
 #include "catalog_mgr_client.h"
+#include "fetch.h"
 #include "file_bundle.h"
 #include "glue_buffer.h"
 #include "lru_md.h"
@@ -32,6 +33,25 @@ class MockCatalogManager : public catalog::ClientCatalogManager {
                const catalog::LookupOptions options,
                catalog::DirectoryEntry *dirent),
               (override));
+};
+
+class MockFetcher : public cvmfs::Fetcher {
+ public:
+  MockFetcher(CacheManager *cache_mgr,
+              download::DownloadManager *download_mgr,
+              BackoffThrottle *backoff_throttle,
+              perf::StatisticsTemplate statistics)
+    : Fetcher(cache_mgr, download_mgr, backoff_throttle, statistics),statistics_(statistics.statistics()) { 
+    };
+  int Fetch(const CacheManager::LabeledObject &object,
+            const std::string &alt_url = "") override {
+    return ++counter_;
+  }
+  virtual ~MockFetcher(){delete statistics_;}
+  void Reset() { counter_ = 0; }
+
+  size_t counter_ = 0;
+  perf::Statistics *statistics_;
 };
 
 class MockInodeTracker : public glue::InodeTracker {
@@ -71,7 +91,7 @@ class MockBundleFileMgr : public BundleFileMgr {
   MOCK_METHOD(size_t, Size, (), (const));
   MOCK_METHOD(UniquePtr<CacheManager::LabeledObject>, GetNext, (), (const));
   void Reset() { counter_ = 0; }
-  size_t counter_=0;
+  size_t counter_ = 0;
 };
 
 
@@ -103,7 +123,7 @@ class T_BundleMgr : public ::testing::Test {
         64 * 1024, new perf::Statistics());
     mock_catalog_mgr_ = new testing::NiceMock<MockCatalogManager>(mount_point_);
     mock_inode_tracker_ = new testing::NiceMock<MockInodeTracker>();
-
+    mock_fetcher_ = new testing::NiceMock<MockFetcher>(nullptr, nullptr, nullptr, perf::StatisticsTemplate("fetch", new perf::Statistics()));
     // Determine mock behavior
     ON_CALL(*mock_catalog_mgr_, LookupPath(testing::_, testing::_, testing::_))
         .WillByDefault([this](const PathString &,
@@ -135,7 +155,7 @@ class T_BundleMgr : public ::testing::Test {
     srand(time(NULL));
     ON_CALL(*bfm_, GetNext).WillByDefault([this]() {
       if (bfm_->counter_ < bfm_->Size()) {
-        bfm_->counter_+=1;
+        bfm_->counter_ += 1;
         shash::Any hash;
         CacheManager::Label label;
         label.path = std::string{};
@@ -148,6 +168,11 @@ class T_BundleMgr : public ::testing::Test {
         return UniquePtr<CacheManager::LabeledObject>{nullptr};
       }
     });
+    if (bundle_mgr_->fetcher_!=nullptr){
+      delete bundle_mgr_->fetcher_;
+    }
+
+    bundle_mgr_->fetcher_ = mock_fetcher_;
     // replace the real bfm_ with the mock
     delete bundle_mgr_->bfm_;
     bundle_mgr_->bfm_ = bfm_;
@@ -167,6 +192,7 @@ class T_BundleMgr : public ::testing::Test {
     //    EXPECT_EQ(used_fds_, GetNoUsedFds()) << ShowOpenFiles();
     delete file_system_;
     delete mount_point_;
+    delete mock_fetcher_;
     delete bundle_mgr_;
   }
 
@@ -198,11 +224,13 @@ class T_BundleMgr : public ::testing::Test {
   testing::NiceMock<MockInodeCache> *mock_inode_cache_;
   testing::NiceMock<MockCatalogManager> *mock_catalog_mgr_;
   testing::NiceMock<MockInodeTracker> *mock_inode_tracker_;
+  testing::NiceMock<MockFetcher> *mock_fetcher_;
 };
 
 TEST_F(T_BundleMgr, Fetch) {
   bfm_->Reset();
   bundle_mgr_->Fetch();
   EXPECT_EQ(bfm_->counter_, bfm_->Size());
+  EXPECT_EQ(mock_fetcher_->counter_, bfm_->Size());
 }
 
