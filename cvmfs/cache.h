3de971f9a45cbd919130561af0edcf5e0dda3fa6
Georgios Christodoulis <christge@cern.ch>
2025-12-04 17:40:21 +0100

    encrypt/decrypt CacheManager::LabeledObject()-s to json format

--- cvmfs/cache.h
+++ cvmfs/cache.h
@@ -104,6 +104,11 @@ class CacheManager : SingleCopy {
     bool IsExternal() const { return flags & kLabelExternal; }
     bool IsCertificate() const { return flags & kLabelCertificate; }
 
+    bool operator==(const Label &other) const {
+      return flags == other.flags and size == other.size
+             and zip_algorithm == other.zip_algorithm
+             and range_offset == other.range_offset and path == other.path;
+    }
     /**
      * The description for the quota manager
      */
@@ -139,6 +144,10 @@ class CacheManager : SingleCopy {
   struct LabeledObject {
     explicit LabeledObject(const shash::Any &id) : id(id), label() { }
     LabeledObject(const shash::Any &id, const Label &l) : id(id), label(l) { }
+
+    bool operator==(const LabeledObject &other) const {
+      return id == other.id && label == other.label;
+    }
 
     shash::Any id;
     Label label;
@@ -252,3 +261,4 @@ class CacheManager : SingleCopy {
 };  // class CacheManager
 
 #endif  // CVMFS_CACHE_H_
+
--- test/unittests/t_bundle_file_mgr.cc
+++ test/unittests/t_bundle_file_mgr.cc
@@ -3,7 +3,12 @@
  */
 
 #include <gtest/gtest.h>
+#include <inttypes.h>
 
+#include <vector>
+
+#include "cache.h"
+#include "crypto/hash.h"
 #include "file_bundle.h"
 #include "json_document.h"
 #include "shortstring.h"
@@ -12,16 +17,91 @@
 class T_BundleFileMgr : public ::testing::Test {
  protected:
   virtual void SetUp() {
+    InitObjects(42);
     UniquePtr<JsonDocument> json_doc(JsonDocument::Create("{}"));
     EXPECT_TRUE(json_doc->IsValid());
     bfm_.Manage(json_doc);
     EXPECT_TRUE(bfm_);
   }
+  std::string ToJsonStr(const CacheManager::LabeledObject &obj) {
+    constexpr size_t obj_size = 200;
+    char buf[obj_size];
+    std::string suf{obj.id.suffix};
+    snprintf(
+        buf, obj_size,
+        "{\"id\":{\"algorithm\":%d,\"suffix\":\"%s\",\"digest\":\"%s\"},\"label\":{"
+        "\"flags\":%d,\"size\":%" PRIu64
+        ",\"zip_algorithm\":%d,\"range_offset\":%d,\"path\":\"%s\"}}",
+        obj.id.algorithm, suf.c_str(), obj.id.ToString().c_str(), obj.label.flags,
+        obj.label.size, obj.label.zip_algorithm,
+        static_cast<int>(obj.label.range_offset), obj.label.path.c_str());
+    return std::string(buf);
+  }
+
+  CacheManager::LabeledObject *ToLabeledObject(const std::string &txt_obj) {
+    EXPECT_GT(txt_obj.size(), 0);
+    UniquePtr<JsonDocument> jdoc(JsonDocument::Create(txt_obj));
+    EXPECT_TRUE(jdoc.weak_ref() != nullptr);
+    auto id_ptr = JsonDocument::SearchInObject(jdoc->root(), "id", JSON_OBJECT);
+    EXPECT_TRUE(id_ptr != nullptr);
+    int algorithm;
+    EXPECT_TRUE(GetFromJSON<int>(id_ptr, "algorithm", &algorithm));
+    std::string suffix;
+    EXPECT_TRUE(GetFromJSON<std::string>(id_ptr, "suffix", &suffix));
+    std::string digest;
+    EXPECT_TRUE(GetFromJSON<std::string>(id_ptr, "digest", &digest));
+    shash::Any id = shash::MkFromHexPtr(shash::HexPtr( digest ),suffix[0]);
+    id.algorithm=static_cast<shash::Algorithms>(algorithm);
+    auto label_ptr = JsonDocument::SearchInObject(jdoc->root(), "label",
+                                                  JSON_OBJECT);
+    EXPECT_TRUE(label_ptr != nullptr);
+
+    int flags;
+    EXPECT_TRUE(GetFromJSON<int>(label_ptr, "flags", &flags));
+    int size;  ///< unzipped size, if known
+    EXPECT_TRUE(GetFromJSON<int>(label_ptr, "size", &size));
+    int zip_algorithm;
+    EXPECT_TRUE(GetFromJSON<int>(label_ptr, "zip_algorithm", &zip_algorithm));
+    int range_offset;
+    EXPECT_TRUE(GetFromJSON<int>(label_ptr, "range_offset", &range_offset));
+    std::string path;
+    EXPECT_TRUE(GetFromJSON<std::string>(label_ptr, "path", &path));
+
+    CacheManager::Label label{};
+    label.flags = flags;
+    label.size = size;
+    label.zip_algorithm = static_cast<zlib::Algorithms>(zip_algorithm);
+    label.range_offset = range_offset;
+    label.path = path;
+    return new CacheManager::LabeledObject(id, label);
+  }
+
   virtual void TearDown() { }
   virtual ~T_BundleFileMgr() { }
 
   BundleFileMgr bfm_;
+  std::vector<CacheManager::LabeledObject> objects_;
+
+ private:
+  void InitObjects(size_t number_of_objects) {
+    for (size_t i = 0; i < number_of_objects; ++i) {
+      shash::Any hash;
+      hash.Randomize(i);
+      CacheManager::Label label;
+      label.path = "/path/to/file/" + std::to_string(i);
+      objects_.push_back(CacheManager::LabeledObject(hash, label));
+    }
+  }
 };
+
+TEST_F(T_BundleFileMgr, Conversions) {
+  for (size_t i = 0; i < objects_.size(); ++i) {
+    auto *obj = ToLabeledObject(ToJsonStr(objects_[i]));
+    EXPECT_EQ(objects_[i].id, (obj)->id);
+    EXPECT_EQ(objects_[i].label, (obj)->label);
+    delete obj;
+  }
+}
 
 TEST_F(T_BundleFileMgr, Construction) {
   PathString path("test_string");
