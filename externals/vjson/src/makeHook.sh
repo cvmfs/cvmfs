373f21f0cad0f214542a4897ca1a7b6ba97d9421
Shivam <shivammadlani5@gmail.com>
2026-02-12 14:19:11 +0530

    remove vjson from externals

--- externals/vjson/src/makeHook.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-make clean
-make CVMFS_BASE_CXX_FLAGS="$CVMFS_BASE_CXX_FLAGS" -j ${CVMFS_BUILD_EXTERNAL_NJOBS}
-strip -S libvjson.a
-
-cp -rv json.h block_allocator.h $EXTERNALS_INSTALL_LOCATION/include/
-cp -rv libvjson.a $EXTERNALS_INSTALL_LOCATION/lib/
