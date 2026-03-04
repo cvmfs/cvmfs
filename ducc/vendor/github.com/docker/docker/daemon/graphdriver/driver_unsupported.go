95a825440890650cf8b810b4663992bb95051b37
Valentin Volkl <valentin.volkl@cern.ch>
2026-03-04 22:29:52 +0100

    golang: Remove vendor dirs from git (#4133)
    Fixes #4132

--- ducc/vendor/github.com/docker/docker/daemon/graphdriver/driver_unsupported.go
+++
@@ -1,6 +0,0 @@
-//go:build !linux && !windows && !freebsd
-
-package graphdriver // import "github.com/docker/docker/daemon/graphdriver"
-
-// List of drivers that should be used in an order
-var priority = "unsupported"
--- ducc/vendor/github.com/docker/docker/daemon/graphdriver/driver_windows.go
+++
@@ -1,4 +0,0 @@
-package graphdriver // import "github.com/docker/docker/daemon/graphdriver"
-
-// List of drivers that should be used in order
-var priority = "windowsfilter"
