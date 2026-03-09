7188bb976323d41385a2e5e4f1921eb494782173
Georgios Christodoulis <christge@cern.ch>
2026-03-09 13:10:27 +0100

    Make feature runtime selectible

--- cvmfs/cvmfs.cc
+++ cvmfs/cvmfs.cc
@@ -1200,11 +1200,19 @@ static void cvmfs_open(fuse_req_t req, fuse_ino_t ino,
 
   perf::Inc(file_system_->n_fs_open());  // Count actual open / fetch operations
 
-  if (dirent.IsBundleTrigger()) {
+  std::string is_prefetching_enabled;
+  if (options_mgr_) {
+    options_mgr_->GetValue("CVMFS_FUSE_NOTIFY_INVALIDATION",
+                           &is_prefetching_enabled);
+  }
+  if (dirent.IsBundleTrigger() and (options_mgr_)
+          ? options_mgr_->IsOn(is_prefetching_enabled)
+          : false) {
     // fetch dependences if not there already
     PathString trigger_path;
-    assert(GetPathForInode(ino,&trigger_path) && "Unable to retrieve the path of the trigger file");
-    BundleMgr bundle_mgr(mount_point_,  trigger_path);
+    assert(GetPathForInode(ino, &trigger_path)
+           && "Unable to retrieve the path of the trigger file");
+    BundleMgr bundle_mgr(mount_point_, trigger_path);
     if (bundle_mgr) {
       bundle_mgr.Fetch();
     } else {
