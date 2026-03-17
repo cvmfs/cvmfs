3cc629807013771dd5a523aec95dde252417bb31
Valentin Volkl <valentin.volkl@cern.ch>
2026-03-17 09:14:51 +0100

    cleanup

--- test/common/container/test_migration.sh
+++ test/common/container/test_migration.sh
@@ -7,5 +7,5 @@ MIGRATION_TEST_LOGFILE=/tmp/cvmfs-migration-test.log
 echo "running CernVM-FS hotpatch/migration tests..."
 export CVMFS_PACKAGE_DIR="/tmp"
 CVMFS_TEST_CLASS_NAME=MigrationTests ./run.sh $MIGRATION_TEST_LOGFILE         \
+                              migration_tests/001*                            \
                               migration_tests/500*
-#                              migration_tests/001*                            \
