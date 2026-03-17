da608f8229a78c5793725594ddeb3a7eb8b65568
Valentin Volkl <valentin.volkl@cern.ch>
2026-03-17 12:28:22 +0100

    Set (internal) gomod proxy for old unittests (#4155)

--- ci/build_incremental_multi.sh
+++ ci/build_incremental_multi.sh
@@ -45,6 +45,8 @@ if can_build_gateway; then
   build_gateway="ON"
 fi
 
+host cvm-gomod-proxy1.cern.ch > /dev/null 2>&1 && export GOPROXY=http://cvm-gomod-proxy1.cern.ch:3000
+
 echo "configuring using CMake..."
 cmake -DBUILD_SERVER=$build_server          \
       -DBUILD_SERVER_DEBUG=$build_server    \
