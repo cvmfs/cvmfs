c157a82f5a7cc44b672dfa1a0f69738c38ab27e1
Andriy Utkin <hello@autkin.net>
2026-03-30 02:19:06 +0100

    migrate: specify compression_alg param for spooler_definition

--- cvmfs/swissknife_migrate.cc
+++ cvmfs/swissknife_migrate.cc
@@ -140,7 +140,7 @@ int CommandMigrate::Main(const ArgumentList &args) {
 
   // Create an upstream spooler
   temporary_directory_ = tmp_dir;
-  const upload::SpoolerDefinition spooler_definition(spooler, shash::kSha1);
+  const upload::SpoolerDefinition spooler_definition(spooler, shash::kSha1, zip::CompressionAlgFromEnv());
   spooler_ = upload::Spooler::Construct(spooler_definition);
   if (!spooler_.IsValid()) {
     Error("Failed to create upstream Spooler.");
