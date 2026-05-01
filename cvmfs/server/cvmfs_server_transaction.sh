9e676258e359214b04455a4105d8224bf0a0eb75
Andriy Utkin <hello@autkin.net>
2026-05-01 10:41:27 +0100

    cvmfs_server transaction: fix bashism
    My change led to a critical regression on environments where bash is not
    the default /bin/sh:
    
        Initial commit... /usr/bin/cvmfs_server: 4234: eval: Bad substitution
    
    cvmfs_server is meant to be POSIX sh, judging by shebang and lack of
    bashisms.

--- cvmfs/server/cvmfs_server_transaction.sh
+++ cvmfs/server/cvmfs_server_transaction.sh
@@ -7,10 +7,12 @@
 # Migrated to the new cvmfs_publish command
 
 cvmfs_server_transaction() {
-  lastarg="${@: -1}"
-  repo_name_maybe_with_path=$lastarg
-  repo_name=${repo_name_maybe_with_path%%/*}
-  load_repo_config "$repo_name"
-  export CVMFS_COMPRESSION_ALGORITHM
+  if [ $# -gt 0 ]; then
+    for lastarg in "$@"; do true; done
+    repo_name_maybe_with_path=$lastarg
+    repo_name=$(echo "$repo_name_maybe_with_path" | sed -e 's:/.*::')
+    load_repo_config "$repo_name"
+    export CVMFS_COMPRESSION_ALGORITHM
+  fi
   $(__publish_cmd dbg) transaction $@
 }
