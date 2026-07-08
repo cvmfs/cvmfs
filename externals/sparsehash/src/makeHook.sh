8fd7642ed6ea5f52d7a7313f869f05416afa4dd7
Valentin Volkl <valentin.volkl@cern.ch>
2026-07-08 15:24:22 +0200

    externals: move sparsehash to fetchcontent

--- externals/sparsehash/src/makeHook.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# the only thing which needs to be built is a config.h file
-# simply calling `make` would actually mainly build unit tests...
-make clean
-make src/google/sparsehash/sparseconfig.h -j ${CVMFS_BUILD_EXTERNAL_NJOBS}
-
-cp -rv src/google $EXTERNALS_INSTALL_LOCATION/include/
